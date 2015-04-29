424119061e30764057b7ac1216da34c78ff7737e
Linas Vepstas <linasvepstas@gmail.com>
2015-04-29 09:32:39 -0500

    Add versining info

---
+++ opencog/atomspace/atomspace.h
@@ -0,0 +1,33 @@
+/*
+ * opencog/atomspace/atomspace.h
+ *
+ * Copyright (C) 2015 by OpenCog Foundation
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_ATOMSPACE_VER_H
+#define _OPENCOG_ATOMSPACE_VER_H
+
+#define ATOMSPACE_MAJOR_VERSION 5
+#define ATOMSPACE_MINOR_VERSION 0
+#define ATOMSPACE_MICRO_VERSION 0
+
+#define ATOMSPACE_VERSION_STRING "5.0.0"
+
+
+#endif // _OPENCOG_ATOMSPACE_VER_H
