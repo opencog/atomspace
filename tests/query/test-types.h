fd6809f07159a38e8a2ee90532cc85eec787ff78
Linas Vepstas <linasvepstas@gmail.com>
2015-04-28 21:37:25 -0500

    Fix many unit tests so that they pass

--- tests/query/test-types.h
+++ tests/query/test-types.h
@@ -3,18 +3,40 @@
 
 using namespace opencog;
 
+Type AT_TIME_LINK
+	= classserver().addType(ORDERED_LINK, "AtTimeLink");
+Type DEFINED_LINGUISTIC_CONCEPT_NODE
+	= classserver().addType(NODE, "DefinedLinguisticConceptNode");
 Type DEFINED_LINGUISTIC_RELATIONSHIP_NODE
 	= classserver().addType(NODE, "DefinedLinguisticRelationshipNode");
+Type DOCUMENT_NODE
+	= classserver().addType(NODE, "DocumentNode");
+Type FEATURE_LINK
+	= classserver().addType(LINK, "FeatureLink");
+Type FEATURE_NODE
+	= classserver().addType(NODE, "FeatureNode");
+Type HEBBIAN_LINK
+	= classserver().addType(LINK, "HebbianLink");
 Type LEMMA_LINK
 	= classserver().addType(LINK, "LemmaLink");
+Type LEMMA_NODE
+	= classserver().addType(NODE, "LemmaNode");
 Type PARSE_LINK
 	= classserver().addType(LINK, "ParseLink");
 Type PARSE_NODE
 	= classserver().addType(NODE, "ParseNode");
 Type PART_OF_SPEECH_LINK
 	= classserver().addType(LINK, "PartOfSpeechLink");
+Type PART_OF_SPEECH_NODE
+	= classserver().addType(NODE, "PartOfSpeechNode");
+Type PREPOSITIONAL_RELATIONSHIP_NODE
+	= classserver().addType(NODE, "PrepositionalRelationshipNode");
 Type REFERENCE_LINK
 	= classserver().addType(LINK, "ReferenceLink");
+Type SEME_NODE
+	= classserver().addType(NODE, "SemeNode");
+Type SENTENCE_NODE
+	= classserver().addType(NODE, "SentenceNode");
 Type TIME_NODE
 	= classserver().addType(NODE, "TimeNode");
 Type WORD_INSTANCE_LINK
@@ -23,3 +45,5 @@ Type WORD_INSTANCE_NODE
 	= classserver().addType(NODE, "WordInstanceNode");
 Type WORD_NODE
 	= classserver().addType(NODE, "WordNode");
+Type WORD_SENSE_NODE
+	= classserver().addType(NODE, "WordSenseNode");
--- tests/query/test_types.scm
+++ tests/query/test_types.scm
@@ -1,6 +1,14 @@
 
-; TODO
-; Fake out the required atom types.
+; required atom types.
+
+(define (AtTimeLink . x)
+	(apply cog-new-link (append (list 'AtTimeLink) x)))
+
+(define (FeatureLink . x)
+	(apply cog-new-link (append (list 'FeatureLink) x)))
+
+(define (HebbianLink . x)
+	(apply cog-new-link (append (list 'HebbianLink) x)))
 
 (define (LemmaLink . x)
 	(apply cog-new-link (append (list 'LemmaLink) x)))
@@ -11,14 +19,41 @@
 (define (PartOfSpeechLink . x)
 	(apply cog-new-link (append (list 'PartOfSpeechLink) x)))
 
+(define (ReferenceLink . x)
+	(apply cog-new-link (append (list 'ReferenceLink) x)))
+
 (define (WordInstanceLink . x)
 	(apply cog-new-link (append (list 'WordInstanceLink) x)))
+
+(define (DefinedLinguisticConceptNode . x)
+	(apply cog-new-node (append (list 'DefinedLinguisticConceptNode) x)))
 
 (define (DefinedLinguisticRelationshipNode . x)
 	(apply cog-new-node (append (list 'DefinedLinguisticRelationshipNode) x)))
 
+(define (DocumentNode . x)
+	(apply cog-new-node (append (list 'DocumentNode) x)))
+
+(define (FeatureNode . x)
+	(apply cog-new-node (append (list 'FeatureNode) x)))
+
+(define (LemmaNode . x)
+	(apply cog-new-node (append (list 'LemmaNode) x)))
+
 (define (ParseNode . x)
 	(apply cog-new-node (append (list 'ParseNode) x)))
+
+(define (PartOfSpeechNode . x)
+	(apply cog-new-node (append (list 'PartOfSpeechNode) x)))
+
+(define (PrepositionalRelationshipNode . x)
+	(apply cog-new-node (append (list 'PrepositionalRelationshipNode) x)))
+
+(define (SemeNode . x)
+	(apply cog-new-node (append (list 'SemeNode) x)))
+
+(define (SentenceNode . x)
+	(apply cog-new-node (append (list 'SentenceNode) x)))
 
 (define (TimeNode . x)
 	(apply cog-new-node (append (list 'TimeNode) x)))
@@ -28,4 +63,7 @@
 
 (define (WordNode . x)
 	(apply cog-new-node (append (list 'WordNode) x)))
+
+(define (WordSenseNode . x)
+	(apply cog-new-node (append (list 'WordSenseNode) x)))
 
