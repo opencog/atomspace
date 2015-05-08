5967b2e1e15c24363083bf25eb589fbd619713e2
Linas Vepstas <linasvepstas@gmail.com>
2015-05-08 12:08:39 -0500

    Add notes

--- opencog/cython/opencog/__init__.py
+++ opencog/cython/opencog/__init__.py
@@ -1,2 +1,9 @@
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
+
+# The above addresses the issues that came up in
+# https://github.com/opencog/atomspace/issues/20
+# Basically, the various python modules are split over multiple
+# directories, including the build directories. See this:
+# http://stackoverflow.com/questions/2699287/what-is-path-useful-for
+# for an explanation.
