39bad8cebdd9467fcfd69895923438e033224da3
Nil Geisweiller <ngeiswei@gmail.com>
2015-05-12 16:43:32 +0200

    Complete usage doc of simple URE example

---
+++ examples/rule-engine/simple-deduction.scm
@@ -0,0 +1,29 @@
+;
+; Simple crisp deduction example
+;
+; 1. Launch guile under this directory (see examples/guile/README.md
+; for prerequisite if you haven't already)
+;
+; $ guile
+;
+; 2. The load this file
+;
+; scheme@(guile-user)> (load-from-path "simple-deduction.scm")
+;
+; 3. Scroll to the bottom, and run some of the commented-out examples.
+
+(use-modules (opencog))
+(use-modules (opencog rule-engine))
+
+(load-from-path "utilities.scm")
+
+(define A (ConceptNode "A"))
+(define B (ConceptNode "B"))
+(define C (ConceptNode "C"))
+(define AB (ImplicationLink (stv 1 1) A B))
+(define BC (ImplicationLink (stv 1 1) B C))
+
+; (cog-fc AB)
+
+; Expected output should be
+; TODO
