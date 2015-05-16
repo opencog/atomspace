a797765d55492e5b15bf0f322f4c8d9ce00964cf
Linas Vepstas <linasvepstas@gmail.com>
2015-05-15 20:54:03 -0500

    re-distribute the documentation

--- opencog/scm/core-docs.scm
+++ opencog/scm/core-docs.scm
@@ -786,37 +786,6 @@
     Return a TV. Only satisfaction is performed, no implication.
 ")
 
-(set-procedure-property! fetch-atom 'documentation
-"
- fetch-atom handle
-    Fetch indicated atom from SQL/persistent storage.
-")
-
-(set-procedure-property! fetch-incoming-set 'documentation
-"
- fetch-incoming-set
-    Fetch the incoming set of the atom from SQL storage. The fetch is
-    recursive.
-")
-
-(set-procedure-property! store-atom 'documentation
-"
- store-atom handle
-    Store indicated atom to SQL/persistent storage.
-")
-
-(set-procedure-property! load-atoms-of-type 'documentation
-"
- load-atoms-of-type type
-    Fetch atoms of the given type from SQL/persistent storage.
-")
-
-(set-procedure-property! barrier 'documentation
-"
- barrier
-    Block until the SQL Atom write queues are empty.
-")
-
 ;(set-procedure-property! cog-yield 'documentation
 ;"
 ; cog-yield
--- opencog/scm/opencog/persist.scm
+++ opencog/scm/opencog/persist.scm
@@ -5,3 +5,37 @@
 (define-module (opencog persist))
 
 (load-extension "libpersist" "opencog_persist_init")
+
+;; -----------------------------------------------------
+;;
+
+(set-procedure-property! fetch-atom 'documentation
+"
+ fetch-atom handle
+    Fetch indicated atom from SQL/persistent storage.
+")
+
+(set-procedure-property! fetch-incoming-set 'documentation
+"
+ fetch-incoming-set
+    Fetch the incoming set of the atom from SQL storage. The fetch is
+    recursive.
+")
+
+(set-procedure-property! store-atom 'documentation
+"
+ store-atom handle
+    Store indicated atom to SQL/persistent storage.
+")
+
+(set-procedure-property! load-atoms-of-type 'documentation
+"
+ load-atoms-of-type type
+    Fetch atoms of the given type from SQL/persistent storage.
+")
+
+(set-procedure-property! barrier 'documentation
+"
+ barrier
+    Block until the SQL Atom write queues are empty.
+")
