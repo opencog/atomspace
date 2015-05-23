b8d947ce167066d3ab9349991f07c29cd6dfb302
Linas Vepstas <linasvepstas@gmail.com>
2015-05-23 09:22:39 -0500

    Expand the unit test

--- tests/query/get-link.scm
+++ tests/query/get-link.scm
@@ -4,9 +4,27 @@
 
 (use-modules ((opencog query)))
 
-
 (InheritanceLink (ConceptNode "Ben") (ConceptNode "human"))
 (InheritanceLink (ConceptNode "Linas") (ConceptNode "human"))
+(InheritanceLink (ConceptNode "Sparky") (ConceptNode "dog"))
 
-(define is-human 
+(define is-human
 	(GetLink (InheritanceLink (VariableNode "$H") (ConceptNode "human"))))
+
+;; two variables, including type restrictions
+(define is-something
+	(GetLink
+		(VariableList
+			(TypedVariableLink (VariableNode "$A") (TypeNode "ConceptNode"))
+			(VariableNode "$B"))
+		(InheritanceLink (VariableNode "$A") (VariableNode "$B"))))
+
+;; looks for queries.
+;; This binds only the second variable, thus, the first variable
+;; remains free. Running this should results in a grounding to is-human,
+;; above. That is, it should find
+;; (SetLink (ConceptNode "human"))
+(define is-query
+	(GetLink
+		(VariableNode "$B") ;; bind only the second variable.
+		(InheritanceLink (VariableNode "$H") (VariableNode "$B"))))
