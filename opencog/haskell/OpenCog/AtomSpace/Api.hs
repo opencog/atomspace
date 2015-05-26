9691f519c1e399465380765781781dd7957be28c
Marcos Pividori <marcos.pividori@gmail.com>
2015-05-25 22:12:01 -0300

    Make code more general using the monad transformer ReaderT

--- opencog/haskell/OpenCog/AtomSpace/Api.hs
+++ opencog/haskell/OpenCog/AtomSpace/Api.hs
@@ -6,7 +6,7 @@ module OpenCog.AtomSpace.Api (
     , AtomSpace
     ) where
 
---Note that I don't export the AtomSpace data constructors nor the asDelete/asNew functions.
+-- Note that I don't export the AtomSpace data constructors nor the asDelete/asNew functions.
 
 import Foreign
 import Foreign.C.Types
@@ -14,28 +14,18 @@ import Foreign.C.String
 import Control.Exception
 import OpenCog.AtomSpace.Types
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader
 import Data.Functor
 
 -- Internal AtomSpace reference to a mutable C++ instance of the AtomSpace class.
 newtype AtomSpaceRef = AtomSpaceRef (Ptr AtomSpaceRef)
 
 -- Main Data Type for representing programs working on an AtomSpace.
--- The run function inside means: "Once you have a reference to an AtomSpace in memory,
--- working on it reduces to performing IO actions"
 -- We have to use the IO monad because of the use of FFI for calling c functions for working
 -- on a mutable instance of the atomspace, so we have side effects.
-data AtomSpace a = AtomSpace {run :: AtomSpaceRef -> IO a}
+type AtomSpace = ReaderT AtomSpaceRef IO
 
-instance Monad AtomSpace where
-    m1 >>= f = AtomSpace $ \asRef -> do
-                                       a <- run m1 asRef
-                                       run (f a) asRef
-    return a = AtomSpace (\_ -> return a)
-
-instance MonadIO AtomSpace where
-    liftIO m = AtomSpace (\_ -> m)
-
--- Internal Functions new and delete, to create and delete C++ instances of the AtomSpace class.
+-- Internal functions new and delete, to create and delete C++ instances of the AtomSpace class.
 foreign import ccall "AtomSpace_CWrapper.h AtomSpace_new"
                c_atomspace_new :: IO AtomSpaceRef
 asNew :: IO AtomSpaceRef
@@ -46,17 +36,23 @@ foreign import ccall "AtomSpace_CWrapper.h AtomSpace_delete"
 asDelete :: AtomSpaceRef -> IO ()
 asDelete = c_atomspace_delete
 
--- 'asAddNode' This function calls to the addNode method of the AtomSpace C++ class.
+-- Internal function getAtomSpace.
+getAtomSpace :: AtomSpace AtomSpaceRef
+getAtomSpace = ask
+
+-- 'asAddNode' calls to the addNode method of the AtomSpace C++ class.
 foreign import ccall "AtomSpace_CWrapper.h AtomSpace_addNode"
                c_atomspace_addnode :: AtomSpaceRef -> CShort -> CString -> IO CLong
 asAddNode :: Node -> AtomSpace Handle
-asAddNode nod = AtomSpace (\asRef -> let ntype = fromIntegral $ fromEnum $ nodeType nod
-                                         fun = \str -> fromEnum <$> c_atomspace_addnode asRef ntype str
-                                      in withCString (nodeName nod) fun)
+asAddNode nod = do
+                    asRef <- getAtomSpace
+                    let ntype = fromIntegral $ fromEnum $ nodeType nod
+                        fun = \str -> fromIntegral <$> c_atomspace_addnode asRef ntype str
+                     in liftIO $ withCString (nodeName nod) fun
 
--- 'runOnNewAtomSpace' creates a new AtomSpace (C++ object), does some computation run_ over it,
--- and then deletes the AtomSpace.
--- By using bracket, I ensure properly freeing memory in case of exceptions in the computation run_.
+-- 'runOnNewAtomSpace' creates a new AtomSpace (C++ object), does some computation over it,
+-- and then deletes it.
+-- By using bracket, I ensure properly freeing memory in case of exceptions during the computation.
 runOnNewAtomSpace :: AtomSpace a -> IO a
-runOnNewAtomSpace (AtomSpace run_) = bracket asNew asDelete run_
+runOnNewAtomSpace as = bracket asNew asDelete $ runReaderT as
 
