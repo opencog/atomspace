beed32feef3132b069bc03bab8ed951eef8938ef
Linas Vepstas <linasvepstas@gmail.com>
2015-05-29 00:01:54 -0500

    Fix aonther unresolved-symbol bug

--- opencog/scm/opencog/query.scm
+++ opencog/scm/opencog/query.scm
@@ -4,4 +4,8 @@
 
 (define-module (opencog query))
 
+; This is also loaded by (opencog exec) We need it here,
+; else we get undefined symbols in libquery.
+(load-extension "libexecution" "opencog_exec_init")
+
 (load-extension "libquery" "opencog_query_init")
