d8228d2b1b08914c668611c0b0fc7cb41e0aee16
William Ma <williampma@users.noreply.github.com>
2015-06-09 12:30:45 +0800

    Update test to use new URE rule loader

---
+++ tests/rule-engine/bc-config-1.scm
@@ -0,0 +1,14 @@
+(load "bc-modus-ponens.scm")
+
+(MemberLink (stv 1 1)
+    (Node "pln-rule-modus-ponens")
+    (ConceptNode "URE")
+)
+
+; Termination criteria parameters
+(ExecutionLink
+   (SchemaNode "URE:maximum-iterations")
+   (ConceptNode "URE")
+   (NumberNode "10")
+)
+
