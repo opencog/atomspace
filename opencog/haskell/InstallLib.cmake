0b2e1852edc13915c4908d06f84027276bcd1972
Marcos Pividori <marcos.pividori@gmail.com>
2015-06-19 23:40:48 -0300

    Fix many details on CMake running cabal:     - Now, Cmake looks for new changes in opencog/haskell dir before rebuilding       the haskell library.     - Now, Cmake runs all cabal commands at build time. Before, we were running       some commands at install time with some undesirable effects when       executing as root user.     - Now, it builds on the binary dir, creating a dist dir in opencog/haskell.       Before, it was building in source dir.

--- opencog/haskell/InstallLib.cmake
+++ opencog/haskell/InstallLib.cmake
@@ -1,6 +1,6 @@
 
 EXECUTE_PROCESS(
-    COMMAND cabal configure
+    COMMAND cabal configure --builddir=${BINDIR}
     OUTPUT_QUIET
     ERROR_VARIABLE out
 )
@@ -11,3 +11,9 @@ IF(out MATCHES "(.)*'hackage.haskell.org' does not exist(.)*")
         COMMAND cabal update
     )
 ENDIF(out MATCHES "(.)*'hackage.haskell.org' does not exist(.)*")
+
+EXECUTE_PROCESS(
+    COMMAND cabal install -v0 --builddir=${BINDIR}/dist --extra-lib-dirs=${BINDIR}
+    OUTPUT_QUIET
+)
+
