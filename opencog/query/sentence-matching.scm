5e7728bcec4c5d024cb970a00ed60b10f16a64cd
leungmanhin <manhinleung@gmail.com>
2015-07-15 14:05:04 +0800

    Modify get-similr-sentence to support SentenceNode as input

--- opencog/query/sentence-matching.scm
+++ opencog/query/sentence-matching.scm
@@ -2,6 +2,8 @@
 ;
 ; For example:
 ;    (get-similar-sentences "What did Pete eat?")
+; OR:
+;    (get-similar-sentences (SentenceNode "sentence@123"))
 ;
 ; Possible result: 
 ;    (Pete ate apples .)
@@ -13,19 +15,30 @@
     ; Generate sentences from each of the R2L-SetLinks
     (define (generate-sentences r2l-setlinks) (if (> (length r2l-setlinks) 0) (map to-sureal r2l-setlinks) '()))
 
+    ; Check if the input-sentence is an actual sentence or a SentenceNode,
+    ; parse and return the corresponding SentenceNode if it is a sentence
+    (define (get-sentence-node)
+        (if (cog-atom? input-sentence)
+            (if (equal? 'SentenceNode (cog-type input-sentence))
+                (input-sentence)
+                (display "Please input a SentenceNode only")
+            )
+            (car (nlp-parse input-sentence))
+        )
+    )
+
     (begin
         ; Delete identical sentences from the return set
         (delete-duplicates
             ; Use SuReal to generate sentences from their corresponding SetLinks
             (generate-sentences
-                ; TODO: Filter out non-SetLinks here, or do it in (generate-sentences)
                 ; Search for any similar SetLinks in the atomspace
                 (cog-outgoing-set (cog-fuzzy-match
                     ; Get the R2L SetLink of the input sentence
                     (car (cog-chase-link 'ReferenceLink 'SetLink
                         (car (cog-chase-link 'InterpretationLink 'InterpretationNode
                             (car (cog-chase-link 'ParseLink 'ParseNode
-                                (car (nlp-parse input-sentence))
+                                (get-sentence-node)
                             ))
                         ))
                     ))
