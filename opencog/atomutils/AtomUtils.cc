8117fb63385166559589b21434b56f921479bdcf
Nil Geisweiller <ngeiswei@gmail.com>
2015-08-03 15:52:13 +0300

    Implement get_definition AtomUtil (get DefineLink's body given its alias)

--- opencog/atomutils/AtomUtils.cc
+++ opencog/atomutils/AtomUtils.cc
@@ -25,6 +25,7 @@
 
 #include <opencog/atomspace/Link.h>
 #include <opencog/atomspace/Node.h>
+#include <opencog/atoms/bind/DefineLink.h>
 #include "AtomUtils.h"
 
 namespace opencog
@@ -304,8 +305,19 @@ HandleSeq get_predicates_for(const Handle& target,
     return answer;
 }
 
-Handle get_definition(const Handle& name) {
-	return Handle::UNDEFINED;
+Handle get_definition(const Handle& alias) {
+    IncomingSet defs = alias->getIncomingSetByType(DEFINE_LINK);
+
+    // Return the first (supposedly unique) definition
+    for (LinkPtr def : defs)
+        if (def->isSource(alias))
+            return DefineLinkCast(def->getHandle())->get_definition();
+
+    // There is no definition for that alias
+    throw InvalidParamException(TRACE_INFO,
+                                "Cannot find defined hypergraph for atom %s",
+                                alias->toString().c_str());
+    return Handle::UNDEFINED;
 }
 
 } // namespace OpenCog
