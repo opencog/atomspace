fc8c44539a9ffeb02455282a0fc199dbd0f96d47
Marcos Pividori <marcos.pividori@gmail.com>
2015-08-12 23:19:19 -0300

    Check for proper ghc version before working in Haskell bindings. Fixes #190.

--- lib/FindStack.cmake
+++ lib/FindStack.cmake
@@ -1,9 +1,21 @@
 
 FIND_PROGRAM(STACK_EXECUTABLE stack)
 
+SET(STACK_FOUND FALSE)
 IF (STACK_EXECUTABLE)
-    SET(STACK_FOUND TRUE)
-ELSE (STACK_EXECUTABLE)
-	SET(STACK_FOUND FALSE)
-ENDIF (STACK_EXECUTABLE)
+    SET(MESSAGE_STATUS "You should run \"stack setup\" first. Haskell bindings will not be built.")
+    EXECUTE_PROCESS(COMMAND stack
+                      --stack-yaml=${CMAKE_SOURCE_DIR}/opencog/haskell/stack.yaml
+                      ghc -- --version
+                    OUTPUT_VARIABLE _GHCVERNO
+                    ERROR_QUIET)
+    STRING(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" GHC_VERSION "${_GHCVERNO}")
 
+    IF ("${GHC_VERSION}" VERSION_EQUAL "7.8.4")
+        SET(STACK_FOUND TRUE)
+        SET(MESSAGE_STATUS "Stack found.")
+    ENDIF ()
+ELSE (STACK_EXECUTABLE)
+    SET(MESSAGE_STATUS "Stack was not found. Haskell bindings will not be built.")
+ENDIF (STACK_EXECUTABLE)
+MESSAGE(STATUS ${MESSAGE_STATUS})
