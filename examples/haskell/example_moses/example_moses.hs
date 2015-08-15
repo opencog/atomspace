db94bf256920a7aae912709569f5642cc3eecf4f
Marcos Pividori <marcos.pividori@gmail.com>
2015-08-15 15:29:05 -0300

    Improve moses example.

--- examples/haskell/example_moses/example_moses.hs
+++ examples/haskell/example_moses/example_moses.hs
@@ -4,6 +4,11 @@ import OpenCog.AtomSpace            (Atom(..),AtomGen(..),AtomSpace,appGen,cogBi
                                      printAtom,AtomType(..),noTv,stv,(|>),(\>))
 import Control.Monad.IO.Class       (liftIO)
 import BackgroundKnowledge          (bkn)
+import PlnRules                     (plnRuleForAllHack
+                                    ,plnRuleEliminateNeutralElementHack
+                                    ,plnRuleEliminateDanglingJunctorHack
+                                    ,plnRuleEquivalenceHack
+                                    ,plnRuleAverageHack)
 import MosesModel                   (mosesModel)
 
 main :: IO ()
@@ -11,8 +16,26 @@ main = let insertGen :: AtomGen -> AtomSpace ()
            insertGen = appGen insert
            printGen :: AtomGen -> IO ()
            printGen = appGen printAtom
+           showRes :: Maybe AtomGen -> AtomSpace ()
+           showRes at = liftIO $ do
+               putStrLn "Res:"
+               case at of
+                   Nothing -> print at
+                   Just at -> printGen at
         in runOnNewAtomSpace $ do
                 mapM_ insertGen bkn
                 insert mosesModel
-                debug
+                insert plnRuleForAllHack
+                insert plnRuleEliminateNeutralElementHack
+                insert plnRuleEliminateDanglingJunctorHack
+                insert plnRuleEquivalenceHack
+                insert plnRuleAverageHack
+
+                res1 <- cogBind plnRuleForAllHack
+                res2 <- cogBind plnRuleEliminateNeutralElementHack
+                res3 <- cogBind plnRuleEliminateDanglingJunctorHack
+                res4 <- cogBind plnRuleEquivalenceHack
+                res5 <- cogBind plnRuleAverageHack
+
+                mapM_ showRes [res1,res2,res3,res4,res5]
 
