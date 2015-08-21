25a51fbd54f56fef29367df4ca4cb148453a4a9b
Marcos Pividori <marcos.pividori@gmail.com>
2015-08-21 19:57:24 -0300

    Add support for hierarchy of Atomspaces. Also, I add one example using this extension.

---
+++ examples/haskell/example_multiple_atomspaces.hs
@@ -0,0 +1,57 @@
+-- GSoC 2015 - Haskell bindings for OpenCog.
+{-# LANGUAGE GADTs #-}
+
+-- | Simple example on creating different atomspaces and working with them.
+import OpenCog.AtomSpace        (AtomSpace,Atom(..),insert,remove,newAtomSpace,
+                                 getParent,debug,(<:),(|>),(\>),noTv,stv)
+
+main :: IO ()
+main = do
+    parentAs <- newAtomSpace Nothing
+    childAs <- newAtomSpace $ Just parentAs
+
+    parentAs <: do
+        insert $ ConceptNode "GenConcept1" (stv 1 1)
+        insert $ ConceptNode "GenConcept2" (stv 1 1)
+    childAs <: do
+        insert $ ConceptNode "PrivateConcept1" noTv
+        insert $ ConceptNode "PrivateConcept2" noTv
+
+    case getParent childAs of
+        Just parent -> if parentAs == parent
+            then putStrLn "Proper Parent relation."
+            else putStrLn "Error in Parent relation."
+        Nothing     -> putStrLn "Error in Parent relation."
+
+    putStrLn $ replicate 60 '-'
+    putStrLn "State after inserting GenConcepts to Parent Atomspace"
+    putStrLn "and PrivateConcepts to Child AtomSpace:"
+    putStrLn "- Parent AtomSpace:"
+    parentAs <: debug
+    putStrLn "- Child AtomSpace:"
+    childAs <: debug
+
+    putStrLn $ replicate 60 '-'
+    putStrLn "State after removing GenConcept1 from Parent Atomspace:"
+    parentAs <: remove $ ConceptNode "GenConcept1" (stv 1 1)
+    putStrLn "- Parent AtomSpace:"
+    parentAs <: debug
+    putStrLn "- Child AtomSpace:"
+    childAs <: debug
+
+    putStrLn $ replicate 60 '-'
+    putStrLn "State after trying to remove GenConcept2 from Child Atomspace:"
+    childAs <: remove $ ConceptNode "GenConcept2" noTv
+    putStrLn "- Parent AtomSpace:"
+    parentAs <: debug
+    putStrLn "- Child AtomSpace:"
+    childAs <: debug
+
+    putStrLn $ replicate 60 '-'
+    putStrLn "State after removing PrivateConcept1 from Child Atomspace:"
+    childAs <: remove $ ConceptNode "PrivateConcept1" noTv
+    putStrLn "- Parent AtomSpace:"
+    parentAs <: debug
+    putStrLn "- Child AtomSpace:"
+    childAs <: debug
+
