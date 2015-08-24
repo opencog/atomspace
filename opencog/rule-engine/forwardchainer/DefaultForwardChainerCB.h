09600de8a2c776179448c9c8bbcad05037a07264
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-08-24 18:06:42 +0800

    Design changes to method derive_rule

--- opencog/rule-engine/forwardchainer/DefaultForwardChainerCB.h
+++ opencog/rule-engine/forwardchainer/DefaultForwardChainerCB.h
@@ -36,16 +36,17 @@ class Rule;
 class DefaultForwardChainerCB: public virtual ForwardChainerCallBack {
 private:
     friend class ::DefaultForwardChainerCBUTest;
+
     AtomSpace& _as;
     ForwardChainerPMCB _fcpm;
     source_selection_mode _ts_mode;
 
     Handle gen_sub_varlist(const Handle& parent, const Handle& parent_varlist);
     bool unify(Handle source,Handle target,Rule* rule);
-    HandleSeq derive_rules(Handle source,Rule* rule);
+    HandleSeq derive_rules(Handle source, Handle target, Rule* rule);
     bool subatom_unify(Handle source,Rule* rule);
     bool is_valid_implicant(const Handle& h);
-    HandleSeq get_subatoms(Rule *rule);
+    UnorderedHandleSet get_subatoms(Rule *rule);
 
 public:
     HandleSeq substitute_rule_part(AtomSpace& as, Handle hrule,const std::set<Handle>& vars,const std::vector<std::map<Handle,Handle>>& var_groundings);
@@ -57,6 +58,8 @@ public:
     virtual HandleSeq choose_premises(FCMemory& fcmem);
     virtual Handle choose_next_source(FCMemory& fcmem);
     virtual HandleSeq apply_rule(FCMemory& fcmem);
+
+    HandleSeq derive_rules(Handle source,Rule* rule,bool subatomic=false);
 
     // new_rules formed by substituting source to matching implicant
     std::map<Handle,std::vector<Handle> > rule_derivations;
