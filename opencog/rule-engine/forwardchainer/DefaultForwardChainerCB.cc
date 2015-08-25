4454b041032c658fa8beb048d7d176a2e9893a9d
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-08-25 14:38:22 +0800

    Move derive_rules to base class

--- opencog/rule-engine/forwardchainer/DefaultForwardChainerCB.cc
+++ opencog/rule-engine/forwardchainer/DefaultForwardChainerCB.cc
@@ -62,7 +62,6 @@ vector<Rule*> DefaultForwardChainerCB::choose_rules(FCMemory& fcmem)
     if (source == Handle::UNDEFINED)
         throw InvalidParamException(TRACE_INFO, "Needs a valid source atom");
 
-    rule_derivations.clear(); //clear previous mapping
 
     vector<Rule*> chosen_rules;
     auto rules = fcmem.get_rules();
@@ -149,79 +148,6 @@ HandleSeq DefaultForwardChainerCB::derive_rules(Handle source, Rule* rule,bool s
         for (Handle target : rule->get_implicant_seq())
             add_result(derive_rules(source, target, rule));
 
-    }
-
-    return derived_rules;
-}
-
-
-/**
- * Derives new rules by replacing variables that are unfiable in @param target
- *  with source.
- *
- * @param  source    A source atom that will be matched with the rule.
- * @param  rule      A rule object that contains @param target in its implicant.
- * @param  subatomic A flag that sets subatom unification.
- *
- * @return  A HandleSeq of derived rule handles.
- */
-HandleSeq DefaultForwardChainerCB::derive_rules(Handle source, Handle target,
-                                               Rule* rule)
-{
-    //exceptions
-    if (not is_valid_implicant(target))
-        return {};
-
-    HandleSeq derived_rules = { };
-
-    AtomSpace temp_pm_as;
-    Handle hcpy = temp_pm_as.add_atom(target);
-    Handle implicant_vardecl = temp_pm_as.add_atom(
-            gen_sub_varlist(target, rule->get_vardecl()));
-    Handle sourcecpy = temp_pm_as.add_atom(source);
-
-    BindLinkPtr bl =
-    createBindLink(HandleSeq { implicant_vardecl, hcpy, hcpy });
-
-    VarGroundingPMCB gcb(&temp_pm_as);
-    gcb.implicand = bl->get_implicand();
-
-    bl->imply(gcb);
-
-    auto del_by_value =
-            [] (std::vector<std::map<Handle,Handle>>& vec_map,const Handle& h) {
-                for (auto& map: vec_map)
-                for(auto& it:map) {if (it.second == h) map.erase(it.first);}
-            };
-
-    //We don't want implicant_var list to be matched as
-    //in the case of free vars in modus-ponens rules.
-    del_by_value(gcb.term_groundings, implicant_vardecl);
-    del_by_value(gcb.var_groundings, implicant_vardecl);
-
-    FindAtoms fv(VARIABLE_NODE);
-    for (const auto& termg_map : gcb.term_groundings) {
-        for (const auto& it : termg_map) {
-            if (it.second == sourcecpy) {
-
-                fv.search_set(it.first);
-
-                Handle rhandle = rule->get_handle();
-                HandleSeq new_candidate_rules = substitute_rule_part(
-                        temp_pm_as, temp_pm_as.add_atom(rhandle), fv.varset,
-                        gcb.var_groundings);
-
-                for (Handle nr : new_candidate_rules) {
-                    if (find(derived_rules.begin(), derived_rules.end(), nr) == derived_rules.end()) {
-                        //Adding back to _as avoids UUID clashes.
-                        Handle h = _as.add_atom(nr);
-                        //Avoid adding original rule to derived rule list
-                        if (h != rhandle)
-                            derived_rules.push_back(h);
-                    }
-                }
-            }
-        }
     }
 
     return derived_rules;
--- opencog/rule-engine/forwardchainer/ForwardChainerCallBack.h
+++ opencog/rule-engine/forwardchainer/ForwardChainerCallBack.h
@@ -39,6 +39,7 @@ class ForwardChainerCallBack
 {
 private:
     AtomSpace* as_;
+
 public:
     ForwardChainerCallBack(AtomSpace* as) :
             as_(as)
@@ -74,7 +75,82 @@ public:
      * @return a set of handles created as a result of applying current choosen rule
      */
     virtual HandleSeq apply_rule(FCMemory& fcmem) = 0;
+
+    HandleSeq derive_rules(Handle source,Rule* rule,bool subatomic=false);
 };
+
+
+/**
+ * Derives new rules by replacing variables that are unfiable in @param target
+ *  with source.
+ *
+ * @param  source    A source atom that will be matched with the rule.
+ * @param  rule      A rule object that contains @param target in its implicant.
+ * @param  subatomic A flag that sets subatom unification.
+ *
+ * @return  A HandleSeq of derived rule handles.
+ */
+HandleSeq ForwardChainerCallBack::derive_rules(Handle source, Handle target,
+                                               Rule* rule)
+{
+    //exceptions
+    if (not is_valid_implicant(target))
+        return {};
+
+    HandleSeq derived_rules = { };
+
+    AtomSpace temp_pm_as;
+    Handle hcpy = temp_pm_as.add_atom(target);
+    Handle implicant_vardecl = temp_pm_as.add_atom(
+            gen_sub_varlist(target, rule->get_vardecl()));
+    Handle sourcecpy = temp_pm_as.add_atom(source);
+
+    BindLinkPtr bl =
+    createBindLink(HandleSeq { implicant_vardecl, hcpy, hcpy });
+
+    VarGroundingPMCB gcb(&temp_pm_as);
+    gcb.implicand = bl->get_implicand();
+
+    bl->imply(gcb);
+
+    auto del_by_value =
+            [] (std::vector<std::map<Handle,Handle>>& vec_map,const Handle& h) {
+                for (auto& map: vec_map)
+                for(auto& it:map) {if (it.second == h) map.erase(it.first);}
+            };
+
+    //We don't want implicant_var list to be matched as
+    //in the case of free vars in modus-ponens rules.
+    del_by_value(gcb.term_groundings, implicant_vardecl);
+    del_by_value(gcb.var_groundings, implicant_vardecl);
+
+    FindAtoms fv(VARIABLE_NODE);
+    for (const auto& termg_map : gcb.term_groundings) {
+        for (const auto& it : termg_map) {
+            if (it.second == sourcecpy) {
+
+                fv.search_set(it.first);
+
+                Handle rhandle = rule->get_handle();
+                HandleSeq new_candidate_rules = substitute_rule_part(
+                        temp_pm_as, temp_pm_as.add_atom(rhandle), fv.varset,
+                        gcb.var_groundings);
+
+                for (Handle nr : new_candidate_rules) {
+                    if (find(derived_rules.begin(), derived_rules.end(), nr) == derived_rules.end()) {
+                        //Adding back to _as avoids UUID clashes.
+                        Handle h = _as.add_atom(nr);
+                        //Avoid adding original rule to derived rule list
+                        if (h != rhandle)
+                            derived_rules.push_back(h);
+                    }
+                }
+            }
+        }
+    }
+
+    return derived_rules;
+}
 
 } // ~namespace opencog
 
