4df33b110fca98543bd5fadc60ba9444a3ca50b1
Linas Vepstas <linasvepstas@gmail.com>
2015-08-28 14:55:05 -0500

    Brush up the example a bit

--- examples/pattern-matcher/define.scm
+++ examples/pattern-matcher/define.scm
@@ -38,6 +38,7 @@
 		(VariableNode "car")))
 
 ;; Define the concept of electrical parts of things.
+;; Both clauses must be present, for this to evaluate to true!
 (DefineLink
 	(DefinedPredicateNode "Electrical Part Of")
 	(PresentLink
@@ -48,15 +49,12 @@
 			(PredicateNode "PartOf")
 			(ListLink
 				(VariableNode "$x")
-				(VariableNode "$y")))))
+				(VariableNode "$y"))))
 
-;; Define a pattern to find the electrical parts of things
-;; At this time, an explicit variable declaration has to be done;
-;; this needs to be fixed...
+;; Define a pattern to find the electrical parts of things.
+;; Variables are automatically extracted from the definition.
 (define get-elect
-	(GetLink 
-		(VariableList (VariableNode "$x") (VariableNode "$y"))
-		(DefinedPredicateNode "Electrical Part Of")))
+	(GetLink (DefinedPredicateNode "Electrical Part Of")))
 
 ;; Search the atomspace for electrical things.
 (cog-execute! get-elect)
