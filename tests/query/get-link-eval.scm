4596f3be928fb097dea7f121358356ee848e2050
Jacek Świergocki <jswiergo@gmail.com>
2015-08-30 22:43:06 +0200

    Add GetLink with eval clause unit test

---
+++ tests/query/get-link-eval.scm
@@ -0,0 +1,26 @@
+; https://github.com/opencog/atomspace/issues/211
+
+(EvaluationLink
+   (ConceptNode "arkle")
+   (ConceptNode "barkle")
+   (ConceptNode "curry"))
+
+(EvaluationLink
+   (ConceptNode "glib")
+   (ConceptNode "blab"))
+
+(define get
+(GetLink
+   (VariableList (VariableNode "$a") (VariableNode "$b")
+      (TypedVariableLink (VariableNode "$lnk")
+          (TypeNode "EvaluationLink")))
+   (AndLink
+       (VariableNode "$lnk")
+       (EvaluationLink
+           (VariableNode "$a")
+           (VariableNode "$b"))
+       (EqualLink
+           (VariableNode "$lnk")
+           (EvaluationLink
+               (VariableNode "$a")
+               (VariableNode "$b"))))))
