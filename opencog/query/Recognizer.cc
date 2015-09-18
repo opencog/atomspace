7b1a33fd4204347362a2e9d3189539ee23503d5c
Linas Vepstas <linasvepstas@gmail.com>
2015-09-18 15:45:13 -0500

    More rough-in of basic infrasttructure

--- opencog/query/Recognizer.cc
+++ opencog/query/Recognizer.cc
@@ -2,12 +2,52 @@
  * Recognizer experiment
  */
 
+#include <opencog/atoms/bind/PatternLink.h>
+#include <opencog/query/InitiateSearchCB.h>
+#include <opencog/query/DefaultPatternMatchCB.h>
+
+
 #include "BindLinkAPI.h"
 
 using namespace opencog;
 
-Handle recognize(AtomSpace* as, const Handle& data)
+class Recognizer :
+   public virtual InitiateSearchCB,
+   public virtual DefaultPatternMatchCB
 {
+	public:
+		Recognizer(AtomSpace* as) :
+			InitiateSearchCB(as),
+			DefaultPatternMatchCB(as) {}
+
+		virtual void set_pattern(const Variables& vars,
+										 const Pattern& pat)
+		{
+			InitiateSearchCB::set_pattern(vars, pat);
+			DefaultPatternMatchCB::set_pattern(vars, pat);
+		}
+
+		virtual bool grounding(const std::map<Handle, Handle> &var_soln,
+		                       const std::map<Handle, Handle> &term_soln);
+};
+
+bool Recognizer::grounding(const std::map<Handle, Handle> &var_soln,
+                           const std::map<Handle, Handle> &term_soln)
+{
+printf("duuuude ola!!\n");
+
+	// Look for more groundings.
+	return false;
+}
+
+Handle recognize(AtomSpace* as, const Handle& hlink)
+{
+	PatternLinkPtr bl(PatternLinkCast(hlink));
+	if (NULL == bl)
+		bl = createPatternLink(hlink);
+
+	Recognizer reco(as);
+	bl->satisfy(reco);
 
 	return Handle::UNDEFINED;
 }
