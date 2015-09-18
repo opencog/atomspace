1d0efc68ec7f432e97cf13ebe7087c9beadbc3c0
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-09-18 12:55:41 +0800

    Methods moved from DefaultForwardChainer class

--- opencog/rule-engine/forwardchainer/ForwardChainer.h
+++ opencog/rule-engine/forwardchainer/ForwardChainer.h
@@ -24,9 +24,9 @@
 #ifndef FORWARDCHAINERX_H_
 #define FORWARDCHAINERX_H_
 
-#include <opencog/util/Logger.h>
-#include <opencog/rule-engine/UREConfigReader.h>
 #include <opencog/rule-engine/URECommons.h>
+#include <opencog/rule-engine/UREConfigReader.h>
+
 #include "FCMemory.h"
 
 class ForwardChainerUTest;
@@ -34,7 +34,14 @@ class ForwardChainerUTest;
 namespace opencog
 {
 
-class ForwardChainerCallBack;
+enum source_selection_mode {
+    TV_FITNESS_BASED, STI_BASED
+};
+
+class FCMemory;
+class Logger;
+class Rule;
+
 class ForwardChainer {
 private:
     friend class ::ForwardChainerUTest;
@@ -42,35 +49,84 @@ private:
     AtomSpace& _as;
     URECommons _rec;            // utility class
 	Handle _rbs;                // rule-based system
-
 	UREConfigReader _configReader;
-
-
     FCMemory _fcmem;            // stores history
     Logger * _log;
     int _iteration = 0;
+    source_selection_mode _ts_mode;
 
-    /**
-     * initialize config methods
-     */
     void init();
     void add_to_source_list(Handle h);
 
-    void do_pm();
-    void do_pm(const Handle& hsource, const UnorderedHandleSet& var_nodes,
-               ForwardChainerCallBack& fcb);
+    void apply_all_rules(bool search_focus_set = false);
+
+    void do_pm(const Handle& hsource, const UnorderedHandleSet& var_nodes);
+
+    UnorderedHandleSet do_step(bool search_focus_set = false);
+
+    bool is_valid_implicant(const Handle& h);
+
+    void validate(Handle hsource, HandleSeq hfocus_set);
+
+    UnorderedHandleSet get_subatoms(Rule *rule);
+
+    Handle gen_sub_varlist(const Handle& parent, const Handle& parent_varlist);
+
+    HandleSeq substitute_rule_part(
+            AtomSpace& as, Handle hrule, const std::set<Handle>& vars,
+            const std::vector<std::map<Handle, Handle>>& var_groundings);
+
+    bool unify(Handle source, Handle target, Rule* rule);
+    bool subatom_unify(Handle source, Rule* rule);
+    HandleSeq derive_rules(Handle source, Handle target, Rule* rule);
+
 protected:
-    enum source_selection_mode {
-        TV_FITNESS_BASED, STI_BASED
-    };
+    /**
+     * Choose an applicable rules from the rule base by selecting
+     * rules whose premise structurally matches with the source.
+     *
+     * @return  A rule that in which @param source could ground.
+     */
+    virtual Rule* choose_rule(Handle hsource, bool subatom_match );
+
+    /**
+     * Choose additional premises for the rule.
+     *
+     * @fcmem  An object holding the current source/target and other inform
+     *         ation of the forward chaining instance.
+     *
+     * @return  A set of Handles chosen as a result of applying fitness
+     *          criteria with respect to the current source.
+     */
+
+    virtual HandleSeq choose_premises(FCMemory& fcmem);
+    /**
+     * choose next source from the source list
+     *
+     * @return  A handle to the chosen source from source list
+     */
+    virtual Handle choose_next_source(FCMemory& fcmem);
+
+    /**
+     * Apply chosen rule. the default will wrap a custom PM callback class.
+     * i.e invokes _pattern_matcher.
+     *
+     * @return  A set of handles created as a result of applying current
+     *          choosen rule.
+     */
+    virtual HandleSeq apply_rule(Handle rhandle, bool search_focus_set_only =
+            false);
+
+    HandleSeq derive_rules(Handle source, Rule* rule, bool subatomic = false);
+
 public:
 	/**
 	 * Ctor. rbs is a Handle pointing to rule-based system.
 	 */
     ForwardChainer(AtomSpace& as, Handle rbs);
-    void do_chain(ForwardChainerCallBack& fcb, Handle hsource =
-            Handle::UNDEFINED);
-    void do_step(ForwardChainerCallBack& fcb);
+
+    void do_chain(Handle hsource,HandleSeq focus_set ={});
+
     HandleSeq get_chaining_result(void);
 
     void setLogger(Logger* log);
