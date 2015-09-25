fd719fe2b305cba1deaa0790e6e44b857150caea
Linas Vepstas <linasvepstas@gmail.com>
2015-09-25 14:07:21 -0500

    An example that should work, but doesn't

---
+++ examples/guile/filter.scm
@@ -0,0 +1,36 @@
+;
+; Filtering example
+;
+; The PutLink can be used to filter a set of atoms. This filtering
+; is accomplished by using the built-in type-checking facilities
+; that accompany any LambdaLink.
+;
+
+(use-modules (opencog))
+(use-modules (opencog exec))
+(use-modules (opencog query))
+
+(define filter-it
+	(PutLink
+		; Declare a single variable: The variable must be a ConceptNode.
+		(TypedVariableLink
+			(VariableNode "%x")
+			(TypeNode "ConceptNode"))
+		; The body of the put is just the variable.
+		(VariableNode "%x")
+
+		; This is the set of things that will be filetered
+		(SetLink
+			(NumberNode "42")
+			(ConceptNode "foo")
+			(PredicateNode "biffle")
+			(SchemaNode "finagle")
+			(ConceptNode "bar")))
+
+; Now, perform the actual filtering:
+(cog-execute! filter-it)
+
+; It is expected that the above returns just two items:
+(SetLink
+	(ConceptNode "foo")
+	(ConceptNode "bar"))
