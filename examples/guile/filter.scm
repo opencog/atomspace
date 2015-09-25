84112fab06f6bd91fc51a3f87fed16350433461e
Linas Vepstas <linasvepstas@gmail.com>
2015-09-25 14:34:12 -0500

    Extend the example a bit

--- examples/guile/filter.scm
+++ examples/guile/filter.scm
@@ -10,16 +10,19 @@
 (use-modules (opencog exec))
 (use-modules (opencog query))
 
+; Define a PutLink that serves to filter out ConceptNodes from
+; a set of atoms.
 (define filter-it
 	(PutLink
 		; Declare a single variable: The variable must be a ConceptNode.
 		(TypedVariableLink
 			(VariableNode "%x")
 			(TypeNode "ConceptNode"))
+
 		; The body of the put is just the variable.
 		(VariableNode "%x")
 
-		; This is the set of things that will be filetered
+		; This is the set of things that will be filtered.
 		(SetLink
 			(NumberNode "42")
 			(ConceptNode "foo")
@@ -36,3 +39,28 @@
 (SetLink
 	(ConceptNode "foo")
 	(ConceptNode "bar"))
+
+; Similar to the above, but this time, we accept only EvaluationLinks.
+(define filter-links
+	(PutLink
+		; Declare a single variable: The variable must be an
+		; EvaluationLink.
+		(TypedVariableLink
+			(VariableNode "%x")
+			(TypeNode "EvaluationLink"))
+
+		; The body of the put is just the variable.
+		(VariableNode "%x")
+
+		; This is the set of things that will be filtered.
+		(SetLink
+			(NumberNode "42")
+			(ConceptNode "foo")
+			(PredicateNode "biffle")
+			(EvaluationLink
+				(ConceptNode "thingy"))
+			(SchemaNode "finagle")
+			(ConceptNode "bar"))))
+
+; Now, perform the actual filtering:
+(cog-execute! filter-links)
