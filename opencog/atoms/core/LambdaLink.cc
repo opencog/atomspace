edada40cf2e5bb173b898f6fa1e05f93570618c0
Linas Vepstas <linasvepstas@gmail.com>
2015-10-07 17:19:11 -0500

    Restore a LambdaLink class, much neutered.

---
+++ opencog/atoms/core/LambdaLink.cc
@@ -0,0 +1,71 @@
+/*
+ * LambdaLink.cc
+ *
+ * Copyright (C) 2009, 2014, 2015 Linas Vepstas
+ *
+ * Author: Linas Vepstas <linasvepstas@gmail.com>  January 2009
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the
+ * exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atomspace/ClassServer.h>
+#include <opencog/atoms/TypeNode.h>
+#include <opencog/atoms/core/FreeLink.h>
+
+#include "LambdaLink.h"
+
+using namespace opencog;
+
+LambdaLink::LambdaLink(const HandleSeq& oset,
+                       TruthValuePtr tv, AttentionValuePtr av)
+	: ScopeLink(LAMBDA_LINK, oset, tv, av)
+{
+}
+
+LambdaLink::LambdaLink(const Handle& vars, const Handle& body,
+                       TruthValuePtr tv, AttentionValuePtr av)
+	: ScopeLink(LAMBDA_LINK, HandleSeq({vars, body}), tv, av)
+{
+}
+
+LambdaLink::LambdaLink(Type t, const Handle& body,
+                       TruthValuePtr tv, AttentionValuePtr av)
+	: ScopeLink(t, HandleSeq({body}), tv, av)
+{
+}
+
+LambdaLink::LambdaLink(Type t, const HandleSeq& oset,
+                       TruthValuePtr tv, AttentionValuePtr av)
+	: ScopeLink(t, oset, tv, av)
+{
+}
+
+LambdaLink::LambdaLink(Link &l)
+	: ScopeLink(l)
+{
+	// Type must be as expected
+	Type tscope = l.getType();
+	if (not classserver().isA(tscope, LAMBDA_LINK))
+	{
+		const std::string& tname = classserver().getTypeName(tscope);
+		throw InvalidParamException(TRACE_INFO,
+			"Expecting a LambdaLink, got %s", tname.c_str());
+	}
+}
+
+/* ===================== END OF FILE ===================== */
---
+++ opencog/atoms/core/LambdaLink.h
@@ -0,0 +1,86 @@
+/*
+ * opencog/atoms/core/LambdaLink.h
+ *
+ * Copyright (C) 2015 Linas Vepstas
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_LAMBDA_LINK_H
+#define _OPENCOG_LAMBDA_LINK_H
+
+#include <map>
+
+#include <opencog/atoms/core/ScopeLink.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/// The LambdaLink is a ScopeLink that implements beta-reduction.
+/// It does little more than to provide a method that subsitutes values
+/// into the variables bound by ScopeLink.
+///
+class LambdaLink : public ScopeLink
+{
+protected:
+
+	LambdaLink(Type, const Handle&,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	LambdaLink(Type, const HandleSeq&,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+public:
+	LambdaLink(const HandleSeq&,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	LambdaLink(const Handle& varcdecls, const Handle& body,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	LambdaLink(Link &l);
+
+	// Take the list of values `vals`, and substitute them in for the
+	// variables in the body of this lambda. The values must satisfy all
+	// type restrictions, else an exception will be thrown.
+/*
+	Handle substitute(const HandleSeq& vals) const
+	{
+		return VariableList::substitute(_body, vals);
+	}
+*/
+};
+
+typedef std::shared_ptr<LambdaLink> LambdaLinkPtr;
+static inline LambdaLinkPtr LambdaLinkCast(const Handle& h)
+	{ AtomPtr a(h); return std::dynamic_pointer_cast<LambdaLink>(a); }
+static inline LambdaLinkPtr LambdaLinkCast(AtomPtr a)
+	{ return std::dynamic_pointer_cast<LambdaLink>(a); }
+
+// XXX temporary hack ...
+#define createLambdaLink std::make_shared<LambdaLink>
+
+/** @}*/
+}
+
+#endif // _OPENCOG_LAMBDA_LINK_H
