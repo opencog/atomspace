c2f876527babcb65ff727e53aed2c6bd03c9c6f4
Linas Vepstas <linasvepstas@gmail.com>
2015-10-07 13:10:20 -0500

    Extend example, bug-fix

--- examples/guile/state.scm
+++ examples/guile/state.scm
@@ -24,3 +24,21 @@
 
 ; Lets make sure the state changed:
 (cog-incoming-set (AnchorNode "fruit"))
+
+; Query the current state: the cog-execute! function always returns an
+; atom; in this case, it should be the current state.  Note that GetLink
+; always returns a SetLink; so that the current state is wrapped in a
+; SetLink.
+(cog-execute! (GetLink
+	(StateLink (AnchorNode "fruit") (VariableNode "$x"))))
+
+; cog-evaluate! is used to evaluate expressions that return truth
+; values. So, evaluate, to see if the current state is "apple": this
+; should return a truth value corresponding to "true"
+(cog-evaluate! (EqualLink (SetLink (ConceptNode "apple"))
+	(GetLink (StateLink (AnchorNode "fruit") (VariableNode "$x")))))
+
+; Same as above, but should be false:
+(cog-evaluate! (EqualLink (SetLink (ConceptNode "bananna"))
+	(GetLink (StateLink (AnchorNode "fruit") (VariableNode "$x")))))
+
