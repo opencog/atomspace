9466e2c380b506f529be9db8eba972217e6029ce
Linas Vepstas <linasvepstas@gmail.com>
2015-10-08 00:36:35 -0500

    Cleanup the example, explain it a bit

--- examples/guile/bindlink-example.scm
+++ examples/guile/bindlink-example.scm
@@ -1,28 +1,12 @@
 ;
-; cog-bind-example.scm
+; bindlink-example.scm
 ;
 ; Linas Vepstas January 2009
 ;
-; This file contains a simple, visual-inspection demo example for the 
+; This file contains a simple, visual-inspection demo example for the
 ; use of the query engine to find pattern matches using the scheme
 ; interfaces.  The below defines a single BindLink and two chunks of
 ; data, and then calls the pattern matcher.
-;
-; This example can be run in two different ways.  The first way requires
-; that the cogserver be running. Then either cut-n-paste, or cat the file:
-;    cat cog-bind-example.scm | telnet localhost 17001
-;
-; To see the results, telnet to either port 17001 or 18001
-;    telnet localhost 17001
-; and then run the final step:
-;    (cog-bind x)
-;
-; The second way of running this is to run it without the cogserver,
-; using only guile:
-;
-;    export LTDL_LIBRARY_PATH=build/opencog/guile:build/opencog/query
-;    cd ../..
-;    cat opencog/query/cog-bind-example.scm | guile -L build -L opencog/scm
 ;
 ; The expected result, after running the below, is that the following
 ; should be printed:
@@ -32,7 +16,7 @@
 ;     (ListLink (ConceptNode "pottery")
 ;            (ConceptNode "clay"))))
 ;
-; Note the outermost ListLink is simply an enumeration of all of the 
+; Note the outermost ListLink is simply an enumeration of all of the
 ; possible results from the implication; in this case, there is only one
 ; possible result.
 ;
@@ -41,14 +25,25 @@
 (use-modules (opencog query))
 (load-from-path "utilities.scm")
 
-(define x
+; Create a "semantic triple" by combining a verb, an object and a
+; preposition. This searches the atomsace for a verb-object pair,
+; and a verb-preposition pair, and if it finds them, creates a triple.
+;
+; Note: this is a highly simplified example; doing this well, for
+; general English language sentences, as parsed by RelEx, is
+; considerably more complex.
+(define make-semantic-triple
 	(BindLink
-		(VariableList
+		(VariableList   ; Optional variable declaration
 			(VariableNode "$var0")
 			(VariableNode "$var1")
 			(VariableNode "$verb")
 		)
+
+		; AndLink means each of the clauses must be satisfied in the
+		; atomspace.
 		(AndLink
+			; Look for _obj($verb, $var0)
 			(EvaluationLink
 				(PredicateNode "_obj")
 				(ListLink
@@ -56,6 +51,7 @@
 					(VariableNode "$var0") ; (ConceptNode "pottery")
 				)
 			)
+			; Look for from($verb, $var1)
 			(EvaluationLink
 				(PredicateNode "from")
 				(ListLink
@@ -64,6 +60,7 @@
 				)
 			)
 		)
+		; Combine the two above into one.
 		(EvaluationLink
 			(PredicateNode "make_from")
 			(ListLink
@@ -74,23 +71,19 @@
 	)
 )
 
-(define o
-	(EvaluationLink (stv 1.0 1.0)
-		(PredicateNode "_obj")
-		(ListLink
-			(ConceptNode "make")
-			(ConceptNode "pottery")
-		)
-	)
-)
-(define f
-	(EvaluationLink  (stv 1.0 1.0)
-		(PredicateNode "from")
-		(ListLink
-			(ConceptNode "make")
-			(ConceptNode "clay")
-		)
-	)
-)
+; Place some data in the atomspace that the above pattern will
+; be able to find.
+(EvaluationLink (stv 1.0 1.0)
+	(PredicateNode "_obj")
+	(ListLink
+		(ConceptNode "make")
+		(ConceptNode "pottery")))
 
-(cog-bind x)
+(EvaluationLink  (stv 1.0 1.0)
+	(PredicateNode "from")
+	(ListLink
+		(ConceptNode "make")
+		(ConceptNode "clay")))
+
+; Run the pattern matcher.
+(cog-bind make-semantic-triple)
