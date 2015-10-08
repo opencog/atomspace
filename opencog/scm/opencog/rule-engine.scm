96b5d2dec9d1470610de3b4989ce97c43008bd0c
Linas Vepstas <linasvepstas@gmail.com>
2015-10-07 23:44:50 -0500

    Merge rule-engine utils

--- opencog/scm/opencog/rule-engine.scm
+++ opencog/scm/opencog/rule-engine.scm
@@ -6,5 +6,90 @@
 
 (load-extension "libruleengine" "opencog_ruleengine_init")
 
-(load-from-path "rule-engine-utils.scm")
-(export-rule-engine-utils)
+;
+;;; Commentary:
+;
+; Handy utilities for working with the rule-engine. In particular to
+; configure a rule-based system (rbs).
+;
+; Utilities include:
+; -- ure-add-rules -- Associate rules to an rbs
+; -- ure-set-num-parameter -- Set a numeric parameter of an rbs
+; -- ure-set-fuzzy-bool-parameter -- Set a fuzzy boolean parameter of an rbs
+;
+; If you add more utilities don't forget to add them in the
+; export-rule-engine-utils function.
+;
+;;; Code:
+; Copyright (c) 2015, OpenCog Foundation
+;
+
+; Given an rbs and a list of pairs (rule weight) create for each rule
+;
+; MemberLink (stv weight 1)
+;    rule
+;    rbs
+(define (ure-add-rules rbs rules)
+  (define (ure-add-rule weighted-rule)
+    (let ((rule (car weighted-rule))
+          (weight (cadr weighted-rule)))
+      (MemberLink (stv weight 1)
+         rule
+         rbs)))
+  (for-each ure-add-rule rules)
+)
+
+; Set numerical parameters. Given an rbs, a parameter name and its
+; value, create
+;
+; ExecutionLink
+;    SchemaNode name
+;    rbs
+;    (NumberNode "value")
+;
+; It will also delete the any
+;
+; ExecutionLink
+;    SchemaNode name
+;    rbs
+;    *
+;
+; to be sure there is ever only one value associated to that
+; parameter. The value is automatically converted into string.
+(define (ure-set-num-parameter rbs name value)
+  (define (param-hypergraph value)
+    (ExecutionLink
+       (SchemaNode name)
+       rbs
+       value)
+  )
+  (let ((del-prev-val (BindLink
+                          (param-hypergraph (VariableNode "__VALUE__"))
+                          (DeleteLink
+                             (param-hypergraph (VariableNode "__VALUE__"))))))
+       ; Delete any previous value for that parameter
+       (cog-bind del-prev-val)
+       ; Delete pattern to not create to much junk in the atomspace
+       (purge-hypergraph del-prev-val)
+  )
+
+  ; Set new value for that parameter
+  (param-hypergraph (NumberNode (number->string value)))
+)
+
+; Set (fuzzy) bool parameters. Given an RBS, a parameter name and its
+; value, create (or overwrite)
+;
+; EvaluationLink (stv value 1)
+;    PredicateNode name
+;    rbs
+(define (ure-set-fuzzy-bool-parameter rbs name value)
+  (EvaluationLink (stv value 1)
+     (PredicateNode name)
+     rbs)
+)
+
+(export ure-add-rules
+        ure-set-num-parameter
+        ure-set-fuzzy-bool-parameter
+        export-rule-engine-utils)
