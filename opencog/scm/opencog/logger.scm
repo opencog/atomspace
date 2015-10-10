4d139955a0d3c852c792fbd1d1eb7db9c01f5f44
Linas Vepstas <linasvepstas@gmail.com>
2015-10-09 23:07:16 -0500

    Add documentation for the logger

--- opencog/scm/opencog/logger.scm
+++ opencog/scm/opencog/logger.scm
@@ -18,12 +18,27 @@
 (use-modules (ice-9 format))
 
 (define (cog-logger-error msg . args)
+"
+ cog-logger-error MSG ARGS
+    Print MSG into the logfile, at the \"error\" logging level.
+    The MSG can be in any ice-9 printing format.
+"
   (cog-logger-error-str (apply format #f msg args)))
 
 (define (cog-logger-warn msg . args)
+"
+ cog-logger-error MSG ARGS
+    Print MSG into the logfile, at the \"warning\" logging level.
+    The MSG can be in any ice-9 printing format.
+"
   (cog-logger-warn-str (apply format #f msg args)))
 
 (define (cog-logger-info msg . args)
+"
+ cog-logger-error MSG ARGS
+    Print MSG into the logfile, at the \"info\" logging level.
+    The MSG can be in any ice-9 printing format.
+"
   (cog-logger-info-str (apply format #f msg args)))
 
 (define (cog-logger-debug msg . args)
