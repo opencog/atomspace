b752afac94923bb78de73b835024c61b3d1dc6a1
Linas VEPSTAS <linasvepstas@gmail.com>
2015-10-26 20:22:40 +0800

    Fix cut-n-paste bug

--- opencog/atoms/reduct/MinusLink.cc
+++ opencog/atoms/reduct/MinusLink.cc
@@ -6,7 +6,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License v3 as
- * published by the Minus Software Foundation and including the exceptions
+ * published by the Free Software Foundation and including the exceptions
  * at http://opencog.org/wiki/Licenses
  *
  * This program is distributed in the hope that it will be useful,
