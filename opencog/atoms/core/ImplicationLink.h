139a4b619d9a9ca55e6498b4428cd7d606fe9d7d
Nil Geisweiller <ngeiswei@gmail.com>
2015-11-05 16:12:07 +0200

    Implement ImplicationLink, inherit from ScopeLink
    Now ImplicationLink has either 2 or 3 arguments. In the case of 2 it has
    to be 2 predicates or equivalent (though no checking is performed). In
    the case it has 3 arguments, the first one must be variable
    declarations (and a checking a performed).

---
+++ opencog/atoms/core/ImplicationLink.h
@@ -0,0 +1,97 @@
+/*
+ * opencog/atoms/core/ImplicationLink.h
+ *
+ * Copyright (C) 2015 OpenCog Foundation
+ * All Rights Reserved
+ * Author: Nil Geisweiller
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_IMPLICATION_LINK_H
+#define _OPENCOG_IMPLICATION_LINK_H
+
+#include <map>
+
+#include <opencog/atoms/core/ScopeLink.h>
+
+#include <opencog/util/Logger.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/// The ImplicationLink has 2 forms
+///
+/// ImplicationLink
+///    <P>
+///    <Q>
+///
+/// where P and Q are predicates. Or
+///
+/// ImplicationLink
+///    <variables>
+///    <P-body>
+///    <Q-body>
+///
+/// A syntactic sugar form for
+///
+/// ImplicationLink
+///    LambdaLink
+///       <variables>
+///       <P-body>
+///    LambdaLink
+///       <variables>
+///       <Q-body>
+///
+/// Due to this syntactic sugar form it must inherit from a
+/// ScopeLink. In the case where the former (non syntactic sugar) form
+/// is used, it will still inherit from a ScopeLink (due to C++
+/// inheritance mechanics), but will merely have an empty bound
+/// variable set.
+///
+class ImplicationLink : public ScopeLink
+{
+protected:
+	ImplicationLink(Type, const HandleSeq&,
+	                TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	                AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	void init(void);
+	void extract_variables(const HandleSeq& oset);
+	// Useless for now, but...
+	Handle _implicand;
+public:
+	ImplicationLink(const HandleSeq&,
+	                TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	                AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	ImplicationLink(Link &l);
+};
+
+typedef std::shared_ptr<ImplicationLink> ImplicationLinkPtr;
+ImplicationLinkPtr ImplicationLinkCast(const Handle& h);
+ImplicationLinkPtr ImplicationLinkCast(AtomPtr a);
+
+// XXX temporary hack ...
+#define createImplicationLink std::make_shared<ImplicationLink>
+
+/** @}*/
+}
+
+#endif // _OPENCOG_SCOPE_LINK_H
--- opencog/atomspace/AtomTable.cc
+++ opencog/atomspace/AtomTable.cc
@@ -49,6 +49,7 @@
 #include <opencog/atoms/core/StateLink.h>
 #include <opencog/atoms/core/UniqueLink.h>
 #include <opencog/atoms/core/VariableList.h>
+#include <opencog/atoms/core/ImplicationLink.h>
 #include <opencog/atoms/execution/EvaluationLink.h>
 #include <opencog/atoms/execution/ExecutionOutputLink.h>
 #include <opencog/util/exceptions.h>
@@ -285,6 +286,9 @@ AtomPtr AtomTable::do_factory(Type atom_type, AtomPtr atom)
     } else if (LAMBDA_LINK == atom_type) {
         if (NULL == LambdaLinkCast(atom))
             return createLambdaLink(*LinkCast(atom));
+    } else if (IMPLICATION_LINK == atom_type) {
+        if (NULL == ImplicationLinkCast(atom))
+            return createImplicationLink(*LinkCast(atom));
     } else if (classserver().isA(atom_type, FUNCTION_LINK)) {
 /* More circular-dependency heart-ache
         if (NULL == FunctionLinkCast(atom))
@@ -375,6 +379,8 @@ static AtomPtr do_clone_factory(Type atom_type, AtomPtr atom)
         return createVariableList(*LinkCast(atom));
     if (LAMBDA_LINK == atom_type)
         return createLambdaLink(*LinkCast(atom));
+    if (IMPLICATION_LINK == atom_type)
+        return createImplicationLink(*LinkCast(atom));
     if (classserver().isA(atom_type, FUNCTION_LINK))
         // XXX FIXME more circular-dependency heart-ache
         // return FunctionLink::factory(LinkCast(atom));
