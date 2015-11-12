3f14aa9fa66ca6d4387aaf8b8a1fb775ba8f4b5e
Amen Belayneh <amenbelayneh@gmail.com>
2015-11-12 14:29:43 +0000

    export functions in apply.scm

--- opencog/scm/apply.scm
+++ opencog/scm/apply.scm
@@ -9,28 +9,28 @@
 ; ForAllLink
 ;    VariableList X1,...,Xn
 ;    Body
-(define (get-variables forAll) (cog-outgoing-set (gar forAll)))
+(define-public (get-variables forAll) (cog-outgoing-set (gar forAll)))
 ; Get the body from forAll
-(define (get-body forAll) (gadr forAll))
+(define-public (get-body forAll) (gadr forAll))
 ; Return a list of pairs (variable . atom) given a forAllLink and a
 ; list of atoms corresponding to arguments.
-(define (get-bindings forAll arguments)
+(define-public (get-bindings forAll arguments)
     (map (lambda (v a) (cons v a)) (get-variables forAll) arguments))
 
 ;------------------------------------------------------------------------
 ; Substitute variables by arguments in body given bindings
-(define (substitute-var bindings body)
+(define-public (substitute-var bindings body)
   (let ((body_type (cog-type body)))
     (cond ; recursive case
           ((cog-subtype? 'Link body_type)
-           (apply cog-new-link 
+           (apply cog-new-link
                   (cons body_type
-                        (map (lambda (child) (substitute-var bindings child)) 
+                        (map (lambda (child) (substitute-var bindings child))
                              (cog-outgoing-set body)))))
           ; base cases
           ((cog-subtype? 'VariableNode body_type) (cdr (assoc body bindings)))
           (else body))))
 ; Perform universal instantiation given ForAll atom and a list of
 ; atoms representing the arguments
-(define (universal-instantiate forAll arguments)
+(define-public (universal-instantiate forAll arguments)
   (substitute-var (get-bindings forAll arguments) (get-body forAll)))
