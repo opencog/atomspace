b375b73fbba09422f7d069dce1f675a8402f796d
Linas Vepstas <linasvepstas@gmail.com>
2015-12-28 19:46:21 -0600

    Start work on type-matching.

--- opencog/atomutils/TypeUtils.h
+++ opencog/atomutils/TypeUtils.h
@@ -42,6 +42,72 @@ namespace opencog
  */
 bool value_is_type(const Handle& type_spec, const Handle& val);
 
+/**
+ * Type matcher. Returns true if `left` can mate with `right`.
+ * Here, `left` can be a type definition, and `right` can be
+ * another type defintion, or a value.  Mating is possible whenever
+ * `left` is broader, less restricitve than `right`; equivalently
+ * if `right` is narrower than 'left`.
+ *
+ * Mating types and values:
+ * left == (Type "Concept")    right == (Concept "foo")  can mate.
+ * left == (Type "Concept")    right == (Number 13)  cannot.
+ *
+ * Mating types and types:
+ * left == (Type "Concept")    right == (Type "Concept")  can mate.
+ *
+ * Left is wider (polymorphic, in this case)
+ *   left == (TypeChoice (Type "Concept") (Type "Number"))
+ *   right == (Type "Number")  can mate.
+ *
+ * Function call arguments can be checked:
+ *   left == (Arrow (Type "Concept") (Type "Number"))
+ *   right == (Concept "foo")  can mate.
+ *
+ *   left == (Arrow (Type "Concept") (Type "Number"))
+ *   right == (Number 13)  cannot.
+ *
+ * Function call chains can be checked:
+ *   left == (Arrow (Type "Concept") (Type "Number"))
+ *   right == (Type "Concept")  can mate.
+ *
+ * The following can mate, because left accepts a concept as input,
+ * and right generates a concept as output:
+ *   left == (Arrow (Type "Concept") (Type "Number"))
+ *   right == (Arrow (Type "Evaluation") (Type "Concept")
+ *
+ * Any type specification is valid: SignatureLinks, etc work too.
+ */
+
+bool type_match(const Handle&, const Handle&);
+
+/**
+ * Same as above, but return the composition (beta-reduction) of the
+ * match. If the types do NOT match, theundefined handle is returned.
+ * If the types do match, then, for many cases, the right side is the
+ * result.  The compostion of arrows, however, results either in a
+ * new arrow, or a simple return type.
+ *
+ * Examples:
+ *
+ * Function call arguments can be checked:
+ *   left == (Arrow (Type "Concept") (Type "Number"))
+ *   right == (Concept "foo")  can mate.
+ *   result = (Type "Number")
+ *
+ * Function call chains:
+ *   left == (Arrow (Type "Concept") (Type "Number"))
+ *   right == (Type "Concept")  can mate.
+ *   result = (Type "Number")
+ *
+ * The following can mate, because left accepts a concept as input,
+ * and right generates a concept as output:
+ *   left == (Arrow (Type "Concept") (Type "Number"))
+ *   right == (Arrow (Type "Evaluation") (Type "Concept")
+ *   result = (Arrow (Type "Evaluation") (Type "Number"))
+ */
+Handle type_compose(const Handle&, const Handle&);
+
 /** @}*/
 }
 
