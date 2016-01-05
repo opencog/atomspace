7f28979ff0d284d1a789c85a893ff819cba702ba
Linas Vepstas <linasvepstas@gmail.com>
2016-01-05 15:51:40 -0600

    Start work on a unit test.

---
+++ tests/atoms/maplink.scm
@@ -0,0 +1,55 @@
+
+(use-modules (opencog) (opencog exec))
+
+(define single
+	(MapLink
+		(ScopeLink
+			(Variable "$x")
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Variable "$x"))))
+		(EvaluationLink
+			(Predicate "foo")
+			(ListLink (Concept "bar") (Concept "baz"))))
+)
+
+(define single-set
+	(MapLink
+		(ScopeLink
+			(Variable "$x")
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Variable "$x"))))
+		(SetLink
+			; Not in alphaebtical or type-order!
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Number 3)))
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Concept "ah two")))
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Concept "ah one")))
+		))
+)
+
+(define single-link
+	(MapLink
+		(ScopeLink
+			(Variable "$x")
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Variable "$x"))))
+		(ListLink
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Concept "ah one")))
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Concept "ah two")))
+			(EvaluationLink
+				(Predicate "foo")
+				(ListLink (Concept "bar") (Number 3)))
+		))
+)
