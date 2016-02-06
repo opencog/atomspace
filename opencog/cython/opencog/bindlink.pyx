78b92d6684e831b0e8d981548b573eb8539a3fa3
Curtis Faith <curtis.m.faith@gmail.com>
2016-02-05 19:24:00 -0500

    Update Exception text

--- opencog/cython/opencog/bindlink.pyx
+++ opencog/cython/opencog/bindlink.pyx
@@ -12,28 +12,28 @@ def stub_bindlink(AtomSpace atomspace, Atom atom):
     return result
 
 def bindlink(AtomSpace atomspace, Atom atom):
-    if atom == None: raise ValueError("stub_bindlink atom is: None")
+    if atom == None: raise ValueError("bindlink atom is: None")
     cdef cHandle c_result = c_bindlink(atomspace.atomspace,
                                        deref(atom.handle))
     cdef Atom result = Atom(c_result.value(), atomspace)
     return result
 
 def single_bindlink(AtomSpace atomspace, Atom atom):
-    if atom == None: raise ValueError("stub_bindlink atom is: None")
+    if atom == None: raise ValueError("single_bindlink atom is: None")
     cdef cHandle c_result = c_single_bindlink(atomspace.atomspace,
                                               deref(atom.handle))
     cdef Atom result = Atom(c_result.value(), atomspace)
     return result
 
 def af_bindlink(AtomSpace atomspace, Atom atom):
-    if atom == None: raise ValueError("stub_bindlink atom is: None")
+    if atom == None: raise ValueError("af_bindlink atom is: None")
     cdef cHandle c_result = c_af_bindlink(atomspace.atomspace, 
                                           deref(atom.handle))
     cdef Atom result = Atom(c_result.value(), atomspace)
     return result
 
 def satisfaction_link(AtomSpace atomspace, Atom atom):
-    if atom == None: raise ValueError("stub_bindlink atom is: None")
+    if atom == None: raise ValueError("satisfaction_link atom is: None")
     cdef tv_ptr result_tv_ptr = c_satisfaction_link(atomspace.atomspace,
                                                  deref(atom.handle))
     cdef cTruthValue* result_tv = result_tv_ptr.get()
@@ -42,13 +42,13 @@ def satisfaction_link(AtomSpace atomspace, Atom atom):
     return TruthValue(strength, confidence)
 
 def execute_atom(AtomSpace atomspace, Atom atom):
-    if atom == None: raise ValueError("stub_bindlink atom is: None")
+    if atom == None: raise ValueError("execute_atom atom is: None")
     cdef cHandle c_result = c_execute_atom(atomspace.atomspace,
                                            deref(atom.handle))
     return Atom(c_result.value(), atomspace)
 
 def evaluate_atom(AtomSpace atomspace, Atom atom):
-    if atom == None: raise ValueError("stub_bindlink atom is: None")
+    if atom == None: raise ValueError("evaluate_atom atom is: None")
     cdef tv_ptr result_tv_ptr = c_evaluate_atom(atomspace.atomspace,
                                                 deref(atom.handle))
     cdef cTruthValue* result_tv = result_tv_ptr.get()
