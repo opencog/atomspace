5d4ac12efcb704c53cfbd5a006adc013e1e6f4f3
Nil Geisweiller <ngeiswei@gmail.com>
2016-02-15 08:23:22 +0200

    Use double semi-colon comment + indentation fixes
    Using double semi-colon comment align the comment with the code's
    indentation in emacs. I can only assume that it is a popular convention.

--- tests/rule-engine/bc-deduction.scm
+++ tests/rule-engine/bc-deduction.scm
@@ -1,21 +1,23 @@
-; =============================================================================
-; Deduction Rule
-; TODO The rule must be applicable to ImplicationLink, SubsetLink and PartOfLink
-;
-; AND(Inheritance A B, Inheritance B C) entails Inheritance A C
-; -----------------------------------------------------------------------------
-(define pln-rule-deduction
+;; =============================================================================
+;; Simple Crisp Deduction Rule
+;;
+;; Inheritance A B
+;; Inheritance B C
+;; |-
+;; Inheritance A C
+;; -----------------------------------------------------------------------------
+(define bc-deduction-rule
     (BindLink
         (VariableList
-                (TypedVariableLink
-                   (VariableNode "$A")
-                   (TypeNode "ConceptNode"))
-                (TypedVariableLink
-                   (VariableNode "$B")
-                   (TypeNode "ConceptNode"))
-                (TypedVariableLink
-                   (VariableNode "$C")
-                   (TypeNode "ConceptNode")))
+            (TypedVariableLink
+                (VariableNode "$A")
+                (TypeNode "ConceptNode"))
+            (TypedVariableLink
+                (VariableNode "$B")
+                (TypeNode "ConceptNode"))
+            (TypedVariableLink
+                (VariableNode "$C")
+                (TypeNode "ConceptNode")))
         (AndLink
             (InheritanceLink
                 (VariableNode "$A")
