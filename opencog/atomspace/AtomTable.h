c917fe216ffb00cf3060f72dcc1357621d80cc56
Linas Vepstas <linasvepstas@gmail.com>
2016-02-18 11:00:53 -0600

    Fix typoes

--- opencog/atomspace/AtomTable.h
+++ opencog/atomspace/AtomTable.h
@@ -102,7 +102,7 @@ private:
     std::unordered_map<UUID, Handle> _atom_set;
 
     //!@{
-    //! Index for quick retreival of certain kinds of atoms.
+    //! Index for quick retrieval of certain kinds of atoms.
     TypeIndex typeIndex;
     NodeIndex nodeIndex;
     LinkIndex linkIndex;
@@ -277,8 +277,8 @@ public:
      * XXX The async code path doesn't really do anything yet, since
      * it also uses the big global lock, at the moment.  This needs
      * fixing, mostly be creating a second mutex for the atom insertion,
-     * and also giving each index its own uique mutex, to avoid
-     * collsions.  So teh API is here, but more work is stil needed.
+     * and also giving each index its own unique mutex, to avoid
+     * collisions.  So the API is here, but more work is still needed.
      *
      * @param The new atom to be added.
      * @return The handle of the newly added atom.
