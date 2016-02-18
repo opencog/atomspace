d90012886da515a308b479911b06bb5a41ec7055
Linas Vepstas <linasvepstas@gmail.com>
2016-02-18 17:49:04 -0600

    Remove dead code.
    We don't want to use this code, its slow.  Don't leave it as cruft,
    in case someone else starts using it.

--- opencog/atomspace/TypeIndex.h
+++ opencog/atomspace/TypeIndex.h
@@ -64,7 +64,6 @@ class TypeIndex : public FixedIntegerIndex
 		{
 			remove(a->getType(), a);
 		}
-		size_t getNumAtomsOfType(Type type, bool subclass) const;
 
 		class iterator
 			: public std::iterator<std::forward_iterator_tag, Handle>
