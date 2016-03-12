a955008ef3f4454fc27ca6947708ec0d136a61e0
Linas Vepstas <linasvepstas@gmail.com>
2016-03-12 11:51:53 -0600

    Iinital work on unit-test craziness

---
+++ tests/rule-engine/crisp.scm
@@ -0,0 +1,55 @@
+;
+; Simple crisp deduction example
+;
+; 1. Launch guile under this directory (see examples/guile/README.md
+; for prerequisite if you haven't already)
+;
+; $ guile
+;
+; 2. The load this file
+;
+; scheme@(guile-user)> (load-from-path "crisp.scm")
+;
+; 3. Scroll to the bottom, and run some of the commented-out examples.
+
+(use-modules (opencog))
+(use-modules (opencog rule-engine))
+
+; I still don't understand this module thing, it still crashes so I
+; still need to have that
+; (load-from-path "av-tv.scm")
+; (load-from-path "utilities.scm")
+; (load-from-path "rule-engine-utils.scm")
+
+; Load URE configuration (add the current file dir so it can be loaded
+; from anywhere)
+; (add-to-load-path (dirname (current-filename)))
+(load "crisp-config.scm")
+
+; Define knowledge base
+(define A (PredicateNode "A" (stv 1 1)))
+(define B (PredicateNode "B"))
+(define C (PredicateNode "C"))
+(define AB (ImplicationLink (stv 1 1) A B))
+(define BC (ImplicationLink (stv 1 1) B C))
+
+; 1. Test forward chaining (based on the deduction rule)
+
+;; scheme@(guile-user)> (crisp-fc AB)
+;; $1 = (ListLink
+;;    (ImplicationLink (stv 1 0.99999982)
+;;       (PredicateNode "A")
+;;       (PredicateNode "C")
+;;    )
+;; )
+
+; 2. Test backward chaining (based on the modus ponens rule)
+
+;; scheme@(guile-user)> (crisp-bc C)
+;; $1 = (ListLink
+;; )
+
+; while the TV of C will be suitably updated.
+
+;; scheme@(guile-user)> C
+;; $2 = (PredicateNode "C" (stv 1 0.99999982))
