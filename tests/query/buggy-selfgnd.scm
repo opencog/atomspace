cd8cb4f52f4110f451b625810bdd704ca0fbaea4
Linas Vepstas <linasvepstas@gmail.com>
2016-03-17 20:43:41 -0500

    New `IdenticalLink` link, for syntactic equality.

--- tests/query/buggy-selfgnd.scm
+++ tests/query/buggy-selfgnd.scm
@@ -1,5 +1,6 @@
-; In this case instatiator tried to instantiate and add new BindLink
-; atom to Atomspace throwing exception. More details here:
+;
+; In this case, the instatiator tried to instantiate and add a new
+; BindLink atom to Atomspace, throwing exception. More details here:
 ; https://github.com/opencog/atomspace/issues/210
 
 (EvaluationLink
@@ -11,6 +12,11 @@
     (ConceptNode "glib")
     (ConceptNode "blab"))
 
+; This is a kind-of-ish poorly-formed expression.
+; When the EqualLink is evaluated, its arguments are executed first.
+; If $lnk was bound to the BindLink (which it could be, because there
+; is no type restriction on $lnk), this causes the BindLink to be run
+; again .. and again .. infinite regress.
 (define bnd
     (BindLink
         (AndLink
