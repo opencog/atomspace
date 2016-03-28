f37e1eeee1805acb8167be3e6854e23368bd90be
Linas Vepstas <linasvepstas@gmail.com>
2016-03-28 12:55:44 -0500

    Functional change: this should affect things!
    In the past, it was a no-op, because not lll was true only
    if the atom was NOT a link!

--- opencog/rule-engine/backwardchainer/BackwardChainer.cc
+++ opencog/rule-engine/backwardchainer/BackwardChainer.cc
@@ -837,12 +837,8 @@ static void get_all_unique_atoms(const Handle& h, UnorderedHandleSet& atom_set)
 {
     atom_set.insert(h);
 
-    LinkPtr lll(LinkCast(h));
-    if (not lll)
-    {
-        for (const Handle& o : lll->getOutgoingSet())
-            get_all_unique_atoms(o, atom_set);
-    }
+    for (const Handle& o : h->getOutgoingSet())
+        get_all_unique_atoms(o, atom_set);
 }
 
 /**
