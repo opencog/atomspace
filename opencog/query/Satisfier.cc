ddbd8a3b4e768e0d63f62da0e332a43d526e697d
Linas Vepstas <linasvepstas@gmail.com>
2016-04-06 13:59:45 -0500

    Use a generic ctor for all PatternLink's

--- opencog/query/Satisfier.cc
+++ opencog/query/Satisfier.cc
@@ -149,9 +149,8 @@ Handle opencog::satisfying_set(AtomSpace* as, const Handle& hlink, size_t max_re
 	PatternLinkPtr bl(PatternLinkCast(hlink));
 	if (NULL == bl)
 	{
-		// If it is a BindLink (for example), we want to use that ctor
-		// instead of the default ctor.  Note BindLink isA GetLink.
-		if (classserver().isA(blt, GET_LINK))
+		// Use the correct constructor for derived types.
+		if (classserver().isA(blt, SATISFYING_LINK))
 			bl = createPatternLink(*LinkCast(hlink));
 		else
 			bl = createPatternLink(hlink);
