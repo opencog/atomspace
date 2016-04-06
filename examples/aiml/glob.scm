1b65526117c558bc843f316d014d6408b3f0c606
Linas Vepstas <linasvepstas@gmail.com>
2016-04-06 15:40:27 -0500

    iDemonstrate typed globs

--- examples/aiml/glob.scm
+++ examples/aiml/glob.scm
@@ -33,14 +33,9 @@
 (define glob-you
 	(BindLink
 		(ListLink
-			(Concept "I")
-			(Glob "$star")
-			(Concept "you"))
+			(Concept "I") (Glob "$star") (Concept "you"))
 		(ListLink
-			(Concept "I")
-			(Glob "$star")
-			(Concept "you")
-			(Concept "too"))))
+			(Concept "I") (Glob "$star") (Concept "you") (Concept "too"))))
 
 ;; This one implements "I love *" -> "Hey! I love * too"
 (define love-glob
@@ -59,3 +54,27 @@
 ;; Both of these patterns should "work as expected".
 ; (cog-execute! glob-you)
 ; (cog-execute! love-glob)
+
+; -----------------------------------------------------------------
+; Globs can be typed, just like variables:
+
+(define love-type-glob
+	(BindLink
+		(TypedVariable (Glob "$star") (Type "NumberNode"))
+		(ListLink
+			(Concept "I")
+			(Concept "love")
+			(Glob "$star"))
+		(ListLink
+			(Concept "Hey!")
+			(Concept "I")
+			(Concept "like")
+			(Glob "$star")
+			(Concept "also"))))
+
+
+;;; Populate the atomspace with some more "sentences".
+(ListLink (Concept "I") (Concept "love") (Number 42))
+
+; This will find the match (Number 42); it will NOT match (Concept "you")
+; (cog-execute! love-type-glob)
