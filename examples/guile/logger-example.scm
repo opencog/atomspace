a5a6cf1e732a165fb049499096ccb91a092284d6
Nil Geisweiller <ngeiswei@gmail.com>
2016-04-22 08:14:33 +0300

    Fix logger examples according to binding changes

--- examples/guile/logger-example.scm
+++ examples/guile/logger-example.scm
@@ -8,21 +8,21 @@
 (cog-logger-debug "First debug message")
 
 ;; Let's set it to debug
-(cog-logger-set-level "debug")
+(cog-logger-set-level! "debug")
 
 ;; Now we can log a debug message
 (cog-logger-debug "Second debug message")
 
 ;; By default the log file is opencog.log, located in the current
 ;; folder. Let's change that
-(cog-logger-set-filename "opencog-scm-logger-example.log")
+(cog-logger-set-filename! "opencog-scm-logger-example.log")
 
 ;; Now log message go to the newly created log file
 (cog-logger-info "Third info message")
 
 ;; Actually, screw those log files, I want the messages to be printed
 ;; to the stdout
-(cog-logger-set-stdout #t)
+(cog-logger-set-stdout! #t)
 
 ;; Let's see (well it is still logged to the current log file as well)
 (cog-logger-info "Fourth info message")
@@ -33,7 +33,7 @@
 
 ;; Finally, in case you want the message to be logged exactly as the
 ;; logger is called you can set the sync flag to true
-(cog-logger-set-sync #t)
+(cog-logger-set-sync! #t)
 
 ;; The logger supports scheme format (see
 ;; https://www.gnu.org/software/guile/manual/html_node/Formatted-Output.html
