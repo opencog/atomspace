7c366488afc6e26d5a960efdc18d17d95747ca69
Linas Vepštas <linasvepstas@gmail.com>
2016-06-06 17:24:27 +0800

    Add a lazy/eager evaluation unit test

--- tests/atoms/random-choice.scm
+++ tests/atoms/random-choice.scm
@@ -5,14 +5,51 @@
 ; -- DefinedPredicate ad DefinedSchema
 ; -- DivideLink
 ; -- Tail recursion optimization in the SequentialAndLink
+; -- Lazy/Eager execution of FunctionLink arguments.
 ;
 (use-modules (opencog) (opencog exec))
 
 ; Pick A with 70% probability, pick B with 30% probability
-(Define (DefinedSchema "randy")
+(Define (DefinedSchema "rand-transpose")
    (RandomChoice
       (List (Number 0.7) (Number 0.3))
       (List (Concept "A") (Concept "B"))))
+
+; Pick A with 70% probability, pick B with 30% probability
+(Define (DefinedSchema "rand-set-choice")
+   (RandomChoice
+      (SetLink
+         (List (Number 0.7) (Concept "A"))
+         (List (Number 0.3) (Concept "B")))))
+
+; Pick A with 70% probability, pick B with 30% probability
+; Use a Getlink to fish out the probabilities, isntead of
+; hard-coding them, as above. This forces the lazy/eager
+; evaluation subsystem to "do the right thing" -- to evaluate
+; the GetLink before running the RandomChoice.
+(Define (DefinedSchema "randy")
+   (RandomChoice
+      (GetLink
+         (VariableList (VariableNode "$prob") (VariableNode "$expr"))
+         (AndLink
+            (EvaluationLink
+               (PredicateNode "Emotion-expression")
+               (ListLink (ConceptNode "wake-up") (VariableNode "$expr")))
+            (StateLink
+               (ListLink
+                  (ConceptNode "wake-up")
+                  (VariableNode "$expr"))
+               (VariableNode "$prob"))))
+      ))
+
+; Data to allow the GetLink above to find the needed data.
+(Evaluation (Predicate "Emotion-expression")
+   (ListLink (Concept "wake-up") (Concept "A")))
+(State (ListLink (ConceptNode "wake-up") (Concept "A")) (Number 0.7))
+
+(Evaluation (Predicate "Emotion-expression")
+   (ListLink (Concept "wake-up") (Concept "B")))
+(State (ListLink (ConceptNode "wake-up") (Concept "B")) (Number 0.3))
 
 ; Initialize counts to zero.
 (State (Anchor "sum-A") (Number 0))
