0376c640db7d49448480d4c016a373f492331587
Linas Vepstas <linasvepstas@gmail.com>
2016-10-14 14:32:26 -0500

    Actually run the rule, also

--- tests/query/scope-confound.scm
+++ tests/query/scope-confound.scm
@@ -7,7 +7,7 @@
 
 ; -------------------------------------------------------------
 ; From "opencog/pln/rules/evaluation-to-member-rule.scm"
-(define (evaluation-to-member-2-rule)
+(define (evaluation-to-member-2-rule-loose)
     (BindLink
         ; var decls
         (VariableList
@@ -54,7 +54,57 @@
                             (VariableNode "$B")))))))
 
 
-(define (evaluation-to-member-2-formula MAXDXB MBXDAX DAB)
-    (List (Node "A place holder")))
+(define (evaluation-to-member-2-rule)
+    (BindLink
+        ; var decls
+        (VariableList
+            (TypedVariableLink
+                (VariableNode "$A")
+                (TypeNode "ConceptNode"))
+            (TypedVariableLink
+                (VariableNode "$B")
+                (TypeNode "ConceptNode"))
+            (TypedVariableLink
+                (VariableNode "$D")
+                (TypeNode "PredicateNode")))
+
+        ; pattern
+        (EvaluationLink
+            (VariableNode "$D")
+            (ListLink
+                (VariableNode "$A")
+                (VariableNode "$B")))
+
+        ; result ...
+        (ExecutionOutputLink
+            (GroundedSchemaNode "scm: evaluation-to-member-2-formula")
+                (ListLink
+                    (MemberLink
+                        (VariableNode "$A")
+                        (SatisfyingSetLink
+                            (VariableNode "$X")
+                            (EvaluationLink
+                                (VariableNode "$D")
+                                (ListLink
+                                    (VariableNode "$X")
+                                    (VariableNode "$B")))))
+
+                    (MemberLink
+                        (VariableNode "$B")
+                        (SatisfyingSetLink
+                            (VariableNode "$Y")
+                            (EvaluationLink
+                                (VariableNode "$D")
+                                (ListLink
+                                    (VariableNode "$A")
+                                    (VariableNode "$Y")))))
+                    (EvaluationLink
+                        (VariableNode "$D")
+                        (ListLink
+                            (VariableNode "$A")
+                            (VariableNode "$B")))))))
+
+
+(define (evaluation-to-member-2-formula MAXDXB MBXDAX DAB) MAXDXB)
 
 ;; -----------------------------------------------------------
