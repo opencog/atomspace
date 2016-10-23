bf4c378d90fae1a16090836c9bc33adcc7f01ead
Nil Geisweiller <ngeiswei@gmail.com>
2016-10-23 18:10:12 +0300

    First stab at EvidenceCountTruthValue

---
+++ opencog/truthvalue/EvidenceCountTruthValue.cc
@@ -0,0 +1,137 @@
+/*
+ * opencog/atomspace/EvidenceCountTruthValue.cc
+ *
+ * Copyright (C) 2016 Novamente LLC
+ * All Rights Reserved
+ *
+ * Written by Nil Geisweiller
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <math.h>
+#include <typeinfo>
+
+#include <opencog/util/platform.h>
+#include <opencog/util/exceptions.h>
+#include <opencog/util/numeric.h>
+
+#include "EvidenceCountTruthValue.h"
+
+//#define DPRINTF printf
+#define DPRINTF(...)
+
+using namespace opencog;
+
+EvidenceCountTruthValue::EvidenceCountTruthValue(count_t pos_count,
+                                                 count_t total_count)
+{
+    _pos_count = pos_count;
+    _total_count = total_count;
+}
+
+EvidenceCountTruthValue::EvidenceCountTruthValue(const TruthValue& source)
+{
+	_pos_count = source.getMean() * source.getCount();
+    _total_count = source.getCount();
+}
+EvidenceCountTruthValue::EvidenceCountTruthValue(EvidenceCountTruthValue const& source)
+{
+    _pos_count = source._pos_count;
+    _total_count = source._total_count;
+}
+
+strength_t EvidenceCountTruthValue::getMean() const
+{
+	if (_pos_count <= _total_count)
+		return _pos_count / _total_count;
+    return NAN;
+}
+
+count_t EvidenceCountTruthValue::getCount() const
+{
+	return _total_count;
+}
+
+confidence_t EvidenceCountTruthValue::getConfidence() const
+{
+	if (_pos_count <= _total_count)
+		return _total_count / (DEFAULT_K + _total_count);
+	return NAN;
+}
+
+// This is the merge formula appropriate for PLN.
+TruthValuePtr EvidenceCountTruthValue::merge(TruthValuePtr other,
+                                             const MergeCtrl& mc) const
+{
+	// TODO
+	switch(mc.tv_formula)
+    {
+        case MergeCtrl::TVFormula::HIGHER_CONFIDENCE:
+            return higher_confidence_merge(other);
+
+        case MergeCtrl::TVFormula::PLN_BOOK_REVISION:
+        {
+            // Based on Section 5.10.2(A heuristic revision rule for STV)
+            // of the PLN book
+            if (other->getType() != SIMPLE_TRUTH_VALUE)
+                throw RuntimeException(TRACE_INFO,
+                                   "Don't know how to merge %s into a "
+                                   "EvidenceCountTruthValue using the default style",
+                                   typeid(*other).name());
+
+            confidence_t cf = std::min(getConfidence(), 0.9999998f);
+            auto count = static_cast<count_t>(DEFAULT_K * cf / (1.0f - cf));
+            auto count2 = other->getCount();
+#define CVAL  0.2f
+            auto count_new = count + count2 - std::min(count, count2) * CVAL;
+            auto mean_new = (getMean() * count + other->getMean() * count2)
+                / (count + count2);
+            confidence_t confidence_new = static_cast<confidence_t>(count_new / (count_new + DEFAULT_K));
+            return std::make_shared<EvidenceCountTruthValue>(mean_new, confidence_new);
+        }
+        default:
+            throw RuntimeException(TRACE_INFO,
+                                   "EvidenceCountTruthValue::merge: case not implemented");
+            return nullptr;
+       }
+}
+
+std::string EvidenceCountTruthValue::toString() const
+{
+    char buf[1024];
+    sprintf(buf, "(ectv %f %f)",
+            static_cast<float>(_pos_count),
+            static_cast<float>(_total_count));
+    return buf;
+}
+
+bool EvidenceCountTruthValue::operator==(const TruthValue& rhs) const
+{
+    const EvidenceCountTruthValue *stv = dynamic_cast<const EvidenceCountTruthValue *>(&rhs);
+    if (NULL == stv) return false;
+
+#define FLOAT_ACCEPTABLE_ERROR 0.000001
+    return isWithin(_pos_count, stv->_pos_count, FLOAT_ACCEPTABLE_ERROR)
+	    and isWithin(_total_count, stv->_total_count, FLOAT_ACCEPTABLE_ERROR);
+#undef FLOAT_ACCEPTABLE_ERROR
+}
+
+TruthValueType EvidenceCountTruthValue::getType() const
+{
+    return EVIDENCE_COUNT_TRUTH_VALUE;
+}
---
+++ opencog/truthvalue/EvidenceCountTruthValue.h
@@ -0,0 +1,100 @@
+/*
+ * opencog/truthvalue/EvidenceCountTruthValue.h
+ *
+ * Copyright (C) 2016 Novamente LLC
+ * All Rights Reserved
+ *
+ * Written by Nil Geisweiller
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_EVIDENCE_TRUTH_VALUE_H_
+#define _OPENCOG_EVIDENCE_TRUTH_VALUE_H_
+
+#include <opencog/truthvalue/TruthValue.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+class EvidenceCountTruthValue;
+typedef std::shared_ptr<EvidenceCountTruthValue> EvidenceCountTruthValuePtr;
+
+//! a TruthValue that stores a strength and confidence.
+class EvidenceCountTruthValue : public TruthValue
+{
+protected:
+
+    /// Positive evidence count, i.e. number of observations
+    /// corroborating with the atom's truth.
+    count_t _pos_count;
+
+    /// Total number of observations. If _total_count < _pos_count
+    /// then _total_count is considered undefined.
+    count_t _total_count;
+
+public:
+	EvidenceCountTruthValue(count_t pos_count, count_t total_count = -1);
+    EvidenceCountTruthValue(const TruthValue&);
+    EvidenceCountTruthValue(EvidenceCountTruthValue const&);
+
+    virtual bool operator==(const TruthValue& rhs) const;
+
+    std::string toString() const;
+    TruthValueType getType() const;
+
+    strength_t getMean() const;
+    count_t getCount() const;
+    confidence_t getConfidence() const;
+
+    /**
+     * Truth value merge formula, as specified by PLN.
+     *
+     * Currently tv1.merge(tv2) works as follows:
+     * the resulting TV is either tv1 or tv2, the result being the one
+     * with the highest confidence.
+     */
+    TruthValuePtr merge(TruthValuePtr,
+                        const MergeCtrl& mc=MergeCtrl()) const;
+
+    static EvidenceCountTruthValuePtr createPETV(count_t pos_count,
+                                                 count_t total_count = -1.0)
+    {
+        return std::make_shared<EvidenceCountTruthValue>(pos_count, total_count);
+    }
+    static TruthValuePtr createTV(count_t pos_count, count_t total_count = -1.0)
+    {
+        return std::static_pointer_cast<TruthValue>(createPETV(pos_count,
+                                                               total_count));
+    }
+
+    TruthValuePtr clone() const
+    {
+        return std::make_shared<EvidenceCountTruthValue>(*this);
+    }
+    TruthValue* rawclone() const
+    {
+        return new EvidenceCountTruthValue(*this);
+    }
+};
+
+/** @}*/
+} // namespace opencog
+
+#endif // _OPENCOG_EVIDENCE_COUNT_TRUTH_VALUE_H_
