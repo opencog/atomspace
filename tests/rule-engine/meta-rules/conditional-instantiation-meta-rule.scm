0031abd091dd2ca1e45160a78a968c9ba557fc6f
Nil Geisweiller <ngeiswei@gmail.com>
2016-11-18 16:48:07 +0200

    Keep fixing the musician friend utest

--- tests/rule-engine/meta-rules/conditional-instantiation-meta-rule.scm
+++ tests/rule-engine/meta-rules/conditional-instantiation-meta-rule.scm
@@ -48,15 +48,15 @@
 ;; targets, however that implies to lay them out explicitly, so for
 ;; sake of simplicity they are ignored for now.
 (define conditional-full-instantiation-meta-rewrite
-  (LocalQuote (Bind
-     (Variable "$TyVs")
-     (Variable "$P")
+  (Quote (Bind
+     (Unquote (Variable "$TyVs"))
+     (Unquote (Variable "$P"))
      (ExecutionOutput
         (GroundedSchema "scm: conditional-full-instantiation-formula")
         (ListLink
-           conditional-full-instantiation-meta-body
-           (Variable "$P")
-           (Variable "$Q"))))))
+           (Unquote conditional-full-instantiation-meta-body)
+           (Unquote (Variable "$P"))
+           (Unquote (Variable "$Q")))))))
 
 ;; Bind
 ;;   VariableList
@@ -92,18 +92,24 @@
      conditional-full-instantiation-meta-body
      conditional-full-instantiation-meta-rewrite))
 
+;; And fuzzy eval. Normally this would be handled by a rule but to
+;; simplify the test when hardcore it here
+(define (and-fuzzy-eval a)
+  ;; TODO
+)
+
 ;; Set (stv 1 1) on Q is Impl and P strength are both above 0.5 and
 ;; their confidence is non null.
 (define (conditional-full-instantiation-formula Impl P Q)
   (let* (
          (Impl-s (cog-stv-strength Impl))
          (Impl-c (cog-stv-confidence Impl))
-         (P-s (cog-stv-confidence P))
+         (P-s (cog-stv-strength P))
          (P-c (cog-stv-confidence P))
-         (strong-enough (and (> Impl-s 0.5) (> Impl-c 0) (> P-s 0.5) (> P-c 0)))
-         (Q-s (if strong-enough 1 0))
-         (Q-c (if strong-enough 1 0)))
-    (cog-set-tv! Q (stv Q-s Q-c))))
+         (good-enough (and (> Impl-s 0.5) (> Impl-c 0) (> P-s 0.5) (> P-c 0))))
+    (if (good-enough)
+        (cog-merge-hi-conf-tv! Q (stv 1 1))
+        (cog-undefined-handle))))
 
 ;; Name the meta rule
 (define conditional-full-instantiation-meta-rule-name
