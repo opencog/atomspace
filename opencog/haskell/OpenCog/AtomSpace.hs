a2417dac7b877ac343dcf8b60f6541044a727180
Roman Treutlein <roman.treutlein@gmail.com>
2016-11-20 18:12:37 +0100

    Add various functions to be exported by the Haskell Interface

--- opencog/haskell/OpenCog/AtomSpace.hs
+++ opencog/haskell/OpenCog/AtomSpace.hs
@@ -12,10 +12,13 @@ module OpenCog.AtomSpace
     , onAtomSpace
     , (<:)
     , runOnNewAtomSpace
+    , refToObj
     -- * AtomSpace Interaction
     , insert
+    , insertAndGetHandle
     , remove
     , get
+    , getByHandle
     , debug
     -- * AtomSpace Execution
     , execute
@@ -48,9 +51,7 @@ module OpenCog.AtomSpace
 
 import OpenCog.AtomSpace.Api
 import OpenCog.AtomSpace.Types
-import OpenCog.AtomSpace.Env         (AtomSpace(..),runOnNewAtomSpace,AtomSpaceObj,
-                                      getParent,newAtomSpace,onAtomSpace,(<:),
-                                      AtomSpaceRef(..))
+import OpenCog.AtomSpace.Env
 import OpenCog.AtomSpace.Utils
 import OpenCog.AtomSpace.Sugar
 import OpenCog.AtomSpace.Query
