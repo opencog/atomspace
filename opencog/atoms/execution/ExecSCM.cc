e02bca79de57e6330131655b7669d75126ea37c2
Linas Vepstas <linasvepstas@gmail.com>
2016-12-28 10:15:23 -0600

    Ooops, undo a previous privatization

--- opencog/atoms/execution/ExecSCM.cc
+++ opencog/atoms/execution/ExecSCM.cc
@@ -15,30 +15,7 @@
 #include <opencog/atoms/reduct/FoldLink.h>
 #include <opencog/guile/SchemeModule.h>
 
-#ifndef _OPENCOG_EXEC_SCM_H
-#define _OPENCOG_EXEC_SCM_H
-
-#include <opencog/guile/SchemeModule.h>
-
-namespace opencog {
-
-class ExecSCM : public ModuleWrap
-{
-	protected:
-		virtual void init(void);
-		static std::vector<FunctionWrap*> _binders;
-	public:
-		ExecSCM(void);
-		~ExecSCM();
-};
-
-}
-
-extern "C" {
-void opencog_exec_init(void);
-};
-
-#endif // _OPENCOG_EXEC_SCM_H
+#include "ExecSCM.h"
 
 // ========================================================
 
---
+++ opencog/atoms/execution/ExecSCM.h
@@ -0,0 +1,33 @@
+/*
+ * ExecSCM.h
+ *
+ * Guile Scheme bindings for the execution links
+ * Copyright (c) 2008, 2014, 2015 Linas Vepstas <linas@linas.org>
+ */
+
+#ifndef _OPENCOG_EXEC_SCM_H
+#define _OPENCOG_EXEC_SCM_H
+
+#ifdef HAVE_GUILE
+#include <opencog/guile/SchemeModule.h>
+
+namespace opencog {
+
+class ExecSCM : public ModuleWrap
+{
+	protected:
+		virtual void init(void);
+		static std::vector<FunctionWrap*> _binders;
+	public:
+		ExecSCM(void);
+		~ExecSCM();
+};
+
+}
+
+extern "C" {
+void opencog_exec_init(void);
+};
+
+#endif // HAVE_GUILE
+#endif // _OPENCOG_EXEC_SCM_H
