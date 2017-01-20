0116ea416a6e3aaeea136bdad4b17ad4cb72e81b
Nil Geisweiller <ngeiswei@gmail.com>
2017-01-20 19:32:41 +0200

    Improve Pattern::to_string()

--- opencog/atoms/pattern/Pattern.h
+++ opencog/atoms/pattern/Pattern.h
@@ -139,12 +139,28 @@ struct Pattern
 	std::string to_string() const
 	{
 		std::stringstream ss;
+		if (not clauses.empty())
+			ss << "clauses:" << std::endl << hs_to_string(clauses);
 		if (not constants.empty())
 			ss << "constants:" << std::endl << hs_to_string(constants);
 		if (not mandatory.empty())
 			ss << "mandatory:" << std::endl << hs_to_string(mandatory);
 		if (not optionals.empty())
 			ss << "optionals:" << std::endl << ohs_to_string(optionals);
+		if (not black.empty())
+			ss << "black:" << std::endl << ohs_to_string(black);
+		if (not evaluatable_terms.empty())
+			ss << "evaluatable_terms:" << std::endl
+			   << ohs_to_string(evaluatable_terms);
+		if (not evaluatable_holders.empty())
+			ss << "evaluatable_holders:" << std::endl
+			   << ohs_to_string(evaluatable_holders);
+		if (not executable_terms.empty())
+			ss << "executable_terms:" << std::endl
+			   << ohs_to_string(executable_terms);
+		if (not executable_holders.empty())
+			ss << "executable_holders:" << std::endl
+			   << ohs_to_string(executable_holders);
 		return ss.str();
 	}
 };
