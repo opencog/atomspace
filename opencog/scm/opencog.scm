7c9a188572bdbabea211d81b86c35a47bdaa0eeb
Linas Vepstas <linasvepstas@gmail.com>
2017-01-26 16:03:02 -0600

    New, improved loading function

--- opencog/scm/opencog.scm
+++ opencog/scm/opencog.scm
@@ -13,10 +13,26 @@
 (setlocale LC_CTYPE "")
 
 ; libsmob won't be found unless we setenv where to find it!
+; In theory, we should have installed it into one of these locations:
+;    /usr/lib/guile/2.0/extensions
+;    /usr/local/lib/guile/2.0/extensions
+;    /usr/lib64/guile/2.0/extensions
+;    /usr/local/lib64/guile/2.0/extensions
+;    /usr/lib/guile/2.2/extensions
+;    /usr/local/lib/guile/2.2/extensions
+;    /usr/lib64/guile/2.2/extensions
+;    /usr/local/lib64/guile/2.2/extensions
+;
+; But which one? Its a pain, so we wing it, below, and use
+; LTDL_LIBRARY_PATH
+;
+
+; lib64 is used by various versions of CentOS
+(define path "/usr/local/lib/opencog:/usr/local/lib64/opencog")
 (setenv "LTDL_LIBRARY_PATH"
 	(if (getenv "LTDL_LIBRARY_PATH")
-		(string-append (getenv "LTDL_LIBRARY_PATH") ":/usr/local/lib/opencog")
-		"/usr/local/lib/opencog"))
+		(string-append (getenv "LTDL_LIBRARY_PATH") ":" path)
+		path))
 
 ; Work-around another common usability issue...
 (add-to-load-path "/usr/local/share/opencog/scm")
