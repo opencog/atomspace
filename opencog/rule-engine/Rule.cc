369c4c90a0f891c992761cde214d66c08bd67b45
Nil Geisweiller <ngeiswei@gmail.com>
2017-02-14 08:45:49 +0200

    Remove temporary debug messages

--- opencog/rule-engine/Rule.cc
+++ opencog/rule-engine/Rule.cc
@@ -316,23 +316,13 @@ RuleSet Rule::unify_target(const Handle& target, const Handle& vardecl) const
 
 	Rule alpha_rule = rand_alpha_converted();
 
-	logger().debug() << "Rule::unify_target" << std::endl
-	                 << "alpha_rule:" << std::endl << oc_to_string(alpha_rule);
-
 	RuleSet unified_rules;
 
 	Handle alpha_vardecl = alpha_rule.get_vardecl();
 	for (const Handle& alpha_pat : alpha_rule.get_conclusion_patterns())
 	{
-		logger().debug() << "Rule::unify_target" << std::endl
-		                 << "target:" << std::endl << target
-		                 << "alpha_pat:" << std::endl << alpha_pat
-		                 << "vardecl:" << std::endl << vardecl
-		                 << "alpha_vardecl:" << std::endl << alpha_vardecl;
 		Unify unify(target, alpha_pat, vardecl, alpha_vardecl);
 		Unify::SolutionSet sol = unify();
-		logger().debug() << "Rule::unify_target" << std::endl
-		                 << "sol:" << std::endl << oc_to_string(sol);
 		if (sol.satisfiable) {
 			Unify::TypedSubstitutions tss =
 				unify.typed_substitutions(sol, target);
--- opencog/rule-engine/backwardchainer/BIT.cc
+++ opencog/rule-engine/backwardchainer/BIT.cc
@@ -187,10 +187,6 @@ Handle AndBIT::expand_fcs(const Handle& leaf, const Rule& rule) const
 		noutgoings.insert(noutgoings.begin(), nvardecl);
 	nfcs = fcs->getAtomSpace()->add_link(BIND_LINK, noutgoings);
 
-	LAZY_BC_LOG_DEBUG << "Expanded forward chainer strategy:" << std::endl
-	                  << "from:" << std::endl << fcs
-	                  << "to:" << std::endl << nfcs;
-
 	return nfcs;
 }
 
