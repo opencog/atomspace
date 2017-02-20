2200b4735639b89beb3cadb5d19f86e3047960fb
Linas Vepštas <linasvepstas@gmail.com>
2017-02-20 17:10:09 +0300

    Addtional conversions of TruthValueType

--- opencog/haskell/Utils_CWrapper.cpp
+++ opencog/haskell/Utils_CWrapper.cpp
@@ -5,39 +5,41 @@ int Utils_toRawType(TruthValuePtr tv
                     , Type* tv_type
                     , double* parameters)
 {
-switch(tv->getType())
-    {
-        case SIMPLE_TRUTH_VALUE: {
-            parameters[0]=tv->getMean();
-            parameters[1]=tv->getConfidence();
-            break; }
-        case COUNT_TRUTH_VALUE: {
-            parameters[0]=tv->getMean();
-            parameters[1]=tv->getCount();
-            parameters[2]=tv->getConfidence();
-            break; }
-        case INDEFINITE_TRUTH_VALUE: {
-            IndefiniteTruthValuePtr itv = IndefiniteTVCast(tv);
-            parameters[0]=itv->getMean();
-            parameters[1]=itv->getL();
-            parameters[2]=itv->getU();
-            parameters[3]=itv->getConfidenceLevel();
-            parameters[4]=itv->getDiff();
-            break; }
-        case FUZZY_TRUTH_VALUE: {
-            parameters[0]=tv->getMean();
-            parameters[1]=tv->getConfidence();
-            break; }
-        case PROBABILISTIC_TRUTH_VALUE: {
-            parameters[0]=tv->getMean();
-            parameters[1]=tv->getCount();
-            parameters[2]=tv->getConfidence();
-            break; }
-        default:
-            throw RuntimeException(TRACE_INFO,
-                "Invalid TruthValue Type.");
-            break;
+    Type tvt = tv->getType();
+    if (tvt == SIMPLE_TRUTH_VALUE){
+        parameters[0]=tv->getMean();
+        parameters[1]=tv->getConfidence();
     }
-    *tv_type = tv->getType();
+    else
+    if (tvt == COUNT_TRUTH_VALUE){
+        parameters[0]=tv->getMean();
+        parameters[1]=tv->getCount();
+        parameters[2]=tv->getConfidence();
+    }
+    else
+    if (tvt == INDEFINITE_TRUTH_VALUE){
+        IndefiniteTruthValuePtr itv = IndefiniteTVCast(tv);
+        parameters[0]=itv->getMean();
+        parameters[1]=itv->getL();
+        parameters[2]=itv->getU();
+        parameters[3]=itv->getConfidenceLevel();
+        parameters[4]=itv->getDiff();
+    }
+    else
+    if (tvt == FUZZY_TRUTH_VALUE){
+        parameters[0]=tv->getMean();
+        parameters[1]=tv->getConfidence();
+    }
+    else
+    if (tvt == PROBABILISTIC_TRUTH_VALUE){
+        parameters[0]=tv->getMean();
+        parameters[1]=tv->getCount();
+        parameters[2]=tv->getConfidence();
+    }
+    else {
+        throw RuntimeException(TRACE_INFO,
+            "Invalid TruthValue Type.");
+    }
+    *tv_type = tvt;
     return 0;
 }
