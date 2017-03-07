0123aa1a86a9cb94fd6d516c7d06f8409592d555
Linas Vepštas <linasvepstas@gmail.com>
2017-03-07 22:44:58 +0800

    Partial work

--- opencog/persist/sql/multi-driver/convert.sql
+++ opencog/persist/sql/multi-driver/convert.sql
@@ -6,8 +6,12 @@
 -- generic values, and that truth values are now handled with this
 -- generic value mechanism.
 --
--- Use at your own risk. Backup your data. You may need to fiddle
--- with the UUID for the *-TruthValueKey-*
+-- Use at your own risk. Backup your data.
+--
+-- THIS HAS NEVER BEEN USED OR TESTED. IT MAY DESTROY YOUR DATA.
+--
+-- You may need to fiddle with the UUID for the *-TruthValueKey-*
+--
 
 ALTER TABLE Atoms RENAME TO Atoms_Backup;
 CREATE TABLE Atoms AS SELECT * FROM Atoms_Backup;
@@ -32,4 +36,22 @@ CREATE TABLE Values (
     linkvalue BIGINT[] -- ELEMENT REFERENCES Values(vuid)
 );
 
+-- Change uuid to some, any unused uuid
+INSERT INTO Atoms (uuid, space, type, height, name) VALUES
+	(1, 1, 
+	SELECT type FROM TypeCodes WHERE typename = "PREDICATE_NODE",
+	0, "*-TruthValueKey-*");
 
+-- 'key' must be exactly the same as uuid above.
+INSERT INtO Valuations (key, atom, type, floatvalue) VALUES
+	(1, 
+	SELECT uuid FROM Atoms,
+	16,
+	{0,0,0}
+	); 
+
+-- Drop the columns we no longer use.
+ALTER TABLE Atoms DROP COLUMN tv_type;
+ALTER TABLE Atoms DROP COLUMN stv_mean;
+ALTER TABLE Atoms DROP COLUMN stv_confidence;
+ALTER TABLE Atoms DROP COLUMN stv_count;
