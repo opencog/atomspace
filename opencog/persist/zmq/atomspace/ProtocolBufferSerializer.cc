e5582f773549003271078469483a7e66bd99ca08
Linas Vepštas <linasvepstas@gmail.com>
2017-03-09 17:03:10 +0800

    Fix build break

--- opencog/persist/zmq/atomspace/ProtocolBufferSerializer.cc
+++ opencog/persist/zmq/atomspace/ProtocolBufferSerializer.cc
@@ -219,7 +219,7 @@ void ProtocolBufferSerializer::serializeIndefiniteTruthValue(
     singleTruthValue->set_mean(tv.getMean());
     singleTruthValue->set_count(tv.getCount());
     singleTruthValue->set_confidence(tv.getConfidence());
-    for (float *f : tv.getFirstOrderDistribution())
+    for (double *f const: tv.getFirstOrderDistribution())
     {
         singleTruthValue->add_firstorderdistribution(*f);
     }
