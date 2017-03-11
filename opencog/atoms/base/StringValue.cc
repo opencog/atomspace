0b510411c189c5e6893b32ed4e2e75adc1876088
Linas Vepštas <linasvepstas@gmail.com>
2017-03-11 11:17:28 +0800

    Fix string-value comparison

--- opencog/atoms/base/StringValue.cc
+++ opencog/atoms/base/StringValue.cc
@@ -27,6 +27,14 @@ using namespace opencog;
 bool StringValue::operator==(const ProtoAtom& other) const
 {
 	if (STRING_VALUE != other.getType()) return false;
+
+	ProtoAtomPtr pov(const_cast<ProtoAtom*>(&other));
+	const StringValuePtr sov(StringValueCast(pov));
+
+	if (_value.size() != sov->_value.size()) return false;
+	size_t len = _value.size();
+	for (size_t i=0; i<len; i++)
+		if (_value[i] != sov->_value[i]) return false;
 	return true;
 }
 
