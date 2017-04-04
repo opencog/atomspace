ebfea0546049709daaece5cd0fe173212f4f9142
RomanTreutlein <roman@hansonrobotics.com>
2017-04-04 17:22:47 +0200

    Clean up of the Recent Changes Begin work on being able to support any kind of Value Also fixed arch linux build issues and moved ghc resposibilty to build script

---
+++ opencog/haskell/Value_CWrapper.h
@@ -0,0 +1,57 @@
+#include <opencog/atomspace/AtomSpace.h>
+#include <opencog/atoms/base/ClassServer.h>
+
+/**
+ * C wrapper fro truth values.
+ * An interface necessary for haskell bindings.
+ * (ghc supports FFI for c libraries)
+ */
+
+extern "C"
+{
+    using namespace opencog;
+
+    /**
+     * FloatValue_toRawType Turns a TruthValuePtr ito
+     *                      something that haskell interface
+     *                      can work with
+     *
+     * @param      ptr        FloatValuePtr to convert
+     * @param[out] valuetype  The Type of the FloatValue as a String
+     * @param[out] parameters List of Doubles, the value of the FloatValue
+     *
+     * @return 0 if success
+     */
+    int FloatValue_toRaw(FloatValuePtr ptr
+                        , char** valuetype
+                        , double* parameters);
+
+    /**
+     * FloatValue_getFromAtom  Gets the truthvalue of an Atom
+     *
+     * @param      handle      Handle id of target atom.
+     * @param[out] valuetype   FloatValue type.
+     * @param[out] parameters  List of Doubles, the value of the FloatValue
+     *
+     * @return  0 if success.
+     */
+    int FloatValue_getFromAtom( Handle* atom
+                              , Handle* key
+                              , char** valuetype
+                              , double* parameters );
+    /**
+     * FloatValue_setOnAtom    Sets the truthvalue of an Atom
+     *
+     * @param      handle      Handle id of target atom.
+     * @param      type        FloatValue type to be set.
+     * @param      parameters  List of Doubles, the value of the FloatValue
+     *
+     * @return  0 if success.
+     */
+    int FloatValue_setOnAtom( Handle* atom
+                            , Handle* key
+                            , const char* valuetype
+                            , double* parameters
+                            , int length);
+}
+
--- opencog/haskell/build.sh
+++ opencog/haskell/build.sh
@@ -1,7 +1,18 @@
 BIN_DIR=$1
 
+ghcver="$(stack ghc -- --version)"
+
+if [[ $ghcver == *8.0.2* ]]
+then
+    echo "Correct GHC version installed"
+else
+    echo "Wrong GHC version installed. Running stack setup."
+    stack setup
+fi
+
 if [ "$(id -u)" -ne 0 ];
 then
   # Build haskell bindings package.
+  echo $BIN_DIR
   stack build --extra-lib-dirs=${BIN_DIR}
 fi
