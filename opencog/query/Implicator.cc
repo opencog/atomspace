e10057c852dfdd92298bb7c0f77471fbebeef969
Nil Geisweiller <ngeiswei@gmail.com>
2017-04-05 16:04:01 +0300

    Have Implicator::grounding call instantiate with silent=true

--- opencog/query/Implicator.cc
+++ opencog/query/Implicator.cc
@@ -56,7 +56,7 @@ bool Implicator::grounding(const HandleMap &var_soln,
 	// difficult to insure so meanwhile this try-catch is used. See
 	// issue #950 and pull req #962. XXX FIXME later.
 	try {
-		Handle h = inst.instantiate(implicand, var_soln);
+		Handle h = inst.instantiate(implicand, var_soln, true);
 		insert_result(h);
 	} catch(...) {}
 
