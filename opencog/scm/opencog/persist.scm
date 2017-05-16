8e72fbbfd89e818101922af92ba204896ef7572a
Linas Vepstas <linasvepstas@gmail.com>
2017-05-16 13:24:52 -0500

    Fix documentation

--- opencog/scm/opencog/persist.scm
+++ opencog/scm/opencog/persist.scm
@@ -12,27 +12,37 @@
 
 (set-procedure-property! fetch-atom 'documentation
 "
- fetch-atom handle
-    Fetch indicated atom from SQL/persistent storage.
+ fetch-atom ATOM
+    Fetch indicated ATOM from SQL/persistent storage.
 ")
 
 (set-procedure-property! fetch-incoming-set 'documentation
 "
- fetch-incoming-set
-    Fetch the incoming set of the atom from SQL storage. The fetch is
-    recursive.
+ fetch-incoming-set ATOM
+    Fetch the incoming set of the ATOM from SQL storage. The fetch is
+    NOT recursive.  See load-referers for a recursive fetch.
+
+    See also fetch-incoming-by-type.
+")
+
+(set-procedure-property! fetch-incoming-by-type 'documentation
+"
+ fetch-incoming-by-type ATOM TYPE
+    Fetch those links of the incoming set of ATOM that are of type TYPE.
+    This is a more limited fetch than the one done by fetch-incoming-set
+    and can be useful when the incoming set is large.
 ")
 
 (set-procedure-property! store-atom 'documentation
 "
- store-atom handle
-    Store indicated atom to SQL/persistent storage.
+ store-atom ATOM
+    Store indicated ATOM to SQL/persistent storage.
 ")
 
 (set-procedure-property! load-atoms-of-type 'documentation
 "
- load-atoms-of-type type
-    Fetch atoms of the given type from SQL/persistent storage.
+ load-atoms-of-type TYPE
+    Fetch atoms of the given TYPE from SQL/persistent storage.
 ")
 
 (set-procedure-property! barrier 'documentation
@@ -43,12 +53,14 @@
 
 ;
 ; --------------------------------------------------------------------
-(define-public (store-referers atomo)
+(define-public (store-referers ATOM)
 "
- store-referers -- Store to SQL all hypergraphs that contain given atom
+ store-referers ATOM -- Store to SQL all hypergraphs that contain ATOM
 
- This stores all hypergraphs that the given atom participates in.
+ This stores all hypergraphs that the ATOM participates in.
  It does this by recursively exploring the incoming set of the atom.
+
+ See also load-referers.
 "
 	(define (do-store atom)
 		(let ((iset (cog-incoming-set atom)))
@@ -58,16 +70,18 @@
 			)
 		)
 	)
-	(do-store atomo)
+	(do-store ATOM)
 )
 
 ; --------------------------------------------------------------------
 (define-public (load-referers atom)
 "
- load-referers -- Load from SQL all hypergraphs that contain given atom
+ load-referers ATOM -- Load from SQL all hypergraphs that contain ATOM
 
- This loads all hypergraphs that the given atom participates in.
+ This loads all hypergraphs that the given ATOM participates in.
  It does this by recursively exploring the incoming set of the atom.
+
+ See also store-referers.
 "
 	(if (not (null? atom))
 		; The fetch-incoming-set function for this is defined to perform
