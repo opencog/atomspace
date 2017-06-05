55a1961638da1d2a48805aefc38cc1f393b23023
Leung Man Hin <leungmanhin@users.noreply.github.com>
2017-06-05 16:32:38 +0800

    Revert "Update the recognizer to read scoped glob-restrictions"

--- opencog/query/Recognizer.cc
+++ opencog/query/Recognizer.cc
@@ -28,8 +28,6 @@
 
 #include <opencog/query/DefaultPatternMatchCB.h>
 #include <opencog/atoms/pattern/PatternLink.h>
-#include <opencog/atoms/core/ScopeLink.h>
-#include <opencog/atomutils/FindUtils.h>
 
 #include "BindLinkAPI.h"
 
@@ -56,11 +54,6 @@ namespace opencog {
 class Recognizer :
    public virtual DefaultPatternMatchCB
 {
-	private:
-		bool bypass_node_match = false;
-		std::vector<Variables> _soln_vars;
-		void get_glob_decl(const Handle&);
-
 	protected:
 		const Pattern* _pattern;
 
@@ -106,30 +99,6 @@ using namespace opencog;
 #endif
 
 /* ======================================================== */
-
-void Recognizer::get_glob_decl(const Handle& h)
-{
-	IncomingSet scop = h->getIncomingSetByType(SCOPE_LINK, true);
-
-	if (0 < scop.size())
-	{
-		for (const LinkPtr& lp : scop)
-		{
-			Handle s(lp);
-			ScopeLinkPtr sl(ScopeLinkCast(s));
-			if (NULL == sl)
-				sl = createScopeLink(*LinkCast(s));
-			_soln_vars.push_back(sl->get_variables());
-		}
-	}
-	else
-	{
-		IncomingSet iset = h->getIncomingSet();
-		if (iset.size() == 0) return;
-		for (const LinkPtr& lp : iset)
-			get_glob_decl(lp->getHandle());
-	}
-}
 
 bool Recognizer::do_search(PatternMatchEngine* pme, const Handle& top)
 {
@@ -181,22 +150,14 @@ bool Recognizer::initiate_search(PatternMatchEngine* pme)
 
 bool Recognizer::node_match(const Handle& npat_h, const Handle& nsoln_h)
 {
-	// Note: It is accepted right away as a side-by-side comparsion
-	// has already been done in fuzzy_match, see link_match below.
-	// So if we reach here, that means it passed already so don't
-	// worry about it.
-	if (bypass_node_match) return true;
-
 	if (npat_h == nsoln_h) return true;
-	if (VARIABLE_NODE == nsoln_h->getType()) return true;
-
+	Type tso = nsoln_h->getType();
+	if (VARIABLE_NODE == tso or GLOB_NODE == tso) return true;
 	return false;
 }
 
 bool Recognizer::link_match(const PatternTermPtr& ptm, const Handle& lsoln)
 {
-	_soln_vars.clear();
-	bypass_node_match = false;
 	const Handle& lpat = ptm->getHandle();
 
 	// Self-compares always proceed.
@@ -205,49 +166,10 @@ bool Recognizer::link_match(const PatternTermPtr& ptm, const Handle& lsoln)
 	// mis-matched types are a dead-end.
 	if (lpat->getType() != lsoln->getType()) return false;
 
-	// Find the type and interval restrictions under a ScopeLink, if any
-	if (contains_atomtype(lsoln, GLOB_NODE))
-	{
-		get_glob_decl(lsoln);
-
-		// TODO: Change to something better if possible...
-		// What is happening here is to manually call the
-		// fuzzy_match callback immediately if and only if
-		// lsoln has one or more GlobNodes AND lpat and lsoln
-		// have the same arity.
-		// The reason is, if the pat and soln are having the
-		// same arity, pattern matcher will then do a
-		// side-by-side comparsion of their outgoing atoms.
-		// In the typical use cases we are facing at the
-		// moment, the comparsion will be done in the node_match
-		// callback. However that will cause problems in some
-		// situations, for example if we have:
-		// === lpat ===      === lsoln ===
-		// Concept "A"       Glob $x
-		// Concept "B"       Concept "A"
-		// Concept "C"       Glob $y
-		// and both of the globs $x and $y have an interval
-		// restriction of zero to infinity, it should be a
-		// match by grounding $x to nothing and $y to Concept
-		// "B" and "C". But a side-by-side comparsion here is
-		// a node-level comparsion (i.e. A-$x, B-A, C-$y),
-		// it makes it really hard to decide whether or not
-		// to ground a particular glob without knowing the
-		// rest of the sibling. And the reasons of calling the
-		// fuzzy_match callback are that it itself is a link-
-		// level comparsion and the glob-matching logic happens
-		// to be all there...
-		if (lpat->getArity() == lsoln->getArity())
-		{
-			if (fuzzy_match(lpat, lsoln))
-			{
-				bypass_node_match = true;
-				return true;
-			}
-			else return false;
-		}
-	}
-
+	// Globs are arity-changing. But there is a minimum length.
+	// Note that the inequality is backwards, here: the soln has the
+	// globs! (and so lpat must have arity equal or greater than soln)
+	if (lpat->getArity() < lsoln->getArity()) return false;
 	return true;
 }
 
@@ -293,139 +215,32 @@ bool Recognizer::fuzzy_match(const Handle& npat_h, const Handle& nsoln_h)
 	size_t osp_size = osp.size();
 	size_t max_size = std::max(osg_size, osp_size);
 
-	size_t ks = 0;
-	bool is_scoped = (0 < _soln_vars.size());
-	Variables svar;
-	if (is_scoped) svar = _soln_vars[ks];
-
 	// Do a side-by-side compare. This is not as rigorous as
 	// PatternMatchEngine::tree_compare() nor does it handle the bells
 	// and whistles (ChoiceLink, QuoteLink, etc).
 	size_t ip=0, jg=0;
-	for (; ip<osp_size or jg<osg_size; ip++, jg++)
+	for (; ip<osp_size and jg<osg_size; ip++, jg++)
 	{
-		bool pat_end = false;
-		if (ip == osp_size)
-		{
-			pat_end = true;
-			ip--;
-		}
-		if (jg == osg_size) jg--;
-
 		if (GLOB_NODE != osg[jg]->getType())
 		{
 			if (loose_match(osp[ip], osg[jg])) continue;
-
-			// Sometimes we end up here because the glob
-			// in the previous iteration failed to
-			// match anything, so although we are not
-			// looking at a glob right now, we still
-			// need to do the below to see if it can be
-			// rejected yet.
-
-			if (not is_scoped)
-				return false;
-			// If we have gone through all the scopes,
-			// it's not a match and we are done.
-			else if (ks+1 == _soln_vars.size())
-				return false;
-			// Otherwise reset everything and try again
-			// with the next scope.
-			else
-			{
-				// The for-loop increment will turn them back to zero.
-				ip = -1;
-				jg = -1;
-				ks++;
-				svar = _soln_vars[ks];
-				continue;
-			}
+			return false;
 		}
 
-		// If we are here, we have a glob in the soln.
-		const Handle& glob = osg[jg];
-		size_t match_cnt = 0;
-
-		// If the glob is at the end, see if it can eat everything.
-		if (jg+1 == osg_size)
-		{
-			// If the glob is not scoped, it eats everything, so its a match.
-			if (not is_scoped) return true;
-
-			while (ip < osp_size)
-			{
-				// Make sure it satisfies both type and interval restrictions.
-				if (svar.is_type(glob, osp[ip]) and
-				    svar.is_interval(glob, match_cnt+1))
-				{
-					match_cnt++;
-					ip++;
-				}
-				else break;
-			}
-
-			// Return true if it managed to eat everything.
-			if (ip == osp_size) return true;
-			// If this ending glob can be grounded to nothing, it's a match!
-			else if (svar.is_interval(glob, 0) and pat_end)
-				return true;
-			else
-			{
-				if (not is_scoped)
-					return false;
-				// If we have gone through all the scopes,
-				// it's not a match and we are done.
-				else if (ks+1 == _soln_vars.size())
-					return false;
-				// Otherwise reset everything and try again
-				// with the next scope.
-				else
-				{
-					// The for-loop increment will turn them back to zero.
-					ip = -1;
-					jg = -1;
-					ks++;
-					svar = _soln_vars[ks];
-					continue;
-				}
-			}
-		}
+		// If we are here, we have a glob in the soln. If the glob is at
+		// the end, it eats everything, so its a match. Else, resume
+		// matching at the end of the glob.
+		if ((jg+1) == osg_size) return true;
 
 		const Handle& post(osg[jg+1]);
+		ip++;
 		while (ip < max_size and not loose_match(osp[ip], post))
 		{
-			// If it's scoped and either the type or interval
-			// restirctions is not satisfied, break
-			if (is_scoped and
-			   ((not svar.is_type(glob, osp[ip])) or
-			    (not svar.is_interval(glob, match_cnt+1))))
-				break;
-
-			match_cnt++;
 			ip++;
 		}
 		// If ip ran past the end, then the post was not found. This is
 		// a mismatch.
-		if (not (ip < max_size))
-		{
-			if (not is_scoped)
-				return false;
-			// If we have gone through all the scopes,
-			// it's not a match and we are done.
-			else if (ks+1 == _soln_vars.size())
-				return false;
-			// Otherwise reset everything and try again
-			// with the next scope.
-			else
-			{
-				// The for-loop increment will turn them back to zero.
-				ip = -1;
-				jg = -1;
-				ks++;
-				svar = _soln_vars[ks];
-				continue;
-			}
-		}
+		if (not (ip < max_size)) return false;
 
 		// Go around again, look for more GlobNodes. Back up by one, so
 		// that the for-loop increment gets us back on track.
@@ -433,7 +248,9 @@ bool Recognizer::fuzzy_match(const Handle& npat_h, const Handle& nsoln_h)
 	}
 
 	// If we are here, then we should have matched up all the atoms;
-	// any mismatch should have rejected already.
+	// if we exited the loop because pattern or grounding was short,
+	// then its a mis-match.
+	if (ip != osp_size or jg != osg_size) return false;
 	return true;
 }
 
