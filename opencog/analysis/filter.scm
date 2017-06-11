eb2a9b565acd62e3e0702d4b6f68db7070b431ef
Linas Vepstas <linasvepstas@gmail.com>
2017-06-11 17:40:58 -0500

    Create and use a generic filter

--- opencog/analysis/filter.scm
+++ opencog/analysis/filter.scm
@@ -22,40 +22,22 @@
 
 ; ---------------------------------------------------------------------
 
-(define-public (add-subtotal-filter LLOBJ LEFT-CUT RIGHT-CUT PAIR-CUT)
+(define-public (add-generic-filter LLOBJ
+	LEFT-BASIS-PRED RIGHT-BASIS-PRED LEFT-STAR-PRED RIGHT-STAR-PRED)
 "
-  add-subtotal-filter LLOBJ - Modify LLOBJ so that any columns and
-  rows with counts less than LEFT-CUT and RIGHT-CUT are removed, and that
-  individual entries with counts less than PAIR-CUT are removed. This
-  provides an API compatible with the star-object API; i.e. it provides
-  the same row and column addressability that star-object does, but
-  just returns fewer rows and columns.
+  add-generic-filter LLOBJ - Modify LLOBJ so that only the columns and
+  rows that satisfy the predicates are retained.
 
-  Thhe filtering is done 'on demand', on a row-by-row, column-by-column
-  basis.  Currenly, computations for the left and right stars are not
-  cached, and are recomputed for each request.  Currently, this seems
-  like a reasonable thing to do.
+  The LEFT-BASIS-PRED and RIGHT-BASIS-PRED should be functions that
+  accept atoms in the left and right basis, and return #t if they
+  should be kept.
 
-  Note that by removing rows and columns, the frequencies will no longer
-  sum to 1.0. Likewise, row and column subtotals, entropies and mutual
-  information will no long sum or behave as in the whole dataset.  If
-  accurate values for these are needed, then they would need to be
-  recomputed for the reduced matrix.
-
-  Some terminology: Let N(x,y) be the observed count for the pair (x,y).
-  Let N(*,y) be the column subtotals, AKA the left-subtotals.
-  Let N(x,*) be the row subtotals, AKA the right subtotals.
-
-  This object removes all columns where  N(*,y) <= RIGHT-CUT and where
-  N(x,*) <= LEFT-CUT.  Pairs are not reported in the 'left-stars and
-  'right-stars methods when N(x,y) <= PAIR-CUT.
-
-  The net effect of the cuts is that when LEFT-CUT is increased, the
-  left-dimension of the dataset drops; likewise on the right.
+  The LEFT-STAR-PRED and RIGHT-STAR-PRED should be functions that
+  accept left and right wild-card pairs, and return #t if they should
+  be kept.
 "
 	(let* ((llobj LLOBJ)
 			(stars-obj (add-pair-stars LLOBJ))
-			(cnt-obj (add-pair-count-api stars-obj))
 			(l-basis '())
 			(r-basis '())
 			(l-size 0)
@@ -63,22 +45,13 @@
 		)
 
 		; ---------------
-		; Filter out rows and columns that are below-count.
-		;
-		; Yes, we want LEFT-CUT < right-wild-count this looks weird,
-		; but is correct: as LEFT-CUT gets larger, the size of the
-		; left-basis shrinks.
+		; Filter out rows and columns that pass the left and right
+		; predicates.
 		(define (do-left-basis)
-			(filter
-				(lambda (ITEM)
-					(< LEFT-CUT (cnt-obj 'right-wild-count ITEM)))
-				(stars-obj 'left-basis)))
+			(filter LEFT-PRED (stars-obj 'left-basis)))
 
 		(define (do-right-basis)
-			(filter
-				(lambda (ITEM)
-					(< RIGHT-CUT (cnt-obj 'left-wild-count ITEM)))
-				(stars-obj 'right-basis)))
+			(filter RIGHT-PRED (stars-obj 'right-basis)))
 
 		; ---------------
 		; Use the cached value, if its there.
@@ -104,18 +77,12 @@
 		; See comments above: LEFT-CUT < right-wild-count is correct.
 		(define (do-left-stars ITEM)
 			(filter
-				(lambda (PAIR)
-					(and
-						(< PAIR-CUT (llobj 'pair-count PAIR))
-						(< LEFT-CUT (cnt-obj 'right-wild-count (gar PAIR)))))
+				(lambda (PAIR) LEFT-STAR-PRED)
 				(stars-obj 'left-stars ITEM)))
 
 		(define (do-right-stars ITEM)
 			(filter
-				(lambda (PAIR)
-					(and
-						(< PAIR-CUT (llobj 'pair-count PAIR))
-						(< RIGHT-CUT (cnt-obj 'left-wild-count (gdr PAIR)))))
+				(lambda (PAIR) RIGHT-STAR-PRED)
 				(stars-obj 'right-stars ITEM)))
 
 		; Cache the results above, so that we don't recompute over and over.
@@ -147,6 +114,75 @@
 				((provides)         (apply provides args))
 				(else               (apply llobj (cons message args))))
 		)))
+
+; ---------------------------------------------------------------------
+
+(define-public (add-subtotal-filter LLOBJ LEFT-CUT RIGHT-CUT PAIR-CUT)
+"
+  add-subtotal-filter LLOBJ - Modify LLOBJ so that any columns and
+  rows with counts less than LEFT-CUT and RIGHT-CUT are removed, and that
+  individual entries with counts less than PAIR-CUT are removed. This
+  provides an API compatible with the star-object API; i.e. it provides
+  the same row and column addressability that star-object does, but
+  just returns fewer rows and columns.
+
+  Thhe filtering is done 'on demand', on a row-by-row, column-by-column
+  basis.  Currenly, computations for the left and right stars are not
+  cached, and are recomputed for each request.  Currently, this seems
+  like a reasonable thing to do.
+
+  Note that by removing rows and columns, the frequencies will no longer
+  sum to 1.0. Likewise, row and column subtotals, entropies and mutual
+  information will no long sum or behave as in the whole dataset.  If
+  accurate values for these are needed, then they would need to be
+  recomputed for the reduced matrix.
+
+  Some terminology: Let N(x,y) be the observed count for the pair (x,y).
+  Let N(*,y) be the column subtotals, AKA the left-subtotals.
+  Let N(x,*) be the row subtotals, AKA the right subtotals.
+
+  This object removes all columns where  N(*,y) <= RIGHT-CUT and where
+  N(x,*) <= LEFT-CUT.  Pairs are not reported in the 'left-stars and
+  'right-stars methods when N(x,y) <= PAIR-CUT.
+
+  The net effect of the cuts is that when LEFT-CUT is increased, the
+  left-dimension of the dataset drops; likewise on the right.
+"
+	(let* ((stars-obj (add-pair-stars LLOBJ))
+			(cnt-obj (add-pair-count-api stars-obj))
+		)
+
+		; ---------------
+		; Filter out rows and columns that are below-count.
+		;
+		; Yes, we want LEFT-CUT < right-wild-count this looks weird,
+		; but is correct: as LEFT-CUT gets larger, the size of the
+		; left-basis shrinks.
+		(define (left-basis-pred ITEM)
+			(< LEFT-CUT (cnt-obj 'right-wild-count ITEM)))
+
+		(define (right-basis-pred ITEM)
+			(< RIGHT-CUT (cnt-obj 'left-wild-count ITEM)))
+
+		; ---------------
+		; Return only those stars that pass the cutoff.
+		;
+		; See comments above: LEFT-CUT < right-wild-count is correct.
+		(define (left-stars-pred PAIR)
+			(and
+				(< PAIR-CUT (llobj 'pair-count PAIR))
+				(< LEFT-CUT (cnt-obj 'right-wild-count (gar PAIR)))))
+
+		(define (right-stars-pred PAIR)
+			(and
+				(< PAIR-CUT (llobj 'pair-count PAIR))
+				(< RIGHT-CUT (cnt-obj 'left-wild-count (gdr PAIR)))))
+
+		; ---------------
+		(add-generic-filter LLOBJ
+			left-basis-pred right-basis-pred
+			left-stars-pred right-stars-pred)
+)
 
 ; ---------------------------------------------------------------------
 
