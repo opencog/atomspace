b5092c7a3c9d1d7d5253da24f517c597af6b18f2
Linas Vepstas <linasvepstas@gmail.com>
2017-06-11 17:14:23 -0500

    Start working on a second filter.

--- opencog/analysis/filter.scm
+++ opencog/analysis/filter.scm
@@ -19,11 +19,10 @@
 ; ---------------------------------------------------------------------
 
 (use-modules (srfi srfi-1))
-(use-modules (ice-9 optargs)) ; for define*-public
 
 ; ---------------------------------------------------------------------
 
-(define*-public (add-subtotal-filter LLOBJ LEFT-CUT RIGHT-CUT PAIR-CUT)
+(define-public (add-subtotal-filter LLOBJ LEFT-CUT RIGHT-CUT PAIR-CUT)
 "
   add-subtotal-filter LLOBJ - Modify LLOBJ so that any columns and
   rows with counts less than LEFT-CUT and RIGHT-CUT are removed, and that
@@ -122,6 +121,85 @@
 		; Cache the results above, so that we don't recompute over and over.
 		(define cache-left-stars (make-afunc-cache do-left-stars))
 		(define cache-right-stars (make-afunc-cache do-right-stars))
+
+		; ---------------
+		; Return a pointer to each method that this class overloads.
+		(define (provides meth)
+			(case meth
+				((left-stars)       cache-left-stars)
+				((right-stars)      cache-right-stars)
+				((left-basis)       get-left-basis)
+				((right-basis)      get-right-basis)
+				((left-basis-size)  get-left-size)
+				((right-basis-size) get-right-size)
+				(else               (llobj 'provides meth))))
+
+		; -------------
+		; Methods on this class.
+		(lambda (message . args)
+			(case message
+				((left-stars)       (apply cache-left-stars args))
+				((right-stars)      (apply cache-right-stars args))
+				((left-basis)       (get-left-basis))
+				((right-basis)      (get-right-basis))
+				((left-basis-size)  (get-left-size))
+				((right-basis-size) (get-right-size))
+				((provides)         (apply provides args))
+				(else               (apply llobj (cons message args))))
+		)))
+
+; ---------------------------------------------------------------------
+
+(define-public (add-knockout-filter LLOBJ LEFT-KNOCKOUT RIGHT-KNOCKOUT)
+"
+  add-knockout-filter LLOBJ - Modify LLOBJ so that the explicitly
+  indicated rows and columns are removed.
+"
+	(let* ((llobj LLOBJ)
+			(stars-obj (add-pair-stars LLOBJ))
+			(cnt-obj (add-pair-count-api stars-obj))
+			(l-basis '())
+			(r-basis '())
+			(l-size 0)
+			(r-size 0)
+		)
+
+		; ---------------
+		; Filter out rows and columns in the knockout lists.
+		;
+		(define (do-left-basis)
+			(filter
+				(lambda (ITEM)
+					(any
+						(lambda (knockout) (equal? knockout ITEM))
+						LEFT-KNOCKOUT))
+				(stars-obj 'left-basis)))
+
+		(define (do-right-basis)
+			(filter
+				(lambda (ITEM)
+					(any
+						(lambda (knockout) (equal? knockout ITEM))
+						RIGHT-KNOCKOUT))
+				(stars-obj 'right-basis)))
+
+		; ---------------
+		; Use the cached value, if its there.
+		(define (get-left-basis)
+			(if (null? l-basis) (set! l-basis (do-left-basis)))
+			l-basis)
+
+		(define (get-right-basis)
+			(if (null? r-basis) (set! r-basis (do-right-basis)))
+			r-basis)
+
+		(define (get-left-size)
+			(if (eq? 0 l-size) (set! l-size (length (get-left-basis))))
+			l-size)
+
+		(define (get-right-size)
+			(if (eq? 0 r-size) (set! r-size (length (get-right-basis))))
+			r-size)
 
 		; ---------------
 		; Return a pointer to each method that this class overloads.
