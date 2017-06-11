f72abd7bdbebfac333b95f0c3e68a8f153bfa05f
Linas Vepstas <linasvepstas@gmail.com>
2017-06-11 17:45:38 -0500

    Finish the impl of the knockout filter

--- opencog/analysis/filter.scm
+++ opencog/analysis/filter.scm
@@ -182,6 +182,7 @@
 		(add-generic-filter LLOBJ
 			left-basis-pred right-basis-pred
 			left-stars-pred right-stars-pred)
+	)
 )
 
 ; ---------------------------------------------------------------------
@@ -191,76 +192,33 @@
   add-knockout-filter LLOBJ - Modify LLOBJ so that the explicitly
   indicated rows and columns are removed.
 "
-	(let* ((llobj LLOBJ)
-			(stars-obj (add-pair-stars LLOBJ))
-			(cnt-obj (add-pair-count-api stars-obj))
-			(l-basis '())
-			(r-basis '())
-			(l-size 0)
-			(r-size 0)
-		)
+	; ---------------
+	; Filter out rows and columns in the knockout lists.
+	;
+	(define (left-basis-pred ITEM)
+		(any
+			(lambda (knockout) (equal? knockout ITEM))
+			LEFT-KNOCKOUT))
 
-		; ---------------
-		; Filter out rows and columns in the knockout lists.
-		;
-		(define (do-left-basis)
-			(filter
-				(lambda (ITEM)
-					(any
-						(lambda (knockout) (equal? knockout ITEM))
-						LEFT-KNOCKOUT))
-				(stars-obj 'left-basis)))
+	(define (right-basis-pred ITEM)
+		(any
+			(lambda (knockout) (equal? knockout ITEM))
+			RIGHT-KNOCKOUT))
 
-		(define (do-right-basis)
-			(filter
-				(lambda (ITEM)
-					(any
-						(lambda (knockout) (equal? knockout ITEM))
-						RIGHT-KNOCKOUT))
-				(stars-obj 'right-basis)))
+	; ---------------
+	; Return only those stars that pass the cutoff.
+	;
+	; See comments above: LEFT-CUT < right-wild-count is correct.
+	(define (left-stars-pred PAIR)
+		(left-basis-pred (gar PAIR)))
 
-		; ---------------
-		; Use the cached value, if its there.
-		(define (get-left-basis)
-			(if (null? l-basis) (set! l-basis (do-left-basis)))
-			l-basis)
+	(define (right-stars-pred PAIR)
+		(right-basis-pred (gdr PAIR)))
 
-		(define (get-right-basis)
-			(if (null? r-basis) (set! r-basis (do-right-basis)))
-			r-basis)
-
-		(define (get-left-size)
-			(if (eq? 0 l-size) (set! l-size (length (get-left-basis))))
-			l-size)
-
-		(define (get-right-size)
-			(if (eq? 0 r-size) (set! r-size (length (get-right-basis))))
-			r-size)
-
-		; ---------------
-		; Return a pointer to each method that this class overloads.
-		(define (provides meth)
-			(case meth
-				((left-stars)       cache-left-stars)
-				((right-stars)      cache-right-stars)
-				((left-basis)       get-left-basis)
-				((right-basis)      get-right-basis)
-				((left-basis-size)  get-left-size)
-				((right-basis-size) get-right-size)
-				(else               (llobj 'provides meth))))
-
-		; -------------
-		; Methods on this class.
-		(lambda (message . args)
-			(case message
-				((left-stars)       (apply cache-left-stars args))
-				((right-stars)      (apply cache-right-stars args))
-				((left-basis)       (get-left-basis))
-				((right-basis)      (get-right-basis))
-				((left-basis-size)  (get-left-size))
-				((right-basis-size) (get-right-size))
-				((provides)         (apply provides args))
-				(else               (apply llobj (cons message args))))
-		)))
+	; ---------------
+	(add-generic-filter LLOBJ
+		left-basis-pred right-basis-pred
+		left-stars-pred right-stars-pred)
+)
 
 ; ---------------------------------------------------------------------
