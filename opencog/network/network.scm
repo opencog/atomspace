95dc3c34dc1066a4c7ab4b5a6fd3df027eefd252
Linas Vepstas <linasvepstas@gmail.com>
2017-06-15 18:43:33 -0500

    Anothe file rename

--- opencog/network/network.scm
+++ opencog/network/network.scm
@@ -8,4 +8,4 @@
 ; In general, the later files depend on definitions contained
 ; in the earlier files.
 (load "analysis/mst-parser.scm")
-(load "analysis/make-sheaf.scm")
+(load "analysis/make-section.scm")
