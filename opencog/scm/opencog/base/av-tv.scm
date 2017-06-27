37d77984d0d0d366d662ccfc254899fd9ac403fc
Linas Vepstas <linasvepstas@gmail.com>
2017-06-26 23:50:53 -0500

    Fix bad documentation style

--- opencog/scm/opencog/base/av-tv.scm
+++ opencog/scm/opencog/base/av-tv.scm
@@ -39,90 +39,126 @@
 )
 
 ; -----------------------------------------------------------------------
-; cog-af-length
-; Length of the list of atoms in the attentional focus
-(define-public (cog-af-length) (length (cog-af)))
+(define-public (cog-af-length)
+" cog-af-length -- Length of the list of atoms in the attentional focus."
+	(length (cog-af)))
 
 ; -----------------------------------------------------------------------
-; cog-av-sti
-; Return the STI of an atom
-(define-public (cog-av-sti x) (cdr (assoc 'sti (cog-av->alist (cog-av x)))))
+(define-public (cog-av-sti x)
+" cog-av-sti -- Return the STI of an atom."
+	(cdr (assoc 'sti (cog-av->alist (cog-av x)))))
 
 ; -----------------------------------------------------------------------
-; cog-sti-above
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms
-; with STI above the threshold
-(define-public (cog-sti-above y z) (filter (lambda (x) (> (cog-av-sti x) y)) z))
+(define-public (cog-sti-above y z)
+"
+  cog-sti-above
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms
+  with STI above the threshold
+"
+	(filter (lambda (x) (> (cog-av-sti x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-sti-below
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms
-; with STI below the threshold
-(define-public (cog-sti-below y z) (filter (lambda (x) (< (cog-av-sti x) y)) z))
+(define-public (cog-sti-below y z)
+"
+  cog-sti-below
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms
+  with STI below the threshold
+"
+	(filter (lambda (x) (< (cog-av-sti x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-stv-strength
-; Return the truth value strength of an atom
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-strength x) (cdr (assoc 'mean (cog-tv->alist (cog-tv x)))))
+(define-public (cog-stv-strength x)
+"
+  cog-stv-strength
+  Return the truth value strength of an atom
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(cdr (assoc 'mean (cog-tv->alist (cog-tv x)))))
 
 ; -----------------------------------------------------------------------
-; cog-stv-strength-above
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
-; truth value strength above the threshold
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-strength-above y z) (filter (lambda (x) (> (cog-stv-strength x) y)) z))
+(define-public (cog-stv-strength-above y z)
+"
+  cog-stv-strength-above
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
+  truth value strength above the threshold
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(filter (lambda (x) (> (cog-stv-strength x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-stv-strength-below
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
-; truth value strength above the threshold
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-strength-below y z) (filter (lambda (x) (< (cog-stv-strength x) y)) z))
+(define-public (cog-stv-strength-below y z)
+"
+  cog-stv-strength-below
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
+  truth value strength above the threshold
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(filter (lambda (x) (< (cog-stv-strength x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-stv-confidence
-; Return the truth value confidence of an atom
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-confidence x) (cdr (assoc 'confidence (cog-tv->alist (cog-tv x)))))
+(define-public (cog-stv-confidence x)
+"
+  cog-stv-confidence
+  Return the truth value confidence of an atom
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(cdr (assoc 'confidence (cog-tv->alist (cog-tv x)))))
 
 ; -----------------------------------------------------------------------
-; cog-stv-confidence-above
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
-; truth value confidence above the threshold
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-confidence-above y z) (filter (lambda (x) (> (cog-stv-confidence x) y)) z))
+(define-public (cog-stv-confidence-above y z)
+"
+  cog-stv-confidence-above
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
+  truth value confidence above the threshold
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(filter (lambda (x) (> (cog-stv-confidence x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-stv-confidence-below
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
-; truth value confidence above the threshold
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-confidence-below y z) (filter (lambda (x) (< (cog-stv-confidence x) y)) z))
+(define-public (cog-stv-confidence-below y z)
+"
+  cog-stv-confidence-below
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
+  truth value confidence above the threshold
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(filter (lambda (x) (< (cog-stv-confidence x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-stv-count
-; Return the truth value count of an atom
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-count x) (cdr (assoc 'count (cog-tv->alist (cog-tv x)))))
+(define-public (cog-stv-count x)
+"
+  cog-stv-count
+  Return the truth value count of an atom
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(cdr (assoc 'count (cog-tv->alist (cog-tv x)))))
 
 ; -----------------------------------------------------------------------
-; cog-stv-count-above
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
-; truth value count above the threshold
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-count-above y z) (filter (lambda (x) (> (cog-stv-count x) y)) z))
+(define-public (cog-stv-count-above y z)
+"
+  cog-stv-count-above
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
+  truth value count above the threshold
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(filter (lambda (x) (> (cog-stv-count x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-stv-count-below
-; Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
-; truth value count above the threshold
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-count-below y z) (filter (lambda (x) (< (cog-stv-count x) y)) z))
+(define-public (cog-stv-count-below y z)
+"
+  cog-stv-count-below
+  Given a threshold 'y' and a list of atoms 'z', returns a list of atoms with
+  truth value count above the threshold
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(filter (lambda (x) (< (cog-stv-count x) y)) z))
 
 ; -----------------------------------------------------------------------
-; cog-stv-positive-filter
-; Given a list of atoms, returns a list containing the subset that has
-; truth value count > 0 and truth value strength > 0
-; (Compatible with atoms that have a SimpleTruthValue)
-(define-public (cog-stv-positive-filter x) (cog-stv-strength-above 0 (cog-stv-count-above 0 x)))
+(define-public (cog-stv-positive-filter x)
+"
+  cog-stv-positive-filter
+  Given a list of atoms, returns a list containing the subset that has
+  truth value count > 0 and truth value strength > 0
+  (Compatible with atoms that have a SimpleTruthValue)
+"
+	(cog-stv-strength-above 0 (cog-stv-count-above 0 x)))
