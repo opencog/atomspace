b2d5d6474468a6afa86400b6d3e7f44f858583e6
Nil Geisweiller <ngeiswei@gmail.com>
2017-08-01 09:25:14 +0300

    Fetch expansion control rules

--- opencog/rule-engine/backwardchainer/ControlPolicy.cc
+++ opencog/rule-engine/backwardchainer/ControlPolicy.cc
@@ -24,10 +24,15 @@
 #include "ControlPolicy.h"
 
 #include <opencog/util/random.h>
+#include <opencog/query/BindLinkAPI.h>
 
+#include "TraceRecorder.h"
 #include "../URELogger.h"
 
 using namespace opencog;
+
+#define al _query_as.add_link
+#define an _query_as.add_node
 
 ControlPolicy::ControlPolicy(const RuleSet& rs, const BIT& bit,
                              AtomSpace* control_as) :
@@ -78,15 +83,26 @@ RuleTypedSubstitutionMap ControlPolicy::get_valid_rules(const AndBIT& andbit,
 		RuleTypedSubstitutionMap unified_rules
 			= rule.unify_target(bitleaf.body, vardecl);
 
-		// Insert only rules with positive probability of success
-		RuleTypedSubstitutionMap pos_rules;
-		for (const auto& rule : unified_rules) {
-			double p = (_bit.is_in(rule, bitleaf) ? 0.0 : 1.0)
-				* rule.first.get_weight();
-			if (p > 0) pos_rules.insert(rule);
+		// Remove rules already in the BIT, or that happen to have a
+		// null weight.
+		for (RuleTypedSubstitutionMap::iterator it = unified_rules.begin();
+		     it != unified_rules.end();) {
+			double w = (_bit.is_in(*it, bitleaf) ? 0.0 : 1.0)
+				* it->first.get_weight();
+			if (w <= 0)
+				it = unified_rules.erase(it);
+			else
+				++it;
 		}
 
-		valid_rules.insert(pos_rules.begin(), pos_rules.end());
+		// Reweight and insert
+		double N(unified_rules.size());
+		for (std::pair<Rule, Unify::TypedSubstitution> pos_rule : unified_rules) {
+			// Divide the weights of the rules by their numbers so
+		    // that the sum equals the initial rule weight.
+			pos_rule.first.set_weight(pos_rule.first.get_weight() / N);
+			valid_rules.insert(pos_rule);
+		}
 	}
 	return valid_rules;
 }
@@ -97,10 +113,163 @@ RuleTypedSubstitutionPair ControlPolicy::select_rule(const AndBIT& andbit,
 {
 	// Build weight vector to do weighted random selection
 	std::vector<double> weights;
-	for (const auto& rule : rules)
-		weights.push_back(rule.first.get_weight());
 
-	// No rule exhaustion, sample one according to the distribution
+	// If a control policy atomspace is provided then calculate the
+	// distribution according to the provided control rules. Otherwise
+	// use the default rule weights.
+	if (_control_as)
+		weights = learn_rule_weights(andbit, bitleaf, rules);
+	else
+		for (const auto& rule : rules)
+			weights.push_back(rule.first.get_weight());
+
+	// Sample one according to the distribution
 	std::discrete_distribution<size_t> dist(weights.begin(), weights.end());
 	return rand_element(rules, dist);
 }
+
+std::vector<double> ControlPolicy::learn_rule_weights(const AndBIT& andbit,
+                                                      const BITNode& bitleaf,
+                                                      const RuleTypedSubstitutionMap& rules)
+{
+	std::vector<double> weights;
+	HandleSet ra = rule_aliases(rules);
+	for (const Handle& rule : ra)
+		HandleSet pattern_free_control_rules =
+			fetch_pattern_free_expansion_control_rules(rule);
+	// TODO
+	return weights;
+}
+
+HandleSet ControlPolicy::rule_aliases(const RuleTypedSubstitutionMap& rules)
+{
+	HandleSet aliases;
+	for (auto& rule : rules)
+		aliases.insert(rule.first.get_alias());
+	return aliases;
+}
+
+HandleSet ControlPolicy::fetch_pattern_free_expansion_control_rules(Handle rule)
+{
+	Handle query = mk_pattern_free_expansion_control_rules_query(rule);
+	Handle result = bindlink(_control_as, query);
+	HandleSeq outgoings(result->getOutgoingSet());
+	HandleSet results(outgoings.begin(), outgoings.end());
+	return results;
+}
+
+HandleSet ControlPolicy::fetch_pattern_expansion_control_rules(Handle rule)
+{
+	Handle query = mk_pattern_expansion_control_rules_query(rule);
+	Handle result = bindlink(_control_as, query);
+	HandleSeq outgoings(result->getOutgoingSet());
+	HandleSet results(outgoings.begin(), outgoings.end());
+	return results;
+}
+
+Handle ControlPolicy::mk_vardecl_vardecl(Handle vardecl_var)
+{
+	return al(TYPED_VARIABLE_LINK,
+	          vardecl_var,
+	          al(TYPE_CHOICE,
+	             an(TYPE_NODE, "VariableList"),
+	             an(TYPE_NODE, "VariableNode"),
+	             an(TYPE_NODE, "TypedVariableLink")));
+}
+
+Handle ControlPolicy::mk_list_of_args_vardecl(Handle args_var)
+{
+	return al(TYPED_VARIABLE_LINK,
+	          args_var,
+	          an(TYPE_NODE, "ListLink"));
+}
+
+Handle ControlPolicy::mk_expand_exec(Handle expand_args_var)
+{
+	Handle expand_schema = an(SCHEMA_NODE,
+	                          TraceRecorder::expand_andbit_predicate_name);
+	return al(EXECUTION_LINK,
+	          expand_schema,
+	          al(UNQUOTE_LINK, expand_args_var));
+}
+
+Handle ControlPolicy::mk_preproof_eval(Handle preproof_args_var)
+{
+	Handle preproof_pred = an(PREDICATE_NODE, preproof_predicate_name);
+	return al(EVALUATION_LINK,
+	          preproof_pred,
+	          al(UNQUOTE_LINK, preproof_args_var));
+}
+
+Handle ControlPolicy::mk_pattern_free_expansion_control_rules_query(Handle rule)
+{
+	Handle vardecl_var = an(VARIABLE_NODE, "$vardecl"),
+		vardecl_vardecl = mk_vardecl_vardecl(vardecl_var),
+
+		expand_args_var = an(VARIABLE_NODE, "$expand_args"),
+		expand_args_vardecl = mk_list_of_args_vardecl(expand_args_var),
+		expand_exec = mk_expand_exec(expand_args_var),
+
+		preproof_args_var = an(VARIABLE_NODE, "$preproof_args"),
+		preproof_args_vardecl = mk_list_of_args_vardecl(preproof_args_var),
+		preproof_eval = mk_preproof_eval(preproof_args_var),
+
+		// ImplicationScope to retrieve
+		expand_preproof_impl = al(QUOTE_LINK,
+		                          al(IMPLICATION_SCOPE_LINK,
+		                             al(UNQUOTE_LINK, vardecl_var),
+		                             expand_exec,
+		                             preproof_eval)),
+
+		// BindLink of ImplicationScope to retrieve
+		expand_preproof_impl_bl = al(BIND_LINK,
+		                             al(VARIABLE_LIST,
+		                                vardecl_vardecl,
+		                                expand_args_vardecl,
+		                                preproof_args_vardecl),
+		                             expand_preproof_impl,
+		                             expand_preproof_impl);
+
+	return expand_preproof_impl_bl;
+}
+
+Handle ControlPolicy::mk_pattern_expansion_control_rules_query(Handle rule)
+{
+	Handle vardecl_var = an(VARIABLE_NODE, "$vardecl"),
+		vardecl_vardecl = mk_vardecl_vardecl(vardecl_var),
+
+		expand_args_var = an(VARIABLE_NODE, "$expand_args"),
+		expand_args_vardecl = mk_list_of_args_vardecl(expand_args_var),
+		expand_exec = mk_expand_exec(expand_args_var),
+
+		preproof_args_var = an(VARIABLE_NODE, "$preproof_args"),
+		preproof_args_vardecl = mk_list_of_args_vardecl(preproof_args_var),
+		preproof_eval = mk_preproof_eval(preproof_args_var),
+
+		// ImplicationScope with a pattern in its antecedent to
+		// retrieve
+		pattern_var = an(VARIABLE_NODE, "$pattern"),
+		pat_expand_preproof_impl = al(QUOTE_LINK,
+		                              al(IMPLICATION_SCOPE_LINK,
+		                                 al(UNQUOTE_LINK, vardecl_var),
+		                                 al(AND_LINK,
+		                                    expand_exec,
+		                                    al(UNQUOTE_LINK, pattern_var)),
+		                                 preproof_eval)),
+
+		// Bind of ImplicationScope with a pattern in its antecedent
+		// to retrieve
+		pat_expand_preproof_impl_bl = al(BIND_LINK,
+		                                 al(VARIABLE_LIST,
+		                                    vardecl_vardecl,
+		                                    expand_args_vardecl,
+		                                    preproof_args_vardecl,
+		                                    pattern_var),
+		                                 pat_expand_preproof_impl,
+		                                 pat_expand_preproof_impl);
+
+	return pat_expand_preproof_impl_bl;
+}
+
+#undef al
+#undef an
--- opencog/rule-engine/backwardchainer/ControlPolicy.h
+++ opencog/rule-engine/backwardchainer/ControlPolicy.h
@@ -64,8 +64,13 @@ private:
 	// Reference to the BackwardChainer BIT
 	const BIT& _bit;
 
-	// AtomSpace holding the inference control rules
+	// AtomSpace holding the inference control rules (or simply
+	// control rules for short).
 	AtomSpace* _control_as;
+
+	// AtomSpace holding the pattern matcher queries to fetch the
+	// various control rule
+	AtomSpace _query_as;
 	
 	/**
 	 * Return all valid rules, in the sense that they may possibly be
@@ -80,6 +85,105 @@ private:
 	RuleTypedSubstitutionPair select_rule(const AndBIT& andbit,
 	                                      const BITNode& bitleaf,
 	                                      const RuleTypedSubstitutionMap& rules);
+
+	/**
+	 * Given a list of valid rules, use the control rules in
+	 * _control_as to infer/learn the weights of the optimal policy
+	 * (in Thomson sampling sense).
+	 *
+	 * TODO: add documentation (taken from the pln
+	 * inference-control-learning README.md example).
+	 */
+	std::vector<double> learn_rule_weights(const AndBIT& andbit,
+	                                       const BITNode& bitleaf,
+	                                       const RuleTypedSubstitutionMap& rules);
+
+	/**
+	 * Return the set of rule aliases, as aliases of inference rules
+	 * are used in control rules.
+	 */
+	HandleSet rule_aliases(const RuleTypedSubstitutionMap& rules);
+
+	/**
+	 * Fetch control rules from _control_as involved in BIT
+	 * expansion. Informally that if and-BIT, A, expands into B from L
+	 * with the given rule then B has a probability TV of being a
+	 * preproof of T. Formally
+	 *
+	 * ImplicationScope <TV>
+	 *  VariableList
+	 *    Variable "$T"  ;; Theorem/target to prove
+	 *    TypedVariable  ;; and-BIT to expand
+	 *      Variable "$A"
+	 *      Type "BindLink"
+	 *    Variable "$L"  ;; Leaf from A to expand
+	 *    TypedVariable  ;; Resulting and-BIT from the expansion of L from A with rule R
+	 *      Variable "$B"
+	 *      Type "BindLink"
+	 *  Execution
+	 *    Schema "expand-and-BIT"
+	 *    List
+	 *      BontExec Variable "$A"
+	 *      Variable "$L"
+	 *      DontExec <rule>
+	 *    DontExec Variable "$B"
+	 *  Evaluation
+	 *    Predicate "preproof"
+	 *    List
+	 *      DontExec Variable "$B"
+	 *      Variable "$T"
+	 *
+	 * Although this rule is likely to be very useful in practice we
+	 * still keep it as it may provide some initial guidance when no
+	 * predictive patterns have been extracted from the inference
+	 * history so far. It may also provide a reference when building
+	 * the mixture model in case the so called patterns are actually
+	 * overfit.
+	 */
+	HandleSet fetch_pattern_free_expansion_control_rules(Handle rule);
+
+	/**
+	 * Fetch control rules from _control_as involved in BIT
+	 * expansion. Informally that if and-BIT, A, expands into B from L
+	 * with the given rule, and follow some pattern, then B has a
+	 * probability TV of being a preproof of T. Formally
+	 *
+	 * ImplicationScope <TV>
+	 *  VariableList
+	 *    Variable "$T"  ;; Theorem/target to prove
+	 *    TypedVariable  ;; and-BIT to expand
+	 *      Variable "$A"
+	 *      Type "BindLink"
+	 *    Variable "$L"  ;; Leaf from A to expand
+	 *    TypedVariable  ;; Resulting and-BIT from the expansion of L from A with rule R
+	 *      Variable "$B"
+	 *      Type "BindLink"
+	 *  And
+	 *    Execution
+	 *      Schema "expand-and-BIT"
+	 *      List
+	 *        BontExec Variable "$A"
+	 *        Variable "$L"
+	 *        DontExec <rule>
+	 *      DontExec Variable "$B"
+	 *    <pattern>
+	 *  Evaluation
+	 *    Predicate "preproof"
+	 *    List
+	 *      DontExec Variable "$B"
+	 *      Variable "$T"
+	 */
+	HandleSet fetch_pattern_expansion_control_rules(Handle rule);
+
+	/**
+	 * Helpers to build various hypergraphs used to build various queries
+	 */
+	Handle mk_vardecl_vardecl(Handle vardecl_var);
+	Handle mk_list_of_args_vardecl(Handle args_var);
+	Handle mk_expand_exec(Handle expand_args_var);
+	Handle mk_preproof_eval(Handle preproof_args_var);
+	Handle mk_pattern_free_expansion_control_rules_query(Handle rule);
+	Handle mk_pattern_expansion_control_rules_query(Handle rule);
 };
 
 
