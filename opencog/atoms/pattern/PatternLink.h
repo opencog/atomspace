f6823f22fb9251996dfcf65f00a2d206943f9ae2
Linas Vepstas <linasvepstas@gmail.com>
2017-08-10 00:18:55 -0500

    Minor whitespace cleanup

--- opencog/atoms/pattern/PatternLink.h
+++ opencog/atoms/pattern/PatternLink.h
@@ -6,16 +6,16 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License v3 as
- * published by the Free Software Foundation and including the exceptions
- * at http://opencog.org/wiki/Licenses
+ * published by the Free Software Foundation and including the
+ * exceptions at http://opencog.org/wiki/Licenses
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, write to:
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program; if not, write to:
  * Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
@@ -147,7 +147,7 @@ public:
 	PatternLink(const Handle& body);
 	PatternLink(const Handle& varcdecls, const Handle& body);
 	PatternLink(const Variables&, const Handle&);
-	PatternLink(const Link &l);
+	PatternLink(const Link&);
 
 	// Used only to set up multi-component links.
 	// DO NOT call this! (unless you are the component handler).
