68c04fc213e4471e64186234740e49649cc8e724
Linas Vepstas <linasvepstas@gmail.com>
2017-08-12 15:02:25 -0500

    Done creating data

--- opencog/bench-mst/mst-bench.scm
+++ opencog/bench-mst/mst-bench.scm
@@ -9,6 +9,8 @@
 
 ; Step 1: create a bunch of random pair data.
 
+(define mi-pred (Predicate "Faux MI Key"))
+
 ; Create MPAIRS different random pairs, connecting NVERTS different
 ; vertexes.
 (define (make-words MPAIRS NVERTS)
@@ -18,4 +20,18 @@
 	(define (mkpair)
 		(Evaluation (Predicate "benchy") (List (mkvert) (mkvert))))
 
+	; Use a Gaussian distribution, centerd around 2, having a standard
+	; deviation of about 5. This is close to reality for English
+	; word-pairs, I think.
+	(define (mkmi) (+ 4 (* 5 (random:normal))))
+
+	; Create a random pair, and assign a random weight to it.
+	(define (mkwpair)
+		(cog-set-value! (mkpair) mi-pred (FloatValue (mkmi))))
+
+	;; Loop M times.
+	(define (mkmprs M)
+		(if (< 0 M) (begin (mkwpair) (mkmprs (- M 1)))))
+
+	(mkmprs MPAIRS)
 )
