1fa0a0787e014930757a08a64ef5acb0bc7d5cf2
Linas Vepstas <linasvepstas@gmail.com>
2017-08-12 15:24:18 -0500

    Make a sentence

--- opencog/bench-mst/mst-bench.scm
+++ opencog/bench-mst/mst-bench.scm
@@ -7,6 +7,14 @@
 
 (use-modules (opencog) (opencog sheaf))
 
+; Create a random vertex, drawing from a vocabulary having a mean
+; of NVERTS.
+; Uses an exponential distribution to cheesily approximate a
+; Zipfian distribution.
+(define (mkvert NVERTS)
+	(Concept (format #f "vertex ~D"
+		(round (inexact->exact (* NVERTS (random:exp)))))))
+
 ; Step 1: create a bunch of random pair data.
 
 (define mi-pred (Predicate "Faux MI Key"))
@@ -14,11 +22,8 @@
 ; Create MPAIRS different random pairs, connecting NVERTS different
 ; vertexes.
 (define (make-words MPAIRS NVERTS)
-	(define (mkvert)
-		(Concept (format #f "vertex ~D" (random NVERTS))))
-
 	(define (mkpair)
-		(Evaluation (Predicate "benchy") (List (mkvert) (mkvert))))
+		(Evaluation (Predicate "benchy") (List (mkvert NVERTS) (mkvert NVERTS))))
 
 	; Use a Gaussian distribution, centerd around 2, having a standard
 	; deviation of about 5. This is close to reality for English
@@ -34,4 +39,18 @@
 		(if (< 0 M) (begin (mkwpair) (mkmprs (- M 1)))))
 
 	(mkmprs MPAIRS)
+)
+
+; Create a "sentence" that is of length LEN, and draws from a vocabulary
+; of NVERTS.
+(define (mksent LEN NVERTS)
+	(define (mkwlist lst len)
+		(if (< 0 len)
+			(mkwlist (cons (mkvert NVERTS) lst) (- len 1))
+			lst))
+	(mkwlist '() LEN)
+)
+
+(define (score-faux PAIR)
+	42
 )
