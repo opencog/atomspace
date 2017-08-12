67496c87ba96ceeede1e42f9a3eb2ca91acecf9d
Linas Vepstas <linasvepstas@gmail.com>
2017-08-12 15:50:04 -0500

    Add a performance-measurement function

--- opencog/bench-mst/mst-bench.scm
+++ opencog/bench-mst/mst-bench.scm
@@ -8,20 +8,18 @@
 (use-modules (opencog) (opencog sheaf))
 
 ; Create a random vertex, drawing from a vocabulary having a mean
-; of NVERTS.
+; of NVERTS different "words".
 ; Uses an exponential distribution to cheesily approximate a
 ; Zipfian distribution.
 (define (mkvert NVERTS)
 	(Concept (format #f "vertex ~D"
 		(round (inexact->exact (* NVERTS (random:exp)))))))
 
-; Step 1: create a bunch of random pair data.
-
 (define mi-pred (Predicate "Faux MI Key"))
 
 ; Create MPAIRS different random pairs, connecting NVERTS different
 ; vertexes.
-(define (make-words MPAIRS NVERTS)
+(define (make-pairs MPAIRS NVERTS)
 	(define (mkpair)
 		(Evaluation (Predicate "benchy") (List (mkvert NVERTS) (mkvert NVERTS))))
 
@@ -59,4 +57,20 @@
 			(cog-value (Evaluation (Predicate "benchy") llpr) mi-pred) 0))
 )
 
-(mst-parse-atom-seq (mksent 10 30) score-faux)
+; A performance measurement function
+(define (report-rate THUNK CNT)
+	(define start (get-internal-real-time))
+	(let ((foo (THUNK)))
+		(define ela (exact->inexact (/ (- (get-internal-real-time) start)
+			 internal-time-units-per-second)))
+		(format #t "Performed ~D ops in ~A seconds, at ~A ops/sec\n"
+			CNT ela (/ CNT ela))))
+
+; Step 1: create a bunch of random pair data.
+(define nvocab 5000)
+(define npairs 101000)
+(report-rate (lambda () (make-pairs npairs nvocab)) 1)
+
+(report-rate
+	(lambda ()
+		(mst-parse-atom-seq (mksent 10 30) score-faux)) 1)
