58f7b31b4ab0faca9ccd47df963fe02925db32fd
Nil Geisweiller <ngeiswei@gmail.com>
2017-08-18 23:37:30 +0300

    Add TraceRecorder methods to retreive a fcs expansion trace

--- opencog/rule-engine/backwardchainer/TraceRecorder.cc
+++ opencog/rule-engine/backwardchainer/TraceRecorder.cc
@@ -22,24 +22,56 @@
  */
 
 #include "TraceRecorder.h"
+#include <opencog/util/algorithm.h>
 
 using namespace opencog;
 
 const std::string TraceRecorder::target_predicate_name = "URE:BC:target";
 const std::string TraceRecorder::andbit_predicate_name = "URE:BC:and-BIT";
-const std::string TraceRecorder::expand_andbit_predicate_name = "URE:BC:expand-and-BIT";
+const std::string TraceRecorder::expand_andbit_schema_name = "URE:BC:expand-and-BIT";
 const std::string TraceRecorder::proof_predicate_name = "URE:BC:proof";
 
-TraceRecorder::TraceRecorder(AtomSpace* tr_as) : _trace_as(tr_as) {}
+TraceRecorder::TraceRecorder(AtomSpace* tr_as) : _trace_as(tr_as) {
+	if (_trace_as) {
+		_target_predicate =
+			_trace_as->add_node(PREDICATE_NODE, target_predicate_name);
+		_andbit_predicate =
+			_trace_as->add_node(PREDICATE_NODE, andbit_predicate_name);
+		_expand_andbit_schema =
+			_trace_as->add_node(SCHEMA_NODE, expand_andbit_schema_name);
+		_proof_predicate =
+			_trace_as->add_node(PREDICATE_NODE, proof_predicate_name);
+	}
+}
+
+HandleSeqSet TraceRecorder::traces()
+{
+	HandleSeqSet trs;
+	for (const Handle& fcs_proof : get_fcs_proofs())
+		set_union(trs, traces(fcs_proof));
+	return trs;
+}
+
+HandleSeqSet TraceRecorder::traces(const Handle& fcs)
+{
+	HandleSeqSet trs;
+	for (const Handle& h : get_expansion_sources(fcs)) {
+		for (HandleSeq hs : traces(h)) {
+			hs.push_back(fcs);
+			trs.insert(hs);
+		}
+	}
+	return trs;
+}
 
 void TraceRecorder::target(const Handle& target)
 {
-	add_evaluation(target_predicate_name, target, TruthValue::TRUE_TV());
+	add_evaluation(_target_predicate, target, TruthValue::TRUE_TV());
 }
 
 void TraceRecorder::andbit(const AndBIT& andbit)
 {
-	add_evaluation(andbit_predicate_name,
+	add_evaluation(_andbit_predicate,
 	               dont_exec(andbit.fcs),
 	               TruthValue::TRUE_TV());
 }
@@ -47,7 +79,7 @@ void TraceRecorder::andbit(const AndBIT& andbit)
 void TraceRecorder::expansion(const Handle& andbit_fcs, const Handle& bitleaf_body,
                               const Rule& rule, const AndBIT& new_andbit)
 {
-	add_execution(expand_andbit_predicate_name,
+	add_execution(_expand_andbit_schema,
 	              dont_exec(andbit_fcs), bitleaf_body,
 	              dont_exec(rule.get_alias()),
 	              dont_exec(new_andbit.fcs), TruthValue::TRUE_TV());
@@ -55,7 +87,7 @@ void TraceRecorder::expansion(const Handle& andbit_fcs, const Handle& bitleaf_bo
 
 void TraceRecorder::proof(const Handle& andbit_fcs, const Handle& target_result)
 {
-	add_evaluation(proof_predicate_name,
+	add_evaluation(_proof_predicate,
 	               dont_exec(andbit_fcs), target_result,
 	               target_result->getTruthValue());
 }
@@ -68,20 +100,19 @@ Handle TraceRecorder::dont_exec(const Handle& h)
 	return _trace_as->add_link(DONT_EXEC_LINK, h);
 }
 
-Handle TraceRecorder::add_execution(const std::string& schema_name,
+Handle TraceRecorder::add_execution(const Handle& schema,
                                     const Handle& input, const Handle& output,
                                     TruthValuePtr tv)
 {
 	if (not _trace_as)
 		return Handle::UNDEFINED;
 
-	Handle schema = _trace_as->add_node(SCHEMA_NODE, schema_name);
 	Handle execution = _trace_as->add_link(EXECUTION_LINK, schema, input, output);
 	execution->setTruthValue(tv);
 	return execution;
 }
 
-Handle TraceRecorder::add_execution(const std::string& schema_name,
+Handle TraceRecorder::add_execution(const Handle& schema,
                                     const Handle& input1,
                                     const Handle& input2,
                                     const Handle& input3,
@@ -92,23 +123,22 @@ Handle TraceRecorder::add_execution(const std::string& schema_name,
 		return Handle::UNDEFINED;
 
 	Handle inputs = _trace_as->add_link(LIST_LINK, input1, input2, input3);
-	return add_execution(schema_name, inputs, output, tv);
+	return add_execution(schema, inputs, output, tv);
 }
 
-Handle TraceRecorder::add_evaluation(const std::string& predicate_name,
+Handle TraceRecorder::add_evaluation(const Handle& predicate,
                                      const Handle& argument,
                                      TruthValuePtr tv)
 {
 	if (not _trace_as)
 		return Handle::UNDEFINED;
 	
-	Handle predicate = _trace_as->add_node(PREDICATE_NODE, predicate_name);
 	Handle evaluation = _trace_as->add_link(EVALUATION_LINK, predicate, argument);
 	evaluation->setTruthValue(tv);
 	return evaluation;
 }
 
-Handle TraceRecorder::add_evaluation(const std::string& predicate_name,
+Handle TraceRecorder::add_evaluation(const Handle& predicate,
                                      const Handle& arg1, const Handle& arg2,
                                      TruthValuePtr tv)
 {
@@ -116,5 +146,22 @@ Handle TraceRecorder::add_evaluation(const std::string& predicate_name,
 		return Handle::UNDEFINED;
 
 	Handle arguments = _trace_as->add_link(LIST_LINK, arg1, arg2);
-	return add_evaluation(predicate_name, arguments, tv);
+	return add_evaluation(predicate, arguments, tv);
+}
+
+HandleSet TraceRecorder::get_expansion_sources(const Handle& fcs_target)
+{
+	HandleSet sources;
+	for (auto& exec_link : fcs_target->getIncomingSetByType(EXECUTION_LINK))
+		if (exec_link->getOutgoingAtom(0) == _expand_andbit_schema)
+			sources.insert(exec_link->getOutgoingAtom(1)->getOutgoingAtom(0));
+	return sources;
+}
+
+HandleSet TraceRecorder::get_fcs_proofs()
+{
+	HandleSet fcs_proofs;
+	for (auto& eval_link : _proof_predicate->getIncomingSetByType(EVALUATION_LINK))
+		fcs_proofs.insert(eval_link->getOutgoingAtom(1)->getOutgoingAtom(0));
+	return fcs_proofs;
 }
