3e7c2c9ce3fce112d04cda21b283270ec83d7dda
Linas Vepštas <linasvepstas@gmail.com>
2017-09-23 15:03:24 +0800

    Install the new file.

--- opencog/sheaf/sheaf.scm
+++ opencog/sheaf/sheaf.scm
@@ -7,6 +7,7 @@
 ; The files are loaded in pipeline order.
 ; In general, the later files depend on definitions contained
 ; in the earlier files.
+(load "sheaf/sections.scm")
 (load "sheaf/mst-parser.scm")
 (load "sheaf/vo-graph.scm")
 (load "sheaf/make-section.scm")
