37bddab0234b5f952ba2de4a666aa523130a49cc
Linas Vepstas <linasvepstas@gmail.com>
2017-10-18 12:21:39 -0500

    Fix the load path; missing prefix

--- opencog/scm/opencog/rule-engine.scm
+++ opencog/scm/opencog/rule-engine.scm
@@ -8,5 +8,5 @@
 
 (load-extension "libruleengine" "opencog_ruleengine_init")
 
-(load-from-path "rule-engine/rule-engine-utils.scm")
+(load-from-path "opencog/rule-engine/rule-engine-utils.scm")
 (export-rule-engine-utils)
