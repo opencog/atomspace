7763970fb2e99d907fa6e54a4dd3fa7962a19021
Nil Geisweiller <ngeiswei@gmail.com>
2017-10-27 06:40:00 -0400

    Fix control-rules.scm

--- tests/rule-engine/backwardchainer/control-rules.scm
+++ tests/rule-engine/backwardchainer/control-rules.scm
@@ -4,42 +4,29 @@
       (VariableNode "$T")
       (TypedVariableLink
          (VariableNode "$A")
-         (TypeNode "DontExecLink")
-      )
+         (TypeNode "DontExecLink"))
       (VariableNode "$L")
       (TypedVariableLink
          (VariableNode "$B")
-         (TypeNode "DontExecLink")
-      )
-   )
+         (TypeNode "DontExecLink")))
    (AndLink
       (ExecutionLink
          (SchemaNode "URE:BC:expand-and-BIT")
          (ListLink
             (VariableNode "$A")
             (VariableNode "$L")
-            (DontExecLink
-               (DefinedSchemaNode "rule-1")
-            )
-         )
-         (VariableNode "$B")
-      )
+            (DontExecLink (DefinedSchemaNode "rule-1")))
+         (VariableNode "$B"))
       (EvaluationLink
          (PredicateNode "URE:BC:preproof-of")
          (ListLink
             (VariableNode "$A")
-            (VariableNode "$T")
-         )
-      )
-   )
+            (VariableNode "$T"))))
    (EvaluationLink
       (PredicateNode "URE:BC:preproof-of")
       (ListLink
          (VariableNode "$B")
-         (VariableNode "$T")
-      )
-   )
-)
+         (VariableNode "$T"))))
 
 ;; Context-sensitive control rules for inference rule-2
 (ImplicationScopeLink (stv 1 0.001)
@@ -47,14 +34,11 @@
       (VariableNode "$T")
       (TypedVariableLink
          (VariableNode "$A")
-         (TypeNode "DontExecLink")
-      )
+         (TypeNode "DontExecLink"))
       (VariableNode "$X")
       (TypedVariableLink
          (VariableNode "$B")
-         (TypeNode "DontExecLink")
-      )
-   )
+         (TypeNode "DontExecLink")))
    (AndLink
       (ExecutionLink
          (SchemaNode "URE:BC:expand-and-BIT")
@@ -62,26 +46,16 @@
             (VariableNode "$A")
             (InheritanceLink
               (ConceptNode "a")
-              (VariableNode "$X")
-            (DontExecLink
-               (DefinedSchemaNode "rule-2")
-            )
-         )
-         (VariableNode "$B")
-      )
+              (VariableNode "$X"))
+            (DontExecLink (DefinedSchemaNode "rule-2")))
+         (VariableNode "$B"))
       (EvaluationLink
          (PredicateNode "URE:BC:preproof-of")
          (ListLink
             (VariableNode "$A")
-            (VariableNode "$T")
-         )
-      )
-   )
+            (VariableNode "$T"))))
    (EvaluationLink
       (PredicateNode "URE:BC:preproof-of")
       (ListLink
          (VariableNode "$B")
-         (VariableNode "$T")
-      )
-   )
-)
+         (VariableNode "$T"))))
