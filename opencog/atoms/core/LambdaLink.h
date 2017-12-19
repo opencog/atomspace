5f4e2071f0f87864a455fee27f7df8fed8d531b9
Linas Vepstas <linasvepstas@gmail.com>
2017-12-19 00:44:49 -0600

    Clarify what ambdaLInk really is

--- opencog/atoms/core/LambdaLink.h
+++ opencog/atoms/core/LambdaLink.h
@@ -31,7 +31,24 @@ namespace opencog
  *  @{
  */
 
-/// The LambdaLink is a RewriteLink that ...
+/// The LambdaLink is supposed to closely model the traditional concept
+/// of a lambda from lambda calculus (or functional programming). It
+/// is meant to behave just like a combinator, and supports the standard
+/// operations of beta-reduction and alpha-conversion (modulo that the
+/// atomspace enforces alpha-equivalence).
+///
+/// The actual implementation of the alpha and beta reduction sits on
+/// the RewriteLink, so this class is effectively a no-op, from the
+/// C++ point of view. However...
+///
+/// However, we want to have this to minimize confusion in other,
+/// distant parts of the code base.  The issue is that there are many
+/// other classes derived from RewriteLink, and they are NOT lambdas!
+/// The most prominent example are the various PatternLinks; a simpler
+/// example is the PutLink, which is a beta-redex and therefore cannot
+/// ever be an actual lambda, elthough it derives from RewriteLink
+/// to do it's beta-reduction.  And so that's why we have a no-op C++
+/// class, here.
 ///
 class LambdaLink : public RewriteLink
 {
