1fcf59f1fee277ba2405fb5bfcd586bc8a145d09
Nil Geisweiller <ngeiswei@gmail.com>
2017-12-21 13:19:23 +0200

    Relax PutLink::static_typecheck_values for quotation

--- opencog/atoms/core/PutLink.cc
+++ opencog/atoms/core/PutLink.cc
@@ -122,6 +122,11 @@ void PutLink::static_typecheck_values(void)
 	if (classserver().isA(btype, TYPE_NODE) or TYPE_CHOICE == btype)
 		return;
 
+	// If it's an UnquoteLink then the PutLink is likely quoted and
+	// thus there is nothing to do
+	if (btype == UNQUOTE_LINK)
+		return;
+
 	size_t sz = _varlist.varseq.size();
 
 	Handle valley = _values;
