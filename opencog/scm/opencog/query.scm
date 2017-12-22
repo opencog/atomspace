73f5d3ef92927f57ddb70ca87bc4a85bd50dcec2
Linas Vepstas <linasvepstas@gmail.com>
2017-12-22 15:38:34 -0600

    Mark functions as deprecated

--- opencog/scm/opencog/query.scm
+++ opencog/scm/opencog/query.scm
@@ -14,12 +14,14 @@
 (load-extension "libquery" "opencog_query_init")
 
 (define-public (cog-bind handle)
+	(display "Obsolete! Do not use cog-bind, use cog-execute! insead.\n")
 	(cog-bind-first-n handle -1)
 )
 (define-public (cog-bind-single handle)
 	(cog-bind-first-n handle 1)
 )
 (define-public (cog-satisfying-set handle)
+	(display "Obsolete! Do not use cog-satisfying-set, use cog-execute! insead.\n")
 	(cog-satisfying-set-first-n handle -1)
 )
 (define-public (cog-satisfying-element handle)
@@ -28,36 +30,38 @@
 
 (set-procedure-property! cog-bind 'documentation
 "
- cog-bind handle
-    Run pattern matcher on handle.  handle must be a BindLink.
+ cog-bind HANDLE
+    OBSELETE! DO NOT USE IN NEW CODE!
+
+    Run pattern matcher on HANDLE.  HANDLE must be a BindLink.
     Uses crisp (non-probabilistic) logic during the evaluation
     of evaluatable terms.
 ")
 
 (set-procedure-property! cog-bind-first-n 'documentation
 "
- cog-bind-first-n
-    Run pattern matcher on handle.  handle must be a BindLink.
+ cog-bind-first-n HANDLE
+    Run pattern matcher on HANDLE.  HANDLE must be a BindLink.
     The search is terminated after the first N matches are found.
 ")
 
 (set-procedure-property! cog-bind-single 'documentation
 "
- cog-bind-single handle
-    Run pattern matcher on handle.  handle must be a BindLink.
+ cog-bind-single HANDLE
+    Run pattern matcher on HANDLE.  HANDLE must be a BindLink.
     The search is terminated after the first match is found.
 ")
 
 (set-procedure-property! cog-bind-af 'documentation
 "
- cog-bind-af handle
-    Run pattern matcher on handle.  handle must be a BindLink.
+ cog-bind-af HANDLE
+    Run pattern matcher on HANDLE.  HANDLE must be a BindLink.
     A special-purpose pattern matcher used by the URE.
 ")
 
 (set-procedure-property! cog-satisfy 'documentation
 "
- cog-satisfy handle
-    Run pattern matcher on handle.  handle must be a SatisfactionLink.
+ cog-satisfy HANDLE
+    Run pattern matcher on HANDLE.  HANDLE must be a SatisfactionLink.
     Return a TV. Only satisfaction is performed, no implication.
 ")
