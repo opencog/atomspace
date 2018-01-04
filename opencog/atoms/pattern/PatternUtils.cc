816e1f9ef63e8c3d56c425fc444f80b518e791f2
Linas Vepstas <linasvepstas@gmail.com>
2018-01-03 22:36:30 -0600

    Typo, and missing types

--- opencog/atoms/pattern/PatternUtils.cc
+++ opencog/atoms/pattern/PatternUtils.cc
@@ -117,13 +117,16 @@ bool is_constant(const HandleSet& vars, const Handle& clause)
 	            or contains_atomtype(clause, DEFINED_SCHEMA_NODE)
 	            or contains_atomtype(clause, GROUNDED_PREDICATE_NODE)
 	            or contains_atomtype(clause, GROUNDED_SCHEMA_NODE)
+	            // TODO: should not the below be any VirtualLink?
+	            // Or contains any EvaluatableLink ??
 	            or contains_atomtype(clause, IDENTICAL_LINK)
 	            or contains_atomtype(clause, EQUAL_LINK)
+	            or contains_atomtype(clause, GREATER_THAN_LINK)
 	            // If it is an EvaluatableLink then is is not a
 	            // constant, unless it is a closed EvaluationLink over
 	            // a PredicateNode.
 	            or (classserver().isA(clause->get_type(), EVALUATABLE_LINK)
-	                and (0 == clause->get_arity()
+	                and (0 != clause->get_arity()
 	                     or
 	                     clause->getOutgoingAtom(0)->get_type() != PREDICATE_NODE)));
 }
