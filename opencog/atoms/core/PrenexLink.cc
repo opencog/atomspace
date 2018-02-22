1ab69c61362958fadf3a0502253f1116c4237851
Nil Geisweiller <ngeiswei@gmail.com>
2018-02-22 15:25:36 +0000

    Fix issue #1585

--- opencog/atoms/core/PrenexLink.cc
+++ opencog/atoms/core/PrenexLink.cc
@@ -75,7 +75,26 @@ Handle PrenexLink::reassemble(const HandleMap& vm,
 
 	// Reassemble if necessary
 	if (not final_varlist.empty())
-		return Handle(createLink(get_type(), vdecl, newbod));
+	{
+		Type ntype = get_type();
+		// Due to the fact that
+		//
+		// PutLink
+		//   <vardecl>
+		//   <body>
+		//   <values>
+		//
+		// is equivalent to
+		//
+		// PutLink
+		//   Lambda
+		//     <vardecl>
+		//     <body>
+		//   <values>
+		if (ntype == PUT_LINK)
+			ntype = LAMBDA_LINK;
+		return Handle(createLink(ntype, vdecl, newbod));
+	}
 
 	return newbod;
 }
