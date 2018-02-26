575dd2eb9d0994ae6fe5b92db79fafda16de2e97
Linas Vepstas <linasvepstas@gmail.com>
2018-02-26 16:30:37 -0600

    Primary bugfix for bug #1598
    The calculation of evaluatable clauses was wrong.

--- opencog/atoms/pattern/PatternLink.cc
+++ opencog/atoms/pattern/PatternLink.cc
@@ -54,7 +54,7 @@ void PatternLink::common_init(void)
 	validate_clauses(_varlist.varset, _pat.clauses, _pat.constants);
 	extract_optionals(_varlist.varset, _pat.clauses);
 
-	// Locate as well as black-box and clear-box clauses.
+	// Locate the black-box and clear-box clauses.
 	unbundle_virtual(_varlist.varset, _pat.cnf_clauses,
 	                 _fixed, _virtual, _pat.black);
 
@@ -76,7 +76,7 @@ void PatternLink::common_init(void)
 	// Tweak the evaluatable_holders to reflect this.
 	TypeSet connectives({AND_LINK, SEQUENTIAL_AND_LINK,
 	                     OR_LINK, SEQUENTIAL_OR_LINK, NOT_LINK});
-	trace_connectives(connectives, _pat.clauses);
+	trace_connectives(connectives, _pat.body);
 
 	// Split the non-virtual clauses into connected components
 	get_connected_components(_varlist.varset, _fixed,
@@ -321,13 +321,8 @@ PatternLink::PatternLink(const Link& l)
 void PatternLink::unbundle_clauses(const Handle& hbody)
 {
 	Type t = hbody->get_type();
-	// For just right now, unpack PresentLink, although that is not
-	// technically correct in the long-run. XXX FIXME In the long run,
-	// nothing should be unpacked, since everything should be run-time
-	// evaluatable. i.e. everything should be a predicate, and that's
-	// that. ??? But how can this be done? We have to pattern-match
-	// disjoint clauses, connected via variables only; how else can this
-	// be done, except by unpacking?  I'm confused.
+	// For just right now, unpack PresentLink, although this might not
+	// be correct in the long-run. (???)
 	//
 	// For SequentialAndLinks, which are expected to be evaluated
 	// in-order, we need to fish out any PresentLinks, and add them
@@ -748,30 +743,32 @@ bool PatternLink::add_dummies()
 
 /* ================================================================= */
 
-/// Starting from the top of a clause, trace down through the tree
-/// of connectives.  If a term appears under a connective, and there
+/// Starting from the main body, trace down through the tree of (logical)
+/// connectives.  If a term appears under a (logic) connective, and there
 /// is a path of connectives all the way to the top, then we have to
 /// assume the term is evaluatable, as the whole point of connectives
 /// to to connect evaluatable terms.  Thus, for example, for a clause
 /// having the form (AndLink stuff (OrLink more-stuff (NotLink not-stuff)))
 /// we have to assume that stuff, more-stuff and not-stuff are all
-/// evaluatable.
+/// evaluatable. Tracning halts as soon as something that isn't a
+/// connective is encountered.
 void PatternLink::trace_connectives(const TypeSet& connectives,
-                                    const HandleSeq& oset,
+                                    const Handle& term,
                                     Quotation quotation)
 {
-	for (const Handle& term: oset)
+	Type t = term->get_type();
+
+	quotation.update(t);
+
+	if (quotation.is_quoted() or connectives.find(t) == connectives.end())
+		return;
+
+	_pat.evaluatable_holders.insert(term);
+	add_to_map(_pat.in_evaluatable, term, term);
+	for (const Handle& pred: term->getOutgoingSet())
 	{
-		Type t = term->get_type();
-
-		quotation.update(t);
-
-		if (quotation.is_quoted() or connectives.find(t) == connectives.end())
-			continue;
-		_pat.evaluatable_holders.insert(term);
-		add_to_map(_pat.in_evaluatable, term, term);
-		if (term->is_link())
-			trace_connectives(connectives, term->getOutgoingSet(), quotation);
+		if (pred->is_link())
+			trace_connectives(connectives, pred, quotation);
 	}
 }
 
