6ea22e8a117067503d25ac5fda5147a4d8e11590
Linas Vepstas <linasvepstas@gmail.com>
2018-05-09 02:27:46 -0500

    Finish implementing the bindings

--- opencog/scm/opencog/attention-bank.scm
+++ opencog/scm/opencog/attention-bank.scm
@@ -8,19 +8,14 @@
 (load-extension "libattentionbank" "opencog_attention_init")
 
 ; This avoids complaints, when the docs are set, below.
-(export cog-af cog-af-size cog-set-af-size! cog-stimulate)
+(export cog-update-af cog-af-size cog-set-af-size! cog-stimulate)
 
 (use-modules (opencog))
-
-; -----------------------------------------------------------------------
-(define-public (cog-af-length)
-" cog-af-length -- Length of the list of atoms in the attentional focus."
-	(length (cog-af)))
 
 ;; -----------------------------------------------------
 ;;
 
-(set-procedure-property! cog-af 'documentation
+(define* (cog-af #:optional (n -1))
 "
  cog-af n
     Return the top n atoms in the AttentionalFocus or
@@ -34,7 +29,22 @@
 
     guile> (cog-af 1)
     (ConceptNode \"ArtificialIntelligence\" (av 15752 0 0))
-")
+"
+	(map gar
+		(cog-incoming-by-type (cog-update-af n) 'MemberLink))
+)
+
+(export cog-af)
+
+; -----------------------------------------------------------------------
+
+(define-public (cog-af-length)
+" cog-af-length -- Length of the list of atoms in the attentional focus."
+	(length (cog-af))
+)
+
+;; -----------------------------------------------------
+;;
 
 (set-procedure-property! cog-af-size 'documentation
 "
