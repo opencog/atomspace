548fe1aeee863e31c41674c52453cfd95b1abd3f
Vitaly Bogdanov <vsbogd@gmail.com>
2018-05-22 17:32:13 +0300

    Add unit test for ChoiceLink containing nodes case

---
+++ tests/query/choice-nodes.scm
@@ -0,0 +1,25 @@
+;
+; Basic unit testing for ChoiceLinks in the pattern matcher.
+;
+(use-modules (opencog))
+(use-modules (opencog exec))
+
+;;; Populate the atomspace with four small trees.
+(ListLink
+	(ConceptNode "A")
+	(ConceptNode "B")
+)
+
+;;; Two clauses; they both connected with a common variable.
+(define query
+	(GetLink
+		(TypedVariableLink (VariableNode "$x") (TypeNode "ConceptNode"))
+		(ListLink
+			(ChoiceLink
+				(ConceptNode "A")
+				(ConceptNode "C")
+			)
+			(VariableNode "$x")
+		)
+	)
+)
