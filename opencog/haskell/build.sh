ac529e416154c2e5de837ffbe85fc67e5c5aa8d6
Roman Treutlein <roman@singularitynet.io>
2018-05-30 11:25:44 +0800

    Revert Prvious Changes

--- opencog/haskell/build.sh
+++ opencog/haskell/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 BIN_DIR=$1
 
-ghcver="$(stack ghc -- --version)"
+ghcver="$(stack --allow-different-user ghc -- --version)"
 
 if [[ "$ghcver" == *8.0.2* ]]
 then
     echo "Correct GHC version installed."
 else
     echo "Wrong GHC version installed. Running stack setup."
-    stack setup
+    stack setup --allow-different-user
 fi
 
 if [ "$(id -u)" -ne 0 ];
 then
   # Build haskell bindings package.
-  stack build --extra-lib-dirs=${BIN_DIR}
+  stack build --allow-different-user --extra-lib-dirs=${BIN_DIR}
 fi
