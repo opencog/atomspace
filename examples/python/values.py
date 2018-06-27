f376eb478783448b5e372c2c4a7f7ee0e4891e88
Vitaly Bogdanov <vsbogd@gmail.com>
2018-06-27 20:10:58 +0300

    Add example of scheme_eval_v usage

--- examples/python/values.py
+++ examples/python/values.py
@@ -8,9 +8,10 @@ An example of using values via Python API
 
 from opencog.atomspace import AtomSpace, TruthValue
 from opencog.type_constructors import *
+from opencog.scheme_wrapper import scheme_eval_v
 
-a = AtomSpace()
-set_type_ctor_atomspace(a)
+atomspace = AtomSpace()
+set_type_ctor_atomspace(atomspace)
 
 a = FloatValue([1.0, 2.0, 3.0])
 b = FloatValue([1.0, 2.0, 3.0])
@@ -28,3 +29,8 @@ boundingBox.set_value(featureKey, featureValue)
 print('set value to atom: {}'.format(boundingBox))
 
 print('get value from atom: {}'.format(boundingBox.get_value(featureKey)))
+
+value = scheme_eval_v(atomspace, '(ValueOf (ConceptNode "boundingBox") '
+                      '(PredicateNode "features"))')
+value = boundingBox.get_value(featureKey)
+print('get value from atom using Scheme program: {}'.format(value))
