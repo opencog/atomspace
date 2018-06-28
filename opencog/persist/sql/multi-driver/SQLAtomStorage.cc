b8854aadf4c5fcb3cc08887c8e39eeecfdf27898
Linas Vepštas <linasvepstas@gmail.com>
2018-06-27 21:04:12 -0500

    Move AtomSpace handling into it's own file

--- opencog/persist/sql/multi-driver/SQLAtomStorage.cc
+++ opencog/persist/sql/multi-driver/SQLAtomStorage.cc
@@ -31,10 +31,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <opencog/atoms/base/Atom.h>
-#include <opencog/atoms/proto/NameServer.h>
-#include <opencog/atoms/base/Link.h>
-#include <opencog/atoms/base/Node.h>
 #include <opencog/atomspace/AtomSpace.h>
 #include <opencog/atomspaceutils/TLB.h>
 
@@ -186,34 +182,6 @@ bool SQLAtomStorage::connected(void)
 	bool have_connection = db_conn->connected();
 	conn_pool.push(db_conn);
 	return have_connection;
-}
-
-/* ================================================================== */
-/* AtomTable UUID stuff */
-void SQLAtomStorage::store_atomtable_id(const AtomTable& at)
-{
-	UUID tab_id = at.get_uuid();
-	if (table_id_cache.count(tab_id)) return;
-
-	table_id_cache.insert(tab_id);
-
-	// Get the parent table as well.
-	UUID parent_id = 1;
-	AtomTable *env = at.get_environ();
-	if (env)
-	{
-		parent_id = env->get_uuid();
-		store_atomtable_id(*env);
-	}
-
-#define BUFSZ 80
-	char buff[BUFSZ];
-	snprintf(buff, BUFSZ,
-		"INSERT INTO Spaces (space, parent) VALUES (%ld, %ld);",
-		tab_id, parent_id);
-
-	Response rp(conn_pool);
-	rp.exec(buff);
 }
 
 /* ================================================================== */
