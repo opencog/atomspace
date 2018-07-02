7c1b957195843a37d3aaa9e11097a96dee20a47d
Vitaly Bogdanov <vsbogd@gmail.com>
2018-07-02 21:10:24 +0300

    Add unit test for StringValue Python API

---
+++ tests/cython/atomspace/test_stringvalue.py
@@ -0,0 +1,55 @@
+import unittest
+
+from opencog.atomspace import AtomSpace
+from opencog.type_constructors import *
+from opencog.utilities import initialize_opencog, finalize_opencog
+
+class StringValueTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.space = AtomSpace()
+        initialize_opencog(self.space)
+
+    def tearDown(self):
+        finalize_opencog()
+        del self.space
+
+    def test_create_single_value(self):
+        value = StringValue('foo')
+        self.assertTrue(value is not None)
+
+    def test_create_list_value(self):
+        value = StringValue(['foo', 'bar'])
+        self.assertTrue(value is not None)
+    
+    def test_value_equals(self):
+        self.assertEqual(StringValue('foo'), StringValue(['foo']))
+        self.assertEqual(StringValue(['foo', 'bar']),
+                         StringValue(['foo', 'bar']))
+        self.assertNotEqual(StringValue('foo'), StringValue('bar'))
+        self.assertNotEqual(StringValue(['foo', 'bar']), 
+                            StringValue(['bar', 'foo']))
+
+    def test_add_value_to_atom(self):
+        atom = ConceptNode('object')
+        key = PredicateNode('predicate')
+        value = StringValue(['foo', 'bar'])
+        atom.set_value(key, value)
+        self.assertEqual(StringValue(['foo', 'bar']), atom.get_value(key))
+
+    def test_get_list_of_items_from_value(self):
+        value = StringValue(['foo', 'bar'])
+        self.assertEqual(['foo', 'bar'], value.to_list())
+
+    def test_str(self):
+        value = StringValue('foo')
+        self.assertEqual('(StringValue "foo")\n', str(value))
+
+    def test_is_a(self):
+        value = StringValue('foo')
+        self.assertEqual(types.StringValue, value.type)
+        self.assertEqual('StringValue', value.type_name)
+        self.assertFalse(value.is_node())
+        self.assertFalse(value.is_atom())
+        self.assertFalse(value.is_link())
+        self.assertTrue(value.is_a(types.Value))
