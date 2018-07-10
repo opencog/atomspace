2483179a16dcb5cbff1fa5c3044832f7f8532349
Linas Vepstas <linasvepstas@gmail.com>
2018-07-09 22:59:49 -0500

    Also test the unwrapping inside a SetLink.
    This was failing to happen, until the previous
    commit, which fixed it.

--- tests/query/dont-exec-simple.scm
+++ tests/query/dont-exec-simple.scm
@@ -10,3 +10,23 @@
 (define put-and
 	(Put (DontExec (AndLink (Variable "$x")(Variable "$y")))
 		(ListLink (Concept "a") (Concept "b"))))
+
+; A multi-argument put whose EvaluationLink should not evaluate
+(define put-eval
+	(Put (DontExec (EvaluationLink
+			(PredicateNode "foo")
+			(Variable "$x")
+			(Variable "$y")
+			(Variable "$z")))
+		(SetLink
+			(ListLink (Concept "a") (Concept "b") (Concept "c"))
+			(ListLink (Concept "d") (Concept "e") (Concept "f"))
+			(ListLink (Concept "g") (Concept "h") (Concept "i"))
+			(ListLink (Concept "j") (Concept "k") (Concept "l")))))
+
+; The result we expect from the above
+(define eval-expected (SetLink
+	(Evaluation (Predicate "foo") (Concept "a") (Concept "b") (Concept "c"))
+	(Evaluation (Predicate "foo") (Concept "d") (Concept "e") (Concept "f"))
+	(Evaluation (Predicate "foo") (Concept "g") (Concept "h") (Concept "i"))
+	(Evaluation (Predicate "foo") (Concept "j") (Concept "k") (Concept "l"))))
