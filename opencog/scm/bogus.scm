3732252247a6c533445cf68f6f3b38863969214e
Linas Vepstas <linasvepstas@gmail.com>
2018-12-27 20:50:03 -0600

    Some bogus testing

---
+++ opencog/scm/bogus.scm
@@ -0,0 +1,7 @@
+;
+(setlocale LC_CTYPE "")
+(setlocale LC_NUMERIC "C")
+
+(define-module (bogus)
+
+(define-public (foo) 222)
--- tests/scm/scm-opencog-python.scm
+++ tests/scm/scm-opencog-python.scm
@@ -1,19 +1,21 @@
 (add-to-load-path "../../opencog/scm")
 
+(use-modules (bogus))
+
 (load-from-path "opencog.scm")
 ; (use-modules (opencog))
 
 (use-modules (opencog test-runner))
 (use-modules (opencog python))
 
-;;;(opencog-test-runner)
-;;;(define t "opencog-python-sniff-test")
-;;;
-;;;(test-begin t)
-;;;
-;;;; Ideally, this will not crash.  Currently, it returns an empty string.
-;;;(define rc (python-eval "print ('Hello world\\n', 2+2)"))
-;;;
-;;;(test-assert "python-eval is borken" (string=? rc ""))
-;;;
-;;;(test-end t)
+(opencog-test-runner)
+(define t "opencog-python-sniff-test")
+
+(test-begin t)
+
+; Ideally, this will not crash.  Currently, it returns an empty string.
+(define rc (python-eval "print ('Hello world\\n', 2+2)"))
+
+(test-assert "python-eval is borken" (string=? rc ""))
+
+(test-end t)
