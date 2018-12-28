6282817e7d69a9b34382ca898198ea2fd786fe28
Linas Vepstas <linasvepstas@gmail.com>
2018-12-27 19:25:25 -0600

    Ongoing debugging

--- tests/scm/scm-opencog-python.scm
+++ tests/scm/scm-opencog-python.scm
@@ -1,15 +1,18 @@
 (use-modules (opencog))
-(use-modules (opencog test-runner))
-(use-modules (opencog python))
 
-(opencog-test-runner)
-(define t "opencog-python-sniff-test")
+(display "hello world")
 
-(test-begin t)
-
-; Ideally, this will not crash.  Currently, it returns an empty string.
-(define rc (python-eval "print ('Hello world\\n', 2+2)"))
-
-(test-assert "python-eval is borken" (string=? rc ""))
-
-(test-end t)
+;;;(use-modules (opencog test-runner))
+;;;(use-modules (opencog python))
+;;;
+;;;(opencog-test-runner)
+;;;(define t "opencog-python-sniff-test")
+;;;
+;;;(test-begin t)
+;;;
+;;;; Ideally, this will not crash.  Currently, it returns an empty string.
+;;;(define rc (python-eval "print ('Hello world\\n', 2+2)"))
+;;;
+;;;(test-assert "python-eval is borken" (string=? rc ""))
+;;;
+;;;(test-end t)
