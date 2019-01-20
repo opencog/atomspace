ef54c743e08672521b1158371d46b9ac48c9f502
Linas Vepstas <linasvepstas@gmail.com>
2019-01-19 19:45:39 -0600

    Revise the example further.

--- examples/atomspace/truthvalues.scm
+++ examples/atomspace/truthvalues.scm
@@ -34,90 +34,54 @@
 ;
 (use-modules (opencog))
 
-; Values can store vectors of floats ...
-(define f (FloatValue 0.1 0.2 3.3 4.5678))
+; TruthValues normally consist of two floats:
+; by convention, a "strength" and a "confidence".
+(define tv (SimpleTruthValue 0.1 0.2))
 
-; or lists of strings:
-(define s (StringValue "asdf" "gh" "jkl;"))
-
-; or lists of other values or atoms.  Thus, they can be heirarchical.
-(define l (LinkValue
-  (Concept "foobar") (StringValue "property") (FloatValue 42)))
-
-; A list of values can be converted into an ordinary scheme list:
-(cog-value->list f)
-(cog-value->list s)
-(cog-value->list l)
+; A truth value can be converted to a scheme list
+(cog-value->list tv)
 
 ; Alternately, individual elements in the list can be accessed directly.
 ; This behaves just like   (list-ref (cog-value->list VAL) NUM)
 ; but is computationally faster.
-(cog-value-ref f 2)
-(cog-value-ref s 0)
-(cog-value-ref l 1)
+(cog-value-ref tv 0)
+(cog-value-ref tv 1)
 
 ; Help for these functions can be viewed at the guile command line:
 ,d cog-value->list
 ,d cog-value-ref
 
-; Values can be attached to atoms:
+; TruthValues can be attached to atoms:
 (define a (Concept "some atom"))
-(define k1 (PredicateNode "first key"))
-(cog-set-value! a k1 f)
+(cog-set-tv! a tv)
 
-; The attached value can be fetched.
-(cog-value a k1)
+; The attached truth value can be fetched.
+(cog-tv a)
 
 ; The value can be changed ...
-(cog-set-value! a k1 l)
+(cog-set-tv! a (SimpleTruthValue 0.333 0.789))
 
 ; Verify that the value changed.
-(cog-value a k1)
-
-; Multipe values can be attached using different keys.
-(define k2 (PredicateNode "second key"))
-(cog-set-value! a k2 s)
-(cog-value a k2)
-
-; Verify that the value for the first key is still there.
-(cog-value a k1)
-
-; List all of the keys on the atom.
-(cog-keys a)
-
-; Add a truth value to the atom
-(cog-set-tv! a (stv 0.9 0.8))
-
-; List all of the keys on the atom.
-; Note that the tv is stored with a key, so that truth values
-; behave like other values.
-(cog-keys a)
-
-; Lets play with truth values. First, define the truth value key:
-(define ktv (PredicateNode "*-TruthValueKey-*"))
-
-; Verify that it works as expected; that is, this should return
-; exactly the same thing as (cog-tv a)
-(cog-value a ktv)
 (cog-tv a)
-(equal? (cog-value a ktv) (cog-tv a))
 
-; Truth Values are values, just like the rest. So are Attention Values:
-(define l2 (LinkValue
-  (stv 0.1 0.2) (stv 0.3 0.4) (Concept "foobar") (av 3 2 1) (av 4 5 0)))
+; The CountTruthValue is a sequence of three values.
+; By convention, these are
+;   the probability p, with 0 < p < 1.
+;   the entropy measured in bits, i.e. -log_2(p).
+;   the count, an integer.
+; Although the probability and entropy are obviously related, there is
+; no way to get the probability from the count, without referencing
+; some total count that has to be stored elsewhere. The CountTruthValue
+; does not actually force the entropy to be log2 of the probability.
 
-(cog-set-value! a k2 l2)
-(cog-value a k2)
+(cog-set-tv! a (CountTruthValue 1.0e-6 -19.9316 55))
 
-; Attention values are stored under a special key as well:
-(cog-set-av! a (av 3 2 1))
-(cog-keys a)
+; Attention Values are a different kind of sequence of floats
+; that can be attached to an atom.
+(cog-set-av! a (AttentionValue 3 2 1))
 
-; and can be accessed as values:
-(define kav (PredicateNode "*-AttentionValueKey-*"))
-(cog-value a kav)
+; The can be accessed as expected:
 (cog-av a)
-(equal? (cog-value a kav) (cog-av a))
 
 ; Example of use of `Values`, mixed with regular atoms.
 ; ProtoAtoms are similar to regular atoms, except that:
