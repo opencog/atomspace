1edeef7bc014b1fc7695a8e1eefd18cb322f3698
Linas Vepstas <linasvepstas@gmail.com>
2019-01-26 16:59:05 -0600

    Add documentation

--- examples/atomspace/formulas.scm
+++ examples/atomspace/formulas.scm
@@ -1,43 +1,67 @@
 ;
 ; formulas.scm -- Declaring formulas that compute truth values.
 ;
+; The rule engine, PLN and other subsystems need to be able to compute
+; and alter truth values. If those formulas are known a-priori, then
+; thay can be hard-coded into GroundedPredicateNodes.  However, it is
+; possible that those formulas are not yet known: that they will be
+; learned using some learning algorithm, for example, MOSES, or maybe
+; the Pattern Miner, or some neural network.
+;
+; In this case, the formulas need to be placed where they can be
+; accessed during computation: in the AtomSpace. The example below
+; shows how formulas can be declared in the AtomSpace, but in such a
+; way that they can also be evaluated to yeild an actual truth value,
+; which is then attached to some Atom.
+;
 (use-modules (opencog) (opencog exec))
 
-; Declare a very simplified example of a truth-value formula used by
-; PLN.  This merely multiplies a pair of strengths.
-
+; The StrengthOfLink returns a single floating-point number,
+; the strength of a TruthValue.
 (cog-execute! (StrengthOf (Concept "A" (stv 0.8 1.0))))
 
+; The demo needs at least one more Atom.
 (Concept "B" (stv 0.6 0.9))
 
+; Multiple the strength of the TV's of two atoms.
 (cog-execute!
-	(TimesLink (StrengthOf (Concept "A")) (StrengthOf (Concept "B"))))
+	(Times (StrengthOf (Concept "A")) (StrengthOf (Concept "B"))))
 
+; Create a SimpleTruthValue with a non-trivial formula:
+; It will be the TV := (1-sA*sB, cA*cB) where sA and sB are strenghts
+; and cA, cB are confidence values. The PredicateFormulaLink assembles
+; two floating-point values, and create a SimpleTruthValue out of them.
+;
 (cog-evaluate!
-	(PredicateFormulaLink
-		(TimesLink (StrengthOf (Concept "A")) (StrengthOf (Concept "B")))
-		(TimesLink (ConfidenceOf (Concept "A")) (ConfidenceOf (Concept "B")))))
+	(PredicateFormula
+		(Minus
+			(Number 1)
+			(Times (StrengthOf (Concept "A")) (StrengthOf (Concept "B"))))
+		(Times (ConfidenceOf (Concept "A")) (ConfidenceOf (Concept "B")))))
 
+; The values do not need to be formulas; tehy can be hard-coded numbers.
 (cog-evaluate!
-	(PredicateFormulaLink (Number 0.7) (Number 0.314)))
+	(PredicateFormula (Number 0.7) (Number 0.314)))
 
+; The below computes a truth value, and attaches it to the
+; EvaluationLink.
 (cog-evaluate!
-	(EvaluationLink
-		(PredicateFormulaLink (Number 0.7) (Number 0.314))
+	(Evaluation
+		(PredicateFormula (Number 0.7) (Number 0.314))
 		(List
 			(Concept "A")
 			(Concept "B"))))
 
 (cog-evaluate!
-	(EvaluationLink
+	(Evaluation
 		; Compute TV = (1-sA*sB, cA*cB)
-		(PredicateFormulaLink
-			(MinusLink
+		(PredicateFormula
+			(Minus
 				(Number 1)
-				(TimesLink
+				(Times
 					(StrengthOf (Variable "$X"))
 					(StrengthOf (Variable "$Y"))))
-			(TimesLink
+			(Times
 				(ConfidenceOf (Variable "$X"))
 				(ConfidenceOf (Variable "$Y"))))
 		(List
@@ -45,15 +69,15 @@
 			(Concept "B"))))
 
 (cog-evaluate!
-	(EvaluationLink
+	(Evaluation
 		; Compute TV = (1-sA*sB, cA*cB)
-		(PredicateFormulaLink
-			(Lambda (MinusLink
+		(PredicateFormula
+			(Lambda (Minus
 				(Number 1)
-				(TimesLink
+				(Times
 					(StrengthOf (Variable "$X"))
 					(StrengthOf (Variable "$Y")))))
-			(Lambda (TimesLink
+			(Lambda (Times
 				(ConfidenceOf (Variable "$X"))
 				(ConfidenceOf (Variable "$Y")))))
 		(List
