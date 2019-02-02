c771715dcd9911df526506a22e0fdce0fd9f6c17
Linas Vepstas <linasvepstas@gmail.com>
2019-02-01 21:36:55 -0600

    Extend the example a bit farther

--- examples/pattern-matcher/value-of.scm
+++ examples/pattern-matcher/value-of.scm
@@ -4,6 +4,15 @@
 ; A common task is to search for patterns that have a sufficiently
 ; large truth value or attention value (or some other value). The
 ; various `ValueOfLink`s can be used to apply such limits to searches.
+;
+; Useful `ValueOfLink`s include:
+;    `TruthValueOf`
+;    `StrengthOfLink`
+;    `ConfidenceOfLink`
+;    `AttentionValueOfLink`
+;    `STIOfLink`
+;    `LTIOfLink`
+;    `ValueOfLink`
 
 (use-modules (opencog) (opencog exec))
 
@@ -15,7 +24,7 @@
 ; a low truth value.
 (define find-false
 	(Bind
-		; Search only for concept nodes.
+		; Search only for ConceptNodes.
 		(TypedVariable (Variable "$X") (Type 'ConceptNode))
 		(And
 			(Present (Variable "$X"))
@@ -25,3 +34,26 @@
 
 ; Run it. Lo and Behold!
 (cog-execute! find-false)
+
+; --------------
+; Create some goofy numbers
+(define key (PredicateNode "some key"))
+
+(cog-set-value! (Concept "thing-a") key (FloatValue 42))
+(cog-set-value! (Concept "thing-b") key (FloatValue 35))
+
+(define find-answer
+	(Bind
+		; Search only for ConceptNodes.
+		(TypedVariable (Variable "$X") (Type 'ConceptNode))
+		(And
+			(Present (Variable "$X"))
+			; Divide by twelve, then see if its more than 3.
+			(GreaterThan
+				(Divide
+					(ValueOf (Variable "$X") key)
+					(Number 12))
+				(Number 3)))
+		(Variable "$X")))
+
+(cog-execute! find-answer)
