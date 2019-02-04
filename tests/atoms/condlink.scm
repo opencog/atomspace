d271c1fc1aa32d3115bc8ed24a8b72873bc946d4
kasim <se.kasim.ebrahim@gmail.com>
2019-02-04 14:21:41 +0300

    Add more tests
    Add test for list_link wrapped cond_link

--- tests/atoms/condlink.scm
+++ tests/atoms/condlink.scm
@@ -15,3 +15,35 @@
 	 (PlusLink (Number 1)(Number 2))
 	 (PlusLink (Number 1)(Number -2)))
 )
+
+(define nondefault
+	(CondLink
+   ; If this evaluates to true then the next expresion will execute.
+		(GreaterThanLink
+		 (PlusLink
+			(NumberNode 2)
+			(NumberNode 2))
+		 (TimesLink
+			(NumberNode 1)
+			(NumberNode 2)))
+		(PlusLink (Number 3)(Number 2))
+		(FalseLink )
+		(PlusLink (Number 1)(Number 2)))
+)
+
+(define listwrapped
+ (CondLink
+	(ListLink
+	 (GreaterThanLink
+		(NumberNode 3)
+		(NumberNode 4))
+	 (NumberNode 0))
+	(ListLink
+	 (GreaterThanLink
+		(NumberNode 4)
+		(NumberNode 3))
+	 (NumberNode 1))
+	(ListLink
+	 (TrueLink )
+	 (NumberNode 2)))
+)
