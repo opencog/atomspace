7f565dd742a45d67bde8c6b2f1d85aa71db82332
Linas Vepstas <linasvepstas@gmail.com>
2019-02-20 20:35:09 -0600

    Quick fix for #2094
    More work is needed  -- it really should return a ValuePtr.

--- opencog/cython/opencog/BindlinkStub.cc
+++ opencog/cython/opencog/BindlinkStub.cc
@@ -1,13 +1,18 @@
 #include <opencog/atoms/base/Handle.h>
 #include <opencog/atoms/base/Atom.h>
 #include <opencog/atomspace/AtomSpace.h>
+#include <opencog/atoms/execution/Instantiator.h>
 #include <opencog/atoms/value/Value.h>
 
 #include "BindlinkStub.h"
 
 using namespace opencog;
 
-Handle opencog::do_execute(AtomSpace* atomspace, Handle handle)
+// XXX FIXME -- signature should be
+// ValuePtr opencog::do_execute(AtomSpace* atomspace, Handle h)
+Handle opencog::do_execute(AtomSpace* atomspace, Handle h)
 {
-    return HandleCast(handle->execute(atomspace));
+	Instantiator inst(atomspace);
+	ValuePtr pap(inst.execute(h));
+	return HandleCast(pap);
 }
