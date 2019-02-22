00cdc48114e18d0705f6c50aa8c9fb3800d6931e
Linas Vepstas <linasvepstas@gmail.com>
2019-02-22 17:11:57 -0600

    Strip out unwanted stuff.

--- scripts/run_lcov.sh
+++ scripts/run_lcov.sh
@@ -2,20 +2,26 @@
 # This file is a wrapper for running cxxtests and creating per test coverage
 # results. It takes a long time, and tests added outside of the patched
 # ADD_CXXTEST won't be included.
-bindir=$1 # the root bin dir
+builddir=$1 # the root build dir
 name=$3 # name of the test (and executable)
 dir=$2 # dir the the test is built in
 echo "test $name" 1>&2
-cd $bindir
+cd $builddir
 mkdir -p coverage
 rm coverage/$name.info
 
-# is run in the BUILDDIR/tests directory so .. is the root
+# This is run in the BUILDDIR/tests directory so .. is the root
 lcov --directory . --zerocounter
 # run test
 $dir/$name
 ret_val=$?
 # capture coverage data to info file
 lcov --directory . --capture --output-file coverage/$name.info --test-name $name
+
+# strip out unwanted stuff
+# XXX FIXME the "*/build/*" should be some hacked $builddir
+lcov --remove coverage/$name.info "/usr*" -o coverage/$name.info
+lcov --remove coverage/$name.info "*/tests/*" -o coverage/$name.info
+lcov --remove coverage/$name.info "*/build/*" -o coverage/$name.info
 
 exit $ret_val
