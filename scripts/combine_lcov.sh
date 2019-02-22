d69aeb069e176b4c6825918ce6b27f55b8a6c493
Linas Vepstas <linasvepstas@gmail.com>
2019-02-22 17:32:08 -0600

    Just AtomSpace, not OpenCog

--- scripts/combine_lcov.sh
+++ scripts/combine_lcov.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This script is used to merge separate lcov output from each test.
 # Once they are in a single file, we create the html output with genhtml
+# XXX FIXME .. there is no reason to merge them. This just east CPU
+# time...
 BASEDIR=.
 rm -r ${BASEDIR}/lcov
 
@@ -11,7 +13,7 @@ done
 echo "*** Combining all lcov .info files into single file"
 echo Command is: lcov --directory ${BASEDIR} $ifiles --output-file ${BASEDIR}/coverage/alltemp.info
 lcov --directory ${BASEDIR} $ifiles --output-file ${BASEDIR}/coverage/alltemp.info
-echo "*** Removing coverage info for non-OpenCog files"
+echo "*** Removing coverage info for non-AtomSpace files"
 # Get the name of the current dir
 BUILD_DIR=${PWD##*/}
 echo Command is: lcov --directory ${BASEDIR} --output-file ${BASEDIR}/coverage/all.info \
@@ -25,8 +27,8 @@ lcov --directory ${BASEDIR} --output-file ${BASEDIR}/coverage/all.info \
 #rm ${BASEDIR}/coverage/alltemp.info
 
 echo "Creating lcov html summary"
-echo Command is: genhtml -s -o ${BASEDIR}/lcov --demangle-cpp --num-spaces 4 --title "OpenCog Coverage Analysis" ${BASEDIR}/coverage/all.info
-genhtml -s -o ${BASEDIR}/lcov --demangle-cpp --num-spaces 4 --title "AtomSpace Coverage Analysis" ${BASEDIR}/coverage/all.info
+echo Command is: genhtml -s -o ${BASEDIR}/lcov --demangle-cpp --num-spaces 4 --title "AtomSpace Coverage Analysis" ${BASEDIR}/coverage/all.info
+time genhtml -s -o ${BASEDIR}/lcov --demangle-cpp --num-spaces 4 --title "AtomSpace Coverage Analysis" ${BASEDIR}/coverage/all.info
 echo "Moving coverage files to ${BASEDIR}/coverage/old"
 mkdir -p ${BASEDIR}/coverage/old
 for f in ${BASEDIR}/coverage/*.info; do
