5b47b937b7cb1d03785042bf636ae0e2e90a5444
Linas Vepstas <linasvepstas@gmail.com>
2019-02-22 20:36:53 -0600

    test-coverage fixes

--- scripts/combine_lcov.sh
+++ scripts/combine_lcov.sh
@@ -12,7 +12,9 @@ for f in ${BASEDIR}/coverage/*.info; do
 done
 echo "*** Combining all lcov .info files into single file"
 echo Command is: lcov --directory ${BASEDIR} $ifiles --output-file ${BASEDIR}/coverage/alltemp.info
-lcov --directory ${BASEDIR} $ifiles --output-file ${BASEDIR}/coverage/alltemp.info
+time lcov --directory ${BASEDIR} $ifiles --output-file ${BASEDIR}/coverage/alltemp.info
+
+# ------------
 echo "*** Removing coverage info for non-AtomSpace files"
 # Get the name of the current dir
 BUILD_DIR=${PWD##*/}
@@ -20,13 +22,14 @@ echo Command is: lcov --directory ${BASEDIR} --output-file ${BASEDIR}/coverage/a
     --remove ${BASEDIR}/coverage/alltemp.info \
         /usr/include/\* \
         ${BUILD_DIR}/tests/\*
-time lcov --directory ${BASEDIR} --output-file ${BASEDIR}/coverage/all.info \
+lcov --directory ${BASEDIR} --output-file ${BASEDIR}/coverage/all.info \
     --remove ${BASEDIR}/coverage/alltemp.info \
         /usr/include/\* \
         ${BUILD_DIR}/tests/\*
-#rm ${BASEDIR}/coverage/alltemp.info
+rm ${BASEDIR}/coverage/alltemp.info
 
-echo "Creating lcov html summary"
+# ------------
+echo "*** Creating lcov html summary"
 echo Command is: genhtml -s -o ${BASEDIR}/lcov --demangle-cpp --num-spaces 4 --title "AtomSpace Coverage Analysis" ${BASEDIR}/coverage/all.info
 time genhtml -s -o ${BASEDIR}/lcov --demangle-cpp --num-spaces 4 --title "AtomSpace Coverage Analysis" ${BASEDIR}/coverage/all.info
 echo "Moving coverage files to ${BASEDIR}/coverage/old"
@@ -34,5 +37,5 @@ mkdir -p ${BASEDIR}/coverage/old
 for f in ${BASEDIR}/coverage/*.info; do
     mv $f ${BASEDIR}/coverage/old
 done
-echo "All done!"
-echo "Coverage reports are in ${BASEDIR}/lcov; use web browser to view."
+echo "*** All done!"
+echo "*** Coverage reports are in ${BASEDIR}/lcov; use web browser to view."
