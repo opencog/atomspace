ccfe831bc38ff36da9f524ca69994a56da18ca4b
Vitaly Bogdanov <vsbogd@gmail.com>
2019-03-06 15:53:40 +0300

    Unit test refactoring

--- tests/cython/atomspace/test_groundedobjectnode.py
+++ tests/cython/atomspace/test_groundedobjectnode.py
@@ -17,24 +17,27 @@ class GroundedObjectNodeTest(unittest.TestCase):
         finalize_opencog()
         del self.space
 
-    def test_call_grounded_object_predicate(self):
-        grounded_object_node = GroundedObjectNode("test_grounded_object_node",
-                                                  TestObject("some object"))
+    def test_call_grounded_object_call(self):
         exec_link = ApplyLink(
-                        MethodOfLink(grounded_object_node, ConceptNode("foo")),
-                        ListLink(ConceptNode("arg"))
+                        MethodOfLink(
+                            GroundedObjectNode("obj", TestObject("obj")),
+                            ConceptNode("get_argument")
+                        ),
+                        ListLink(
+                            ConceptNode("arg")
                         )
+                    )
 
         result = execute_atom(self.space,  exec_link)
 
         self.assertEqual(result, ConceptNode("arg"))
 
     def test_call_grounded_object_no_arguments(self):
-        grounded_object_node = GroundedObjectNode("test_grounded_object_node",
-                                                  TestObject("some object"))
         exec_link = ApplyLink(
-                        MethodOfLink(grounded_object_node,
-                                     ConceptNode("noarguments")),
+                        MethodOfLink(
+                            GroundedObjectNode("obj", TestObject("obj")),
+                            ConceptNode("no_arguments")
+                        ),
                         ListLink()
                     )
 
@@ -43,29 +46,29 @@ class GroundedObjectNodeTest(unittest.TestCase):
         self.assertEqual(result, ConceptNode("empty"))
 
     def test_call_grounded_object_predicate_two_args(self):
-        grounded_object_node = GroundedObjectNode("test_grounded_object_node",
-                                                  TestObject("some object"))
         exec_link = ApplyLink(
-                        MethodOfLink(grounded_object_node, ConceptNode("second")),
+                        MethodOfLink(
+                            GroundedObjectNode("obj", TestObject("obj")),
+                            ConceptNode("get_second")
+                        ),
                         ListLink(
-                            ConceptNode("firstArg"),
-                            ConceptNode("secondArg")
+                            ConceptNode("first"),
+                            ConceptNode("second")
                         )
                     )
 
         result = execute_atom(self.space,  exec_link)
 
-        self.assertEqual(result, ConceptNode("secondArg"))
+        self.assertEqual(result, ConceptNode("second"))
 
     def test_set_object(self):
-        grounded_object_node = GroundedObjectNode("test_grounded_object_node",
-                                                  TestObject("some object"))
+        grounded_object_node = GroundedObjectNode("a", TestObject("some object"))
         grounded_object_node.set_object(TestObject("other object"))
 
         self.assertEqual(grounded_object_node.get_object().name, "other object")
 
     def test_create_grounded_object_node_without_object(self):
-        grounded_object_node = GroundedObjectNode("test_grounded_object_node")
+        grounded_object_node = GroundedObjectNode("obj")
 
         self.assertTrue(grounded_object_node.get_object() is None)
 
@@ -74,13 +77,13 @@ class TestObject:
     def __init__(self, name):
         self.name = name
 
-    def foo(self, arg):
+    def get_argument(self, arg):
         return arg
 
-    def noarguments(self):
+    def no_arguments(self):
         return ConceptNode("empty")
 
-    def second(self, first, second):
+    def get_second(self, first, second):
         return second
 
 if __name__ == '__main__':
