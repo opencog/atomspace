883d7c8ade42771b898844b71ddfe72e3fefea58
Joel Pitt <joel@joelpitt.com>
2019-06-12 14:12:42 +1200

    Workaround for distutils bug in Ubuntu/Debian's Python3 versions

---
+++ scripts/get_python_lib.py
@@ -0,0 +1,24 @@
+import sys
+import sysconfig
+import site
+
+if __name__ == '__main__':
+    # This is a hack due to the distutils in debian/ubuntu's python3 being misconfigured
+    # see discussion https://github.com/opencog/atomspace/issues/1782
+    #
+    # If the bug is fixed, this script could be replaced by:
+    #
+    # from distutils.sysconfig import get_python_lib; print(get_python_lib(plat_specific=True, prefix=prefix))
+    
+    prefix = sys.argv[1]
+
+    # use sites if the prefix is recognized and the sites module is available
+    # (virtualenv is missing getsitepackages())
+    if hasattr(site, 'getsitepackages'):
+        paths = [p for p in site.getsitepackages() if p.startswith(prefix)]
+        if len(paths) == 1:
+            print(paths[0])
+            exit(0)
+    
+    # use sysconfig platlib as the fall back
+    print(sysconfig.get_paths()['platlib'])
