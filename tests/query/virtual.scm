3c652b1561bb22b602e9650104849e39db7098bd
Linas Vepstas <linasvepstas@gmail.com>
2019-06-13 15:30:00 -0500

    Extend the unit test some more

--- tests/query/virtual.scm
+++ tests/query/virtual.scm
@@ -34,6 +34,33 @@
 					(Inheritance (Variable "x") (Variable "y"))
 					(Not (Equal (Variable "y") (Concept "animal")))
 					(Not (Equal (Variable "y") (Concept "mammal")))
+					(Not (Equal (Variable "y") (Concept "cephalopod")))
+				))
+		))
+)
+
+(Inheritance (Concept "squid") (Concept "animal"))
+(Inheritance (Concept "squid") (Concept "cephalopod"))
+
+; Grounding this one requires the joining-together of two
+; disconnected sub-patterns, that only the SatisfactionLink
+; can perform. This should go down the same code as any
+; other multi-component search.
+(define get-mouse-pair
+	(GetLink (VariableList
+			(TypedVariable (Variable "x") (Type 'ConceptNode))
+			(TypedVariable (Variable "y") (Type 'ConceptNode)))
+		(And
+			(Inheritance (Variable "x") (Concept "animal"))
+			(Inheritance (Variable "y") (Concept "animal"))
+			(Not (Equal (Variable "x") (Variable "y")))
+
+			; Satisfiable, because they are both animals.
+			(Satisfaction (Variable "z")
+				(And
+					(Inheritance (Variable "x") (Variable "z"))
+					(Inheritance (Variable "y") (Variable "z"))
+					(Inheritance (Variable "x") (Concept "mammal"))
 				))
 		))
 )
