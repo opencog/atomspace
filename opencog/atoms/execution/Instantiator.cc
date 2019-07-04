2482e7299893bc277e1cd20e6a49a97f5363e2d1
Alexander Scherbatiy <stellarspot@gmail.com>
2019-07-04 16:53:18 +0300

    Propely handle variable scheme in ExecutionOutput

--- opencog/atoms/execution/Instantiator.cc
+++ opencog/atoms/execution/Instantiator.cc
@@ -116,6 +116,9 @@ Handle Instantiator::reduce_exout(const Handle& expr, bool silent)
 	Handle sn(eolp->get_schema());
 	Handle args(eolp->get_args());
 
+        if (not _vmap->empty())
+		sn = beta_reduce(sn, *_vmap);
+
 	// If its a DSN, obtain the correct body for it.
 	if (DEFINED_SCHEMA_NODE == sn->get_type())
 		sn = DefineLink::get_definition(sn);
