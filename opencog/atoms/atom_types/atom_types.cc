7ab983892447ba5f700d7758dff03b8ece2bbe36
Nil Geisweiller <ngeiswei@gmail.com>
2019-07-10 11:57:26 +0300

    Remove WIN32 and CYGWIN support as it is unmaintained

--- opencog/atoms/atom_types/atom_types.cc
+++ opencog/atoms/atom_types/atom_types.cc
@@ -7,33 +7,6 @@
  */
 
 // library initialization
-#if defined(WIN32) && defined(_DLL)
-namespace win {
-#include <windows.h>
-}
-
-win::BOOL APIENTRY DllMain(win::HINSTANCE hinstDLL,  // handle to DLL module
-                           win::DWORD fdwReason,     // reason for calling function
-                           win::LPVOID lpvReserved)  // reserved
-{
-    System::setModuleHandle(hinstDLL);
-    switch (fdwReason) {
-        case DLL_PROCESS_ATTACH:
-            #include INHERITANCE_FILE
-            #ifdef INHERITANCE_FILE2
-            #include INHERITANCE_FILE2
-            #endif
-            break;
-        case DLL_THREAD_ATTACH:
-            break;
-        case DLL_THREAD_DETACH:
-            break;
-        case DLL_PROCESS_DETACH:
-            break;
-    }
-    return TRUE;
-}
-#elif __GNUC__
 
 static __attribute__ ((constructor)) void init(void)
 {
@@ -53,8 +26,6 @@ static __attribute__ ((constructor)) void init(void)
 static __attribute__ ((destructor)) void fini(void)
 {
 }
-
-#endif
 
 extern "C" {
 // Calling this forces this shared-lib to load, thus calling the 
