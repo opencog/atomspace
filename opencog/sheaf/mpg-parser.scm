f29aea4ede9cc43aecdb31517de4934ccfb2d488
Linas Vepstas <linasvepstas@gmail.com>
2019-07-13 13:08:59 -0500

    refector the graphs

--- opencog/sheaf/mpg-parser.scm
+++ opencog/sheaf/mpg-parser.scm
@@ -35,17 +35,28 @@
 
 ; ---------------------------------------------------------------------
 
-(define-public (mpg-parse-atom-seq ATOM-LIST SCORE-FN NUM-LOOPS)
+(define-public (graph-add-mpg GRAPH ATOM-LIST SCORE-FN NUM-EDGES)
 "
-  Projective, Undirected Maximum Planar Graph parser.
+  Projective, Undirected Maximum Planar Graph (MPG) parser.
 
-  Given a sequence of atoms, find an unlabeled, undirected, projective
-  maximum spanning-tree parse. To this parse, add additional edges
-  until NUM-LOOPS have been created. The resulting graph is planar
-  (projective) in that no edges cross.
+  Given an existing GRAPH, add up to NUM-EDGES additional edges, such
+  that each added edge has the highest possible score, and no added
+  edge intersects any existing edge.  The non-intersection constraint
+  keeps the graph planar or "projective". If NUM-EDGES is set to -1,
+  then as many edges as possible are added, resulting in the maximal
+  planar graph.
+
+  The GRAPH should be an existing (possibly empty) list of 'wedges'
+  connecting Atom pairs. Each 'wedge' is a weighted pair of numbered
+  atoms, having the scheme form of `((NL . AL) (NR . AR) . W)` where
+  AL and AR are the left and right Atoms of the edge; NL and NR are
+  ordinal numbers (integers), such that NL is less than NR, and W is
+  a floating-point weight. The dot represents a scheme pair, built
+  with `cons`.
 
   The ATOM-LIST should be a scheme-list of atoms, all presumably of
-  a uniform atom type.
+  a uniform atom type. It should be ordered in the same way as the
+  the Atoms appearing in the 'wedges'.
 
   The SCORE-FN should be a function that, when give a left-right ordered
   pair of atoms, and the distance between them, returns a numeric score
@@ -54,26 +65,25 @@
   pair of atoms.  The SCORE-FN should take three arguments: left-atom,
   right-atom and the (numeric) distance between them (i.e. when the
   atoms are ordered sequentially, this is the difference between the
-  ordinal numbers).
+  ordinal numbers). If no such edge exists or is impossible to score,
+  then minus infinity should be returned; such edges will not be
+  considered. It is invoked as `(SCORE-FN Atom Atom Dist)`.
 
-  The NUM-LOOPS should be an integer, indicating the number of extra
-  edges to add to the MST tree. The highest-scoring edges are added
-  first, until either NUM-LOOPS edges have been added, or it is not
+  The NUM-EDGES should be an integer, indicating the number of extra
+  edges to add to the GRAPH. The highest-scoring edges are added
+  first, until either NUM-EDGES edges have been added, or it is not
   possible to add any more edges.  There are two reasons for not being
   able to add more edges: (1) there is no room or (2) no such edges are
   recorded in the AtomSpace. To iterate until maximum, pass -1 for
-  NUM-LOOPS.
+  NUM-EDGES.
 "
 	; Terminology:
 	; A "numa" is a numbered atom, viz a scheme-pair (number . atom)
 	; A wedge" is a weighted edge, having the form
 	;    ((left-numa . right-num) . weight).
 
-	; Start with the MST parse
-	(define mst-tree (mst-parse-atom-seq ATOM-LIST SCORE-FN))
-
 	; The the list of nodes in it.
-	(define node-list (sort-numalist (numas-in-wedge-list mst-tree)))
+	(define node-list (sort-numalist (numas-in-wedge-list GRAPH)))
 
 	; Define a losing score.
 	(define min-acceptable-mi -1e15)
@@ -107,7 +117,7 @@
 	)
 
 	; A candidate list of links to add.
-	(define candidates (non-intersecting-links node-list mst-tree))
+	(define candidates (non-intersecting-links node-list GRAPH))
 
 	; Candidates sorted by weight
 	(define sorted-cands
@@ -126,7 +136,43 @@
 				(add-link NED (cdr CANDS) RSLT)
 				(add-link (- NED 1) (cdr CANDS) (cons (car CANDS) RSLT)))))
 
-	(add-link NUM-LOOPS sorted-cands mst-tree)
+	(add-link NUM-EDGES sorted-cands GRAPH)
+)
+
+; ---------------------------------------------------------------------
+
+(define-public (mpg-parse-atom-seq ATOM-LIST SCORE-FN NUM-LOOPS)
+"
+  Projective, Undirected Maximum Planar Graph parser.
+
+  Given a sequence of atoms, find an unlabeled, undirected, projective
+  maximum spanning-tree parse. To this parse, add additional edges
+  until NUM-LOOPS have been created. The resulting graph is planar
+  (projective) in that no edges cross.
+
+  The ATOM-LIST should be a scheme-list of atoms, all presumably of
+  a uniform atom type.
+
+  The SCORE-FN should be a function that, when give a left-right ordered
+  pair of atoms, and the distance between them, returns a numeric score
+  for that pair. This numeric score will be maximized during the parse.
+  The most basic choice is to use the mutual information between the
+  pair of atoms.  The SCORE-FN should take three arguments: left-atom,
+  right-atom and the (numeric) distance between them (i.e. when the
+  atoms are ordered sequentially, this is the difference between the
+  ordinal numbers).
+
+  The NUM-LOOPS should be an integer, indicating the number of extra
+  edges to add to the MST tree. The highest-scoring edges are added
+  first, until either NUM-LOOPS edges have been added, or it is not
+  possible to add any more edges.  There are two reasons for not being
+  able to add more edges: (1) there is no room or (2) no such edges are
+  recorded in the AtomSpace. To iterate until maximum, pass -1 for
+  NUM-LOOPS.
+"
+	; Start with the MST parse
+	(define mst-tree (mst-parse-atom-seq ATOM-LIST SCORE-FN))
+	(graph-add-mpg mst-tree ATOM-LIST SCORE-FN NUM-LOOPS)
 )
 
 ; ---------------------------------------------------------------------
