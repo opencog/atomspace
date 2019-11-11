e59285d49e21f68d46a6d883b8d25046211ea2ee
Linas Vepstas <linasvepstas@gmail.com>
2019-11-11 17:12:06 -0600

    Extend the unit test to check the reported bug.

---
+++ tests/query/unordered-arcane.scm
@@ -0,0 +1,46 @@
+;
+; Test for infinite loop caused in #2357 fixed in #2360
+; The stuff in here is kind of crazy, but all if it is needed
+; to provoke the bug.
+
+(use-modules (opencog) (opencog exec) (opencog logger))
+; (cog-logger-set-level! "fine")
+; (cog-logger-set-stdout! #t)
+
+; Target graph to be found
+(ListLink
+	(OrderedLink (VariableList (VariableNode "$W"))
+		(PresentLink
+			(InheritanceLink (VariableNode "$W") (Concept "A"))
+			(InheritanceLink (Concept "A") (Concept "B"))))
+	(NumberNode "5.000000"))
+
+; This is never matched, but needs to be present to trigger
+; several explores in `do_term_up()`
+(AndLink
+	(InheritanceLink (VariableNode "$W") (Concept "A"))
+	(InheritanceLink (Concept "A") (Concept "B")))
+
+; Query to run.
+; The Quote...Unquote is needed to trigger the bug; removing
+; the quotes hides the bug.  Note that in the original bug report,
+; the OrderedLink was a LambdaLink.
+(define query
+	(GetLink (PresentLink
+
+		; Matches the target graph
+		(ListLink (QuoteLink
+			(OrderedLink (UnquoteLink (VariableNode "$f-vardecl"))
+				(PresentLink (UnquoteLink (VariableNode "$cnj-bodies-1"))
+					(UnquoteLink (VariableNode "$cnj-bodies-0")))))
+			(VariableNode "$ms-0"))
+
+		; Also matches the target graph
+		(ListLink (QuoteLink
+			(OrderedLink (UnquoteLink (VariableNode "$f-vardecl"))
+				(PresentLink (UnquoteLink (VariableNode "$cnj-bodies-1"))
+					(UnquoteLink (VariableNode "$cnj-bodies-0")))))
+			(VariableNode "$ms-1")))))
+
+; Run the query.
+; (cog-execute! query)
