0e23ea24e1eda3f72352968aa80d1d3d4ebc7754
Linas Vepstas <linasvepstas@gmail.com>
2019-11-23 12:52:56 -0600

    Add the large jswiergo unit test from #148

--- tests/query/unordered-jswiergo.scm
+++ tests/query/unordered-jswiergo.scm
@@ -6,10 +6,10 @@
 ;
 (use-modules (opencog) (opencog exec))
 
-(define shared (Similarity (Concept "A")))
+(define shared (Unordered (Concept "A")))
 (define tree (List shared (List shared (Concept "B"))))
 
-(define query1 (Similarity (Variable "$a")))
+(define query1 (Unordered (Variable "$a")))
 (define query2 (Variable "$a"))
 
 (define (tree-query input-query)
@@ -26,4 +26,74 @@
 ; (cog-execute! tree-query2)
 
 (define expect-q1 (Set (List (Concept "A") (Concept "B"))))
-(define expect-q2 (Set (List (Similarity (Concept "A")) (Concept "B"))))
+(define expect-q2 (Set (List (Unordered (Concept "A")) (Concept "B"))))
+
+; -------------------------------------------------------------------
+; Similar structure, but more complicated
+
+(define big-tree1
+	(List
+		(Unordered (Concept "X"))
+		(Unordered (Concept "A1") (Concept "B1"))
+		(List
+			(Unordered (Concept "X"))
+			(Unordered (Concept "B1") (Concept "A1")))))
+
+
+(define big-tree2
+	(List
+		(Unordered (Concept "X"))
+		(Unordered (Concept "A2") (Concept "B2"))
+		(List
+			(Unordered (Concept "X"))
+			(Unordered (Concept "B2") (Concept "A2")))))
+
+
+(define big-tree3
+	(List
+		(Unordered (Concept "X"))
+		(Unordered (Concept "A3") (Concept "B3"))
+		(List
+			(Unordered (Concept "X"))
+			(Unordered (Concept "B3") (Concept "A3")))))
+
+
+(define (big-tree-query input-query)
+	(Bind
+		(VariableList (Variable "$a") (Variable "$b") (Variable "$c"))
+		(Present
+			(List
+				input-query
+				(Unordered (Variable "$b") (Variable "$c"))
+				(List
+					input-query
+					(Unordered (Variable "$b") (Variable "$c")))))
+		(List (Variable "$a") (Variable "$b") (Variable "$c"))))
+
+(define big-tree-q1 (big-tree-query query1))
+(define big-tree-q2 (big-tree-query query2))
+
+; (cog-execute! big-tree-q1)
+; (cog-execute! big-tree-q2)
+
+(define big-expect1
+	(Set
+		(List (Concept "X") (Concept "A1") (Concept "B1"))
+		(List (Concept "X") (Concept "A2") (Concept "B2"))
+		(List (Concept "X") (Concept "A3") (Concept "B3"))
+		(List (Concept "X") (Concept "B1") (Concept "A1"))
+		(List (Concept "X") (Concept "B2") (Concept "A2"))
+		(List (Concept "X") (Concept "B3") (Concept "A3"))
+	))
+
+(define big-expect2
+	(Set
+		(List (Unordered (Concept "X")) (Concept "A1") (Concept "B1"))
+		(List (Unordered (Concept "X")) (Concept "A2") (Concept "B2"))
+		(List (Unordered (Concept "X")) (Concept "A3") (Concept "B3"))
+		(List (Unordered (Concept "X")) (Concept "B1") (Concept "A1"))
+		(List (Unordered (Concept "X")) (Concept "B2") (Concept "A2"))
+		(List (Unordered (Concept "X")) (Concept "B3") (Concept "A3"))
+	))
+
+*unspecified*
