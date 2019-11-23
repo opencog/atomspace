37fe3f44f7b2adc1fcf20b1ab60f5669dfc16d73
Linas Vepstas <linasvepstas@gmail.com>
2019-11-23 11:54:06 -0600

    Another odometer test.

--- tests/query/unordered-odo-equpr.scm
+++ tests/query/unordered-odo-equpr.scm
@@ -1,14 +1,29 @@
 ;
-; unordered-odo-equal.scm
+; unordered-odo-equpr.scm
 ;
 ; Multiple unordered links, arragned in series (so that the odometer
 ; runs) but then constrained so that neighboring terms must be
 ; identical.  This sharply limits the possible orderings.
 ; See also unordered-odo-couple.scm for same test case, but with
-; direct constraints. See also unordered-odo-equpr.scm for the
-; same test, but with equality predicates.
+; direct constraints.  See unordered-odo-equal.scm for the same test,
+; but with virtual EqualLink.
 ;
 (use-modules (opencog) (opencog exec))
+
+(Evaluation (Predicate "equal") (List (Concept "L") (Concept "L")))
+(Evaluation (Predicate "equal") (List (Concept "M") (Concept "M")))
+(Evaluation (Predicate "equal") (List (Concept "N") (Concept "N")))
+(Evaluation (Predicate "equal") (List (Concept "P") (Concept "P")))
+(Evaluation (Predicate "equal") (List (Concept "Q") (Concept "Q")))
+(Evaluation (Predicate "equal") (List (Concept "R") (Concept "R")))
+(Evaluation (Predicate "equal") (List (Concept "S") (Concept "S")))
+(Evaluation (Predicate "equal") (List (Concept "T") (Concept "T")))
+(Evaluation (Predicate "equal") (List (Concept "U") (Concept "U")))
+(Evaluation (Predicate "equal") (List (Concept "V") (Concept "V")))
+(Evaluation (Predicate "equal") (List (Concept "W") (Concept "W")))
+(Evaluation (Predicate "equal") (List (Concept "X") (Concept "X")))
+(Evaluation (Predicate "equal") (List (Concept "Y") (Concept "Y")))
+(Evaluation (Predicate "equal") (List (Concept "Z") (Concept "Z")))
 
 ; ----------------------------------------------------
 ; Coupled sets, expect 2! * 2! = 4 permutations
@@ -17,18 +32,19 @@
 	(Set (Concept "P") (Concept "Q") (Concept "R"))
 	(Set (Concept "R") (Concept "S") (Concept "T")))
 
-(define equ-dim-two
+(define epr-dim-two
 	(Bind
 		(And
 			(Present (List (Variable "$CPT")
 				(Set (Variable "$U") (Variable "$V") (Variable "$W"))
 				(Set (Variable "$X") (Variable "$Y") (Variable "$Z"))))
-			(Equal (Variable "$W") (Variable "$X")))
+			(Present (Evaluation (Predicate "equal")
+				(List (Variable "$W") (Variable "$X")))))
 		(Associative
 			(Variable "$U") (Variable "$V") (Variable "$W")
 			(Variable "$X") (Variable "$Y") (Variable "$Z"))))
 
-; (cog-execute! equ-dim-two)
+; (cog-execute! epr-dim-two)
 
 ; ----------------------------------------------------
 ; Like above, but 2! * 1! * 2! = 4 permutations
@@ -38,21 +54,23 @@
 	(Set (Concept "R") (Concept "S") (Concept "T"))
 	(Set (Concept "T") (Concept "U") (Concept "V")))
 
-(define equ-dim-three
+(define epr-dim-three
 	(Bind
 		(And
 			(Present (List (Variable "$CPT")
 				(Set (Variable "$A") (Variable "$B") (Variable "$C"))
 				(Set (Variable "$U") (Variable "$V") (Variable "$W"))
 				(Set (Variable "$X") (Variable "$Y") (Variable "$Z"))))
-			(Equal (Variable "$C") (Variable "$U"))
-			(Equal (Variable "$W") (Variable "$X")))
+			(Present (Evaluation (Predicate "equal")
+				(List (Variable "$C") (Variable "$U"))))
+			(Present (Evaluation (Predicate "equal")
+				(List (Variable "$W") (Variable "$X")))))
 		(Associative
 			(Variable "$A") (Variable "$B") (Variable "$C")
 			(Variable "$U") (Variable "$V") (Variable "$W")
 			(Variable "$X") (Variable "$Y") (Variable "$Z"))))
 
-; (cog-execute! equ-dim-three)
+; (cog-execute! epr-dim-three)
 
 ; ----------------------------------------------------
 ; Like above, but 2*1*1*2 = 4 permutations
@@ -63,7 +81,7 @@
    (Set (Predicate "T") (Predicate "U") (Predicate "V"))
    (Set (Predicate "V") (Predicate "W") (Predicate "X")))
 
-(define equ-dim-four
+(define epr-dim-four
 	(Bind
 		(And
 			(Present (List (Variable "$CPT")
@@ -71,15 +89,18 @@
 				(Set (Variable "$D") (Variable "$E") (Variable "$F"))
 				(Set (Variable "$U") (Variable "$V") (Variable "$W"))
 				(Set (Variable "$X") (Variable "$Y") (Variable "$Z"))))
-			(Equal (Variable "$C") (Variable "$D"))
-			(Equal (Variable "$F") (Variable "$U"))
-			(Equal (Variable "$W") (Variable "$X")))
+			(Present (Evaluation (Predicate "equal")
+				(List (Variable "$C") (Variable "$D"))))
+			(Present (Evaluation (Predicate "equal")
+				(List (Variable "$F") (Variable "$U"))))
+			(Present (Evaluation (Predicate "equal")
+				(List (Variable "$W") (Variable "$X")))))
 		(Associative
 			(Variable "$A") (Variable "$B") (Variable "$C")
 			(Variable "$D") (Variable "$E") (Variable "$F")
 			(Variable "$U") (Variable "$V") (Variable "$W")
 			(Variable "$X") (Variable "$Y") (Variable "$Z"))))
 
-; (cog-execute! equ-dim-four)
+; (cog-execute! epr-dim-four)
 
 ; ----------------------------------------------------
