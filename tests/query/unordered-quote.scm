50813bf64bacf3bd9383c7838fc705a750513911
Linas Vepstas <linasvepstas@gmail.com>
2019-11-28 18:58:10 -0600

    iRename unit test, now that we know it's root cause.

--- tests/query/unordered-quote.scm
+++ tests/query/unordered-quote.scm
@@ -1,7 +1,15 @@
 ;
-; unordered-arcane.scm
+; unordered-quote.scm
 ;
-; Test for infinite loop caused in #2357 fixed in #2360
+; Test for infinite loop caused in #2357; hacked around in #2360;
+; proper fix in #23real-soon-now.
+;
+; The root cause of the bug is the funky handling of quotations in
+; `PatternMatchEngine::record_grounding()` where the recorded grounding
+; does not match up with what the pattern matcher thought it was looking
+; for, thus confusing the unordered-link traverser into either exploring
+; twice (inf loop), or missing a permutation.
+;
 ; The stuff in here looks kind of crazy, but all of it is needed
 ; to provoke the bug. Simplifying further makes the bug go away.
 
