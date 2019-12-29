d22ca8641ffa115bf2758502cd46d4e735d864e8
Linas Vepstas <linasvepstas@gmail.com>
2019-12-29 01:05:42 -0600

    Remove copy concstructor from class Link

--- opencog/atoms/base/Link.cc
+++ opencog/atoms/base/Link.cc
@@ -37,23 +37,6 @@
 
 using namespace opencog;
 
-void Link::init(const HandleSeq& outgoingVector)
-{
-    if (not nameserver().isA(_type, LINK)) {
-        throw InvalidParamException(TRACE_INFO,
-            "Link ctor: Atom type is not a Link: '%d' %s.",
-            _type, nameserver().getTypeName(_type).c_str());
-    }
-
-    // Yes, people actually send us bad data.
-    for (const Handle& h: outgoingVector)
-        if (nullptr == h)
-            throw InvalidParamException(TRACE_INFO,
-                "Link ctor: invalid outgoing set!");
-
-    _outgoing = outgoingVector;
-}
-
 void Link::init(const HandleSeq&& outgoingVector)
 {
     if (not nameserver().isA(_type, LINK)) {
