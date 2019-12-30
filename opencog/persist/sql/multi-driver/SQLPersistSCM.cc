27a5908a80a5756ea4a83085e03c550bea52857f
Linas Vepstas <linasvepstas@gmail.com>
2019-12-30 13:54:04 -0600

    Start work on a create-db function.

--- opencog/persist/sql/multi-driver/SQLPersistSCM.cc
+++ opencog/persist/sql/multi-driver/SQLPersistSCM.cc
@@ -61,6 +61,7 @@ void SQLPersistSCM::init_in_module(void* data)
 
 void SQLPersistSCM::init(void)
 {
+    define_scheme_primitive("sql-create", &SQLPersistSCM::do_create, this, "persist-sql");
     define_scheme_primitive("sql-open", &SQLPersistSCM::do_open, this, "persist-sql");
     define_scheme_primitive("sql-close", &SQLPersistSCM::do_close, this, "persist-sql");
     define_scheme_primitive("sql-stats", &SQLPersistSCM::do_stats, this, "persist-sql");
@@ -73,6 +74,27 @@ void SQLPersistSCM::init(void)
 SQLPersistSCM::~SQLPersistSCM()
 {
     if (_backing) delete _backing;
+}
+
+void SQLPersistSCM::do_create(const std::string& uri)
+{
+// xxxx this is borken.
+    // Use the postgres driver.
+    SQLAtomStorage *store = new SQLAtomStorage(uri);
+    if (!store)
+        throw RuntimeException(TRACE_INFO,
+            "sql-open: Error: Unable to open the database");
+
+    if (!store->connected())
+    {
+        delete store;
+        throw RuntimeException(TRACE_INFO,
+            "sql-open: Error: Unable to connect to the database");
+    }
+
+    store->create_tables();
+
+    delete store;
 }
 
 void SQLPersistSCM::do_open(const std::string& uri)
