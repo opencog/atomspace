09c88bf23c4654da7c6d4978e02131c2b24afdb1
Linas Vepstas <linasvepstas@gmail.com>
2020-02-19 16:53:16 -0600

    More mucking with rvalues

--- opencog/atomspace/AtomSpace.h
+++ opencog/atomspace/AtomSpace.h
@@ -152,7 +152,7 @@ public:
      * \param t     Type of the node
      * \param name  Name of the node
      */
-    Handle add_node(Type t, const std::string& name="");
+    Handle add_node(Type t, std::string name="");
 
     /**
      * Add a link to the Atom Table. If the atom already exists, then
@@ -162,7 +162,7 @@ public:
      * @param outgoing  a const reference to a HandleSeq containing
      *                  the outgoing set of the link
      */
-    Handle add_link(Type t, const HandleSeq& outgoing);
+    Handle add_link(Type, HandleSeq);
 
     inline Handle add_link(Type t)
     {
@@ -376,9 +376,9 @@ public:
      * @param t     Type of the node
      * @param str   Name of the node
     */
-    Handle get_node(Type t, const std::string& name="");
-    inline Handle get_handle(Type t, const std::string& str) {
-        return get_node(t, str);
+    Handle get_node(Type, std::string = "");
+    inline Handle get_handle(Type t, std::string str) {
+        return get_node(t, std::move(str));
     }
 
     /**
@@ -394,7 +394,7 @@ public:
      * @param outgoing a reference to a HandleSeq containing
      *        the outgoing set of the link.
     */
-    Handle get_link(Type t, const HandleSeq& outgoing);
+    Handle get_link(Type, HandleSeq);
     inline Handle get_link(Type t, const Handle& ha) {
         return get_link(t, HandleSeq({ha}));
     }
@@ -407,8 +407,8 @@ public:
     Handle get_link(Type t, const Handle& ha, const Handle& hb, const Handle& hc, const Handle& hd) {
         return get_link(t, {ha, hb, hc, hd});
     }
-    Handle get_handle(Type t, const HandleSeq& outgoing) {
-        return get_link(t, outgoing);
+    Handle get_handle(Type t, HandleSeq outgoing) {
+        return get_link(t, std::move(outgoing));
     }
     Handle get_handle(Type t, const Handle& ha) {
 	    return get_handle(t, HandleSeq({ha}));
