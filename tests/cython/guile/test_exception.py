b7cbe3a1c260ea7407f1bc11b4f89bc263685d01
Linas Vepstas <linasvepstas@gmail.com>
2020-03-02 21:12:36 -0600

    Move guile tests to guile directory

--- tests/cython/guile/test_exception.py
+++ tests/cython/guile/test_exception.py
@@ -2,9 +2,9 @@ import unittest
 from opencog.utilities import initialize_opencog, finalize_opencog
 from opencog.type_constructors import *
 from opencog.bindlink import evaluate_atom
+from opencog.scheme_wrapper import scheme_eval
 
 import __main__
-
 
 # All of these tests try to make sure that python doesn't
 # crash when a C++ exception is thrown.
@@ -18,26 +18,24 @@ class TestExceptions(unittest.TestCase):
         finalize_opencog()
         del self.space
 
-    def test_bogus_get(self):
-        atom1 = ConceptNode("atom1")
+    def test_bogus_scheme(self):
         try:
-           GetLink(atom1, atom1, atom1)
+           code = '''(Get (Concept "a") (Concept "a") (Concept "a"))'''
+           scheme_eval(self.space, code)
            self.assertFalse("call should fail")
         except RuntimeError as e:
            # Use `nosetests3 --nocapture` to see this print...
            print("The exception message is " + str(e))
            self.assertTrue("Expecting" in str(e))
 
-    def test_bogus_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:foobar"),
-                                        atom1, atom1, atom1)
+    def test_bogus_path(self):
         try:
-           evaluate_atom(self.space, eval_link)
+           code = '''(load-from-path "/blargle/Betelgeuse")'''
+           scheme_eval(self.space, code)
            self.assertFalse("call should fail")
         except RuntimeError as e:
            # Use `nosetests3 --nocapture` to see this print...
            print("The exception message is " + str(e))
-           self.assertTrue("not found in module" in str(e))
+           self.assertTrue("Unable to find" in str(e))
 
 # ===================== END OF FILE =================
