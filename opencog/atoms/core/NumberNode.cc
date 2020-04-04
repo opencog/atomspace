a957aa2914b5aa5acb67d58909bb62b15522f471
Linas Vepstas <linasvepstas@gmail.com>
2020-04-04 16:04:17 -0500

    Revert to earlier form of scalar addition.
    The original idea was correct; creating variable-length to perform
    scalar addtion is just too difficult.

--- opencog/atoms/core/NumberNode.cc
+++ opencog/atoms/core/NumberNode.cc
@@ -206,10 +206,20 @@ ValuePtr opencog::plus(const ValuePtr& vi, const ValuePtr& vj, bool silent)
 		return plus(NumberNodeCast(vi), NumberNodeCast(vj));
 
 	if (NUMBER_NODE == vitype and nameserver().isA(vjtype, FLOAT_VALUE))
-		return plus(NumberNodeCast(vi), FloatValueCast(vj));
+	{
+		NumberNodePtr nn(NumberNodeCast(vi));
+		if (1 == nn->size())
+			return plus(nn->get_value(), FloatValueCast(vj));
+		return plus(nn, FloatValueCast(vj));
+	}
 
 	if (nameserver().isA(vitype, FLOAT_VALUE) and NUMBER_NODE == vjtype)
-		return plus(FloatValueCast(vi), NumberNodeCast(vj));
+	{
+		NumberNodePtr nn(NumberNodeCast(vj));
+		if (1 == nn->size())
+			return plus(nn->get_value(), FloatValueCast(vi));
+		return plus(nn, FloatValueCast(vi));
+	}
 
 	if (nameserver().isA(vitype, FLOAT_VALUE) and
 		 nameserver().isA(vjtype, FLOAT_VALUE))
@@ -232,10 +242,20 @@ ValuePtr opencog::minus(const ValuePtr& vi, const ValuePtr& vj, bool silent)
 		return minus(NumberNodeCast(vi), NumberNodeCast(vj));
 
 	if (NUMBER_NODE == vitype and nameserver().isA(vjtype, FLOAT_VALUE))
-		return minus(NumberNodeCast(vi), FloatValueCast(vj));
+	{
+		NumberNodePtr nn(NumberNodeCast(vi));
+		if (1 == nn->size())
+			return minus(nn->get_value(), FloatValueCast(vj));
+		return minus(nn, FloatValueCast(vj));
+	}
 
 	if (nameserver().isA(vitype, FLOAT_VALUE) and NUMBER_NODE == vjtype)
-		return minus(FloatValueCast(vi), NumberNodeCast(vj));
+	{
+		NumberNodePtr nn(NumberNodeCast(vj));
+		if (1 == nn->size())
+			return minus(FloatValueCast(vi), nn->get_value());
+		return minus(FloatValueCast(vi), nn);
+	}
 
 	if (nameserver().isA(vitype, FLOAT_VALUE) and
 		 nameserver().isA(vjtype, FLOAT_VALUE))
