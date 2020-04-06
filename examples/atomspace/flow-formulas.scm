5aeaf7a30ba6fa6563c9affe7aa435743f3c7435
Linas Vepstas <linasvepstas@gmail.com>
2020-04-05 23:44:41 -0500

    More work on the example

--- examples/atomspace/flow-formulas.scm
+++ examples/atomspace/flow-formulas.scm
@@ -33,6 +33,7 @@
 (cog-evaluate! evlnk)
 (cog-tv evlnk)
 
+; ----------
 ; The FormulaTruthValue is a kind of SimpleTruthValue, such that, every
 ; time that it is accessed, the current value -- that is, the current
 ; pair of floating point numbers -- is recomputed.  The recomputation
@@ -58,6 +59,38 @@
 
 (cog-set-tv! (Concept "B") (stv 0.314159 0.9))
 (display tv-stream) (newline)
+
+; ----------
+; This new kind of TV becomes interesting when it is used to
+; automatically maintain the TV of some relationship. Suppose
+; that A implied B, and the truth-probability of this is given
+; by the formula above. So, first we write the implication:
+
+(define a-implies-b (Implication (Concept "A") (Concept "B")))
+
+; ... and then attach this auto-updating TV to it.
+(cog-set-tv! a-implies-b tv-stream)
+
+; Lets take a look at it's current value:
+(format #t "A implies B has strength ~6F and confidence ~6F\n"
+	(cog-mean a-implies-b) (cog-confidence a-implies-b))
+
+; Change the TV on A and B ...
+(cog-set-tv! (Concept "A") (stv 0.4 0.2))
+(cog-set-tv! (Concept "B") (stv 0.7 0.8))
+
+; ... and the TV on the implication stays current.
+; Note that a different API is demoed below.
+(format #t "A implies B has strength ~6F and confidence ~6F\n"
+	(cog-tv-mean (cog-tv a-implies-b))
+	(cog-tv-confidence (cog-tv a-implies-b)))
+
+; ----------
+; So far, the above is using a lot of scheme scaffolding to accomplish
+; the setting of truth values.
+(SetTV
+	(Implication (Concept "A") (Concept "B"))
+	(FormulaTruthValue evlnk))
 
 ; -------------------------------------------------------------
 ; The FormulaStream is the generalization of FormulaTruthValue, suitable
