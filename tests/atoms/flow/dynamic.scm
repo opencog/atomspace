75a389a79e124e335e74316b443cd1bff3de7364
Linas Vepstas <linasvepstas@gmail.com>
2020-04-07 00:59:48 -0500

    Finish with the unit test.

--- tests/atoms/flow/dynamic.scm
+++ tests/atoms/flow/dynamic.scm
@@ -4,6 +4,7 @@
 
 (use-modules (opencog) (opencog exec))
 
+; For DynamicUTest::test_formula_tv()
 (DefineLink
    (DefinedPredicate "has a reddish color")
    (PredicateFormula
@@ -27,6 +28,8 @@
 (define B (Concept "B"))
 
 ; ----------
+; for DynamicUTest::test_dynamic_formula()
+
 (define a-implies-b (Implication (Concept "A") (Concept "B")))
 
 (cog-execute!
@@ -54,6 +57,8 @@
          (ConfidenceOf (Variable "$X"))
          (ConfidenceOf (Variable "$Y")))))
 
+; -----------
+; For DynamicUTest::test_defined_dynamic()
 (define p-implies-q (Implication (Concept "P") (Concept "Q")))
 (cog-execute!
 	(SetTV
@@ -62,21 +67,15 @@
 		(List (Concept "A") (Concept "B"))))
 
 ; -------------------------------------------------------------
+; for DynamicUTest::test_formula_stream()
 (define foo (Concept "foo"))
 (define bar (Concept "bar"))
 (define akey (Predicate "some key"))
 (define bkey (Predicate "other key"))
 
-(cog-set-value! foo akey (RandomStream 5))
-
-; Take a look at what was created.
-(cog-value foo akey)
-(cog-value->list (cog-value foo akey))
+(cog-set-value! foo akey (FloatValue 1 2 3 4 5))
 
 (define fstream (FormulaStream (Plus (Number 10) (ValueOf foo akey))))
-
 (cog-set-value! bar bkey fstream)
-(cog-value bar bkey)
-(cog-value->list (cog-value bar bkey))
 
 ; ------- THE END -------
