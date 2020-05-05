7f36eb04d314861f4d35882fcbd7f7cbf5d9bd87
Linas Vepstas <linasvepstas@gmail.com>
2020-05-05 11:02:47 -0500

    Remove the temp hack for the pattern miner

--- opencog/query/Satisfier.h
+++ opencog/query/Satisfier.h
@@ -99,10 +99,7 @@ class SatisfyingSet :
 	protected:
 		AtomSpace* _as;
 		HandleSeq _varseq;
-// XXX fixme tempt hack for pattern miner
-public:
 		HandleSet _satisfying_set;
-protected:
 		QueueValuePtr _result_queue;
 
 	public:
