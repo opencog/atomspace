f2e7941aa5ff2d1767f0c20172370325122b41c5
Linas Vepstas <linasvepstas@gmail.com>
2020-05-06 17:21:01 -0500

    Add unit test for the LambdaLink bug-fix.

---
+++ tests/query/eval-lambda.scm
@@ -0,0 +1,16 @@
+;
+; Verify that LambdaLink is handed properly, per discussion
+; on mailing list.
+
+(use-modules (opencog) (opencog exec))
+
+(State (Concept "stop light") (Concept "red light"))
+
+(define is-red-light
+	(Evaluation 
+		(Lambda
+			(VariableList (Variable "a") (Variable "b"))
+			(Equal
+				(Set (Variable "a"))
+				(Get (Variable "x") (State (Variable "b") (Variable "x")))))
+		(List (Concept "red light") (Concept "stop light"))))
