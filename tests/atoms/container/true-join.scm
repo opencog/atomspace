03caa22431254a8984db207b259b96f90010691c
Linas Vepstas <linasvepstas@gmail.com>
2020-05-09 20:26:51 -0500

    Extend the unit test.

--- tests/atoms/container/true-join.scm
+++ tests/atoms/container/true-join.scm
@@ -3,13 +3,21 @@
 ; JoinLink unit test. Performs an actual join, for real,
 ; i.e. an actual set intersection of the filters.
 
+(use-modules (opencog) (opencog exec))
+
 ; Data
 (Evaluation (Predicate "Pa") (List (Concept "A")))
 (Evaluation (Predicate "Pab") (List (Concept "A") (Concept "B")))
 (Evaluation (Predicate "Pabc")
 	(List (Concept "A") (Concept "B") (Concept "C")))
 
-(define filter-ab
+(define min-filter-ab
+	(MinimalJoin
+		(VariableList
+			(TypedVariable (Variable "X") (Signature (Concept "A")))
+			(TypedVariable (Variable "Y") (Signature (Concept "B"))))))
+
+(define max-filter-ab
 	(MaximalJoin
 		(VariableList
 			(TypedVariable (Variable "X") (Signature (Concept "A")))
