3694da5562e178e141202f29ff71b17ca250a774
Linas Vepstas <linasvepstas@gmail.com>
2020-05-11 16:45:52 -0500

    Break the unit test, again

--- tests/atoms/container/gpn-container.scm
+++ tests/atoms/container/gpn-container.scm
@@ -25,19 +25,37 @@
 	(format #t "I was maximally told ~A" ATOM)
 	(if (string-contains label "pie") (stv 1 1) (stv 0 1)))
 
+(define (like-both-pie PRED EVAL)
+	(define pred (cog-outgoing-atom EVAL 0))
+	(define label (cog-name pred))
+	(format #t "I was told ~A and ~A" PRED EVAL)
+	(if (not (equal? PRED pred))
+		(throw 'test-failure "like-both-pie" "You blew it!"))
+	(if (string-contains label "pie") (stv 1 1) (stv 0 1)))
+
 (define min-gpn
 	(MinimalJoin
 		(VariableList
 			(TypedVariable (Variable "P") (Type 'PredicateNode))
 			(TypedVariable (Variable "$top") (Type 'JoinLink)))
-		(Evaluation (GroundedPredicate "scm:min-like-pie") (List (Variable "$top")))))
+		(Evaluation (GroundedPredicate "scm:min-like-pie")
+			(List (Variable "$top")))))
 
 (define max-gpn
 	(MaximalJoin
 		(VariableList
 			(TypedVariable (Variable "P") (Type 'PredicateNode))
 			(TypedVariable (Variable "$top") (Type 'JoinLink)))
-		(Evaluation (GroundedPredicate "scm:max-like-pie") (List (Variable "$top")))))
+		(Evaluation (GroundedPredicate "scm:max-like-pie")
+			(List (Variable "$top")))))
+
+(define both-gpn
+	(MaximalJoin
+		(VariableList
+			(TypedVariable (Variable "P") (Type 'PredicateNode))
+			(TypedVariable (Variable "$top") (Type 'JoinLink)))
+		(Evaluation (GroundedPredicate "scm:like-both-pie")
+			(List (Variable "P") (Variable "$top")))))
 
 (define min-gpn-rep
 	(MinimalJoin
@@ -45,7 +63,8 @@
 			(TypedVariable (Variable "P") (Type 'PredicateNode))
 			(TypedVariable (Variable "$top") (Type 'JoinLink)))
 		(Replacement (Variable "P") (Concept "I Like Pie!"))
-		(Evaluation (GroundedPredicate "scm:min-like-pie") (List (Variable "$top")))))
+		(Evaluation (GroundedPredicate "scm:min-like-pie")
+			(List (Variable "$top")))))
 
 (define max-gpn-rep
 	(MaximalJoin
@@ -53,4 +72,14 @@
 			(TypedVariable (Variable "P") (Type 'PredicateNode))
 			(TypedVariable (Variable "$top") (Type 'JoinLink)))
 		(Replacement (Variable "P") (Concept "I Like Pie!"))
-		(Evaluation (GroundedPredicate "scm:max-like-pie") (List (Variable "$top")))))
+		(Evaluation (GroundedPredicate "scm:max-like-pie")
+			(List (Variable "$top")))))
+
+(define both-gpn-rep
+	(MaximalJoin
+		(VariableList
+			(TypedVariable (Variable "P") (Type 'PredicateNode))
+			(TypedVariable (Variable "$top") (Type 'JoinLink)))
+		(Replacement (Variable "P") (Concept "I Like Pie!"))
+		(Evaluation (GroundedPredicate "scm:like-both-pie")
+			(List (Variable "P") (Variable "$top")))))
