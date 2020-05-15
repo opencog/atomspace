98073a42a2105d6457828f1c201806156c3e2883
Anatoly Belikov <abelikov@singularitynet.io>
2020-05-15 17:06:41 +0300

    test opencog::load_file from scheme

---
+++ tests/scm/test-load-file.scm
@@ -0,0 +1,33 @@
+(use-modules (srfi srfi-1))
+(use-modules (opencog))
+(use-modules (opencog exec))
+(use-modules (opencog persist))
+(use-modules (opencog test-runner))
+
+
+(define (contains? el lst)
+                    (any (lambda (x) (equal? el x)) lst)
+)
+
+(opencog-test-runner)
+
+
+
+(define tname "load_file")
+(test-begin tname)
+
+
+; fill atomspace from a file's content
+(load-file "test_data.scm")
+
+(define names (map cog-name (cog-get-atoms "ConceptNode")))
+
+(define contain_all 
+  (and (contains? "тестирование кода приводит к успеху" names)
+       (contains? "Codeprüfung führt zum Erfolg" names)
+       (contains? "'''''''''1;" names)))
+
+; would fail on slashes at the end of string: (ConceptNode "a\\")
+(test-assert "load data from file"
+	     contain_all
+)
