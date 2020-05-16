e36a298a3dd1f3174ce6492bb36c272f94c06bbf
Linas Vepstas <linasvepstas@gmail.com>
2020-05-16 14:53:02 -0500

    Tighten up the unit test

--- tests/scm/scm-load-file-test-data.scm
+++ tests/scm/scm-load-file-test-data.scm
@@ -11,3 +11,15 @@
 
 
 (ConceptNode "Codeprüfung führt zum Erfolg")(ConceptNode "'''''''''1;")))
+
+(MemberLink
+(LexicalNode "AGXT")
+(ConceptNode "SMP0000055")
+)(MemberLink
+(LexicalNode "Uniprot:P21549")
+(ConceptNode "SMP0000055")
+)(EvaluationLink
+(PredicateNode "has_name")
+(ListLink
+(ConceptNode "SMP0000055")
+(ConceptNode "Alanine Metabolism")))
