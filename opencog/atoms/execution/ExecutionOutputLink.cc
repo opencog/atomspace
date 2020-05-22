82b5ccf397ab308b2902e339ca017e42b6dcdd6e
Linas Vepstas <linasvepstas@gmail.com>
2020-05-22 17:48:10 -0500

    Python can now handle values!

--- opencog/atoms/execution/ExecutionOutputLink.cc
+++ opencog/atoms/execution/ExecutionOutputLink.cc
@@ -286,7 +286,7 @@ ValuePtr ExecutionOutputLink::do_execute(AtomSpace* as,
 #ifdef HAVE_CYTHON
 		// Get a reference to the python evaluator.
 		PythonEval &applier = PythonEval::instance();
-		result = applier.apply(as, fun, args);
+		result = applier.apply_v(as, fun, args);
 #else
 		throw RuntimeException(TRACE_INFO,
 		                       "Cannot evaluate python GroundedSchemaNode!");
