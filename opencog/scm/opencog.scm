6615a254682e0b08ad6acf6a23258d9d5a92d4fc
Linas Vepstas <linasvepstas@gmail.com>
2020-06-03 16:29:23 -0500

    Ooops!

--- opencog/scm/opencog.scm
+++ opencog/scm/opencog.scm
@@ -129,5 +129,4 @@ cog-value-ref
 (include-from-path "opencog/base/debug-trace.scm")
 
 ; Obsolete functions
-(define-public (cog-atom X) "obsolete function" '())
 (define-public (cog-undefined-handle) "obsolete function" '())
