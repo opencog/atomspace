6320a5ae5ff0a9efa1f8baa6049cf3a3c2a9e2e5
Linas Vepstas <linasvepstas@gmail.com>
2020-06-07 19:30:24 -0500

    Start work on a python runner

---
+++ opencog/atoms/grounded/PythonRunner.cc
@@ -0,0 +1,59 @@
+/*
+ * opencog/atoms/grounded/PythonRunner.cc
+ *
+ * Copyright (C) 2009, 2013, 2014, 2015, 2020 Linas Vepstas
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atoms/atom_types/atom_types.h>
+#include <opencog/atoms/core/DefineLink.h>
+#include <opencog/atoms/core/NumberNode.h>
+#include <opencog/atoms/execution/Force.h>
+#include <opencog/atoms/truthvalue/TruthValue.h>
+#include <opencog/atomspace/AtomSpace.h>
+#include <opencog/cython/PythonEval.h>
+#include <opencog/guile/SchemeEval.h>
+
+#include <opencog/atoms/grounded/PythonRunner.h>
+#include "DLScheme.h"
+
+using namespace opencog;
+
+PythonRunner::PythonRunner(std::string s)
+	: _fname(s)
+{
+}
+
+// ----------------------------------------------------------
+
+/// `execute()` -- evaluate a PythonRunner with arguments.
+///
+/// Expects "args" to be a ListLink. These arguments will be
+///     substituted into the predicate.
+///
+/// The arguments are "eager-evaluated", because it is assumed that
+/// the GPN is unaware of the concept of lazy evaluation, and can't
+/// do it itself. The arguments are then inserted into the predicate,
+/// and the predicate as a whole is then evaluated.
+///
+ValuePtr PythonRunner::execute(AtomSpace* as,
+                               const Handle& cargs,
+                               bool silent)
+{
+}
---
+++ opencog/atoms/grounded/PythonRunner.h
@@ -0,0 +1,53 @@
+/*
+ * opencog/atoms/grounded/PythonRunner.h
+ *
+ * Copyright (C) 2020 Linas Vepstas
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_PYTHON_RUNNER_H
+#define _OPENCOG_PYTHON_RUNNER_H
+
+#include <string>
+#include <opencog/atoms/grounded/Runner.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/// Base class for executing guile code.
+class PythonRunner : public Runner
+{
+	std::string _fname;
+
+public:
+	PythonRunner(const std::string);
+	PythonRunner(const PythonRunner&) = delete;
+	PythonRunner& operator=(const PythonRunner&) = delete;
+
+	virtual ValuePtr execute(AtomSpace*, const Handle&, bool=false);
+	virtual TruthValuePtr evaluate(AtomSpace*, const Handle&, bool=false);
+};
+
+/** @}*/
+}
+
+#endif // _OPENCOG_PYTHON_RUNNER_H
