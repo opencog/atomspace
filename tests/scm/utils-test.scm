943e6ef04663cfebdee2729d21727524a2358eeb
Nil Geisweiller <ngeiswei@gmail.com>
2020-06-09 12:19:15 +0300

    Add utest to check that cog-get-atoms conforms with its description

--- tests/scm/utils-test.scm
+++ tests/scm/utils-test.scm
@@ -20,3 +20,17 @@
 ; test cog-get-link
 (define ref (ReferenceLink (ConceptNode "asdf") (WordNode "pqrs")))
 (define wref (car (cog-get-link 'ReferenceLink 'ConceptNode (WordNode "pqrs"))))
+
+; test cog-get-atoms. Warning: it uses previously defined nodes, so
+; should be updated if any new nodes are introduced above.
+(define cpts (Set (cog-get-atoms 'ConceptNode)))
+(define cpts-n-subtypes (Set (cog-get-atoms 'ConceptNode #t)))
+(define nodes-n-subtypes (Set (cog-get-atoms 'Node #t)))
+(define x-cpts (Set (ConceptNode "asdf") (ConceptNode "partner")))
+(define x-cpts-n-subtypes x-cpts)
+(define x-nodes-n-subtypes (Set
+                             (ConceptNode "asdf")
+                             (ConceptNode "partner")
+                             (PredicateNode "key")
+                             (WordNode "asdf")
+                             (WordNode "pqrs")))
