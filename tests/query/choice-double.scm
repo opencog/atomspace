7e0865c2109634a2197eed1f9a1584f86f171787
Linas Vepstas <linasvepstas@gmail.com>
2020-07-01 18:31:03 -0500

    Cleanup for readability. No functional change.

--- tests/query/choice-double.scm
+++ tests/query/choice-double.scm
@@ -1,88 +1,35 @@
 ;
-; Unit testing for ChoiceLinks in the pattern matcher.
+; choice-double.scm
+;
+; Unit testing for Choices in the pattern matcher.
 ; Much link choice-embed, except it ahs two choice links
 ;
 (use-modules (opencog))
 (use-modules (opencog exec))
 
 ;;; Populate the atomspace with four small trees.
-(MemberLink
-	(ConceptNode "Tom")
-	(ConceptNode "ways and means")
-)
-
-(MemberLink
-	(ConceptNode "Joe")
-	(ConceptNode "ways and means")
-)
-
-(MemberLink
-	(ConceptNode "Hank")
-	(ConceptNode "ways and means")
-)
-
-(MemberLink
-	(ConceptNode "Dick")
-	(ConceptNode "agriculture")
-)
+(Member (Concept "Tom") (Concept "ways and means"))
+(Member (Concept "Joe") (Concept "ways and means"))
+(Member (Concept "Hank") (Concept "ways and means"))
+(Member (Concept "Dick") (Concept "agriculture"))
 
 ;;; the list link serves no purpose other than to "embed"
-(ListLink
-	(MemberLink
-		(ConceptNode "Tom")
-		(ConceptNode "Senator")
-	)
-)
-
-(ListLink
-	(MemberLink
-		(ConceptNode "Dick")
-		(ConceptNode "Senator")
-	)
-)
-
-(ListLink
-	(MemberLink
-		(ConceptNode "Joe")
-		(ConceptNode "Representative")
-	)
-)
+(List (Member (Concept "Tom") (Concept "Senator")))
+(List (Member (Concept "Dick") (Concept "Senator")))
+(List (Member (Concept "Joe") (Concept "Representative")))
 
 ;; We should NOT find Hank!
-(ListLink
-	(MemberLink
-		(ConceptNode "Hank")
-		(ConceptNode "CEO")
-	)
-)
+(List (Member (Concept "Hank") (Concept "CEO")))
 
 ;;; Two clauses; they are both connected with a common variable.
-(define (double)
-	(BindLink
-		(AndLink
-			(ChoiceLink
-				(MemberLink
-					(VariableNode "$x")
-					(ConceptNode "ways and means")
-				)
-				(MemberLink
-					(VariableNode "$x")
-					(ConceptNode "agriculture")
-				)
-			)
-			(ListLink
-				(ChoiceLink
-					(MemberLink
-						(VariableNode "$x")
-						(ConceptNode "Senator")
-					)
-					(MemberLink
-						(VariableNode "$x")
-						(ConceptNode "Representative")
-					)
-				)
-			)
-		)
-		(VariableNode "$x")
-	)
-)
+(define double
+	(Bind
+		(And
+			(Choice
+				(Member (Variable "$x") (Concept "ways and means"))
+				(Member (Variable "$x") (Concept "agriculture")))
+			(List
+				(Choice
+					(Member (Variable "$x") (Concept "Senator"))
+					(Member (Variable "$x") (Concept "Representative")))))
+		(Variable "$x")))
