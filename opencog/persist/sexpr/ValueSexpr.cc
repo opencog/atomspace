3469459b1ad834f936aa2fe502978be366703669
Linas Vepstas <linasvepstas@gmail.com>
2020-07-17 12:10:03 -0500

    Add license boiler-plate

--- opencog/persist/sexpr/ValueSexpr.cc
+++ opencog/persist/sexpr/ValueSexpr.cc
@@ -1,9 +1,24 @@
 /*
  * ValueSexpr.cc
- * Evaluate S-Expression containing Atomese Values.
+ * Encode/decode S-Expressions for Atomese Values.
  *
  * Copyright (c) 2019 Linas Vepstas <linas@linas.org>
  * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #include <opencog/atoms/base/Atom.h>
 #include <opencog/atoms/value/FloatValue.h>
