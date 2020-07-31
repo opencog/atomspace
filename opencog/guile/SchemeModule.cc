95fb6ee53f1f96eb0d4475282931ef0c98bd695c
Linas Vepstas <linasvepstas@gmail.com>
2020-07-30 23:16:40 -0500

    Add license boilerplate

--- opencog/guile/SchemeModule.cc
+++ opencog/guile/SchemeModule.cc
@@ -3,6 +3,21 @@
  *
  * Simplified API for creating guile modules.
  * Copyright (c) 2008, 2014, 2015 Linas Vepstas <linas@linas.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include <opencog/atomspace/AtomSpace.h>
