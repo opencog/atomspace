8c7c87e54a2a6a24b2491d0d8ebdc36f9089face
Linas Vepstas <linasvepstas@gmail.com>
2020-08-17 09:38:43 -0500

    Start work on modernizing ParallelLink

---
+++ opencog/atoms/execution/ParallelLink.h
@@ -0,0 +1,64 @@
+/*
+ * opencog/atoms/execution/ParallelLink.h
+ *
+ * Copyright (C) 2020 Linas Vepstas
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_PARALLEL_LINK_H
+#define _OPENCOG_PARALLEL_LINK_H
+
+#include <opencog/atoms/base/Link.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+class AtomSpace;
+
+class ParallelLink : public Link
+{
+private:
+	size_t _nthreads;
+
+public:
+	ParallelLink(const HandleSeq&&, Type=PARALLEL_LINK);
+	ParallelLink(const ParallelLink&) = delete;
+	ParallelLink& operator=(const ParallelLink&) = delete;
+
+	virtual bool is_executable() const { return true; }
+	virtual ValuePtr execute(AtomSpace* as, bool silent);
+
+	static Handle factory(const Handle&);
+};
+
+typedef std::shared_ptr<ParallelLink> ParallelLinkPtr;
+static inline ParallelLinkPtr ParallelLinkCast(const Handle& h)
+   { AtomPtr a(h); return std::dynamic_pointer_cast<ParallelLink>(a); }
+static inline ParallelLinkPtr ParallelLinkCast(AtomPtr a)
+   { return std::dynamic_pointer_cast<ParallelLink>(a); }
+
+#define createParallelLink std::make_shared<ParallelLink>
+
+/** @}*/
+}
+
+#endif // _OPENCOG_PARALLEL_LINK_H
