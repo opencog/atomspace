db2df340c02b24fcb989fe9465ab3f55ed4d5217
Linas Vepstas <linasvepstas@gmail.com>
2021-07-12 15:27:14 -0500

    Another performance optimization

--- opencog/persist/sexpr/fast_load.cc
+++ opencog/persist/sexpr/fast_load.cc
@@ -97,15 +97,38 @@ void opencog::load_file(const std::string& fname, AtomSpace& as)
 // Parse an Atomese string expression and return a Handle to the parsed atom
 Handle opencog::parseExpression(const std::string& exp, AtomSpace &as)
 {
-    Handle h;
-    size_t expr_cnt = 0;
     size_t line_cnt = 0;
-    std::string expr = exp;
+    size_t l = 0;
+    size_t r = exp.length();
+    size_t rr = r;
+    int pcount = Sexpr::get_next_expr(exp, l, r, line_cnt);
+
+    if (0 < pcount)
+        throw std::runtime_error(
+            "Unbalanced parenthesis >>" + exp.substr(r) + "<<");
+
+    Handle h = as.add_atom(Sexpr::decode_atom(exp, l, r, line_cnt));
+
+    l = r+1;
+    r = rr;
+    if (l == r) return h;
+
+    pcount = Sexpr::get_next_expr(exp, l, r, line_cnt);
+    if (0 < pcount)
+        throw std::runtime_error(
+            "Unbalanced parenthesis >>" + exp.substr(r) + "<<");
+
+    if (l == r) return h;
+
+    // If we are here, then ther is more than one atom in the exp.
+    // For that, we pay the price of a string copy.
+    std::string expr = exp.substr(l);
+    size_t expr_cnt = 1;
 
     while (true)
     {
-        size_t l = 0;
-        size_t r = expr.length();
+        l = 0;
+        r = expr.length();
 
         // Zippy the Pinhead says: Are we having fun yet?
         int pcount = Sexpr::get_next_expr(expr, l, r, line_cnt);
