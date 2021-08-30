a41f09af59d39a982132a0ddc81fe0efb3a5de3b
Linas Vepstas <linasvepstas@gmail.com>
2021-08-30 14:05:39 -0500

    Add actual tests to the test

--- tests/persist/sexpr/file-storage.scm
+++ tests/persist/sexpr/file-storage.scm
@@ -13,57 +13,66 @@
 (test-begin tname)
 
 ; Populate the AtomSpace with some data.
-(define a (Concept "foo"))
-(cog-set-value! a (Predicate "num") (FloatValue 1 2 3))
-(cog-set-value! a (Predicate "str") (StringValue "p" "q" "r"))
+(define wa (Concept "foo"))
+(cog-set-value! wa (Predicate "num") (FloatValue 1 2 3))
+(cog-set-value! wa (Predicate "str") (StringValue "p" "q" "r"))
 
-(define li (Link (Concept "foo") (Concept "bar")))
-(cog-set-value! li (Predicate "num") (FloatValue 4 5 6))
-(cog-set-value! li (Predicate "str") (StringValue "x" "y" "z"))
+(define wli (Link (Concept "foo") (Concept "bar")))
+(cog-set-value! wli (Predicate "num") (FloatValue 4 5 6))
+(cog-set-value! wli (Predicate "str") (StringValue "x" "y" "z"))
 
 ; Store some individual Atoms, and then store everything.
-(define fsn (FileStorageNode "/tmp/foo.scm"))
-(cog-open fsn)
+(define wfsn (FileStorageNode "/tmp/foo.scm"))
+(cog-open wfsn)
 
 ; Store just one Atom.
-(store-atom a fsn)
+(store-atom wa wfsn)
 
 ; Store just one value on an Atom. Store it three times;
 ; it will show up in the file three times.
-(store-value a (Predicate "num") fsn)
-(store-value a (Predicate "num") fsn)
-(store-value a (Predicate "num") fsn)
+(cog-set-value! wa (Predicate "num") (FloatValue 11 22 33))
+(store-value wa (Predicate "num") wfsn)
+(store-value wa (Predicate "num") wfsn)
+(store-value wa (Predicate "num") wfsn)
 
 ; The file write might not occur until after the `barrier` call.
 ; File writes are buffered by the operating system.
-(barrier fsn)
+(barrier wfsn)
 
 ; Now store everything. This will appear after the writes above,
 ; and will duplicate some of the earlier data.  During file read,
 ; later data in the file will take precedence over earlier data.
-(store-atomspace fsn)
-(cog-close fsn)
+(store-atomspace wfsn)
+(cog-close wfsn)
 
 (cog-atomspace-clear)
 
 ; ---------------------------------------------------------------------
 
 ; Load everything from the file.
-(define fsn (FileStorageNode "/tmp/foo.scm"))
-(cog-open fsn)
-(load-atomspace fsn)
-(cog-close fsn)
+(define rfsn (FileStorageNode "/tmp/foo.scm"))
+(cog-open rfsn)
+(load-atomspace rfsn)
+(cog-close rfsn)
 
 ; Verify the load
 (cog-prt-atomspace)
 
-; Verify that the keys on the link have been correctly restored.
-(define li (Link (Concept "foo") (Concept "bar")))
-(cog-keys li)
-(cog-value li (Predicate "num"))
-(cog-value li (Predicate "str"))
+; Verify that the keys have been correctly restored.
+(define ra (Concept "foo"))
 
-(test-assert "stuff" #t)
+(test-assert "Concept Keys" (equal? 2 (length (cog-keys ra))))
+(test-assert "Concept Num"
+	(equal? (cog-value ra (Predicate "num")) (FloatValue 11 22 33)))
+(test-assert "Concept Str"
+	(equal? (cog-value ra (Predicate "str")) (StringValue "p" "q" "r")))
+
+(define rli (Link (Concept "foo") (Concept "bar")))
+(test-assert "List Keys" (equal? 2 (length (cog-keys rli))))
+(test-assert "List Num"
+	(equal? (cog-value rli (Predicate "num")) (FloatValue 4 5 6)))
+(test-assert "List Str"
+	(equal? (cog-value rli (Predicate "str")) (StringValue "x" "y" "z")))
 
 ; --------------------------
 ; The End. That's all folks!
