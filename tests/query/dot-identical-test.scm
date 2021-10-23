c768201e5ee3b0fdfb0a9fcb8e289b884c314e46
Linas Vepstas <linasvepstas@gmail.com>
2021-10-23 11:24:58 -0500

    Add a new unit test for the IdenticalLink

--- tests/query/dot-identical-test.scm
+++ tests/query/dot-identical-test.scm
@@ -1,20 +1,15 @@
 ;
-; dot-top.scm -- take dot products of vectors for query
-; Development/test version.
+; dot-identical-test.scm -- take dot products of vectors for query
+; See `dot-product-test.scm` for the basic version and explanation.
+; Variant using IdenticalLinks.
 ;
-; The QueryLink not only performs searches, but it can apply rewrite
-; rules to the results. These re-write rules can generate (numeric)
-; Values, which are returned to the caller.  This example illustrates
-; a query that returns two vectors of numbers, which are multiplied
-; and accumulated, resulting in a dot-product of the two.
-
 (use-modules (opencog) (opencog exec))
+(use-modules (opencog test-runner))
 
-; Define a pair of vectors. One vector is called "dog", the other is
-; called "cat". The basis elements of both vectors are "has legs",
-; "has nose" and so on. The numeric value for that basis element is
-; stored in a CountTruthValue attached to each.
-;
+(opencog-test-runner)
+(define tname "dot-identical-test")
+(test-begin tname)
+
 (Evaluation (Predicate "has legs") (Concept "dog") (CountTruthValue 1 0 1))
 (Evaluation (Predicate "has nose") (Concept "dog") (CountTruthValue 1 0 2))
 (Evaluation (Predicate "has tail") (Concept "dog") (CountTruthValue 1 0 3))
@@ -55,15 +50,9 @@
 			(CountOf (Variable "$dog"))
 			(CountOf (Variable "$cat")))))
 
-(use-modules (opencog logger))
-(cog-logger-set-stdout! #t)
-(cog-logger-set-sync! #t)
-(cog-logger-set-level! "FINE")
-
-; Dry run -- this should return a list of numbers 1,4,9,16,25
-(cog-execute! qdot-math)
-
 ; Accumulate the numeric values: this should return 1+4+9+16+25 = 55.
-; (cog-execute! (Accumulate qdot-math))
+(define five-five (cog-execute! (Accumulate qdot-math)))
 
-; That's all folks! The End.
+(test-assert "final dot product" (equal? (FloatValue 55) five-five))
+
+(test-end tname)
