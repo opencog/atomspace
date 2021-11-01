301c1612eeff497b98e2a88c58f7e5fcf104cdac
Linas Vepstas <linasvepstas@gmail.com>
2021-11-01 18:08:25 -0500

    Add unit test to test the inf loop crash

---
+++ tests/atoms/core/put-recursive-test.scm
@@ -0,0 +1,34 @@
+;
+; put-recursive-test.scm -- test for crash on recursive PutLink
+;
+(use-modules (opencog) (opencog exec))
+(use-modules (opencog test-runner))
+
+(opencog-test-runner)
+(define tname "put-recursive-test")
+(test-begin tname)
+
+; Define a recursive Schema
+(Define
+	(DefinedSchema "is-a relation")
+	(Lambda
+		(VariableList (Variable "this") (Variable "that"))
+		(SequentialOr
+			(Inheritance (Variable "this") (Variable "that"))
+			(SequentialAnd
+				(Inheritance (Variable "this") (Variable "middle"))
+				(Put (DefinedSchema "is-a relation")
+					(List (Variable "middle") (Variable "that")))))))
+
+; Attempt to use it
+(define is-it
+	(Put
+		(DefinedSchema "is-a relation")
+		(List (Concept "human") (Concept "chordate"))))
+
+(cog-execute! is-it)
+
+; If we reached this part without crashing, we're good.
+(test-assert "no crash" #t)
+
+(test-end tname)
