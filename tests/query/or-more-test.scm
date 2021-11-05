7a10030924e0dcdf030b6f1578a163faf3073f55
Linas Vepstas <linasvepstas@gmail.com>
2021-11-04 23:24:31 -0500

    Extened the unit test even more.
    This reflects the case given in
    https://github.com/opencog/atomspace/issues/2644#issuecomment-640637234
    
    of issue opencog/atomspace#2644
    
    I think this is the last of them.

--- tests/query/or-more-test.scm
+++ tests/query/or-more-test.scm
@@ -16,8 +16,8 @@
 (define who-is-hungry-1?
 	(Get
 		(VariableList
-			(TypedVariable (Variable "x") (Type "ConceptNode"))
-			(TypedVariable (Variable "y") (Type "ConceptNode")))
+			(TypedVariable (Variable "x") (Type 'Concept))
+			(TypedVariable (Variable "y") (Type 'Concept)))
 		(Present
 			(State (Variable "x") (Variable "y"))
 			(State (List (Variable "x") (Predicate "hungry")) (Concept "TRUE"))
@@ -31,13 +31,16 @@
 (define who-is-hungry-2?
 	(Get
 		(VariableList
-			(TypedVariable (Variable "x") (Type "ConceptNode"))
-			(TypedVariable (Variable "y") (Type "ConceptNode")))
+			(TypedVariable (Variable "x") (Type 'Concept))
+			(TypedVariable (Variable "y") (Type 'Concept)))
 		(Or
 			(Present (State (Variable "x") (Variable "y")))
 			(Present (State (List (Variable "x") (Predicate "hungry")) (Concept "TRUE")))
 			)))
 
+; Well, look, the naked Variable y appearing in the search results
+; is sinfully ugly, but its associated with a clause that never actually
+; grounds y. C'est la vie.
 (test-assert "alice y at home"
 	(equal? (cog-execute! who-is-hungry-2?)
 		(Set
@@ -48,17 +51,60 @@
 (define who-is-hungry-3?
 	(Get
 		(VariableList
-			(TypedVariable (Variable "x") (Type "ConceptNode"))
-			(TypedVariable (Variable "y") (Type "ConceptNode")))
+			(TypedVariable (Variable "x") (Type 'Concept))
+			(TypedVariable (Variable "y") (Type 'Concept)))
 		(Choice
 			(Present (State (Variable "x") (Variable "y")))
 			(Present (State (List (Variable "x") (Predicate "hungry")) (Concept "TRUE")))
 			)))
 
+; Well, look, the naked Variable y appearing in the search results
+; is sinfully ugly, but its associated with a clause that never actually
+; grounds y. C'est la vie.
 (test-assert "alice why at home"
 	(equal? (cog-execute! who-is-hungry-3?)
 		(Set
 			(List (Concept "alice") (Concept "at home"))
 			(List (Concept "alice") (Variable "y")))))
+
+; -----------------
+(State (List (Concept "alice") (Predicate "hungry")) (Concept "TRUE"))
+(State (List (Concept "alice") (Predicate "thirsty")) (Concept "TRUE"))
+
+(State (List (Concept "bob") (Predicate "hungry")) (Concept "FALSE"))
+(State (List (Concept "bob") (Predicate "thirsty")) (Concept "FALSE"))
+
+(State (List (Concept "charlie") (Predicate "hungry")) (Concept "FALSE"))
+(State (List (Concept "charlie") (Predicate "thirsty")) (Concept "TRUE"))
+(State (Concept "charlie") (Concept "at home"))
+(State (List (Concept "at home") (Predicate "is cozy")) (Concept "TRUE"))
+
+(define whos-on-first?
+	(Get
+		(VariableList
+			(TypedVariable (Variable "x") (Type 'Concept))
+			(TypedVariable (Variable "y") (Type 'Concept)))
+		(Choice
+			(Present
+				(State (Variable "x") (Variable "y"))
+				(State (List (Variable "y") (Predicate "is cozy")) (Concept "TRUE"))
+				(State (List (Variable "x") (Predicate "hungry")) (Concept "FALSE"))
+				(State (List (Variable "x") (Predicate "thirsty")) (Concept "TRUE")))
+			(Present
+				(State (List (Variable "x") (Predicate "hungry")) (Concept "FALSE"))
+				(State (List (Variable "x") (Predicate "thirsty")) (Concept "FALSE")))
+			(Present
+				(State (List (Variable "x") (Predicate "hungry")) (Concept "TRUE")))
+			)))
+
+; Well, look, the naked Variable y appearing in the search results
+; is sinfully ugly, but its associated with a clause that never actually
+; grounds y. C'est la vie.
+(test-assert "whats on first"
+	(equal? (cog-execute! whos-on-first?)
+		(Set
+			(List (Concept "alice") (Variable "y"))
+			(List (Concept "bob") (Variable "y"))
+			(List (Concept "charlie") (Concept "at home")))))
 
 (test-end tname)
