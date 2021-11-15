c97aab9bad267e1ae33747c28bf21a7d76c54525
Linas Vepstas <linasvepstas@gmail.com>
2021-11-14 21:54:37 -0600

    Refactor the python writing

--- cmake/OpenCogAtomTypes.cmake
+++ cmake/OpenCogAtomTypes.cmake
@@ -204,6 +204,55 @@ MACRO(OPENCOG_PYTHON_SETUP PYTHON_FILE)
 	)
 ENDMACRO(OPENCOG_PYTHON_SETUP PYTHON_FILE)
 
+# ------------
+# Print out the python definitions. Note: We special-case Atom
+# since we don't want to create a function with the same
+# identifier as the Python Atom object.
+MACRO(OPENCOG_PYTHON_WRITE_DEFS)
+	IF (NOT TYPE_NAME STREQUAL "Atom")
+		IF (ISVALUE STREQUAL "VALUE" OR ISSTREAM STREQUAL "STREAM")
+			LIST(FIND PYTHON_SUPPORTED_VALUE_LIST ${TYPE_NAME} _INDEX)
+			IF (${_INDEX} GREATER -1)
+				# Single arg will work as all of value constructors has
+				# single argument: either value or vector.
+				FILE(APPEND "${PYTHON_FILE}"
+					"def ${TYPE_NAME}(arg):\n"
+					"    return createValue(types.${TYPE_NAME}, arg)\n"
+				)
+			ENDIF (${_INDEX} GREATER -1)
+		ENDIF (ISVALUE STREQUAL "VALUE" OR ISSTREAM STREQUAL "STREAM")
+		IF (ISNODE STREQUAL "NODE")
+			FILE(APPEND "${PYTHON_FILE}"
+				"def ${TYPE_NAME}(node_name, tv=None):\n"
+				"    return add_node(types.${TYPE_NAME}, node_name, tv)\n"
+			)
+		ENDIF (ISNODE STREQUAL "NODE")
+		IF (ISLINK STREQUAL "LINK")
+			FILE(APPEND "${PYTHON_FILE}"
+				"def ${TYPE_NAME}(*args, tv=None):\n"
+				"    return add_link(types.${TYPE_NAME}, args, tv=tv)\n"
+			)
+		ENDIF (ISLINK STREQUAL "LINK")
+	ENDIF (NOT TYPE_NAME STREQUAL "Atom")
+
+	# If not named as a node or a link, assume its a link
+	# This is kind of hacky, but I don't know what else to do ...
+	IF (NOT ISATOMSPACE STREQUAL "ATOMSPACE" AND
+		NOT ISNODE STREQUAL "NODE" AND
+		NOT ISLINK STREQUAL "LINK" AND
+		NOT ISVALUE STREQUAL "VALUE" AND
+		NOT ISSTREAM STREQUAL "STREAM")
+		FILE(APPEND "${PYTHON_FILE}"
+			"def ${TYPE_NAME}(*args):\n"
+			"    return add_link(types.${TYPE_NAME}, args)\n"
+		)
+	ENDIF (NOT ISATOMSPACE STREQUAL "ATOMSPACE" AND
+		NOT ISNODE STREQUAL "NODE" AND
+		NOT ISLINK STREQUAL "LINK" AND
+		NOT ISVALUE STREQUAL "VALUE" AND
+		NOT ISSTREAM STREQUAL "STREAM")
+ENDMACRO(OPENCOG_PYTHON_WRITE_DEFS)
+
 #####################################################################
 # Macro example call:
 # OPENCOG_ADD_ATOM_TYPES(
@@ -337,43 +386,8 @@ FOREACH (LINE ${TYPE_SCRIPT_CONTENTS})
         OPENCOG_SCM_WRITE_DEFS()
 
         # -----------------------------------------------------------
-        # Print out the python definitions. Note: We special-case Atom
-        # since we don't want to create a function with the same
-        # identifier as the Python Atom object.
-        IF (NOT TYPE_NAME STREQUAL "Atom")
-            IF (ISVALUE STREQUAL "VALUE" OR ISSTREAM STREQUAL "STREAM")
-                LIST(FIND PYTHON_SUPPORTED_VALUE_LIST ${TYPE_NAME} _INDEX)
-                IF (${_INDEX} GREATER -1)
-                    # Single arg will work as all of value constructors has
-                    # single argument: either value or vector.
-                    FILE(APPEND "${PYTHON_FILE}" "def ${TYPE_NAME}(arg):\n")
-                    FILE(APPEND "${PYTHON_FILE}" "    return createValue(types.${TYPE_NAME}, arg)\n")
-                ENDIF (${_INDEX} GREATER -1)
-            ENDIF (ISVALUE STREQUAL "VALUE" OR ISSTREAM STREQUAL "STREAM")
-            IF (ISNODE STREQUAL "NODE")
-                FILE(APPEND "${PYTHON_FILE}" "def ${TYPE_NAME}(node_name, tv=None):\n")
-                FILE(APPEND "${PYTHON_FILE}" "    return add_node(types.${TYPE_NAME}, node_name, tv)\n")
-            ENDIF (ISNODE STREQUAL "NODE")
-            IF (ISLINK STREQUAL "LINK")
-                FILE(APPEND "${PYTHON_FILE}" "def ${TYPE_NAME}(*args, tv=None):\n")
-                FILE(APPEND "${PYTHON_FILE}" "    return add_link(types.${TYPE_NAME}, args, tv=tv)\n")
-            ENDIF (ISLINK STREQUAL "LINK")
-        ENDIF (NOT TYPE_NAME STREQUAL "Atom")
-
-        # If not named as a node or a link, assume its a link
-        # This is kind of hacky, but I don't know what else to do ...
-        IF (NOT ISATOMSPACE STREQUAL "ATOMSPACE" AND
-            NOT ISNODE STREQUAL "NODE" AND
-            NOT ISLINK STREQUAL "LINK" AND
-            NOT ISVALUE STREQUAL "VALUE" AND
-            NOT ISSTREAM STREQUAL "STREAM")
-            FILE(APPEND "${PYTHON_FILE}" "def ${TYPE_NAME}(*args):\n")
-            FILE(APPEND "${PYTHON_FILE}" "    return add_link(types.${TYPE_NAME}, args)\n")
-        ENDIF (NOT ISATOMSPACE STREQUAL "ATOMSPACE" AND
-            NOT ISNODE STREQUAL "NODE" AND
-            NOT ISLINK STREQUAL "LINK" AND
-            NOT ISVALUE STREQUAL "VALUE" AND
-            NOT ISSTREAM STREQUAL "STREAM")
+        # Print out the python definitions.
+        OPENCOG_PYTHON_WRITE_DEFS()
 
         # -----------------------------------------------------------
         # Create the type inheritance C++ file.
