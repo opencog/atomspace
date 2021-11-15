2985da01c9fb93d59b98f1a455192a162633634c
Linas Vepstas <linasvepstas@gmail.com>
2021-11-15 15:27:12 -0600

    Attempt to autogenerate dependencies

---
+++ cmake/OpenCogOCaml.cmake
@@ -0,0 +1,19 @@
+# Convert one ml file to an mli file.
+# Additional arguments are dependencies
+
+MACRO(OCAML_MAKE_INTERFACE name)
+	SET(DEPENDS ${name})
+
+	# Allow the user to specify dependencies as optional arguments
+	SET(DEPENDS ${DEPENDS} ${ARGN})
+
+	ADD_CUSTOM_COMMAND(
+		OUTPUT ${name}i
+		COMMAND ${CMAKE_OCaml_FIND} ocamlc -i ${name}
+		DEPENDS ${DEPENDS}
+		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+		COMMENT "Building the ${name}i file"
+	)
+
+	list(APPEND ADDITIONAL_MAKE_CLEAN_FILES "${name}i")
+ENDMACRO()
