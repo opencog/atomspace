4c1ca71f4bbbd45ba01fa0afe37aef23d7d5f5d5
Linas Vepstas <linasvepstas@gmail.com>
2021-11-15 09:28:46 -0600

    Restore the older code

--- cmake/OpenCogMacros.cmake
+++ cmake/OpenCogMacros.cmake
@@ -120,15 +120,46 @@ ENDMACRO()
 # =========================================================
 # Deprecated, do not use in new code.
 #
+
 MACRO(OPENCOG_ADD_ATOM_TYPES SCRIPT_FILE
       HEADER_FILE DEFINITIONS_FILE INHERITANCE_FILE SCM_FILE PYTHON_FILE)
 
-	OPENCOG_GEN_SCM_ATOMTYPES(${SCRIPT_FILE} ${SCM_FILE})
-	OPENCOG_GEN_PYTHON_ATOMTYPES(${SCRIPT_FILE} ${PYTHON_FILE})
+# We want to be able to say just this:
+#
+#	OPENCOG_GEN_SCM_ATOMTYPES(${SCRIPT_FILE} ${SCM_FILE})
+#	OPENCOG_GEN_PYTHON_ATOMTYPES(${SCRIPT_FILE} ${PYTHON_FILE})
+#	OPENCOG_GEN_CPP_ATOMTYPES(${SCRIPT_FILE}
+#		${HEADER_FILE} ${DEFINITIONS_FILE} ${INHERITANCE_FILE})
+#
+# but we can't, because missing dependencies in assorted projects
+# would cause the scheme and python files to not be generated.
+# No amount of ADD_CUSTOM_TARGET here seeems able to fix this,
+# So we do the below instead.
 
-	OPENCOG_GEN_CPP_ATOMTYPES(${SCRIPT_FILE}
-		${HEADER_FILE} ${DEFINITIONS_FILE} ${INHERITANCE_FILE})
-
+	ADD_CUSTOM_COMMAND (
+		COMMAND "${CMAKE_COMMAND}"
+			-DBUILD_CSP=yes
+			-DOC_CMAKE_PATH=\"${OC_CMAKE_PATH}\"
+			-DSCRIPT_FILE=\"${CMAKE_CURRENT_SOURCE_DIR}/${SCRIPT_FILE}\"
+			-DHEADER_FILE=\"${CMAKE_CURRENT_BINARY_DIR}/${HEADER_FILE}\"
+			-DDEFINITIONS_FILE=\"${CMAKE_CURRENT_BINARY_DIR}/${DEFINITIONS_FILE}\"
+			-DINHERITANCE_FILE=\"${CMAKE_CURRENT_BINARY_DIR}/${INHERITANCE_FILE}\"
+			-DSCM_FILE=\"${CMAKE_CURRENT_BINARY_DIR}/${SCM_FILE}\"
+			-DPYTHON_FILE=\"${CMAKE_CURRENT_BINARY_DIR}/${PYTHON_FILE}\"
+			-P "${OC_ATOM_TYPES_PATH}"
+		OUTPUT  "${CMAKE_CURRENT_BINARY_DIR}/${HEADER_FILE}"
+		        "${CMAKE_CURRENT_BINARY_DIR}/${DEFINITIONS_FILE}"
+		        "${CMAKE_CURRENT_BINARY_DIR}/${INHERITANCE_FILE}"
+		        "${CMAKE_CURRENT_BINARY_DIR}/${SCM_FILE}"
+		        "${CMAKE_CURRENT_BINARY_DIR}/${PYTHON_FILE}"
+		DEPENDS "${SCRIPT_FILE}" "${OC_ATOM_TYPES_PATH}"
+		# Set working directory of atom types generator to current binary dir.
+		# It will effectively set CMAKE_BINARY_DIR, CMAKE_SOURCE_DIR,
+		# CMAKE_CURRENT_BINARY_DIR and CMAKE_CURRENT_SOURCE_DIR of the
+		# OpenCogAtomTypes.cmake to the CMAKE_CURRENT_BINARY_DIR
+		WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+		COMMENT "Generating opencog types"
+	)
 ENDMACRO()
 
 # =========================================================
