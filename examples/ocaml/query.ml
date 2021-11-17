78471f4edffa527e079e9b20dcd8da553ee7680f
Linas Vepstas <linasvepstas@gmail.com>
2021-11-17 10:05:52 -0600

    Start the query example.

---
+++ examples/ocaml/query.ml
@@ -0,0 +1,40 @@
+(**
+ * query.ml -- A simple query example.
+ *
+ * Example of performing AtomSpace queries in OCaml.
+ *)
+
+(* Populate the AtomSpace with contents *)
+evaluation [
+	predicate "likely";
+	list_link [
+		concept "foo" ;
+		concept "bar" ]
+	] ;;
+
+evaluation [
+	predicate "perhaps";
+	list_link [
+		concept "sunny" ;
+		concept "outdoors" ]
+	] ;;
+
+(* Take a look at the atomspace *)
+prtspace ()  ;;
+
+(* Build a query *)
+let qry =
+	query [
+		variable_list [
+			typed_variable [
+				variable "?x" ;
+				type_node "Concept" ]] ;
+		present [
+			evaluation [
+				predicate "perhaps";
+				list_link [
+					variable "?x" ;
+					concept "outdoors" ]]]] ;;
+
+
+(* That's all folks! *)
