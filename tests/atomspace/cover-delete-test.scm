8f4105261f242c328e413e76d677c1e66662980e
Linas Vepstas <linasvepstas@gmail.com>
2022-04-07 15:43:00 -0500

    Extend the the unit test for changing values

--- tests/atomspace/cover-delete-test.scm
+++ tests/atomspace/cover-delete-test.scm
@@ -26,7 +26,7 @@
 (define surface-space (cog-new-atomspace mid3-space))
 (cog-atomspace-cow! #t surface-space)
 
-; -------------------------------------------------------------------
+; ===================================================================
 ; Test that deep links are found correctly.
 
 (define deep-delete "test deep delete")
@@ -71,5 +71,44 @@
 
 (test-end deep-delete)
 
-; -------------------------------------------------------------------
+; ===================================================================
+; Building on the above, verify that values work
+
+(define deep-change "test deep change-delete")
+(test-begin deep-change)
+
+; Repeatedly add and remove the same atom
+(cog-set-atomspace! base-space)
+(cog-set-tv! (Concept "foo") (ctv 1 0 2))
+
+(cog-set-atomspace! mid2-space)
+(cog-set-tv! (Concept "foo") (ctv 1 0 4))
+
+(cog-set-atomspace! surface-space)
+(cog-set-tv! (Concept "foo") (ctv 1 0 6))
+
+; -----------------------------------
+; Should be present in the base space.
+(cog-set-atomspace! base-space)
+(test-assert "base-space" (cog-atom? (cog-node 'Concept "foo")))
+(test-equal "base-tv" 2 (inexact->exact (cog-count (cog-node 'Concept "foo"))))
+
+; Should be absent in the next level.
+(cog-set-atomspace! mid1-space)
+(test-assert "mid1-space" (nil? (cog-node 'Concept "foo")))
+
+(cog-set-atomspace! mid2-space)
+(test-assert "mid2-space" (cog-atom? (cog-node 'Concept "foo")))
+(test-equal "mid2-tv" 4 (inexact->exact (cog-count (cog-node 'Concept "foo"))))
+
+(cog-set-atomspace! mid3-space)
+(test-assert "mid3-space" (nil? (cog-node 'Concept "foo")))
+
+(cog-set-atomspace! surface-space)
+(test-assert "surface-space" (cog-atom? (cog-node 'Concept "foo")))
+(test-equal "surface-tv" 6 (inexact->exact (cog-count (cog-node 'Concept "foo"))))
+
+(test-end deep-change)
+
+; ===================================================================
 (opencog-test-end)
