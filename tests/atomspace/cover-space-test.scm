e1a40a5aa470ad6474baddd54b97ba9f92a3da6e
Linas Vepstas <linasvepstas@gmail.com>
2022-05-03 13:29:30 -0500

    Extend the unit test to check code in last commit

--- tests/atomspace/cover-space-test.scm
+++ tests/atomspace/cover-space-test.scm
@@ -8,6 +8,8 @@
 
 (opencog-test-runner)
 
+(define (get-cnt ATOM) (inexact->exact (cog-count ATOM)))
+
 ; -------------------------------------------------------------------
 ; Common setup, used by all tests.
 
@@ -15,12 +17,6 @@
 (define mid1-space (cog-new-atomspace base-space))
 (define mid2-space (cog-new-atomspace mid1-space))
 (define surface-space (cog-new-atomspace mid2-space))
-
-; -------------------------------------------------------------------
-; Test that deep links are found correctly.
-
-(define deep-link "test deep links")
-(test-begin deep-link)
 
 ; Splatter some atoms into the various spaces.
 (cog-set-atomspace! base-space)
@@ -32,6 +28,12 @@
 (cog-set-atomspace! mid2-space)
 (ListLink (Concept "foo") (Concept "bar") (ctv 1 0 5))
 
+; -------------------------------------------------------------------
+; Test that deep links are found correctly.
+
+(define deep-link "test deep links")
+(test-begin deep-link)
+
 ; Even though we work on the current surface, we expect to find
 ; the deeper ListLink.
 (cog-set-atomspace! surface-space)
@@ -42,6 +44,39 @@
 (test-equal "bar-space" mid1-space (cog-atomspace (gdr lilly)))
 
 (test-end deep-link)
+
+; -------------------------------------------------------------------
+; Test that deep links are covered correctly.
+
+(define deep-cover "test deep links")
+(test-begin deep-cover)
+
+; Alter counts in the outgoing set.
+; The new ListLink should pick up the counts on the deeper one.
+(Concept "foo" (ctv 1 0 9))
+(define litop (ListLink (Concept "foo") (Concept "bar")))
+
+; The mid-grade ListLink should be unchanged.
+(test-equal "link-space" mid2-space (cog-atomspace lilly))
+(test-equal "foo-space" base-space (cog-atomspace (gar lilly)))
+(test-equal "bar-space" mid1-space (cog-atomspace (gdr lilly)))
+
+; TV should be as before.
+(test-equal "link-tv" 5 (get-cnt lilly))
+(test-equal "foo-tv" 3 (get-cnt (gar lilly)))
+(test-equal "bar-tv" 4 (get-cnt (gdr lilly)))
+
+; The top-most ListLink should be different.
+(test-equal "link-top" surface-space (cog-atomspace litop))
+(test-equal "foo-top" surface-space (cog-atomspace (gar litop)))
+(test-equal "bar-top" mid1-space (cog-atomspace (gdr litop)))
+
+; Verify that the count on ListLink was copied over correctly
+(test-equal "top-link-tv" 5 (get-cnt litop))
+(test-equal "top-foo-tv" 9 (get-cnt (gar litop)))
+(test-equal "top-bar-tv" 4 (get-cnt (gdr litop)))
+
+(test-end deep-cover)
 
 ; -------------------------------------------------------------------
 (opencog-test-end)
