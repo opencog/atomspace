c0ee542871f559e96a2253328983da98a31393a7
Linas Vepstas <linasvepstas@gmail.com>
2022-07-18 09:48:02 -0500

    Expand unit test so it fails again.

--- tests/query/unordered-glob.scm
+++ tests/query/unordered-glob.scm
@@ -30,6 +30,42 @@
 	(Unordered (Any "C") (Predicate "B"))
 	(Unordered (Procedure "D") (Any "C"))
 	(Unordered (Anchor "E") (Procedure "D"))
+	(Unordered (Anchor "F") (Anchor "E"))
+	(Unordered (Anchor "G") (Anchor "F"))
 )
 
 ; (cog-execute! est)
+; ---------------------------------------------------------------------
+
+(define palin
+	(GetLink
+
+		(VariableList
+
+			(TypedVariable (Variable "$vA") (Type 'Predicate))
+			(TypedVariable (Variable "$vB") (Type 'Concept))
+			(TypedVariable (Variable "$vC") (Type 'Any))
+
+			(Variable "$vD")
+			(Glob "rest")
+		)
+
+		(Unordered
+			(Unordered (Variable "$vB") (Variable "$vA"))
+			(Unordered (Variable "$vC") (Variable "$vB"))
+			(Unordered (Variable "$vD") (Variable "$vC"))
+			(Glob "rest")
+		)
+	)
+)
+
+(Unordered
+	(Unordered (Concept "BB") (Predicate "AA"))
+	(Unordered (Any "CC") (Concept "BB"))
+	(Unordered (Procedure "DD") (Any "CC"))
+	(Unordered (Any "EE") (Procedure "DD"))
+	(Unordered (Concept "FF") (Any "EE"))
+	(Unordered (Predicate "GG") (Concept "FF"))
+)
+
+; ---------------------------------------------------------------------
