85cba1f7c3b58891483c7c09635b861c8749a88f
Linas Vepstas <linasvepstas@gmail.com>
2022-07-23 11:00:18 -0500

    Create a scheme module

---
+++ examples/type-system/demo-types/demo-types.scm
@@ -0,0 +1,17 @@
+;
+; OpenCog example module exposing new Atom Types.
+;
+; The code below is all "boilerplate" -- keep the general form, and
+; adjust the various names and paths to suit your needs.
+;
+(define-module (opencog demo-types))
+
+(use-modules (opencog))
+(use-modules (opencog demotype-config))
+
+; Load the C library that calls the classserver to load the types.
+(load-extension
+	(string-append opencog-ext-path-chemodemo "libchem-types")
+	"chem_types_init")
+
+(load-from-path "opencog/demo-types/chem_types.scm")
