64e3e3209871b28320ae85e120ce14923f6d176c
Linas Vepstas <linasvepstas@gmail.com>
2022-07-23 19:16:51 -0500

    Extend the unit test to chemistry with ordered links

--- tests/query/sparse-react-ordered.scm
+++ tests/query/sparse-react-ordered.scm
@@ -1,8 +1,7 @@
 ;
-; sparse-react.scm
+; sparse-react-ordered.scm
 ;
-; Simple chemistry demo. The chemistry here is wildly incorrect,
-; for several reasons. The biggest is missing constraints.
+; Same as sparse-react.scm but using ordered links.
 ;
 (use-modules (opencog))
 (use-modules (opencog exec))
@@ -10,7 +9,7 @@
 ; (load-from-path "chem_types.scm")
 
 ; This defines an esterification reaction.
-(define esterification
+(define estero
 	(BindLink
 		; Variable declaration
 		(VariableList
@@ -34,10 +33,10 @@
 		(AndLink
 			; Look for carboxyl group
 			(Molecule
-				(DB (Variable "$carboxyC1") (Variable "$carboxyO2"))
-				(SB (Variable "$carboxyC1") (Variable "$carboxyO1"))
-				(SB (Variable "$carboxyO1") (Variable "$carboxyH1"))
-				(SB (Variable "$carboxyC1") (Variable "carboxy moiety"))
+				(DBo (Variable "$carboxyC1") (Variable "$carboxyO2"))
+				(SBo (Variable "$carboxyC1") (Variable "$carboxyO1"))
+				(SBo (Variable "$carboxyO1") (Variable "$carboxyH1"))
+				(SBo (Variable "$carboxyC1") (Variable "carboxy moiety"))
 
 				; Globs match one or more bonds.  To match zero,
 				; change the lower bound by declaring it lik this:
@@ -53,28 +52,33 @@
 
 			; Look for hydroxyl group
 			(Molecule
-				(SB (Variable "$hydroxO1") (Variable "$hydroxH1"))
-				(SB (Variable "$hydroxO1") (Variable "hydroxy moiety"))
+				(SBo (Variable "$hydroxO1") (Variable "$hydroxH1"))
+				(SBo (Variable "$hydroxO1") (Variable "hydroxy moiety"))
 				(Glob "rest of hydroxy")
 			)
+
+			; The above will match the OH's in both groups.
+			; They should be kept distinct.
+			(Not (Identical (Variable "$carboxyO1") (Variable "$hydroxO1")))
+			(Not (Identical (Variable "$carboxyH1") (Variable "$hydroxH1")))
 		)
 		; Clause: Formation of products
 		(AndLink
 			; Produce ester
 			(Molecule
-				(DB (Variable "$carboxyC1") (Variable "$carboxyO2"))
-				(SB (Variable "$carboxyC1") (Variable "$carboxyO1"))
+				(DBo (Variable "$carboxyC1") (Variable "$carboxyO2"))
+				(SBo (Variable "$carboxyC1") (Variable "$carboxyO1"))
 
-				(SB (Variable "$carboxyC1") (Variable "carboxy moiety"))
+				(SBo (Variable "$carboxyC1") (Variable "carboxy moiety"))
 				(Glob "rest of carboxy")
 
-				(SB (Variable "$carboxyO1") (Variable "hydroxy moiety"))
+				(SBo (Variable "$carboxyO1") (Variable "hydroxy moiety"))
 				(Glob "rest of hydroxy")
 			)
 			; Produce water
 			(Molecule
-				(SB (Variable "$hydroxO1") (Variable "$carboxyH1"))
-				(SB (Variable "$hydroxO1") (Variable "$hydroxH1"))
+				(SBo (Variable "$hydroxO1") (Variable "$carboxyH1"))
+				(SBo (Variable "$hydroxO1") (Variable "$hydroxH1"))
 			)
 		)
 	)
@@ -85,22 +89,22 @@
 ;
 ; Carboxyl group
 (Molecule
-	(DB (C "the carboxyl carb") (O "oxy two"))
-	(SB (C "the carboxyl carb") (O "oxy one"))
-	(SB (O "oxy one") (H "carboxyl proton"))
-	(SB (C "the carboxyl carb") (Fe "carbox R"))
+	(DBo (C "the carboxyl carb") (O "oxy two"))
+	(SBo (C "the carboxyl carb") (O "oxy one"))
+	(SBo (O "oxy one") (H "carboxyl proton"))
+	(SBo (C "the carboxyl carb") (Fe "carbox R"))
 	; Some nonsense moiety, for pattern matching only.
-	(SB (Fe "carbox R") (Ni "more carbox junk"))
+	(SBo (Fe "carbox R") (Ni "more carbox junk"))
 )
 
 ; A hydroxyl group
 (Molecule
-	(SB (O "hydroxyl oxy") (H "hydroxyl proton"))
+	(SBo (O "hydroxyl oxy") (H "hydroxyl proton"))
 
 	; Another nonsense moiety, for pattern matching
-	(SB (C "hydroxyl carbon") (O "hydroxyl oxy"))
-	(SB (C "hydroxyl carbon") (Zn "hydrox R"))
-	(SB (Zn "hydrox R") (Cu "junk hydrox moiety"))
+	(SBo (C "hydroxyl carbon") (O "hydroxyl oxy"))
+	(SBo (C "hydroxyl carbon") (Zn "hydrox R"))
+	(SBo (Zn "hydrox R") (Cu "junk hydrox moiety"))
 )
 
 ; ------------------------------------------------
