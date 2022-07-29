3830062c6494447c54713bc6929a7f459a4f5443
Linas Vepštas <linasvepstas@gmail.com>
2022-07-29 16:46:51 -0500

    Add missing factory!

--- opencog/atoms/reduct/NumericFunctionLink.cc
+++ opencog/atoms/reduct/NumericFunctionLink.cc
@@ -326,4 +326,6 @@ ValuePtr NumericFunctionLink::execute(AtomSpace* as, bool silent)
 	return execute_binary(as, silent);
 }
 
+DEFINE_LINK_FACTORY(NumericFunctionLink, NUMERIC_FUNCTION_LINK);
+
 // ===========================================================
