5703f0a249adde4062403e7427e597cbb9e40346
Linas Vepštas <linasvepstas@gmail.com>
2022-07-29 17:40:37 -0500

    Add ImpulseLink unit tests

--- tests/atoms/reduct/bool-library-test.scm
+++ tests/atoms/reduct/bool-library-test.scm
@@ -48,6 +48,28 @@
 				(BoolValueOf foo key)
 				(BoolNot (BoolValueOf foo key))))))
 
+; --------
+
+(test-assert "impulse-link"
+	(equal? (FloatValue 1 0 1 0 1)
+		(cog-execute! (Impulse (BoolNot (BoolValueOf foo key))))))
+
+(test-assert "impulse and-not-link"
+	(equal? (FloatValue 0 0 0 0 0)
+		(cog-execute!
+			(Impulse
+				(BoolAnd
+					(BoolValueOf foo key)
+					(BoolNot (BoolValueOf foo key)))))))
+
+(test-assert "impulse or-not-link"
+	(equal? (FloatValue 1 1 1 1 1)
+		(cog-execute!
+			(Impulse
+				(BoolOr
+					(BoolValueOf foo key)
+					(BoolNot (BoolValueOf foo key)))))))
+
 (test-end tname)
 
 (opencog-test-end)
