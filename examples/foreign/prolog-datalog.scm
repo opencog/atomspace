5f64d27c529d8384f097f5256192736301670b53
Linas Vepstas <linas@linas.org>
2022-09-19 10:57:02 -0500

    More working examples

--- examples/foreign/prolog-datalog.scm
+++ examples/foreign/prolog-datalog.scm
@@ -13,5 +13,13 @@
 ; Take a look at how the above was converted into Atomese
 (cog-outgoing-atom (DatalogAst "likes(john, mary).") 0)
 
+
+; Various other types of expressions.
+; Sue is a girl if she is the daughter of Mary.
+(DatalogAst "girl(sue) :- daughter(sue,mary).")
+
+; Containing variables.
+(DatalogAst "friends(X, Y) :- likes(X, Y), likes(Y, X).")
+
 ; --------
 ; The End. That's all, folks!
