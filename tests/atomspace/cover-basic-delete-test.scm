8b1e13dc3db47455e596d46f8a93b32ab074e7cc
Linas Vepstas <linasvepstas@gmail.com>
2022-10-12 23:15:14 -0500

    Add a failing test

--- tests/atomspace/cover-basic-delete-test.scm
+++ tests/atomspace/cover-basic-delete-test.scm
@@ -17,8 +17,8 @@
 ; ===================================================================
 ; Test basic extraction.
 
-(define basic-delete "test basic extract")
-(test-begin basic-delete)
+(define basic-lo-delete "test basic extract from below")
+(test-begin basic-lo-delete)
 
 ; Setup ----
 (cog-set-atomspace! lower-space)
@@ -47,11 +47,47 @@
 (test-equal "upper-blown" 1 (length (cog-get-atoms 'Atom #t)))
 (test-equal "upper-b-only" (list b) (cog-get-atoms 'Atom #t))
 
-(test-end basic-delete)
+(test-end basic-lo-delete)
 
 ; ===================================================================
-; Building on the above, verify that values work
+; Same as above, except this time, extract from the upper space.
 
+(define basic-hi-delete "test basic extract from above")
+(test-begin basic-hi-delete)
+(begin
+
+	; Setup ----
+	(cog-set-atomspace! lower-space)
+	(define a (Concept "a"))
+	(define b (Concept "b"))
+
+	; Prevent any further modification
+	(cog-atomspace-ro! lower-space)
+
+	(cog-set-atomspace! upper-space)
+	(define li (Link a b))
+
+	; upper space should be COW
+	(cog-atomspace-cow! #t upper-space)
+
+	; Test ---
+	(cog-set-atomspace! lower-space)
+
+	(test-equal "lower-size" 2 (length (cog-get-atoms 'Atom #t)))
+	(cog-set-atomspace! upper-space)
+	(test-equal "upper-size" 3 (length (cog-get-atoms 'Atom #t)))
+
+	(test-assert "extract-fail" (not (cog-extract! a)))
+	(test-equal "ex-upper-size" 3 (length (cog-get-atoms 'Atom #t)))
+
+	(test-assert "extract-works" (cog-extract-recursive! a))
+	(test-equal "post-ex-upper-size" 1 (length (cog-get-atoms 'Atom #t)))
+	(test-equal "empty-income" 0 (length (cog-incoming-set b)))
+
+	; The lower lower space was protected. Bot a and b should be there.
+	(test-equal "lower-ok" 2 (length (cog-get-atoms 'Atom #t)))
+)
+(test-end basic-hi-delete)
 
 ; ===================================================================
 (opencog-test-end)
