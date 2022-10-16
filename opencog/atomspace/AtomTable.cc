118ea58e675f15ca8b2e2fffa92b0d28fa01fbe9
Linas Vepstas <linasvepstas@gmail.com>
2022-10-16 16:27:41 -0500

    Minor fixes for correctness

--- opencog/atomspace/AtomTable.cc
+++ opencog/atomspace/AtomTable.cc
@@ -271,11 +271,12 @@ Handle AtomSpace::add(const Handle& orig, bool force)
 
     // Atomspaces themselves are now Atoms. So it's not impossible for
     // someone to try to add them. But we don't actually want that to
-    // happen, at least, not right now.
+    // happen, at least, not right now. Unsupported, because its not
+    // really clear how this should work or how it should be used.
     if (ATOM_SPACE == orig->get_type()) return orig;
 
     // Is the atom already in this table, or one of its environments?
-    if (not force and in_environ(orig))
+    if (not force and in_environ(orig) and not orig->isAbsent())
         return orig;
 
     // Check to see if we already have this atom in the atomspace.
@@ -298,7 +299,8 @@ Handle AtomSpace::add(const Handle& orig, bool force)
             need_copy = true;
         else
             for (const Handle& h : atom->getOutgoingSet())
-                if (not in_environ(h)) { need_copy = true; break; }
+                if (not in_environ(h) or h->isAbsent())
+                  { need_copy = true; break; }
 
         if (need_copy) {
             // Insert the outgoing set of this link.
