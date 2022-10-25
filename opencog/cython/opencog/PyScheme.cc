6a70e02f6ecd9d6122de5c740a7f5938ed2a86ec
Linas Vepstas <linasvepstas@gmail.com>
2022-10-24 19:42:25 -0500

    iExplictly set the atomspace
    its not automatic any more.

--- opencog/cython/opencog/PyScheme.cc
+++ opencog/cython/opencog/PyScheme.cc
@@ -55,6 +55,7 @@ std::string opencog::eval_scheme(AtomSpace* as, const std::string &s)
 	OC_ASSERT(nullptr != as, "Cython failed to specify an atomspace!");
 	SchemeEval* evaluator = SchemeEval::get_evaluator(as);
 	evaluator->clear_pending();
+	evaluator->set_scheme_as(as);
 	std::string scheme_return_value = evaluator->eval(s);
 
 	// If there's an error, the scheme_return_value will contain
@@ -83,6 +84,7 @@ ValuePtr opencog::eval_scheme_v(AtomSpace* as, const std::string &s)
 
 	SchemeEval* evaluator = SchemeEval::get_evaluator(as);
 	evaluator->clear_pending();
+	evaluator->set_scheme_as(as);
 	ValuePtr scheme_return_value = evaluator->eval_v(s);
 
 	if (evaluator->eval_error())
@@ -104,6 +106,7 @@ Handle opencog::eval_scheme_h(AtomSpace* as, const std::string &s)
 
 	SchemeEval* evaluator = SchemeEval::get_evaluator(as);
 	evaluator->clear_pending();
+	evaluator->set_scheme_as(as);
 	Handle scheme_return_value = evaluator->eval_h(s);
 
 	if (evaluator->eval_error())
