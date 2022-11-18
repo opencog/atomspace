f53e4f5f546c8998162e18a70cdd140966d6fcb6
Linas Vepstas <linasvepstas@gmail.com>
2022-11-17 18:40:56 -0600

    Use the optional argg, and not "rest"

--- opencog/guile/SchemeSmob.cc
+++ opencog/guile/SchemeSmob.cc
@@ -346,8 +346,8 @@ void SchemeSmob::register_procs()
 	register_proc("cog-value",             2, 0, 0, C(ss_value));
 	register_proc("cog-value-type",        2, 0, 0, C(ss_value_type));
 	register_proc("cog-tv",                1, 0, 0, C(ss_tv));
-	register_proc("cog-atomspace",         0, 0, 1, C(ss_as));
-	register_proc("cog-as",                0, 0, 1, C(ss_as));
+	register_proc("cog-atomspace",         0, 1, 0, C(ss_as));
+	register_proc("cog-as",                0, 1, 0, C(ss_as));
 	register_proc("cog-mean",              1, 0, 0, C(ss_get_mean));
 	register_proc("cog-confidence",        1, 0, 0, C(ss_get_confidence));
 	register_proc("cog-count",             1, 0, 0, C(ss_get_count));
