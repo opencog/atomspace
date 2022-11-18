00abf7602e133d6a71aecee842f9307147ea4f2d
Linas Vepstas <linasvepstas@gmail.com>
2022-11-18 15:15:21 -0600

    Provide a basic unit test for the countng api.

---
+++ tests/matrix/marginal-test.scm
@@ -0,0 +1,21 @@
+
+(use-modules (opencog) (opencog test-runner))
+(use-modules (opencog matrix))
+
+(opencog-test-runner)
+
+; ---------------------------------------------------------------
+(define tmarg "simple marginal-counting test")
+(test-begin tmarg)
+
+(define epa (make-evaluation-pair-api
+	(Predicate "foo") 'Concept 'Concept 'Any 'Any "marge" "Marginal Test"))
+
+(define sep (add-pair-stars epa))
+(define cti (add-count-api sep))
+(define mgi (add-marginal-count cti))
+
+(mgi 'pair-inc (Concept "foo") (Concept "bar") 42)
+
+; (test-equal "two-arc l4" l4 lex)
+(test-end tmarg)
