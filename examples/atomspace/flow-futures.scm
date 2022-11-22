c2476d62a106075ed57e9564e7add8011044514e
Linas Vepstas <linasvepstas@gmail.com>
2022-11-21 18:47:21 -0600

    Continue developing the example

--- examples/atomspace/flow-futures.scm
+++ examples/atomspace/flow-futures.scm
@@ -40,4 +40,42 @@
 (observe "goodbye" "Adrian")
 (observe "goodbye" "Mike")
 
+; -------------------------------------------------------------
+; Set up the pipeline.
+
+; Counts are located in the third slot of the TV predicate.
+(define tvp (PredicateNode "*-TruthValueKey-*"))
+
+; We will use the formula MI(a,b) = log_2 N(a,b) N(*,*) / N(a,*) N(*,b)
+; to compute the MI of a pair.
+(DefineLink
+	(DefinedProcedure "dynamic MI")
+	(Lambda
+		(VariableList (Variable "$L") (Variable "$R"))
+		(Log2
+			(Divide
+				(Times
+					(FloatValueOf (List (Variable "$L") (Variable "$R")) tvp)
+					(FloatValueOf (AnyNode "grand total") tvp))
+				(Times
+					(FloatValueOf (List (Variable "$L") (Any "right wildcard")) tvp)
+					(FloatValueOf (List (Any "left wildcard") (Variable "$R")) tvp))))))
+
+; A utility to install the above formula on a pair.
+; The formula does no good if unless we stick it on the object.
+(define (install-formula THING-A THING-B)
+	(define pair (List THING-A THING-B))
+	(cog-set-value! pair (Predicate "MI Key")
+		(FormulaStream
+			(ExecutionOutput (DefinedProcedure "dynamic MI") pair))))
+
+; Convenience wrapper, works with strings.
+(define (install-mi STRING-A STRING-B)
+	(install-formula (Concept STRING-A) (Concept STRING-B)))
+
+; -------------------------------------------------------------
+; OK, we're done. Lets see what happens.
+
+(install-mi "hello" "world)
+
 ; ------- THE END -------
