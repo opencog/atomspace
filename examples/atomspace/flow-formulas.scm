17094d1b19124dbddeb3a84bdd483d0944b495fa
Linas Vepstas <linasvepstas@gmail.com>
2022-11-21 18:19:21 -0600

    Move the basic demo back to it's original location

--- examples/atomspace/flow-formulas.scm
+++ examples/atomspace/flow-formulas.scm
@@ -22,8 +22,9 @@
 ; a vector of arbitrary length.
 ;
 ; The FormulaStream generalizes the FutureTruthValue, so that it can
-; work with any FloatValue, not just TruthValues. This is demo'ed in
-; the `flow-futures.scm` file.
+; work with any FloatValue, not just TruthValues. An introductory demo
+; is provided at the bottom of this file. A more complex demo is in the
+; `flow-futures.scm` file.
 
 (use-modules (opencog) (opencog exec))
 
@@ -208,5 +209,39 @@
 ; And take another look.
 (format #t "A implies B has strength ~6F and confidence ~6F\n"
 	(cog-mean a-implies-b) (cog-confidence a-implies-b))
+
+; -------------------------------------------------------------
+; The FormulaStream is the generalization of FutureTruthValue, suitable
+; for streaming a FloatValue of arbitrary length. As before, whenever it
+; is accessed, the current vector value is recomputed. The recomputation
+; forced by calling `execute()` on the Atom that the stream is created
+; with.
+;
+; Create an Atom, a key, and a random stream of five numbers.
+; The random stream is a FloatValue vector, of length 5; each of
+; the numbers are randomly distributed between 0.0 and 1.0
+(define foo (Concept "foo"))
+(define bar (Concept "bar"))
+(define akey (Predicate "some key"))
+(define bkey (Predicate "other key"))
+
+(cog-set-value! foo akey (RandomStream 5))
+
+; Take a look at what was created.
+(cog-value foo akey)
+
+; Verify that it really is a vector, and that it changes with each
+; access. The StreamValueOfLink will sample from the RandomStream.
+(cog-execute! (StreamValueOf foo akey))
+
+; Apply a formula to that stream, to get a different stream.
+(define fstream (FormulaStream (Plus (Number 10) (ValueOf foo akey))))
+
+; Place it on an atom, take a look at it, and make sure that it works.
+(cog-set-value! bar bkey fstream)
+(cog-value bar bkey)
+(cog-execute! (StreamValueOf bar bkey))
+(cog-execute! (StreamValueOf bar bkey))
+(cog-execute! (StreamValueOf bar bkey))
 
 ; ------- THE END -------
--- examples/atomspace/flow-futures.scm
+++ examples/atomspace/flow-futures.scm
@@ -1,61 +1,23 @@
 ;
 ; flow-futures.scm -- Dynamically changing FloatValue flows.
 ;
-; The concept of a "value flow" is the idea that a Value can change
-; dynamically, recomputed from a formula that draws on it's inputs.
-; Examples of such formulas are provided below, together with the
-; code for wiring them into Atoms.
+; The `flow-formulas.scm` demo showed how to attach dynamically-updating
+; TruthValues to Atoms. This demo is similar, except that it works with
+; general FloatValues.  The specific example here computes the mutual
+; information of an ordered pair, given only counts on the pair, and
+; counts on the marginals.  This requires doing arithmetic on numbers
+; coming from four different places, and then placing the result where
+; it can be found.
 ;
-; The core implementation is in two parts: the FutureTruthValue,
-; which implements a dynamically-variable TruthValue, and the
-; FormulaPredicateLink, which specifies the formula used to compute
-; the TruthValue.
+; This is a fairly complex demo, as it attempts to be more realistic.
 ;
-; The FutureTruthValue is a kind of SimpleTruthValue, such that, every
-; time that it is accessed, the current value -- that is, the current
-; pair of floating point numbers -- is recomputed.  The recomputation
-; occurs every time the numeric value is accessed (i.e. when the
-; strength and confidence of the TV are accessed).
-;
-; The FormulaStream is a generalization of the FutureTruthValue, in
-; that it allows for the computation of any FloatValue. That is, the
-; SimpleTV's are just vectors of length two - the strength and
-; confidence, whereas the FloatValue is a vector of arbitrary length.
+; As before, the core function is provided by the FormulaStream, which
+; which wraps an arithmetic expression so that it behaves like a future.
+; See https://en.wikipedia.org/wiki/Futures_and_promises for the general
+; idea.
 
 (use-modules (opencog) (opencog exec))
 
 ; -------------------------------------------------------------
-; The FormulaStream is the generalization of FutureTruthValue, suitable
-; for streaming a FloatValue of arbitrary length. As before, whenever it
-; is accessed, the current vector value is recomputed. The recomputation
-; forced by calling `execute()` on the Atom that the stream is created
-; with.
-;
-; Create an Atom, a key, and a random stream of five numbers.
-; The random stream is a FloatValue vector, of length 5; each of
-; the numbers are randomly distributed between 0.0 and 1.0
-(define foo (Concept "foo"))
-(define bar (Concept "bar"))
-(define akey (Predicate "some key"))
-(define bkey (Predicate "other key"))
-
-(cog-set-value! foo akey (RandomStream 5))
-
-; Take a look at what was created.
-(cog-value foo akey)
-
-; Verify that it really is a vector, and that it changes with each
-; access. The StreamValueOfLink will sample from the RandomStream.
-(cog-execute! (StreamValueOf foo akey))
-
-; Apply a formula to that stream, to get a different stream.
-(define fstream (FormulaStream (Plus (Number 10) (ValueOf foo akey))))
-
-; Place it on an atom, take a look at it, and make sure that it works.
-(cog-set-value! bar bkey fstream)
-(cog-value bar bkey)
-(cog-execute! (StreamValueOf bar bkey))
-(cog-execute! (StreamValueOf bar bkey))
-(cog-execute! (StreamValueOf bar bkey))
 
 ; ------- THE END -------
