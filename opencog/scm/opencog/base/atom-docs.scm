0ac59f3370c5432d32b52b427abfad8d2cce4e3a
Linas Vepstas <linasvepstas@gmail.com>
2022-11-25 10:41:51 -0600

    More documentation

--- opencog/scm/opencog/base/atom-docs.scm
+++ opencog/scm/opencog/base/atom-docs.scm
@@ -23,9 +23,42 @@
           match-body
           rewrite
 
+  See https://wiki.opencog.org/w/QueryLink for more info.
+
   See also:
      BindLink -- same as above, results placed in SetLink
      GetLink -- no rewrite, results placed in SetLink
      MeetLink -- no rewrite, results place in QueueValue
+     SatisfactionLink -- return true, if there are any results at all.
+     DualLink -- pattern recognition instead of pattern matching
      JoinLink -- union, instead of intersection
 ")
+
+(set-procedure-property! Query 'documentation
+	(procedure-property QueryLink 'documentation))
+
+; ---------------------------------------------------------------
+; XXX FIXME replace below by real docs.
+(set-procedure-property! BindLink 'documentation
+	(procedure-property QueryLink 'documentation))
+
+(set-procedure-property! BindLink 'documentation
+	(procedure-property QueryLink 'documentation))
+
+(set-procedure-property! GetLink 'documentation
+	(procedure-property QueryLink 'documentation))
+
+(set-procedure-property! Get 'documentation
+	(procedure-property QueryLink 'documentation))
+
+(set-procedure-property! MeetLink 'documentation
+	(procedure-property QueryLink 'documentation))
+
+(set-procedure-property! Meet 'documentation
+	(procedure-property QueryLink 'documentation))
+
+(set-procedure-property! SatisfactionLink 'documentation
+	(procedure-property QueryLink 'documentation))
+
+(set-procedure-property! Satisfaction 'documentation
+	(procedure-property QueryLink 'documentation))
