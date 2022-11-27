685f6b442c94d56ee07a093535dfd50ae150526f
Linas Vepstas <linasvepstas@gmail.com>
2022-11-26 19:30:18 -0600

    Add new unit test.

--- tests/persist/flow/value-of-test.scm
+++ tests/persist/flow/value-of-test.scm
@@ -1,5 +1,5 @@
 ;
-; value-of-test.scm -- Unit test for FetchValueOf
+; value-of-test.scm -- Unit test for FetchValueOf, StoreValueOf
 ;
 (use-modules (opencog) (opencog persist) (opencog persist-sql))
 (use-modules (opencog exec))
@@ -10,13 +10,32 @@
 (define tname "fetch-value-of")
 (test-begin tname)
 
-; Could be anything .. rocks, etc. but we just use Postgres for now.
-(define sto (PostgresStorageNode "postgres:///opencog_test"))
+; Could be any StorageNode, e.g. RocksStorageNode, etc.
+; but, for convenience, we just use Postgres for now.
+(define sto (PostgresStorageNode
+	"postgres:///opencog_test?user=opencog_tester&password=cheese"))
 (cog-open sto)
 
 (define fvof (FetchValueOf (Concept "a") (Predicate "foo") sto))
-(test-assert "No val" #f (cog-execute! fvof))
+(test-assert "No val" (nil? (cog-execute! fvof)))
 
+(define sv (StringValue "a" "b" "c"))
+(cog-set-value! (Concept "a") (Predicate "foo") sv)
+(test-assert "Unchanged" (equal? sv (cog-execute! fvof)))
+
+(define fv (FloatValue 1 2 3))
+(cog-set-value! (Concept "a") (Predicate "foo") fv)
+(test-assert "Still Unchanged" (equal? fv (cog-execute! fvof)))
+
+(define svof (StoreValueOf (Concept "a") (Predicate "foo") sto))
+(test-assert "maybe-stored" (equal? fv (cog-execute! svof)))
+
+; Set it to string, it should reset back to float.
+(cog-set-value! (Concept "a") (Predicate "foo") sv)
+(test-assert "Reset" (equal? fv (cog-execute! fvof)))
+
+(cog-delete! (Concept "a"))
+(cog-close sto)
 (test-end tname)
 
 (opencog-test-end)
