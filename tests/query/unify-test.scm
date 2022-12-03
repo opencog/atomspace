e9ff95bb9f913ebf8ca6d85f7fc23de064dd2fcf
Linas Vepstas <linasvepstas@gmail.com>
2022-12-02 20:41:21 -0600

    Port UnifyUTest::test_unify_basic_5

--- tests/query/unify-test.scm
+++ tests/query/unify-test.scm
@@ -10,6 +10,7 @@
 
 (opencog-test-runner)
 
+; --------------------------------------
 (test-begin "UnifyUTest::test_unify_4")
 (define ru4
 	(cog-execute!
@@ -25,5 +26,22 @@
 		(Set (List (Concept "A") (Concept "B")))))
 
 (test-end "UnifyUTest::test_unify_4")
+
+; --------------------------------------
+(test-begin "UnifyUTest::test_unify_5")
+(define ru5
+	(cog-execute!
+		(Get
+			(Variable "$X")
+			(Identical
+				(Inheritance (Variable "$X") (Variable "$Y"))
+				(Inheritance (Concept "A") (Variable "$Y"))))))
+
+(format #t "Got ~A\n" ru5)
+(test-assert "UnifyUTest::test_unify_5"
+	(equal? ru5
+		(Set (Concept "A"))))
+
+(test-end "UnifyUTest::test_unify_5")
 
 (opencog-test-end)
