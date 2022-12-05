990e5e160ae151762387c8b2d7d61c07dcaea846
Linas Vepstas <linasvepstas@gmail.com>
2022-12-05 03:09:03 -0600

    Fix an old and appearantly broken unit test!

--- tests/query/quote-crash.scm
+++ tests/query/quote-crash.scm
@@ -6,16 +6,22 @@
 ; to stop the recursion.
 (define crasher
 	(BindLink
-		(VariableNode "$x")
+		(VariableNode "$x") ; Variable decl
+		(VariableNode "$x") ; body
 		(ListLink
 			(ConceptNode "And the answer is ...")
 			(QuoteLink (VariableNode "$x")))))
+
+; (cog-execute! crasher)
 
 ;; This is an infinite loop, because there are no type restrictions on
 ;; the variable, and the instantiator can get confused.
 (define infloop
 	(BindLink
-		(VariableNode "$x")
+		(VariableNode "$x") ; Variable decl
+		(VariableNode "$x") ; body
 		(ListLink
 			(ConceptNode "And the answer is ...")
 			(VariableNode "$x"))))
+
+; (cog-execute! infloop)
