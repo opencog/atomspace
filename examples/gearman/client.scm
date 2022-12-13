fef554e54c50f8b92777a61fa5594ddf7e0a5e44
Linas Vepstas <linasvepstas@gmail.com>
2022-12-13 13:51:18 -0600

    Remove other traces of gearman

--- examples/gearman/client.scm
+++
@@ -1,7 +0,0 @@
-(use-modules (opencog dist-gearman))
-(use-modules (ice-9 threads))
-
-(define (send-work x) (dist-eval "(do-work 120 120)" "client"))
-(define result (par-map send-work (iota 10)))
-
-;; (exit-all-workers)
