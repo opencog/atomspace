cb232a418c25614dc6834d8383c71dbc52b138a2
Linas Vepstas <linasvepstas@gmail.com>
2023-03-09 13:30:11 -0600

    Work around linker-loader bug in Ubuntu 22.04
    Don't know what the deal is, but shared library constructor
    priorities are not being honored.  This causes derived types to
    be loaded before base types, resulting in obscure crashes in
    unexpeted places.

--- opencog/atoms/atom_types/atom_types.cc
+++ opencog/atoms/atom_types/atom_types.cc
@@ -9,15 +9,32 @@
 // Set constructor priority to 101 because we want this to be
 // the very first one that runs, before any of the other type
 // declarations run, and certainly before the factories run.
+//
+// As of March 2023, Ubuntu 22.04 Jammy LTS is not honoring these
+// priorities. Code below hacks around this. Buggy combination:
+//    gcc (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0
+//    GNU ld (GNU Binutils for Ubuntu) 2.38
+//    ldd (Ubuntu GLIBC 2.35-0ubuntu3.1) 2.35
+//
 #ifdef LOAD_ME_FIRST
 #define CTOR_PRIO (101)
 #else
 #define CTOR_PRIO (102)
 #endif
 
+#ifndef LOAD_ME_FIRST
+extern "C" {
+void base_types_init(void);
+};
+#endif
+
 // Library initialization
 static __attribute__ ((constructor CTOR_PRIO)) void init(void)
 {
+#ifndef LOAD_ME_FIRST
+	base_types_init();
+#endif
+
 #define str(x) #x
 #define xstr(x) str(x)
 
@@ -44,8 +61,14 @@ extern "C" {
 // Calling this forces this shared-lib to load, thus calling the 
 // constructor above, thus causing the atom types to be loaded into
 // the atomspace.
+//
+// See notes above about Ubuntu Jammy. Some 2023-era linkers seem to
+// be bungling the constructor order. Thus, belt and suspenders:
+// explicitly call the shared library ctor, just in case it got called
+// in the wrong order. Sheesh.
+//
 void INITNAME(void)
 {
-	/* No-op */
+	init();
 }
 };
