20e3a50994bc3ef3484f688db593fdff2393e3cb
Linas Vepštas <linasvepstas@gmail.com>
2024-04-03 20:10:58 -0500

    Start working on a demo for the filter.

---
+++ examples/pattern-matcher/filter-value.scm
@@ -0,0 +1,59 @@
+;
+; filter-value.scm -- Using FilterLink for processing data streams
+;
+; The FilterLink implements a link type analogous to the `filter-map`
+; function commonly found in functional programming languages, such as
+; the scheme srfi-1 `filter-map`, or `map` in haskell.
+;
+; This demo focuses on filtering data streams; please review `filter.scm`
+; for a more basic introduction to filtering.
+;
+(use-modules (opencog) (opencog exec))
+
+; Begin by creating a mockup of a complicated data stream. What makes
+; this "complicated" is that it will consist of nested LinkValues,
+; a tree of LinkValues, with different kinds of data in the tree.
+
+(define word-list-a
+	(LinkValue (Concept "this") (Concept "is") (Concept "a") (Concept "test")))
+(define word-list-b
+	(LinkValue (Concept "moar") (Concept "stuffs") (Concept "ok")))
+
+(define edge-a1 (Edge (Bond "pair") (List (Concept "this") (Concept "is"))))
+(define edge-a2 (Edge (Bond "pair") (List (Concept "is") (Concept "test"))))
+(define edge-a3 (Edge (Bond "pair") (List (Concept "a") (Concept "test"))))
+(define edge-b1 (Edge (Bond "pear") (List (Concept "moar") (Concept "stuffs"))))
+(define edge-b2 (Edge (Bond "pear") (List (Concept "moar") (Concept "ok"))))
+
+(define tree-list
+	(LinkValue
+		(LinkValue
+			(LinkValue (Concept "sentence") word-list-a)
+			(LinkValue (Concept "parse") edge-a1 edge-a2 edge-a3))
+		(LinkValue
+			(LinkValue (Concept "sentence") word-list-b)
+			(LinkValue (Concept "parse") edge-b1 edge-b2))))
+
+; Place the tree-list in a well-known location, where we can find it.
+(cog-set-value!
+	(Node "some place") (Predicate "some key") tree-list)
+
+(define get-parse-edges
+	(Filter
+		; Define a pattern that will extract all of the edges from
+		; the tree list.
+		(Lambda
+			(LinkSignature
+				(Type 'LinkValue)
+				(Variable "$x")
+			))
+
+		; The graph from which a value is to be extracted.  Clearly,
+		; the variable $x corresponds to Concept "baz"
+		(ValueOf (Node "some place") (Predicate "some key")))
+)
+
+; This should return ...
+; as that is the extracted value
+; for the variable $x.
+(cog-execute! get-parse-edges)
