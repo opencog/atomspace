6640bb222c8c1b1c35fe182659126cf2dadfc541
Linas Vepštas <linasvepstas@gmail.com>
2024-04-23 13:23:49 -0500

    We really do need to ground here.

--- opencog/atoms/flow/FilterLink.cc
+++ opencog/atoms/flow/FilterLink.cc
@@ -330,9 +330,7 @@ bool FilterLink::extract(const Handle& termpat,
 	{
 		// Check the type of the value.
 		if (not _mvars->is_type(termpat, vgnd)) return false;
-
-		// Not the right place to record a grounding...
-		// valmap.emplace(std::make_pair(termpat, vgnd));
+		valmap.emplace(std::make_pair(termpat, vgnd));
 		return true;
 	}
 
