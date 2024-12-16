df42a3e0341212d5ff83f8b9b8d2e73d1e35283e
Linas Vepštas <linasvepstas@gmail.com>
2024-12-16 14:44:04 -0600

    The group-by test now passes. Add it to the collection.

--- tests/query/group-by-test.scm
+++ tests/query/group-by-test.scm
@@ -1,12 +1,14 @@
 ;
-; group-by-test.scm
-;
-; Unit test for the GroupBy link
+; group-by-test.scm -- Unit test for the GroupLink
 ;
 ; Run this manually by saying "guile -s group-by-test.scm"
 
 (use-modules (opencog) (opencog exec))
 (use-modules (opencog test-runner))
+
+(opencog-test-runner)
+(define tname "group-by-test")
+(test-begin tname)
 
 ; Base data to search over
 
@@ -30,7 +32,11 @@
 					(List (Variable "$X") (Variable "$Y")))))))
 
 (define meet-results (cog-execute! grp-meet))
-(format #t "The meet results are ~A\n" meet-results)
+; (format #t "The meet results are ~A\n" meet-results)
+; (format #t "There are ~A results\n" (length (cog-value->list meet-results)))
+
+(test-assert "meet group size"
+	(equal? 3 (length (cog-value->list meet-results))))
 
 (define grp-query
 	(Query
@@ -40,7 +46,14 @@
 			(Present
 				(Edge (Predicate "property")
 					(List (Variable "$X") (Variable "$Y")))))
-		(List (Variable "$Y") (Variable "$X"))))
+		(Edge (Predicate "go together")
+			(List (Variable "$Y") (Variable "$X")))))
 
 (define query-results (cog-execute! grp-query))
 ; (format #t "The query results are ~A\n" query-results)
+
+(test-assert "query group size"
+	(equal? 3 (length (cog-value->list query-results))))
+
+(test-end tname)
+(opencog-test-end)
