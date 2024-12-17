cab67f2722d6f39590d2114fd4d400314aa99d0a
Linas Vepštas <linasvepstas@gmail.com>
2024-12-16 23:23:49 -0600

    Fix unbounded limit behavior

--- tests/query/group-by-test.scm
+++ tests/query/group-by-test.scm
@@ -97,5 +97,24 @@
 
 ; -------------------------------------------------------------
 
+(define unbounded-range
+	(Query
+		(VariableList (Variable "$X") (Variable "$Y"))
+		(And
+			(Group
+				(Variable "$Y")
+				(Interval (Number 2) (Number -1)))
+			(Present
+				(Edge (Predicate "property")
+					(List (Variable "$X") (Variable "$Y")))))
+		(Variable "$Y")))
+
+(define unbounded-results (cog-execute! unbounded-range))
+; (format #t "The unbounded results are ~A\n" unbounded-results)
+(test-assert "range unbounded size"
+	(equal? 2 (length (cog-value->list unbounded-results))))
+
+; -------------------------------------------------------------
+
 (test-end tname)
 (opencog-test-end)
