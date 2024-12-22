a8ec108d8f489ed338bc6b39a01397eb1978c818
Linas Vepštas <linasvepstas@gmail.com>
2024-12-21 22:29:24 -0600

    Convert unit test to formal test

--- tests/atoms/flow/string-of-test.scm
+++ tests/atoms/flow/string-of-test.scm
@@ -1,9 +1,21 @@
 ;
-; string-of-test.scm
+; string-of-test.scm -- Test the StringOfLink for basic fuction
 ;
 (use-modules (opencog) (opencog exec))
+(use-modules (opencog test-runner))
+
+(opencog-test-runner)
+(define tname "string-of-test")
+(test-begin tname)
+; -----------
 
 (define node-to-node
 	(cog-execute! (StringOf (Type 'Concept) (Predicate "bar"))))
 (format #t "Got ~A\n" node-to-node)
 
+(test-assert "Node-to-node"
+	(equal? node-to-node (Concept "bar")))
+
+; -----------
+(test-end tname)
+(opencog-test-end)
