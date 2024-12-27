01290fede43949864383a65c3f7f4d5ae9f1d745
Linas Vepstas <linas@linas.org>
2024-12-26 20:40:37 -0600

    Start implementation of Concatenation

--- opencog/atoms/flow/ConcatenateLink.cc
+++ opencog/atoms/flow/ConcatenateLink.cc
@@ -1,7 +1,7 @@
 /*
  * ConcatenateLink.cc
  *
- * Copyright (C) 2015, 2022, 2024 Linas Vepstas
+ * Copyright (C) 2015, 2022 Linas Vepstas
  *
  * Author: Linas Vepstas <linasvepstas@gmail.com>  January 2009
  *
@@ -22,6 +22,7 @@
  */
 
 #include <opencog/atomspace/AtomSpace.h>
+#include <opencog/atoms/core/TypeNode.h>
 #include <opencog/atoms/value/LinkValue.h>
 
 #include "ConcatenateLink.h"
@@ -38,25 +39,63 @@ ConcatenateLink::ConcatenateLink(const HandleSeq&& oset, Type t)
 			"Expecting an ConcatenateLink, got %s", tname.c_str());
 	}
 
-	if (1 != _outgoing.size())
+	if (1 != _outgoing.size() and 2 != _outgoing.size())
 		throw InvalidParamException(TRACE_INFO,
-			"ConcatenateLink expects one arg");
+			"ConcatenateLink expects one or two args, got %s",
+				to_string().c_str());
 }
 
 // ---------------------------------------------------------------
 
-/// Return a SetLink vector.
+/// Return a concatenation of lists.
 ValuePtr ConcatenateLink::execute(AtomSpace* as, bool silent)
 {
-	// If the given Atom is executable, then execute it.
-	// In effectively all cases, we expect it to be executable!
-	Handle base(_outgoing[0]);
+	int coff = 0;
+	Type otype = LINK_VALUE;
+
+	// If there are two args, then the first one specifies the
+	// output type.
+	if (2 == _outgoing.size())
+	{
+		coff = 1;
+
+		// FIXME: _outoging[0] could be executable, in which case
+		// is should be executed, first. But I'm lazy. Also:
+		// instead of being a simple type, the output could be
+		// a complicated signature. Again, I'm lazy.
+		if (not _outgoing[0]->is_type(TYPE_NODE))
+			throw InvalidParamException(TRACE_INFO,
+				"Expecting first arg of a ConcatenateLink to be a type, got %s",
+					to_string().c_str());
+		otype = TypeNodeCast(_outgoing[0])->get_kind();
+	}
+
+	// If the given Atom is not executable, we deal with a the
+	// simplest case of just collapsing a plain link.
+	Handle base(_outgoing[coff]);
 	if (not base->is_executable())
-		return as->add_link(SET_LINK, base);
+	{
+		// We expect a link. We can throw, or we can be silent!?
+		// I dunno. Flip a coin.
+		if (not base->is_link()) return base;
+		HandleSeq oset;
+		for (const Handle& oli : base->getOutgoingSet())
+		{
+			if (oli->is_link())
+			{
+				const HandleSeq& los = oli->getOutgoingSet();
+				oset.insert(oset.end(), los.begin(), los.end());
+			}
+			else
+				oset.push_back(oli);
+		}
+		if (0 == coff) otype = base->get_type();
+		return as->add_link(otype, std::move(oset));
+	}
 
 	ValuePtr vp = base->execute(as, silent);
 	if (vp->is_atom())
-		return as->add_link(SET_LINK, HandleCast(vp));
+		return as->add_link(otype, HandleCast(vp));
 
 	// If its a FloatValue, we could maybe return a NumberNode??
 	// so be linke NumberOfLink ???
@@ -69,7 +108,7 @@ ValuePtr ConcatenateLink::execute(AtomSpace* as, bool silent)
 
 	LinkValuePtr lvp = LinkValueCast(vp);
 	HandleSeq hs = lvp->to_handle_seq();
-	return as->add_link(SET_LINK, std::move(hs));
+	return as->add_link(otype, std::move(hs));
 }
 
 DEFINE_LINK_FACTORY(ConcatenateLink, CONCATENATE_LINK)
