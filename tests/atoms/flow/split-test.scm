66644385e3dfd7957579a33a4f6492a75b2ffc8f
Linas Vepstas <linas@linas.org>
2024-12-28 15:47:37 -0600

    Add Unit test for SplitLink

---
+++ tests/atoms/flow/split-test.scm
@@ -0,0 +1,38 @@
+;
+; split-test.scm -- Verify that SplitLin works.
+;
+
+(use-modules (opencog) (opencog exec))
+(use-modules (opencog test-runner))
+
+(opencog-test-runner)
+(define tname "split-test")
+(test-begin tname)
+
+; -------------------------------------------------------------
+; Base function: static non-executable stuff.
+
+(cog-set-value! (Anchor "rock") (Predicate "blab")
+	(StringValue "this is a test" "and so is this"))
+
+(define splitter
+	(Split (ValueOf (Anchor "rock") (Predicate "blab"))))
+
+(define words (cog-execute! splitter))
+
+(test-assert "set-abc"
+	(equal? words
+		(LinkValue
+			(StringValue "this")
+			(StringValue "is")
+			(StringValue "a")
+			(StringValue "test")
+			(StringValue "and")
+			(StringValue "so")
+			(StringValue "is")
+			(StringValue "this"))))
+
+; -------------------------------------------------------------
+(test-end tname)
+
+(opencog-test-end)
