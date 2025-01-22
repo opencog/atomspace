819af4ba7a4359cace950293ac0e37c46a868182
Linas Vepštas <linasvepstas@gmail.com>
2025-01-22 00:20:00 -0600

    Remove the file-loader test; it goes to storage

--- tests/cython/utilities/test_utilities.py
+++ tests/cython/utilities/test_utilities.py
@@ -7,7 +7,7 @@ from unittest import TestCase
 
 from opencog.type_constructors import *
 from opencog.atomspace import AtomSpace
-from opencog.utilities import initialize_opencog, finalize_opencog, load_file, is_closed, get_free_variables
+from opencog.utilities import initialize_opencog, finalize_opencog, is_closed, get_free_variables
 
 __author__ = 'Curtis Faith'
 
@@ -30,27 +30,6 @@ class UtilitiesTest(TestCase):
     def test_initialize_finalize(self):
         initialize_opencog(self.atomspace)
         finalize_opencog()
-
-    def test_fast_load(self):
-        gen_atoms(self.atomspace)
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            tmp_file = os.path.join(tmpdirname, 'tmp.scm')
-            write_sorted_file(tmp_file, self.atomspace)
-            new_space = AtomSpace()
-            load_file(tmp_file, new_space)
-            self.assertTrue(len(new_space) == len(self.atomspace))
-            # files should be binary equal
-            new_tmp = os.path.join(tmpdirname, 'tmp1.scm')
-            write_sorted_file(new_tmp, new_space)
-            self.assertTrue(filecmp.cmp(tmp_file, new_tmp, shallow=False), "files are not equal")
-            checklist = """(ListLink(ConceptNode "vfjv\\"jnvfé")
-                (ConceptNode "conceptIR~~gF\\",KV")
-                (ConceptNode "вверху плыли редкие облачка"))"""
-            with open(tmp_file, 'wt') as f:
-                f.write(checklist)
-            new_space1 = AtomSpace()
-            load_file(tmp_file, new_space1)
-            self.assertTrue(len(new_space1) == 4)
 
     def test_is_closed(self):
         A = self.atomspace.add_node(types.ConceptNode, 'A')
