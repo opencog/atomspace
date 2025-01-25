a393acbc254d9a5238d614ff10a02324566c5a47
Linas Vepštas <linasvepstas@gmail.com>
2025-01-25 13:31:56 -0600

    Avoid issues due to shared results queues

--- tests/atoms/parallel/threaded.scm
+++ tests/atoms/parallel/threaded.scm
@@ -23,22 +23,29 @@
 
 ; (cog-execute! pexec)
 
-; one-hundred things to do in two threads.
+; One-hundred things to do in two threads.
+; This is set up so that each Query differs from all the others.
+; This avoids issues when the same Query/Meet is run in parallel:
+; both instances place results on the same result queue; if one
+; instance closes the queue while the other is still adding to it,
+; an exception gets thrown and everything goes haywire.
 (define pmany
 	(ExecuteThreaded
 		(Number 2)
 		(Set
 			(map
 				(lambda (n)
-					(Meet
+					(Query
 						(TypedVariable (Variable "X") (Type 'Concept))
-						(Inheritance (Variable "X") (Concept "mineral"))))
+						(Inheritance (Variable "X") (Concept "mineral"))
+						(List (Number n) (Variable "X"))))
 				(iota 50))
 			(map
 				(lambda (n)
-					(Meet
+					(Query
 						(TypedVariable (Variable "X") (Type 'Concept))
-						(Inheritance (Variable "X") (Concept "plant"))))
+						(Inheritance (Variable "X") (Concept "plant"))
+						(List (Number n) (Variable "X"))))
 				(iota 50))
 	)))
 
