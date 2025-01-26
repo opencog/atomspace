921f58ecfc314bbf1f12f54902041808e0b92364
Linas Vepštas <linasvepstas@gmail.com>
2025-01-25 23:08:35 -0600

    Start work on a unit test for the marginals

---
+++ tests/query/marginals-test.scm
@@ -0,0 +1,61 @@
+
+(use-modules (opencog) (opencog exec))
+
+(Edge (Predicate "foo") (List (Item "left") (Item "right")))
+(Edge (Predicate "foo") (List (Item "lefter") (Item "right")))
+(Edge (Predicate "foo") (List (Item "leftest") (Item "right")))
+(Edge (Predicate "foo") (List (Item "lefty") (Item "loosey")))
+
+(define m (Meet
+	(Variable "$x")
+	(Present
+		(Edge (Predicate "foo") (List (Variable "$x")(Item "right"))))))
+
+(format #t "Meet keys ~A\n" (cog-keys m))
+(cog-execute! m)
+(format #t "Meet grnd ~A\n" (cog-value m m))
+(format #t "Meet vars ~A\n" (cog-value m (Variable "$x")))
+
+(define q (Query
+	(Variable "$x")
+	(Present
+		(Edge (Predicate "foo") (List (Variable "$x")(Item "right"))))
+	(Link (Item "fumble") (Variable "$x"))))
+
+(format #t "Query keys ~A\n" (cog-keys q))
+(cog-execute! q)
+
+(format #t "Query results ~A\n" (cog-value q q))
+(format #t "Query vars ~A\n" (cog-value q (Variable "$x")))
+
+(define m2 (Meet
+	(Present
+		(Edge (Predicate "foo") (List (Variable "$x")(Item "right"))))))
+
+(define m3 (Meet
+	(TypedVariable (Variable "$x") (Type "Item"))
+	(Present
+		(Edge (Predicate "foo") (List (Variable "$x")(Item "right"))))))
+
+(define m4 (Meet
+	(VariableList (TypedVariable (Variable "$x") (Type "Item")))
+	(Present
+		(Edge (Predicate "foo") (List (Variable "$x")(Item "right"))))))
+
+(define m5 (Meet
+	(VariableList
+		(Variable "$loo")
+		(TypedVariable (Variable "$ro") (Type "Item")))
+	(Present
+		(Edge (Predicate "foo")
+			(List (Variable "$loo")(Variable "$ro"))))))
+
+(format #t "Meet5 keys ~A\n" (cog-keys m5))
+(cog-execute! m5)
+
+(format #t "M5 results ~A\n" (cog-value m5 m5))
+(format #t "M5 left vars ~A\n" (cog-value m5 (Variable "$loo")))
+(format #t "M5 right vars ~A\n" (cog-value m5 (Variable "$ro")))
+(format #t "M5 typed right vars ~A\n"
+	(cog-value m5 (TypedVariable (Variable "$ro") (Type "Item"))))
+
