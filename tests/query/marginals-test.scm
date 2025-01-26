286660d0c78878208d7b3ae43ffdbf4204994e7d
Linas Vepštas <linasvepstas@gmail.com>
2025-01-26 01:43:56 -0600

    Formalize the unit test

--- tests/query/marginals-test.scm
+++ tests/query/marginals-test.scm
@@ -3,9 +3,13 @@
 ;
 ; Unit test for storing results and marginals with the query itself.
 ; This is the future replacement for the old matrix API. Much cleaner.
-
-
+;
 (use-modules (opencog) (opencog exec))
+(use-modules (opencog test-runner))
+
+(opencog-test-runner)
+(define tname "marginals-test")
+(test-begin tname)
 
 ; Data to prime the pump.
 (Edge (Predicate "foo") (List (Item "left") (Item "right")))
@@ -23,6 +27,14 @@
 (format #t "Meet grnd ~A\n" (cog-value m m))
 (format #t "Meet vars ~A\n" (cog-value m (Variable "$x")))
 
+(test-assert "meet keynum" (equal? 2 (length (cog-keys m))))
+(test-assert "meet grnds" (equal? 3 (length (cog-value->list
+	(cog-value m m )))))
+(test-assert "meet vars" (equal? 3 (length (cog-value->list
+	(cog-value m (Variable "$x"))))))
+
+; ----------------------------------------------------------
+
 (define q (Query
 	(Variable "$x")
 	(Present
@@ -36,6 +48,14 @@
 (format #t "Query vars ~A\n" (cog-value q (Variable "$x")))
 (format #t "Query implicand ~A\n"
 	(cog-value q (Link (Item "fumble") (Variable "$x"))))
+
+(test-assert "query keynum" (equal? 3 (length (cog-keys q))))
+(test-assert "query grnds" (equal? 3 (length (cog-value->list
+	(cog-value q q)))))
+(test-assert "query vars" (equal? 3 (length (cog-value->list
+	(cog-value q (Variable "$x"))))))
+
+; ----------------------------------------------------------
 
 (define q2 (Query
 	(Variable "$x")
@@ -56,6 +76,20 @@
 	(cog-value q2 (Link (Concept "bramble") (Variable "$x"))))
 (format #t "Query implicand three ~A\n"
 	(cog-value q2 (Link (Item "borat") (Variable "$x") (Item "culture"))))
+
+(test-assert "q2 keynum" (equal? 5 (length (cog-keys q2))))
+(test-assert "q2 grnds" (equal? 3 (length (cog-value->list
+	(cog-value q2 q2)))))
+(test-assert "q2 vars" (equal? 3 (length (cog-value->list
+	(cog-value q2 (Variable "$x"))))))
+(test-assert "q2 impl one" (equal? 3 (length (cog-value->list
+	(cog-value q2 (Link (Item "fumble") (Variable "$x")))))))
+(test-assert "q2 impl two" (equal? 3 (length (cog-value->list
+	(cog-value q2 (Link (Concept "bramble") (Variable "$x")))))))
+(test-assert "q2 impl three" (equal? 3 (length (cog-value->list
+	(cog-value q2 (Link (Item "borat") (Variable "$x") (Item "culture")))))))
+
+; ----------------------------------------------------------
 
 (define m2 (Meet
 	(Present
@@ -88,3 +122,5 @@
 (format #t "M5 typed right vars ~A\n"
 	(cog-value m5 (TypedVariable (Variable "$ro") (Type "Item"))))
 
+(test-end tname)
+(opencog-test-end)
