d08fdc3e8ef301c3ccfe2dd78a3a2284dcdd6f44
Linas Vepštas <linasvepstas@gmail.com>
2025-01-29 21:38:11 -0600

    Consolidate two demos into one

--- examples/python/nameserver_example.py
+++ examples/python/nameserver_example.py
@@ -22,3 +22,22 @@ print("Is a set unordered?", set_is_unordered)
 A = ConceptNode("A")
 print("Is A a concept?", is_a(A.type, get_type("ConceptNode")))
 print("Is A a predicate?", is_a(A.type, get_type("PredicateNode")))
+
+# Every Atom Type has a corresponding number. These numbers change
+# regularly, and depend on the order in whoch modules are loaded.
+# However ... they do exist. Here they are:
+
+# To get one type name
+print(get_type_name(3) + '\n')
+
+# To get one atom's type name
+myatom = ConceptNode('foo')
+print(get_type_name(myatom.type) + '\n')
+
+# Print a list of the currently loaded type names and numbers
+for key, value in sorted(types.__dict__.items()):
+    if '__' not in key:
+        print(key, value)
+
+
+# THE END. That's All, Folks!
