82365d6371ebe84ba820dd57c8e668180e49b488
Linas Vepštas <linasvepstas@gmail.com>
2025-01-29 20:37:12 -0600

    Revise and modernize the demo

--- examples/python/create_atoms_by_type.py
+++ examples/python/create_atoms_by_type.py
@@ -3,31 +3,50 @@
 # create_atoms_by_type.py
 #
 """
-Simple example of how to create atoms in the AtomSpace.
-See also create_atomspace_simple.py for an alternate interface.
+Example of how to use the low-level interfaces into the AtomSpace.
+
+This provides the fastest python API for dumping raw data into the
+AtomSpace. However, it is a bit hard to use; the Atomese-style
+python is easier. See `create_atomspace_simple.py` for a simpler
+variant.
 """
 
 from opencog.atomspace import AtomSpace, Atom
-from opencog.type_constructors import TruthValue
+from opencog.type_constructors import FloatValue, StringValue
 from opencog.atomspace import types
 
 a = AtomSpace()
 
-# Create a truth value asserting true and mostly confident.
-TV = TruthValue(1, 0.8)
-
-# Add three nodes
+# Add three Nodes
 concept_type = types.ConceptNode
-A = a.add_node(concept_type, 'Apple', TV)
-B = a.add_node(concept_type, 'Berry', TruthValue(0.5, 0.75))
-C = a.add_node(concept_type, 'Comestible', TV)
+A = a.add_node(concept_type, 'Apple')
+B = a.add_node(concept_type, 'Berry')
+C = a.add_node(concept_type, 'Comestible')
 
-# Add three inheritance links, asserting that apples are berries
+# Create a Value holding a vector of numbers.
+weights = FloatValue([1, 0.8, 42, 3.14159])
+
+# Create a key
+key = a.add_node(types.PredicateNode, 'my favorite keyname')
+
+# Place some weights on the Nodes:
+A.set_value(key, weights)
+B.set_value(key, FloatValue([0.5, 0.333, 66, 2.71828]))
+C.set_value(key, StringValue(["just", "some", "words"]))
+
+# Get the weights:
+A.get_value(key)
+B.get_value(key)
+C.get_value(key)
+
+# Weights convert to ptyhon lists:
+print("ello, there:", list(C.get_value(key)))
+
+# Add three InheritanceLinks, asserting that apples are berries
 # and that berries are edible.
 inh_type = types.InheritanceLink
-AB = a.add_link(inh_type, [A, B], TV)
-BC = a.add_link(inh_type, [B, C], TV)
+AB = a.add_link(inh_type, [A, B])
+BC = a.add_link(inh_type, [B, C])
 AC = a.add_link(inh_type, [A, C])
-
 
 print("The atomspace contains:\n\n", a.get_atoms_by_type(types.Atom))
