f2dda22cf2abd78c711f041703d1b1b36ce59995
Linas Vepštas <linasvepstas@gmail.com>
2025-02-06 19:04:52 -0600

    Use short atom names in the python test

--- tests/cython/atomspace/test_exception.py
+++ tests/cython/atomspace/test_exception.py
@@ -18,9 +18,9 @@ class TestExceptions(unittest.TestCase):
         del self.space
 
     def test_bogus_get(self):
-        atom1 = ConceptNode("atom1")
+        atom1 = Concept("atom1")
         try:
-            GetLink(atom1, atom1, atom1)
+            Get(atom1, atom1, atom1)
             self.assertFalse("call should fail")
         except RuntimeError as e:
                    # Use `nosetests3 --nocapture` to see this print...
@@ -30,8 +30,8 @@ class TestExceptions(unittest.TestCase):
     # --------------------------------------------------------------
     # First, make sure that evaluation works.
     def test_good_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:good_tv"),
+        atom1 = Concept("atom1")
+        eval_link = Evaluation(GroundedPredicate("py:good_tv"),
                                         atom1, atom1, atom1)
         okay = evaluate_atom(self.space, eval_link)
 
@@ -43,8 +43,8 @@ class TestExceptions(unittest.TestCase):
     # --------------------------------------------------------------
     # Call a non-existent function.
     def test_bogus_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:foobar"),
+        atom1 = Concept("atom1")
+        eval_link = Evaluation(GroundedPredicate("py:foobar"),
                                         atom1, atom1, atom1)
         try:
             evaluate_atom(self.space, eval_link)
@@ -56,8 +56,8 @@ class TestExceptions(unittest.TestCase):
 
     # Call function that returns None
     def test_pass_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:no_ret"),
+        atom1 = Concept("atom1")
+        eval_link = Evaluation(GroundedPredicate("py:no_ret"),
                                         atom1, atom1, atom1)
         try:
             evaluate_atom(self.space, eval_link)
@@ -68,8 +68,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_num_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:ret_num"),
+        atom1 = Concept("atom1")
+        eval_link = Evaluation(GroundedPredicate("py:ret_num"),
                                         atom1, atom1, atom1)
         try:
             evaluate_atom(self.space, eval_link)
@@ -80,8 +80,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_str_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:ret_str"),
+        atom1 = Concept("atom1")
+        eval_link = Evaluation(GroundedPredicate("py:ret_str"),
                                         atom1, atom1, atom1)
         try:
             evaluate_atom(self.space, eval_link)
@@ -92,8 +92,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_nil_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:ret_nil"),
+        atom1 = Concept("atom1")
+        eval_link = Evaluation(GroundedPredicate("py:ret_nil"),
                                         atom1, atom1, atom1)
         try:
             evaluate_atom(self.space, eval_link)
@@ -104,8 +104,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_lst_evaluation(self):
-        atom1 = ConceptNode("atom1")
-        eval_link = EvaluationLink(GroundedPredicateNode("py:ret_lst"),
+        atom1 = Concept("atom1")
+        eval_link = Evaluation(GroundedPredicate("py:ret_lst"),
                                         atom1, atom1, atom1)
         try:
             evaluate_atom(self.space, eval_link)
@@ -118,8 +118,8 @@ class TestExceptions(unittest.TestCase):
     # --------------------------------------------------------------
     # First, make sure that evaluation works.
     def test_good_execution(self):
-        atom1 = ConceptNode("atom1")
-        exec_link = ExecutionOutputLink(GroundedSchemaNode("py:good_tv"),
+        atom1 = Concept("atom1")
+        exec_link = ExecutionOutput(GroundedSchema("py:good_tv"),
                                         ListLink(atom1, atom1, atom1))
         okay = execute_atom(self.space, exec_link)
 
@@ -131,8 +131,8 @@ class TestExceptions(unittest.TestCase):
     # --------------------------------------------------------------
     # Call a non-existent function.
     def test_bogus_execution(self):
-        atom1 = ConceptNode("atom1")
-        exec_link = ExecutionOutputLink(GroundedSchemaNode("py:foobar"),
+        atom1 = Concept("atom1")
+        exec_link = ExecutionOutput(GroundedSchema("py:foobar"),
                                         ListLink(atom1, atom1, atom1))
         try:
             execute_atom(self.space, exec_link)
@@ -144,8 +144,8 @@ class TestExceptions(unittest.TestCase):
 
     # Call function that returns None
     def test_pass_execution(self):
-        atom1 = ConceptNode("atom1")
-        exec_link = ExecutionOutputLink(GroundedSchemaNode("py:no_ret"),
+        atom1 = Concept("atom1")
+        exec_link = ExecutionOutput(GroundedSchema("py:no_ret"),
                                         ListLink(atom1, atom1, atom1))
         try:
             execute_atom(self.space, exec_link)
@@ -156,8 +156,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_num_execution(self):
-        atom1 = ConceptNode("atom1")
-        exec_link = ExecutionOutputLink(GroundedSchemaNode("py:ret_num"),
+        atom1 = Concept("atom1")
+        exec_link = ExecutionOutput(GroundedSchema("py:ret_num"),
                                         ListLink(atom1, atom1, atom1))
         try:
             execute_atom(self.space, exec_link)
@@ -168,8 +168,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_str_execution(self):
-        atom1 = ConceptNode("atom1")
-        exec_link = ExecutionOutputLink(GroundedSchemaNode("py:ret_str"),
+        atom1 = Concept("atom1")
+        exec_link = ExecutionOutput(GroundedSchema("py:ret_str"),
                                         ListLink(atom1, atom1, atom1))
         try:
             execute_atom(self.space, exec_link)
@@ -180,8 +180,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_nil_execution(self):
-        atom1 = ConceptNode("atom1")
-        exec_link = ExecutionOutputLink(GroundedSchemaNode("py:ret_nil"),
+        atom1 = Concept("atom1")
+        exec_link = ExecutionOutput(GroundedSchema("py:ret_nil"),
                                         ListLink(atom1, atom1, atom1))
         try:
             execute_atom(self.space, exec_link)
@@ -192,8 +192,8 @@ class TestExceptions(unittest.TestCase):
             self.assertTrue("did not return Atomese" in str(e))
 
     def test_lst_execution(self):
-        atom1 = ConceptNode("atom1")
-        exec_link = ExecutionOutputLink(GroundedSchemaNode("py:ret_lst"),
+        atom1 = Concept("atom1")
+        exec_link = ExecutionOutput(GroundedSchema("py:ret_lst"),
                                         ListLink(atom1, atom1, atom1))
         try:
             execute_atom(self.space, exec_link)
