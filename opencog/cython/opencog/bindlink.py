3c38cc73fe7b26ea5d4658b82300804b6568c7ee
Linas Vepštas <linasvepstas@gmail.com>
2025-05-01 12:39:03 -0500

    Remove obsolete python crud

--- opencog/cython/opencog/bindlink.py
+++
@@ -1,5 +0,0 @@
-import warnings
-
-from opencog.execute import *
-
-warnings.warn('opencog.bindlink has been renamed to opencog.execute, update your imports.', DeprecationWarning)
--- opencog/cython/opencog/exec.py
+++
@@ -1,5 +0,0 @@
-import warnings
-
-from opencog.execute import *
-
-warnings.warn('opencog.exec has been renamed to opencog.execute, update your imports.', DeprecationWarning)
