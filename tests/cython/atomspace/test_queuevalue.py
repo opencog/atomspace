28a69b611ac5995c8dc6b264821b061368a2f89f
Linas Vepštas <linasvepstas@gmail.com>
2025-07-11 18:29:21 +0000

    Bug-fix brand-new unit test

--- tests/cython/atomspace/test_queuevalue.py
+++ tests/cython/atomspace/test_queuevalue.py
@@ -4,7 +4,7 @@ from opencog.type_constructors import *
 from opencog.utilities import set_default_atomspace, finalize_opencog
 
 class QueueValueTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.space = AtomSpace()
         set_default_atomspace(self.space)
@@ -27,7 +27,7 @@ class QueueValueTest(unittest.TestCase):
         value = QueueValue([FloatValue(42), StringValue('foo')])
         self.assertTrue(value is not None)
         self.assertEqual(2, len(value))
-    
+
     def test_open_close(self):
         value = QueueValue()
         value.open()
@@ -38,48 +38,48 @@ class QueueValueTest(unittest.TestCase):
     def test_push_pop(self):
         value = QueueValue()
         value.open()
-        
+
         # Push some values
         value.push(FloatValue(1.0))
         value.push(StringValue('hello'))
         value.push(FloatValue(2.0))
-        
+
         self.assertEqual(3, len(value))
-        
+
         # Pop values - should be FIFO
         popped1 = value.pop()
         self.assertEqual(FloatValue(1.0), popped1)
         self.assertEqual(2, len(value))
-        
+
         popped2 = value.pop()
         self.assertEqual(StringValue('hello'), popped2)
         self.assertEqual(1, len(value))
-        
+
         popped3 = value.pop()
         self.assertEqual(FloatValue(2.0), popped3)
         self.assertEqual(0, len(value))
-        
+
         value.close()
 
     def test_append(self):
         value = QueueValue()
         value.open()
-        
+
         # Test append (should work like push)
         value.append(FloatValue(42))
         value.append(StringValue('test'))
-        
+
         self.assertEqual(2, len(value))
-        
+
         popped = value.pop()
         self.assertEqual(FloatValue(42), popped)
-        
+
         value.close()
 
     def test_clear(self):
         value = QueueValue([FloatValue(1), StringValue('a'), FloatValue(2)])
         self.assertEqual(3, len(value))
-        
+
         value.clear()
         self.assertEqual(0, len(value))
 
@@ -88,7 +88,7 @@ class QueueValueTest(unittest.TestCase):
         key = PredicateNode('bar')
         value = QueueValue([StringValue('a'), FloatValue(42)])
         atom.set_value(key, value)
-        
+
         retrieved = atom.get_value(key)
         self.assertTrue(retrieved.is_a(types.QueueValue))
         self.assertEqual(2, len(retrieved))
@@ -101,34 +101,35 @@ class QueueValueTest(unittest.TestCase):
     def test_queue_with_atoms(self):
         value = QueueValue()
         value.open()
-        
+
         # Add some atoms
         a1 = ConceptNode('concept1')
         a2 = ConceptNode('concept2')
         a3 = PredicateNode('predicate1')
-        
+
         value.push(a1)
         value.push(a2)
         value.push(a3)
-        
+
         self.assertEqual(3, len(value))
-        
+
         # Pop and verify
         popped1 = value.pop()
         self.assertEqual(a1, popped1)
         self.assertTrue(popped1.is_node())
-        
+
         value.close()
 
     def test_pop_empty_queue(self):
         value = QueueValue()
         value.open()
-        
-        # Popping from empty queue should return None
+
+        # Close the queue first to prevent blocking
+        value.close()
+
+        # Popping from closed empty queue should return None
         popped = value.pop()
         self.assertIsNone(popped)
-        
-        value.close()
 
     def test_str(self):
         value = QueueValue([FloatValue(42), StringValue('foo')])
