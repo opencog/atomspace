07f15387cae4c6e4715975045f10c68d35a93ca5
Linas Vepštas <linasvepstas@gmail.com>
2025-08-20 13:52:33 -0500

    Start work on DefinedProcedure

---
+++ opencog/atoms/parallel/DefinedProcedureNode.cc
@@ -0,0 +1,50 @@
+/*
+ * opencog/atoms/parallel/DefinedProcedureNode.cc
+ *
+ * Copyright (C) 2009, 2013-2015, 2020, 2024, 2025 Linas Vepstas
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atoms/parallel/DefinedProcedureNode.h>
+#include <opencog/atoms/value/VoidValue.h>
+#include <opencog/atomspace/AtomSpace.h>
+#include <opencog/atomspace/Transient.h>
+
+using namespace opencog;
+
+/// DefinedProcedureNode
+/// Look up the definition, and execute it, when called.
+/// This enables direct calls, such as
+/// (cog-execute! (DefinedProcedure "foo"))
+
+DefinedProcedureNode::DefinedProcedureNode(Type t, const std::string&& str)
+    : Node(t, std::move(str))
+{
+}
+
+ValuePtr DefinedProcedureNode::execute(AtomSpace* as,
+                                       bool silent)
+{
+	ValuePtr result;
+
+
+	return result;
+}
+
+DEFINE_NODE_FACTORY(DefinedProcedureNode, DEFINED_PROCEDURE_NODE)
