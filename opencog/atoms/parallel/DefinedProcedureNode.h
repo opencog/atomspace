e0b324e23baeb5f16aebb4e04cfb169ea2718862
Linas Vepštas <linasvepstas@gmail.com>
2025-08-20 13:46:13 -0500

    Start work on DefinedProcedureNode

---
+++ opencog/atoms/parallel/DefinedProcedureNode.h
@@ -0,0 +1,58 @@
+/*
+ * opencog/atoms/parallel/DefinedProcedureNode.h
+ *
+ * Copyright (C) 2020, 2024 Linas Vepstas
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_DEFINED_PROCEDURE_NODE_H
+#define _OPENCOG_DEFINED_PROCEDURE_NODE_H
+
+#include <opencog/atoms/base/Node.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+class AtomSpace;
+
+class DefinedProcedureNode : public Node
+{
+protected:
+
+public:
+	DefinedProcedureNode(Type=DEFINED_PROCEDURE_NODE, const std::string&&);
+	DefinedProcedureNode(const DefinedProcedureNode&) = delete;
+	DefinedProcedureNode& operator=(const DefinedProcedureNode&) = delete;
+
+	virtual bool is_executable() const { return true; }
+	virtual ValuePtr execute(AtomSpace*, bool);
+
+	static Handle factory(const Handle&);
+};
+
+NODE_PTR_DECL(DefinedProcedureNode)
+#define createDefinedProcedureNode CREATE_DECL(DefinedProcedureNode)
+
+/** @}*/
+}
+
+#endif // _OPENCOG_DEFINED_PROCEDURE_NODE_H
