f1654490150ef4af9e084e0931e5071578d92516
Linas Vepštas <linasvepstas@gmail.com>
2025-08-22 12:19:26 -0500

    Run the new unit test

--- tests/atoms/value/ctor-args-test.scm
+++ tests/atoms/value/ctor-args-test.scm
@@ -5,12 +5,24 @@
 (use-modules (opencog test-runner))
 
 (opencog-test-runner)
-(define tname "ctor-args-test")
+(define tname "empty-ctor-test")
 (test-begin tname)
 
 (define nullb (BoolValue))
 (format #t "Empty bool is ~A\n" nullb)
-(test-assert "nullb" (equal? () (cog-value->list nullb)))
+(test-assert "nullb" (equal? '() (cog-value->list nullb)))
+
+(define nullf (FloatValue))
+(format #t "Empty float is ~A\n" nullf)
+(test-assert "nullf" (equal? '() (cog-value->list nullf)))
+
+(define nullt (LinkValue))
+(format #t "Empty list is ~A\n" nullt)
+(test-assert "nullt" (equal? '() (cog-value->list nullt)))
+
+(define nulls (StringValue))
+(format #t "Empty string is ~A\n" nulls)
+(test-assert "nulls" (equal? '() (cog-value->list nulls)))
 
 (test-end tname)
 
