9764c99d8f6b835c4f489f6e1da073addb2a48b6
Linas Vepštas <linasvepstas@gmail.com>
2025-08-27 19:47:59 -0500

    Start work on moving IncomingIndex out of line

---
+++ opencog/atomspace/IncomeIndex.h
@@ -0,0 +1,117 @@
+/*
+ * opencog/atomspace/IncomeIndex.h
+ *
+ * Copyright (C) 2008,2025 Linas Vepstas <linasvepstas@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_INCOMEINDEX_H
+#define _OPENCOG_INCOMEINDEX_H
+
+#include <mutex>
+#include <set>
+#include <vector>
+
+#include <opencog/util/oc_assert.h>
+#include <opencog/atoms/base/Atom.h>
+#include <opencog/atoms/base/Handle.h>
+#include <opencog/atoms/atom_types/types.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+// The InSet is the IncomingSet map, plus a lock on that set.
+struct InSet
+{
+	mutable std::shared_mutex _mtx;
+	mutable std::map<Handle, InSetMap> _isetmap;
+};
+
+#define INCOME_INDEX_SHARED_LOCK(s) std::shared_lock<std::shared_mutex> lck(s._mtx);
+#define INCOME_INDEX_UNIQUE_LOCK(s) std::unique_lock<std::shared_mutex> lck(s._mtx);
+
+/**
+ * Implements a pool hholding Incoming sets for those Atoms that have
+ * incoming sets. The goal of the pool is to avoid lock contention;
+ * this is possible, because each Handle has a deterministic hash,
+ * which can be used to identify the pool.
+ */
+class IncomeIndex
+{
+	private:
+		mutable std::vector<InSet> _idx;
+
+		static constexpr int POOL_SIZE = 32;
+		Inset& get_inset(const Handle& h)
+		{
+			return _idx[h->get_hash() % POOL_SIZE];
+		}
+	public:
+		IncomeIndex(void);
+
+		void insertAtom(const Handle& h)
+		{
+			InSet& s(get_inset(h));
+			INCOME_INDEX_UNIQUE_LOCK(s);
+#define SANITY_CHECK
+#ifdef SANITY_CHECK
+			auto iter = s.find(h);
+			OC_ASSERT(s.end() == iter, "Double insertion of Incming Set!");
+#endif
+			s.insert(h);
+		}
+
+		void removeAtom(const Handle& h)
+		{
+			InSet& s(get_inset(h));
+			AtomSet& s(get_inset(h));
+			INCOME_INDEX_UNIQUE_LOCK(s);
+			s.erase(h);
+		}
+
+		InSetMap& findInset(const Handle& h) const
+		{
+			InSet& s(get_inset(h));
+			INCOME_INDEX_SHARED_LOCK(s);
+			auto inset = s._isetmap.find(h);
+
+			static const InSetMap empty_map;
+			if (s.end() == inset) return empty_map;
+			return inset;
+		}
+
+		// How many entries are there, anyway?
+		size_t size(void) const
+		{
+			size_t cnt = 0;
+			for (const InSet& s : _idx)
+			{
+				INCOME_INDEX_SHARED_LOCK(s);
+				cnt += s.size();
+			}
+			return cnt;
+		}
+
+		void clear(void);
+};
+
+/** @}*/
+} //namespace opencog
+
+#endif // _OPENCOG_INCOMEINDEX_H
