5b1bbf98f6ec69ac5c375823ca447e982319bb5e
Linas Vepštas <linasvepstas@gmail.com>
2025-08-29 10:28:51 -0500

    IncomeIndex no longer used. Explain why.

--- opencog/atomspace/IncomeIndex.h
+++ opencog/atomspace/IncomeIndex.h
@@ -46,10 +46,36 @@ struct InSet : std::map<Handle, InSetMap>
 #define INCOME_INDEX_UNIQUE_LOCK(s) std::unique_lock<std::shared_mutex> lck(s._mtx);
 
 /**
- * Implements a pool holding Incoming sets for those Atoms that have
- * incoming sets. The goal of the pool is to avoid lock contention;
- * this is possible, because each Handle has a deterministic hash,
- * which can be used to identify the pool.
+ * Implements a pool holding incoming sets for those Atoms that have
+ * incoming sets. This pool is not currently used; it does not help
+ * either with performance or with RAM usage!
+ *
+ * The original idea motivating this pool was the observation that
+ * not all Atoms have incoming sets. Typically, only half of them do,
+ * and so we should be able to save space by moving the InSetMap out
+ * of the Atom, and into the pool, here. But this idea fails miserably,
+ * and here's why.
+ *
+ * sizeof(InSetMap) = 48
+ * sizeof(Handle) = 16
+ * sizeof(std::pair<Handle, InSetMap>) = 64 = 16 + 48
+ * sizeof std::map<Handle, InSetMap> entry approx 96 bytes total.
+ *
+ * The savings of reducing each Atom by 48 bytes is offset by requiring
+ * approx 96 bytes to store the incoming sets for those Atoms that do
+ * have them. So even if half of all Atoms don't have an incoming set,
+ * there's no savings, because twice as much RAM is neded for the other
+ * half.
+ *
+ * Measured on a typical dataset, the sfia dataset, average RAM per Atom
+ * is 8 bytes **more**, using the IncomeIndex, here, than keeping the
+ * incoming set inline with the Atom itself. That, plus using the index
+ * here hurts performance by about 20%. In short, its a disaster. The
+ * code here is left for reference, but it is not compiled.
+ *
+ * A pool is used to avoid lock contention. This is possible, because
+ * each Handle has a deterministic hash, which can be used to identify
+ * the pool.
  */
 class IncomeIndex
 {
