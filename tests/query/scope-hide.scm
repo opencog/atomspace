e4946be14b17c0e98db95f05c2131d615ce030de
Linas Vepštas <linasvepstas@gmail.com>
2025-10-23 00:48:00 -0500

    Fix ailing unit test

--- tests/query/scope-hide.scm
+++ tests/query/scope-hide.scm
@@ -5,23 +5,23 @@
 (use-modules (opencog))
 (use-modules (opencog exec))
 
-;; The ForAll link is a ScopeLink, and so (VariableNode "$X")
+;; The Lambda link is a ScopeLink, and so (VariableNode "$X")
 ;; is a bound var.
 (define forall
-	(ForAllLink
+	(LambdaLink
 		(VariableNode "$X")
 		(EvaluationLink (Predicate "P") (VariableNode "$X"))))
 
 ;; This has the same variable name as the above -- and so,
 ;; during pattern matching, this should not be considered to be
 ;; a self-grounding -- $X should bind to $X just fine.
-(define getx (CollectionOf (Meet (LocalQuote (ForAllLink (Variable "$X") (Variable "$B"))))))
+(define getx (CollectionOf (Meet (LocalQuote (LambdaLink (Variable "$X") (Variable "$B"))))))
 
 ;; Alpha conversion means that getv should be exactly the same atom
 ;; as getx.  Note that chronological order matters: this MUST be
 ;; defined after the above, for this unit test to actually test
 ;; something valid.
-(define getv (CollectionOf (Meet (LocalQuote (ForAllLink (Variable "$V") (Variable "$B"))))))
+(define getv (CollectionOf (Meet (LocalQuote (LambdaLink (Variable "$V") (Variable "$B"))))))
 
 ;; This is to make sure that the following ill-formed scope link isn't
 ;; created
