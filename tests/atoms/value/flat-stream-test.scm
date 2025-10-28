326025cda54932cd06254da68d10a917cb873429
Linas Vepstas <linasvepstas@gmail.com>
2025-10-28 03:03:35 +0000

    Extend the FlatStream unit test

--- tests/atoms/value/flat-stream-test.scm
+++ tests/atoms/value/flat-stream-test.scm
@@ -56,4 +56,168 @@
 
 (test-end tname)
 
+; ---------------------------------------------------------------
+; Test with OrderedLink containing Items
+; As demonstrated in examples/atomspace/flow-flat.scm
+;
+(define tname-ordered "flat-stream-ordered-test")
+(test-begin tname-ordered)
+
+; Create a list of items similar to flow-flat.scm example
+(define item-list
+	(OrderedLink
+		(Item "a")
+		(Item "b")
+		(Item "c")
+		(Edge
+			(Predicate "relation")
+			(List (Item "d") (Item "e") (Item "f") (Item "g")))
+		(Item "p")
+		(Predicate "q")
+		(TagNode "z")))
+
+; Wrap the list with the serializer
+(define fs-ordered (FlatStream item-list))
+
+; Test sequential access - each access should return the next item wrapped in LinkValue
+; Note: format advances stream, then equal? compares current value
+(format #t "Stream access (Item a): ~A\n" fs-ordered)
+(test-assert "ordered-item-a" (equal? fs-ordered (LinkValue (Item "a"))))
+
+(format #t "Stream access (Item b): ~A\n" fs-ordered)
+(test-assert "ordered-item-b" (equal? fs-ordered (LinkValue (Item "b"))))
+
+(format #t "Stream access (Item c): ~A\n" fs-ordered)
+(test-assert "ordered-item-c" (equal? fs-ordered (LinkValue (Item "c"))))
+
+; Next item is an Edge
+(define expected-edge
+	(LinkValue
+		(Edge
+			(Predicate "relation")
+			(List (Item "d") (Item "e") (Item "f") (Item "g")))))
+(format #t "Stream access (Edge): ~A\n" fs-ordered)
+(test-assert "ordered-edge" (equal? fs-ordered expected-edge))
+
+; Continue with remaining items
+(format #t "Stream access (Item p): ~A\n" fs-ordered)
+(test-assert "ordered-item-p" (equal? fs-ordered (LinkValue (Item "p"))))
+
+(format #t "Stream access (Predicate q): ~A\n" fs-ordered)
+(test-assert "ordered-predicate-q" (equal? fs-ordered (LinkValue (Predicate "q"))))
+
+(format #t "Stream access (TagNode z): ~A\n" fs-ordered)
+(test-assert "ordered-tagnode-z" (equal? fs-ordered (LinkValue (TagNode "z"))))
+
+; After reaching the end, it should wrap around to the first item
+(format #t "Stream access (wrap to Item a): ~A\n" fs-ordered)
+(test-assert "ordered-wrap-a" (equal? fs-ordered (LinkValue (Item "a"))))
+
+(test-end tname-ordered)
+
+; ---------------------------------------------------------------
+; Test SetValue with CollectionOf pattern
+; As demonstrated in examples/atomspace/flow-flat.scm
+;
+(define tname-setvalue "flat-stream-setvalue-test")
+(test-begin tname-setvalue)
+
+; Create a simple list
+(define simple-list
+	(OrderedLink
+		(Concept "first")
+		(Concept "second")
+		(Concept "third")))
+
+; Use SetValue with CollectionOf to create stream in "pure Atomese"
+(define junk
+	(cog-execute!
+		(SetValue (Concept "stream-test") (Predicate "location")
+			(CollectionOf (Type 'FlatStream) (OrderedLink simple-list)))))
+
+; Create ValueOf reference to the stream
+(define stream-ref (ValueOf (Concept "stream-test") (Predicate "location")))
+
+; Test that cog-execute! advances the stream (returns FlatStream that wraps LinkValues)
+(format #t "SetValue test first: ~A\n" (cog-execute! stream-ref))
+(test-assert "setvalue-first"
+	(equal? (cog-execute! stream-ref) (LinkValue (Concept "first"))))
+
+(format #t "SetValue test second: ~A\n" (cog-execute! stream-ref))
+(test-assert "setvalue-second"
+	(equal? (cog-execute! stream-ref) (LinkValue (Concept "second"))))
+
+(format #t "SetValue test third: ~A\n" (cog-execute! stream-ref))
+(test-assert "setvalue-third"
+	(equal? (cog-execute! stream-ref) (LinkValue (Concept "third"))))
+
+; Should wrap around
+(format #t "SetValue test wrap: ~A\n" (cog-execute! stream-ref))
+(test-assert "setvalue-wrap-first"
+	(equal? (cog-execute! stream-ref) (LinkValue (Concept "first"))))
+
+(test-end tname-setvalue)
+
+; ---------------------------------------------------------------
+; Test multiple stream references and independence
+;
+(define tname-multi "flat-stream-multi-test")
+(test-begin tname-multi)
+
+; Create two independent streams from the same source
+(define list-abc
+	(OrderedLink
+		(Concept "A")
+		(Concept "B")
+		(Concept "C")))
+
+(define stream-1 (FlatStream list-abc))
+(define stream-2 (FlatStream list-abc))
+
+; Advance stream-1 twice
+(format #t "Stream-1 access A: ~A\n" stream-1)
+(test-assert "multi-s1-a" (equal? stream-1 (LinkValue (Concept "A"))))
+
+(format #t "Stream-1 access B: ~A\n" stream-1)
+(test-assert "multi-s1-b" (equal? stream-1 (LinkValue (Concept "B"))))
+
+; stream-2 should still be at the start
+(format #t "Stream-2 access A: ~A\n" stream-2)
+(test-assert "multi-s2-a" (equal? stream-2 (LinkValue (Concept "A"))))
+
+; Continue advancing stream-1
+(format #t "Stream-1 access C: ~A\n" stream-1)
+(test-assert "multi-s1-c" (equal? stream-1 (LinkValue (Concept "C"))))
+
+; stream-2 should be at second position
+(format #t "Stream-2 access B: ~A\n" stream-2)
+(test-assert "multi-s2-b" (equal? stream-2 (LinkValue (Concept "B"))))
+
+(test-end tname-multi)
+
+; ---------------------------------------------------------------
+; Test cog-value->list interaction with stream
+;
+(define tname-valuelist "flat-stream-valuelist-test")
+(test-begin tname-valuelist)
+
+(define vlist-stream
+	(FlatStream
+		(OrderedLink
+			(Number 1)
+			(Number 2)
+			(Number 3))))
+
+; First access - returns LinkValue wrapping Number
+(format #t "ValueList test first: ~A\n" vlist-stream)
+(test-assert "valuelist-num-1" (equal? vlist-stream (LinkValue (Number 1))))
+
+; cog-value->list also advances the stream and extracts the unwrapped value
+(format #t "ValueList test second (via cog-value->list): ~A\n" vlist-stream)
+(define vlist-result (cog-value->list vlist-stream))
+(test-assert "valuelist-advances"
+	(equal? (car vlist-result) (Number 3)))
+
+(test-end tname-valuelist)
+
 (opencog-test-end)
