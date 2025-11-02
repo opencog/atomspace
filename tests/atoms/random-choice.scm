38a518c65fb85afd6d266580a1bb52b64249e78f
Linas Vepstas <linasvepstas@gmail.com>
2025-11-02 07:31:19 +0000

    Port the RandomNumberUTest away from GetLink

--- tests/atoms/random-choice.scm
+++ tests/atoms/random-choice.scm
@@ -23,13 +23,13 @@
          (List (Number 0.3) (Concept "B")))))
 
 ; Pick A with 70% probability, pick B with 30% probability.
-; Use a Getlink to fish out the probabilities, instead of
+; Use a MeetLink to fish out the probabilities, instead of
 ; hard-coding them, as above. This forces the lazy/eager
 ; evaluation subsystem to "do the right thing" -- to evaluate
-; the GetLink before running the RandomChoice.
+; the MeetLink before running the RandomChoice.
 (Define (DefinedProcedure "randy")
    (RandomChoice
-      (GetLink
+      (MeetLink
          (VariableList (VariableNode "$prob") (VariableNode "$expr"))
          (AndLink
             (EvaluationLink
@@ -42,7 +42,7 @@
                (VariableNode "$prob"))))
       ))
 
-; Data to allow the GetLink above to find the needed data.
+; Data to allow the MeetLink above to find the needed data.
 (Evaluation (Predicate "Emotion-expression")
    (ListLink (Concept "wake-up") (Concept "A")))
 (State (ListLink (ConceptNode "wake-up") (Concept "A")) (Number 0.7))
@@ -63,11 +63,11 @@
          (True (Put
             (State (Anchor "sum-A") (Variable "$x"))
             (Plus (Number 1)
-               (Get (State (Anchor "sum-A") (Variable "$y")))))))
+               (Meet (State (Anchor "sum-A") (Variable "$y")))))))
       (True (Put
          (State (Anchor "sum-B") (Variable "$x"))
          (Plus (Number 1)
-            (Get (State (Anchor "sum-B") (Variable "$y"))))))))
+            (Meet (State (Anchor "sum-B") (Variable "$y"))))))))
 
 ; Run it a few thousand times
 (State (Anchor "loop-count") (Number 0))
@@ -77,17 +77,17 @@
       (DefinedPredicate "counter")
       (TrueLink (PutLink
          (State (Anchor "loop-count") (Variable "$x"))
-         (Plus (Number 1) (Get (State (Anchor "loop-count") (Variable "$x"))))))
+         (Plus (Number 1) (Meet (State (Anchor "loop-count") (Variable "$x"))))))
       (GreaterThan
          (Number 3000)
-         (Get (State (Anchor "loop-count") (Variable "$x"))))
+         (Meet (State (Anchor "loop-count") (Variable "$x"))))
       (DefinedPredicate "loop a lot of times")))
 
 ; Print the ratio
 (Define (DefinedProcedure "ratio")
    (Divide
-      (Get (State (Anchor "sum-A") (Variable "$x")))
-      (Get (State (Anchor "sum-B") (Variable "$x")))))
+      (Meet (State (Anchor "sum-A") (Variable "$x")))
+      (Meet (State (Anchor "sum-B") (Variable "$x")))))
 
 ; Expectation value is 0.7/0.3 = 2.33333
 (Define (DefinedPredicate "test")
