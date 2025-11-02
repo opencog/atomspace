454db368f84ef1da1750ce59c6459b581bbe9bfe
Linas Vepstas <linasvepstas@gmail.com>
2025-11-02 04:21:34 +0000

    Port example from GetLink to MeetLink

--- examples/atomspace/multi-space.scm
+++ examples/atomspace/multi-space.scm
@@ -46,14 +46,14 @@
 
 ; Define a query that will look for all Concepts
 (define get-concepts
-	(Get (TypedVariable (Variable "$x") (Type 'Concept))
+	(Meet (TypedVariable (Variable "$x") (Type 'Concept))
 		(Variable "$x")))
 
 ; Take a look.
 (cog-prt-atomspace)
 
 ; Run the query. It should find both Concepts. This is, it should
-; print out a `SetLink` containing both of the above Concepts.
+; print out a `UnisetValue` containing both of the above Concepts.
 (cog-execute! get-concepts)
 
 ; Take a look.
