816a5e5fbb20715a05a26a45560643190f31deb2
Linas Vepstas <linasvepstas@gmail.com>
2025-11-02 02:47:41 +0000

    Port away from GetLink

--- tests/query/get-link.scm
+++ tests/query/get-link.scm
@@ -9,24 +9,26 @@
 (InheritanceLink (ConceptNode "Sparky") (ConceptNode "dog"))
 
 (define is-human
-	(GetLink (InheritanceLink (VariableNode "$H") (ConceptNode "human"))))
+	(CollectionOf (Meet (InheritanceLink (VariableNode "$H") (ConceptNode "human")))))
 
 ;; Two variables, including type restrictions
 (define is-something
-	(GetLink
-		(VariableList
-			(TypedVariableLink (VariableNode "$A") (TypeNode "ConceptNode"))
-			(VariableNode "$B"))
-		(InheritanceLink (VariableNode "$A") (VariableNode "$B"))))
+	(CollectionOf
+		(Meet
+			(VariableList
+				(TypedVariableLink (VariableNode "$A") (TypeNode "ConceptNode"))
+				(VariableNode "$B"))
+			(InheritanceLink (VariableNode "$A") (VariableNode "$B")))))
 
 ;; An empty TypeChoice means the variable can have no type at all.
 ;; This is the same as the bottom type. It's also the same as
 ;;    (TypeChoice (Type 'Notype))
 ;; See https://github.com/opencog/atomspace/issues/2490
 (define is-nothing
-	(GetLink
-		(TypedVariableLink (VariableNode "$H") (TypeChoice))
-		(InheritanceLink (VariableNode "$H") (ConceptNode "human"))))
+	(CollectionOf
+		(Meet
+			(TypedVariableLink (VariableNode "$H") (TypeChoice))
+			(InheritanceLink (VariableNode "$H") (ConceptNode "human")))))
 
 ;; Look for queries.
 ;; This binds only the second variable, thus, the first variable
@@ -34,9 +36,10 @@
 ;; above. That is, it should find
 ;; (SetLink (ConceptNode "human"))
 (define is-query
-	(GetLink
-		(VariableNode "$B") ;; bind only the second variable.
-		(InheritanceLink (VariableNode "$H") (VariableNode "$B"))))
+	(CollectionOf
+		(Meet
+			(VariableNode "$B") ;; bind only the second variable.
+			(InheritanceLink (VariableNode "$H") (VariableNode "$B")))))
 
 ;; --------------------------------------------------------------
 
