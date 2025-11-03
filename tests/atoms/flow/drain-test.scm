cb13c0bb2ebf2c909de0cafb16e97f9dde5597c3
Linas Vepstas <linasvepstas@gmail.com>
2025-11-03 05:31:33 +0000

    Lets straighten out theunit test a little bit

--- tests/atoms/flow/drain-test.scm
+++ tests/atoms/flow/drain-test.scm
@@ -13,25 +13,36 @@
 (test-begin tname)
 
 ; ------------------------------------------------------------
-; Create test data: a LinkValue with five ConceptNodes
 
+; Null-terminate, else the FlatStream below will loop forever.
 (define test-data
 	(LinkValue
 		(Concept "A")
 		(Concept "C")
 		(Concept "A")
 		(Concept "B")
-		(Concept "A")))
+		(Concept "A")
+		(VoidValue)))
 
-; Attach this LinkValue to an Anchor
-(define data-anchor (Anchor "test-data-anchor"))
-(cog-set-value! data-anchor (Predicate "data-stream") test-data)
+(define data-anchor (Anchor "process-anchor"))
+(cog-set-value! data-anchor (Predicate "test-data") test-data)
 
-; Create a CollectionOf to turn the LinkValue into a stream
+; Use FlatStream to dole out the values one by one.
+; i.e. to turn the static data above into an actual stream.
+(define (reset-stream)
+	(cog-execute!
+		(SetValue data-anchor (Predicate "data-stream")
+			(CollectionOfLink (Type 'FlatStream)
+				(ValueOf data-anchor (Predicate "test-data"))))))
+
+; Call this to rewind back to the start.
+(reset-stream)
+
 (define data-stream
-	(CollectionOfLink (Type 'FlatStream)
-		(OrderedLink
-			(ValueOf data-anchor (Predicate "data-stream")))))
+	(ValueOf data-anchor (Predicate "data-stream")))
+
+; (define strm (cog-execute! data-stream))
+; (format #t "It is ~A\n" strm)
 
 ; ------------------------------------------------------------
 ; Create a FilterLink that counts each atom and increments total
@@ -41,7 +52,10 @@
 		(Rule
 			(TypedVariable (Variable "$item") (Type 'ConceptNode))
 			(Variable "$item")
-			(List
+
+			; Odd ... by default, FilterLink wraps these
+			; results in a ListLink... let's avoid that.
+			(LinkSignature (Type 'LinkValue)
 				; Increment count on the individual atom
 				(IncrementValueOn
 					(Variable "$item")
