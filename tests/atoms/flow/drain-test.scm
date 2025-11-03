c1549655cbd7cc54ac509732ef0c28b2d726ca80
Linas Vepstas <linasvepstas@gmail.com>
2025-11-03 05:49:35 +0000

    Extend the unit test a bit

--- tests/atoms/flow/drain-test.scm
+++ tests/atoms/flow/drain-test.scm
@@ -68,9 +68,12 @@
 					(Number 1))))
 		data-stream))
 
+; (cog-execute! counter)
+
 ; ------------------------------------------------------------
 ; Wrap the counter in a DrainLink and execute it
 
+; Brute-force drain
 (define drainer (Drain counter))
 
 ; Execute the DrainLink - this should process all 5 items
@@ -93,6 +96,44 @@
 
 ; Total count on anchor should be 5
 (test-assert "total-count" (equal? (FloatValue 5)
+	(cog-value data-anchor (Predicate "total-count"))))
+
+; ------------------------------------------------------------
+; Do it again, but indirectly.
+
+(cog-execute!
+	(SetValue data-anchor (Predicate "counter-stream")
+		(DontExec counter)))
+
+(define counter-stream
+	(ValueOf data-anchor (Predicate "counter-stream")))
+
+; Rewind back to the start.
+(reset-stream)
+
+; Indirect drain from the anchor point
+(define drainer (Drain counter-stream))
+
+; Execute the DrainLink - this should process all 5 items
+(cog-execute! drainer)
+
+; ------------------------------------------------------------
+; Verify the counts
+
+; A should appear 3 more times
+(test-assert "count-A" (equal? (FloatValue 6)
+	(cog-value (Concept "A") (Predicate "item-count"))))
+
+; B should appear 1 more time
+(test-assert "count-B" (equal? (FloatValue 2)
+	(cog-value (Concept "B") (Predicate "item-count"))))
+
+; C should appear 1 more time
+(test-assert "count-C" (equal? (FloatValue 2)
+	(cog-value (Concept "C") (Predicate "item-count"))))
+
+; Total count on anchor should increment by 5
+(test-assert "total-count" (equal? (FloatValue 10)
 	(cog-value data-anchor (Predicate "total-count"))))
 
 ; ------------------------------------------------------------
