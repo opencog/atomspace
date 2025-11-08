0c55cae001516bc877f39616fc36effdd38bc4b1
Linas Vepštas <linasvepstas@gmail.com>
2025-11-07 19:03:32 -0600

    Start work on a new demo

---
+++ examples/atomspace/flow-sorted.scm
@@ -0,0 +1,95 @@
+;
+; flow-sorted.scm -- Presenting streams in sorted order.
+;
+; In most conventional usage, it is enough to process a stream in
+; sequential order, on a first-in, first-out (FIFO) basis. Most
+; Atomese stream processing will work in this way. There are two
+; main exceptions: UnisetValue, and SortedValue. The UnisetValue
+; is intended for item deduplication; it will sort items, but in
+; an opaque fashion (nominally, in lexical order of what would have
+; been the string, if the item had been printed.)
+;
+; The SortedValue allows a custom sort order to be applied, with the
+; sorting operation specified in Atomese. The SortedValue is
+; thread-safe buffer: it can be read from and written to simultaneously.
+; The buffered contents will be kept (and returned) in sorted order.
+;
+; As a buffer, this is not really intended for sorting static lists,
+; but for sorting items arriving on a stream.
+;
+; To provide meaningful stream buffering, it will pull from it's
+; upstream source, attempting to "fill itself up". If the upstream
+; source is empty but open, the reader will block. The buufer will
+; remain in sorted order, and readers will be handed out in sorted
+; order to readers.
+
+(use-modules (opencog) (opencog exec))
+
+; Create a list of items.
+(define item-list
+	(OrderedLink
+		(Item "a")
+		(Item "b")
+		(Item "c")
+		(Edge
+			(Predicate "relation")
+			(List (Item "d") (Item "e") (Item "f") (Item "g")))
+		(Item "p")
+		(Predicate "q")
+		(TagNode "z")))
+
+; Wrap the list with the serializer.
+(define fs (FlatStream item-list))
+
+; Display it. Note that, at the bottom of the print, the current
+; sample from the stream is printed. The stream advances every
+; time it is referenced, and so repeated prints cause repeated
+; references, advancing the stream.
+(display fs)
+(display fs)
+(cog-value->list fs)
+(cog-value->list fs)
+(cog-value->list fs)
+(display fs)
+(display fs)
+
+; By convention, streams are attached to fixed locations, where
+; they can be found and accessed. For this demo, let's attach
+; the stream to the Node "foo", located at the key "bar".
+(cog-set-value! (Concept "foo") (Predicate "bar") fs)
+
+; The attachment above works, but it is not "pure Atomese", it
+; requires a call to `cog-set-value!` This can be avoided by
+; using SetValueLink instead. However, since the stream itself is
+; not an Atom, it cannot be place there directly. For this, re-wrap
+; the list into a FlatStream, using the CollectionOfLink utility
+; to perform the re-wrap.  When executed, the re-write will create
+; the stream. The `cog-execute!` below results in exactly the
+; structure as the `cog-set-value!` above; its just "pure Atomese".
+
+(cog-execute!
+	(SetValue (Concept "foo") (Predicate "bar")
+		(CollectionOf (Type 'FlatStream) (OrderedLink item-list))))
+
+; Streams can be referenced with ValueOfLink
+(define ostream (ValueOf (Concept "foo") (Predicate "bar")))
+
+; The Stream now advances by one with each reference to the stream.
+(cog-execute! ostream)
+(cog-execute! ostream)
+(cog-execute! ostream)
+(cog-execute! ostream)
+
+; Reset the stream to the beginning. Earlier, the SetValue result
+; was printed on the screen, referencing the stream, and advancing
+; it to the second element. This accidental advance can be avoided
+; by capturing the result and not printing it. (So, don't print "junk".)
+(define junk
+	(cog-execute!
+		(SetValue (Concept "foo") (Predicate "bar")
+			(CollectionOf (Type 'FlatStream) (OrderedLink item-list)))))
+
+; Verify the stream really does start at the start.
+(cog-execute! ostream)
+
+; ----------------- That's all, Folks! The End! -----------------
