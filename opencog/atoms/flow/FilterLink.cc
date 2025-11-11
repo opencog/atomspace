afce6fb4dcba48f96969e22468fa9ca84c63c385
Linas Vepštas <linasvepstas@gmail.com>
2025-11-10 19:33:44 -0600

    Attempted backstop of StringOfLink

--- opencog/atoms/flow/FilterLink.cc
+++ opencog/atoms/flow/FilterLink.cc
@@ -245,17 +245,6 @@ bool FilterLink::extract(const Handle& termpat,
 		return false;
 	}
 
-	// Search for a specific StringValue. There's not way to
-	// place StringValues directly into the pattern, but it
-	// can be encoded with StringOfLink. Note this is a content
-	// compare, not a pointer compare, because values are not
-	// de-duped.
-	if (STRING_OF_LINK == t)
-	{
-		ValuePtr patval = termpat->execute();
-		return (*patval == *vgnd);
-	}
-
 	// If they are (non-variable) nodes, they must be identical.
 	if (not termpat->is_link())
 		return (termpat == vgnd);
@@ -265,12 +254,21 @@ bool FilterLink::extract(const Handle& termpat,
 	// If not, bounce out.
 	// The LinkSig typespec might be TypeNode, TypeInhNode or TypeCoInhNode
 	// The TypeNode::is_kind() handles all three cases.
+
+	// Search for a specific StringValue. There's not way to
+	// place StringValues directly into the pattern, but it
+	// can be encoded with StringOfLink. Note this is a content
+	// compare, not a pointer compare, because values are not
+	// de-duped.
 	if (LINK_SIGNATURE_LINK == t)
 	{
 		// Type of LinkSig is encoded in the first atom.
 		Handle ts = LinkSignatureLinkCast(termpat)->get_typespec();
 		if (not TypeNodeCast(ts)->is_kind(vgnd->get_type()))
 			return false;
+
+		ValuePtr patval = termpat->execute();
+		return (*patval == *vgnd);
 	}
 
 	// Else straight-up see if pattern and grounding types agree.
