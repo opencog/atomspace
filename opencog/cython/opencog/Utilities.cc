36d9d5d09547be50f99c5d08b7ff1ed4c48a0870
Linas Vepstas <linasvepstas@gmail.com>
2025-11-15 02:48:48 +0000

    Cleanup old code.

--- opencog/cython/opencog/Utilities.cc
+++ opencog/cython/opencog/Utilities.cc
@@ -1,29 +1,25 @@
-#include <opencog/util/Config.h>
-#include <opencog/util/exceptions.h>
 #include <opencog/atomspace/AtomSpace.h>
-#include <opencog/cython/PythonEval.h>
 #include <opencog/cython/executioncontext/Context.h>
-#include <iostream>
-#include <stdexcept>
+#include <opencog/util/exceptions.h>
 
 #include "Utilities.h"
 
 using namespace opencog;
 
-Handle opencog::add_node(Type t, std::string name) {
+Handle opencog::add_node(Type t, std::string name)
+{
     AtomSpacePtr atomspace = get_context_atomspace();
-    if (atomspace == nullptr){
-        throw std::runtime_error("current atomspace is not set");
-    }
+    if (atomspace == nullptr)
+        throw RuntimeException(TRACE_INFO, "current atomspace is not set");
 
     return atomspace->add_node(t, std::move(name));
 }
 
-Handle opencog::add_link(Type t, HandleSeq outgoing) {
+Handle opencog::add_link(Type t, HandleSeq outgoing)
+{
     AtomSpacePtr atomspace = get_context_atomspace();
-    if (atomspace == nullptr){
-        throw std::runtime_error("current atomspace is not set");
-    }
+    if (atomspace == nullptr)
+        throw RuntimeException(TRACE_INFO, "current atomspace is not set");
 
     return atomspace->add_link(t, std::move(outgoing));
 }
