40d932ccefa7ab87ee9fde98ec3d70f81999bf08
Linas Vepstas <linasvepstas@gmail.com>
2025-11-28 23:54:49 +0000

    Python example: remove demo of python-call-with-as

--- examples/atomspace/python.scm
+++ examples/atomspace/python.scm
@@ -5,7 +5,6 @@
 ; below.  It should not be confused with another concept: calling python
 ; functions from ExecutionOutputLinks, which is demoed in `execute.scm`.
 
-
 ; Start by loading the python module:
 (use-modules (opencog) (opencog python))
 
@@ -16,43 +15,8 @@
 (python-eval "exec(open('my_py_func.py').read())")
 
 ; -------------------------------------------------------------------
-; It is possible to communicate an AtomSpace from guile to python.
-; There are two ways to do this: one way is to tell python what the
-; atomspace is; the other is to have python ask what it is. The first
-; example shows how to "tell" python: it calls a python function,
-; supplying some atomspace as an argument.
-;
-; This requires several steps. First, define a python function that
-; takes an atomspace as an argument.  Note that standard python
-; whitespace indentation is used to define a multi-line block of python
-; code.
-(python-eval "
-from opencog.atomspace import AtomSpace, createFloatValue
-from opencog.atomspace import types
-
-def foo(atspace):
-    FV = createFloatValue([0.42, 0.69])
-    apple = atspace.add_node(types.ConceptNode, 'Apple')
-    tvkey = atspace.add_node(types.PredicateNode, 'My Key')
-    apple.set_value(tvkey, FV)
-")
-
-; The (cog-atomspace) function returns the current atomspace being used
-; by scheme.  We pass it to the newly-defined python function "foo":
-(python-call-with-as "foo" (cog-atomspace))
-
-; Verify that a ConceptNode called "Apple" was created, and that it has
-; the correct float value, as set in the python code:
-(define apple (cog-node 'ConceptNode "Apple"))
-(define tvkey (Predicate "My Key"))
-(define fv (cog-value apple tvkey))
-(display "Apple FloatValue: ") (display fv) (newline)
-
-; -------------------------------------------------------------------
-; The other way is to have python ask scheme to tell it about some
-; atomspace.  This is shown below. As always, recall that
-; (cog-atomspace) simply returns the AtomSpace that scheme is currently
-; using (in the current thread).
+; AtomSpaces are (more or less) automatically shared between scheme and
+; python. This is demonstrated below.
 (python-eval "
 from opencog.scheme_wrapper import scheme_eval_as
 from opencog.atomspace import createFloatValue
