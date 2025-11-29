9f808dc9df8f804d0f3f737b86cd4faa62ef8d97
Linas Vepstas <linasvepstas@gmail.com>
2025-11-29 21:27:13 +0000

    Begin long journey of creating a true Evaluatable.

---
+++ opencog/atoms/execution/EvaluatableLink.cc
@@ -0,0 +1,47 @@
+/*
+ * EvaluatableLink.cc
+ *
+ * Copyright (C) 2025 BrainyBlaze Dynamics, LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the
+ * exceptions at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atoms/atom_types/atom_types.h>
+#include "EvaluatableLink.h"
+
+using namespace opencog;
+
+EvaluatableLink::EvaluatableLink(const HandleSeq&& oset, Type t)
+	: Link(std::move(oset), t)
+{
+	if (not nameserver().isA(t, EVALUATABLE_LINK))
+	{
+		const std::string& tname = nameserver().getTypeName(t);
+		throw InvalidParamException(TRACE_INFO,
+			"Expecting an EvaluatableLink, got %s", tname.c_str());
+	}
+}
+
+bool EvaluatableLink::bevaluate(AtomSpace* as, bool silent)
+{
+	return true;
+}
+
+// ---------------------------------------------------------------
+
+DEFINE_LINK_FACTORY(EvaluatableLink, EVALUATABLE_LINK)
+
+/* ===================== END OF FILE ===================== */
