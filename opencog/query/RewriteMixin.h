bb02b812b317e6aecf2a9ed07a2deaace076bf42
Linas Vepštas <linasvepstas@gmail.com>
2025-11-30 15:45:14 -0600

    Move Instantiator to live with query engine

--- opencog/query/RewriteMixin.h
+++ opencog/query/RewriteMixin.h
@@ -28,8 +28,8 @@
 
 #include <opencog/atomspace/AtomSpace.h>
 
-#include <opencog/atoms/execution/Instantiator.h>
 #include <opencog/atoms/value/ContainerValue.h>
+#include <opencog/query/Instantiator.h>
 #include <opencog/query/PatternMatchCallback.h>
 
 
