dc4d588a7b6354218a5ea8c881e709b0e0713b1b
Linas Vepštas <linasvepstas@gmail.com>
2025-11-30 12:32:14 -0600

    Remove dead code

--- opencog/atoms/core/CondLink.cc
+++ opencog/atoms/core/CondLink.cc
@@ -19,9 +19,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <opencog/atoms/base/ClassServer.h>
-#include <opencog/atoms/execution/Instantiator.h>
-
 #include "CondLink.h"
 
 using namespace opencog;
@@ -99,27 +96,14 @@ ValuePtr CondLink::execute(AtomSpace *scratch, bool silent)
 			if (exps[i]->is_executable())
 				return exps[i]->execute(scratch, silent);
 
-			// Instantiator does the wrong kind of things inside of
-			// Evaluatable links. So don't let it go there.
-			if (exps[i]->is_type(EVALUATABLE_LINK))
-				return exps[i];
-
-			// At this time, not every Atom type knows how to execute
-			// itself. So if the above didn't work, try again, forcing
-			// further reduction.
-			Instantiator inst(scratch);
-			return inst.execute(exps[i]);
+			return exps[i];
 		}
 	}
 
 	if (default_exp->is_executable())
 		return default_exp->execute(scratch, silent);
 
-	if (default_exp->is_type(EVALUATABLE_LINK))
-		return default_exp;
-
-	Instantiator inst(scratch);
-	return inst.execute(default_exp);
+	return default_exp;
 }
 
 DEFINE_LINK_FACTORY(CondLink, COND_LINK)
