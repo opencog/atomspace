36dc38a996c7f58d4787a0b2c77a262cfd73bfba
Linas Vepstas <linasvepstas@gmail.com>
2025-12-11 07:48:44 +0000

    Python wrapper for scheme eval: optional first arg
    There is no reason to pass the arg explicitly. Its just a
    hangover from the bad old days of the python interfaces.

--- opencog/cython/opencog/scheme.pyx
+++ opencog/cython/opencog/scheme.pyx
@@ -15,6 +15,7 @@ from opencog.atomspace cimport (cValuePtr, Value, cAtomSpace,
                                 Atom, AtomSpace, cAtom, cHandle,
                                 AtomSpace_factoid, handle_cast,
                                 create_python_value_from_c_value)
+from opencog.type_ctors cimport get_frame
 
 
 # basic wrapping for std::string conversion
@@ -28,19 +29,38 @@ cdef extern from "<string>" namespace "std":
 cdef extern from "opencog/cython/opencog/PyScheme.h" namespace "opencog":
     string eval_scheme(cAtomSpace* as, const string& s) except +
 
-def scheme_eval(AtomSpace a, str pys):
-    """Evaluate Scheme program and return string.
+def scheme_eval(a, str pys=None):
+    """Evaluate Scheme expression and return string.
     Args:
-        a (AtomSpace): atomspace to work on
-        pys (str): Scheme program to evaluate
+        If one arg is supplied, then it must be the Scheme expression
+        to evaluate.  It will be evaluated in the AtomSpace of the
+        current thread.
+
+        If two arguments are given, the first argument must be the
+        AtomSpace to use during evaluation, and the second is the
+        scheme expression. It will be evaluated in the supplied
+        AtomSpace.
     Returns:
-        str: result of a evaluation
+        str: result of the evaluation.
     Raises:
-        RuntimeError: in case of evaluation error
+        RuntimeError: in case of evaluation error.
+    See also:
+        scheme_eval_v() for expressions that return Atoms or Values.
     """
     cdef string ret
     cdef string expr
-    cdef cAtomSpace* asp = <cAtomSpace*>a.shared_ptr.get()
+    cdef cAtomSpace* asp
+    cdef AtomSpace atomspace
+
+    # Handle optional atomspace argument
+    if pys is None:
+        # First argument is the scheme string, use thread atomspace
+        pys = a
+        atomspace = AtomSpace_factoid(handle_cast(get_frame()))
+    else:
+        atomspace = a
+
+    asp = <cAtomSpace*>atomspace.shared_ptr.get()
     expr = pys.encode('UTF-8')
     ret = eval_scheme(asp, expr)
     return ret.c_str().decode('UTF-8')
@@ -48,19 +68,41 @@ def scheme_eval(AtomSpace a, str pys):
 cdef extern from "opencog/cython/opencog/PyScheme.h" namespace "opencog":
     cValuePtr eval_scheme_v(cAtomSpace* as, const string& s) except +
 
-def scheme_eval_v(AtomSpace a, str pys):
-    """Evaluate Scheme program when expected result is Value.
+def scheme_eval_v(a, str pys=None):
+    """Evaluate Scheme expression, with expected result a Value or Atom.
     Args:
-        a (AtomSpace): atomspace to work on
-        pys (str): Scheme program to evaluate
+        If one arg is supplied, then it must be the Scheme expression
+        to evaluate.  It will be evaluated in the AtomSpace of the
+        current thread.
+
+        If two arguments are given, the first argument must be the
+        AtomSpace to use during evaluation, and the second is the
+        scheme expression. It will be evaluated in the supplied
+        AtomSpace.
+
+        The evaluated expression must return a Value or an Atom;
+        if it does not, an excption will be raised.
     Returns:
-        Value: result of a evaluation
+        Atom or Value, the result of the evaluation.
     Raises:
-        RuntimeError: in case of evaluation error
+        RuntimeError: in case of evaluation error.
+    See also:
+        scheme_eval() for expressions that return strings.
     """
     cdef cValuePtr ret
     cdef string expr
-    cdef cAtomSpace* asp = <cAtomSpace*>a.shared_ptr.get()
+    cdef cAtomSpace* asp
+    cdef AtomSpace atomspace
+
+    # Handle optional atomspace argument
+    if pys is None:
+        # First argument is the scheme string, use thread atomspace
+        pys = a
+        atomspace = AtomSpace_factoid(handle_cast(get_frame()))
+    else:
+        atomspace = a
+
+    asp = <cAtomSpace*>atomspace.shared_ptr.get()
     expr = pys.encode('UTF-8')
     ret = eval_scheme_v(asp, expr)
     return create_python_value_from_c_value(ret)
