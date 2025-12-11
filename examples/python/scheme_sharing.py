cc22b651998cc55d50d6af70a80eb6afef8c7880
Linas Vepstas <linasvepstas@gmail.com>
2025-12-11 07:52:33 +0000

    Python: simplify example demo

--- examples/python/scheme_sharing.py
+++ examples/python/scheme_sharing.py
@@ -13,21 +13,21 @@ from opencog.scheme import *
 ConceptNode("this is a test")
 
 # Print the AtomSpace contents, using scheme:
-asp = get_thread_atomspace()
 print("Printing the AtomSpace from scheme:",
-    scheme_eval(asp, '(cog-prt-atomspace)'))
+    scheme_eval('(cog-prt-atomspace)'))
 print("Printing result of running some scheme code:",
-    scheme_eval(asp,
+    scheme_eval(
         '(format #t "Yes this is really scheme: ~A\n" (+ 2 2))'))
 
 # Add an Atom, using scheme. The scheme_eval_v() function returns
 # a handle to that Atom, and python can use that handle, just as if
 # that Atom was created natively, in python. Both are the same Atom.
-foo_atom = scheme_eval_v(asp, '(Concept "foo")')
+foo_atom = scheme_eval_v('(Concept "foo")')
 print("In python it looks like this:", foo_atom)
 
 # Dump the AtomSpace contents into a list. You'll find both Atoms
 # are in there.
+asp = get_thread_atomspace()
 print("The AtomSpace contains:" , list(asp))
 
 # THE END. That's All, Folks!
