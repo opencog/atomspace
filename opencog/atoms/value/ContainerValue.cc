68b1259a2cd4581e7a2ab7a641c9863f5d871443
Linas Vepštas <linasvepstas@gmail.com>
2025-12-19 14:21:26 -0600

    Simplify container printing
    Claude lied to me. And I beleived it. What a shmope I am.

--- opencog/atoms/value/ContainerValue.cc
+++ opencog/atoms/value/ContainerValue.cc
@@ -30,12 +30,9 @@ using namespace opencog;
 
 std::string ContainerValue::to_string(const std::string& indent) const
 {
-   // The default printer for ContainerValue is LinkValue ...
-   // with only two small problems. One is that containers typically
-	// block in certain states. The other is that to_string()
-	// is used for Value::operator<(). So we need a non-blocking
-	// printer which also prints unique (and stable) ID's. Use the
-	// current 'this` pointer as the unique ID.
+	// The default printer for ContainerValue is LinkValue ...
+	// with only one small problem: containers can block under certain
+	// conditions.  This provides a non-blocking printer.
 	//
 	// The BLOCKING_SIG containers will hang if the container is open,
 	// and the STREAMING_SIG containers will hang if the container is
@@ -44,12 +41,8 @@ std::string ContainerValue::to_string(const std::string& indent) const
 	// XXX FIXME, below is correct only for BLOCKING_SIG ...
 	if (is_closed()) return LinkValue::to_string(indent);
 
-	// std::format("{:p}", (void*) this)
-	std::stringstream uid;
-	uid << (void*) this;
-
 	return indent + "(" + nameserver().getTypeName(get_type()) +
-	   ") ;; " + uid.str() + " open for writing";
+	   ") ;; open for writing";
 }
 
 // ==============================================================
