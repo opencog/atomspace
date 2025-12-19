86995953d3dfc81801d8c59026f225ceedd5d07d
Linas Vepstas <linasvepstas@gmail.com>
2025-12-19 05:53:12 +0000

    Implement the equivalence relation.

--- opencog/atoms/value/GroupStream.cc
+++ opencog/atoms/value/GroupStream.cc
@@ -39,17 +39,41 @@ GroupStream::~GroupStream()
 
 // ==============================================================
 
+// Set up the equivalence comparator by wrapping the given relation in
+// an ExecutionOutputLink, fed by a pair of ValueShims that will pass
+// the Values into the relation.
 void GroupStream::init_equiv(void)
 {
-	// TODO: Set up the equivalence comparator
+	// ValueShims for left and right comparison arguments
+	_left_shim = createValueShimLink();
+	_right_shim = createValueShimLink();
+
+	// The ExecutionOutputLink provides general machinery that can run
+	// the schema on the pair to be compared.
+	_exout =
+		createLink(HandleSeq({
+			_schema,
+			createLink(HandleSeq({
+				HandleCast(_left_shim),
+				HandleCast(_right_shim)}),
+				LIST_LINK)}),
+			EXECUTION_OUTPUT_LINK);
+
+	// Scratch space in which temporaries are evaluated.
+	_scratch = createAtomSpace(_schema->getAtomSpace());
 }
 
 // ==============================================================
 
+// Use the provided schema to test if two values are equivalent.
 bool GroupStream::equivalent(const Value& lhs, const Value& rhs) const
 {
-	// TODO: Evaluate the schema to determine equivalence
-	return false;
+	// Set the values to be compared in the shims.
+	_left_shim->set_value(ValuePtr(const_cast<Value*>(&lhs), [](Value*){}));
+	_right_shim->set_value(ValuePtr(const_cast<Value*>(&rhs), [](Value*){}));
+
+	_scratch->clear();
+	return _exout->bevaluate(_scratch.get());
 }
 
 // ==============================================================
