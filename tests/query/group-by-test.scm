1f6ce83c94a0e483cf51f633cea16fa8b07eda9d
Linas Vepstas <linasvepstas@gmail.com>
2025-12-20 01:28:41 +0000

    Use an alternate filtering pipeline.

--- tests/query/group-by-test.scm
+++ tests/query/group-by-test.scm
@@ -3,8 +3,17 @@
 !#
 ;
 ; group-by-test.scm -- Unit test for the GroupLink
+; Except GroupLink is now obsolete, and is replaced by GroupValue,
+; which is used to perform post-query filtering. This complicates
+; the tests considerably, and changes their nature into being more
+; of a test of GroupValue, than of the query itself. The changes
+; aka issues:
+; *) The Variables in the GroupValue Lambda have no relationship
+;    to the Variables in the pattern.
+; *) The grouping can be specified by *any* lambda, and not equality.
+;    The GroupValue is far more general.
 ;
-; Run this manually by saying "guile -s group-by-test.scm"
+; Run this manually by saying "./group-by-test.scm"
 
 (use-modules (opencog))
 (use-modules (opencog test-runner))
@@ -26,7 +35,13 @@
 (Edge (Predicate "property") (List (Item "vague") (Item "cloudy")))
 
 ; -------------------------------------------------------------
-(define grp-meet
+; A basic Meet pattern. As results are matched, they are stuffed
+; into the GroupValue. The GroupValue receives a ListValue wrapping
+; (Variable "$X") (Variable "$Y") because this is how MeetLinks work.
+; The GroupValue extracts the second elt in the list using ElementOf,
+; and then performs an equality check on that.
+;
+(define meet-edges
 	(Meet
 		(VariableList (Variable "$X") (Variable "$Y"))
 		(Present
@@ -34,7 +49,7 @@
 				(List (Variable "$X") (Variable "$Y"))))))
 
 ; The Lambda compares the $Y component (index 1) of result LinkValues
-(define meet-group-value
+(define group-pairs
 	(GroupValue
 		(Lambda
 			(VariableList (Variable "$A") (Variable "$B"))
@@ -42,14 +57,39 @@
 				(ElementOf (Number 1) (Variable "$A"))
 				(ElementOf (Number 1) (Variable "$B"))))))
 
-(cog-set-value! grp-meet grp-meet meet-group-value)
+(cog-set-value! meet-edges meet-edges group-pairs)
 
-(define meet-results (cog-execute! grp-meet))
+(define meet-results (cog-execute! meet-edges))
 ; (format #t "The meet results are ~A\n" meet-results)
 ; (format #t "There are ~A results\n" (length (cog-value->list meet-results)))
 
 (test-assert "meet group size"
 	(equal? 3 (length (cog-value->list meet-results))))
+
+; -------------------------------------------------------------
+; Same as above, but uses a Filter to extract the second element.
+; i.e. the pattern is entirely unchanged, we just rework the GroupValue.
+
+(define destructure-filter
+	(GroupValue
+		(Lambda
+			(VariableList (Variable "$A") (Variable "$B"))
+			(Equal
+				(Filter
+					(Lambda (Variable "$x")
+						(LinkSignature (Type 'LinkValue) (Type 'Item) (Variable "$x")))
+					(Set (Variable "$A")))
+				(Filter
+					(Lambda (Variable "$x")
+						(LinkSignature (Type 'LinkValue) (Type 'Item) (Variable "$x")))
+					(Set (Variable "$B")))))))
+
+(cog-set-value! meet-edges meet-edges destructure-filter)
+
+(define meet-destr (cog-execute! meet-edges))
+
+(test-assert "meet destructure group size"
+	(equal? 3 (length (cog-value->list meet-destr))))
 
 ; -------------------------------------------------------------
 (define grp-query
