01af472232096ad33a2a0b788e88b83db6663c97
Linas Vepstas <linas@linas.org>
2026-01-01 14:02:24 -0600

    Remove an unused, un-needed constructor

--- opencog/atoms/grant/DefineLink.cc
+++ opencog/atoms/grant/DefineLink.cc
@@ -14,14 +14,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public
- * License along with this program; if not, write to:
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
-#include <opencog/atoms/base/ClassServer.h>
 
 #include "DefineLink.h"
 
@@ -34,7 +27,7 @@ void DefineLink::init(void)
 	// Must have name and body
 	if (2 != _outgoing.size())
 		throw SyntaxException(TRACE_INFO,
-			"Expecting name and definition, got size %d", _outgoing.size());
+			"Expecting name and definition, got %s", to_string().c_str());
 
 	// Type-check. The execution and FunctionLink's only expand
 	// definitions anchored with these types; other definitions won't
@@ -46,7 +39,7 @@ void DefineLink::init(void)
 	    DEFINED_TYPE_NODE != dtype)
 		throw SyntaxException(TRACE_INFO,
 			"Expecting Defined(Procedure/Schema/Predicate/Type)Node, got %s",
-				nameserver().getTypeName(dtype).c_str());
+				to_string().c_str());
 
 	// Perform some additional checks in the UniqueLink init method
 	UniqueLink::init();
@@ -54,12 +47,6 @@ void DefineLink::init(void)
 
 DefineLink::DefineLink(const HandleSeq&& oset, Type t)
 	: UniqueLink(std::move(oset), t)
-{
-	init();
-}
-
-DefineLink::DefineLink(const Handle& name, const Handle& defn)
-	: UniqueLink(HandleSeq({name, defn}), DEFINE_LINK)
 {
 	init();
 }
--- opencog/atoms/grant/DefineLink.h
+++ opencog/atoms/grant/DefineLink.h
@@ -13,11 +13,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, write to:
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef _OPENCOG_DEFINE_LINK_H
@@ -57,8 +52,6 @@ protected:
 	void init(void);
 public:
 	DefineLink(const HandleSeq&&, Type=DEFINE_LINK);
-
-	DefineLink(const Handle& alias, const Handle& body);
 
 	DefineLink(const DefineLink&) = delete;
 	DefineLink& operator=(const DefineLink&) = delete;
