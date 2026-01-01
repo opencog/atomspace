20f70762a6e65eca7e2d8abb483f17df11371791
Linas Vepstas <linas@linas.org>
2026-01-01 14:02:53 -0600

    Implement scaffolding for the new PipeLink

---
+++ opencog/atoms/grant/PipeLink.cc
@@ -0,0 +1,51 @@
+/*
+ * PipeLink.cc
+ *
+ * Copyright (C) 2015 Linas Vepstas
+ * Copyright (C) 2026 BrainyBlaze Dynamics, LLC
+ *
+ * Author: Linas Vepstas <linasvepstas@gmail.com>  May 2015
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the
+ * exceptions at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "PipeLink.h"
+
+using namespace opencog;
+
+void PipeLink::init(void)
+{
+	if (_type != PIPE_LINK) return;
+
+	// Must have name and body
+	if (2 != _outgoing.size())
+		throw SyntaxException(TRACE_INFO,
+			"Expecting name and data stream, got %s", to_string().c_str());
+
+	// Type-check. For now, we can only bind NameNodes.
+	Type ntype = _outgoing[0]->get_type();
+	if (NAME_NODE != ntype)
+		throw SyntaxException(TRACE_INFO,
+			"Expecting NameNode, got %s", to_string().c_str());
+
+	// Perform some additional checks in the UniqueLink init method
+	UniqueLink::init();
+}
+
+PipeLink::PipeLink(const HandleSeq&& oset, Type t)
+	: DefineLink(std::move(oset), t)
+{
+	init();
+}
+
+DEFINE_LINK_FACTORY(PipeLink, PIPE_LINK)
+
+/* ===================== END OF FILE ===================== */
---
+++ opencog/atoms/grant/PipeLink.h
@@ -0,0 +1,57 @@
+/*
+ * opencog/atoms/grant/PipeLink.h
+ *
+ * Copyright (C) 2015 Linas Vepstas
+ * Copyright (C) 2026 BrainyBlaze Dynamics, LLC
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _OPENCOG_PIPE_LINK_H
+#define _OPENCOG_PIPE_LINK_H
+
+#include <opencog/atoms/grant/DefineLink.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/// The PipeLink is used to assign names to data flows. It is a
+/// convenience, used for wiring up data flow networks.
+///
+/// Only one name per data flow is allowed; any attempt to attach the
+/// same name to another data stream will throw an exception.  Only
+/// one PipeLink with a given name can exist at a time; to change the
+/// assoociation, the original PipeLink must be deleted first.
+///
+class PipeLink : public DefineLink
+{
+protected:
+	void init(void);
+public:
+	PipeLink(const HandleSeq&&, Type=PIPE_LINK);
+
+	PipeLink(const PipeLink&) = delete;
+	PipeLink& operator=(const PipeLink&) = delete;
+
+	static Handle factory(const Handle&);
+};
+
+LINK_PTR_DECL(PipeLink)
+#define createPipeLink CREATE_DECL(PipeLink)
+
+/** @}*/
+}
+
+#endif // _OPENCOG_PIPE_LINK_H
