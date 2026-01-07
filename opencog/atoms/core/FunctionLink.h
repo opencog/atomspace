421df2e73e749e583adf55fc0419b88e1ed15844
Linas Vepštas <linasvepstas@gmail.com>
2026-01-07 10:07:10 -0600

    FunctionLink: rem commentary that no longer applies.

--- opencog/atoms/core/FunctionLink.h
+++ opencog/atoms/core/FunctionLink.h
@@ -26,33 +26,10 @@ namespace opencog
  *  @{
  */
 /**
- * The FunctionLink provides an important method: execute().
- * The execute() method performs delta-reduction on this atom, and
- * returns a Handle resulting from the execution.
- *
- * Here, "delta-reduction" is the name for the idea that certain Links
- * can be replaced by other Atoms that are operationally equivalent
- * but simpler.  For example, the delta-reduction of (Plus 2 2) is 4.
- * Delta-reduction can be understood in several ways. From the viewpoint
- * of term-rewriting, or inference or theorem-proving, a delta-reduction
- * is a (usually infinite) set of inference rules for reducing the
- * initial expression to the final expression. From the viewpoint of
- * proceedural computation, it just means "perform this computation
- * on this input", viz. "execute this function".
- *
  * The FunctionLink is meant to be a base class for any link type
- * that behaves like a function; i.e. can be executed.  Observe that
- * it derives from the FreeLink, and NOT the LambdaLink.  This may seem
- * counter-intuitive, and deserves an explanation, so here it is:
- * All link types inheriting from this class will always, by definition,
- * have their outgoing set be the arguments to that function. Think of
- * PlusLink, for example.  We don't want to insert a lambda with
- * variable declarations in there; that would just be weird and create
- * confusion.  If the arguments to PlusLink happen to include a
- * variable, that variable is necessarily free; thus, this class
- * derives from FreeLink.
+ * that behaves like a function; i.e. can be executed.
  *
- * Note that this class must NOT be used for user-defined functions;
+ * This class must NOT be used for user-defined functions;
  * users should use the LambdaLink for that.
  */
 class FunctionLink : public Link
