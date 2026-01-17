870e7aecf9ba15c074d7a47373f631d1c3477408
Linas Vepštas <linasvepstas@gmail.com>
2026-01-17 16:30:54 -0600

    The constexpr needs to be in the header

--- opencog/atoms/core/ObjectNode.cc
+++ opencog/atoms/core/ObjectNode.cc
@@ -13,37 +13,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, write to:
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include "ObjectNode.h"
 
 using namespace opencog;
-
-/// Implement Jenkins' One-at-a-Time hash.
-/// For these very short strings, I cannot think of a faster hash.
-/// The 4-byte-at-a-time hashes require knowng the string length :-(
-uint32_t constexpr ObjectNode::dispatch_hash(const char* s)
-{
-	uint32_t hash = 0;
-
-	for(; *s; ++s)
-	{
-		hash += *s;
-		hash += (hash << 10);
-		hash ^= (hash >> 6);
-	}
-
-	hash += (hash << 3);
-	hash ^= (hash >> 11);
-	hash += (hash << 15);
-
-	return hash;
-}
 
 ObjectNode::ObjectNode(Type t, const std::string&& uri) :
    Node(t, std::move(uri))
--- opencog/atoms/core/ObjectNode.h
+++ opencog/atoms/core/ObjectNode.h
@@ -13,11 +13,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, write to:
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef _OPENCOG_OBJECT_NODE_H
@@ -44,7 +39,26 @@ namespace opencog
 class ObjectNode : public Node
 {
 protected:
-	static uint32_t constexpr dispatch_hash(const char*);
+	/// Implement Jenkins' One-at-a-Time hash.
+	/// For these very short strings, I cannot think of a faster hash.
+	/// The 4-byte-at-a-time hashes require knowng the string length :-(
+	static uint32_t constexpr dispatch_hash(const char* s)
+	{
+		uint32_t hash = 0;
+
+		for(; *s; ++s)
+		{
+			hash += *s;
+			hash += (hash << 10);
+			hash ^= (hash >> 6);
+		}
+
+		hash += (hash << 3);
+		hash ^= (hash >> 11);
+		hash += (hash << 15);
+
+		return hash;
+	}
 
 	ObjectNode(Type, const std::string&&);
 	virtual void addMessage(const char*) const = 0;
