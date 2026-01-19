f339d580c71d720b7bce1a75a9a36fc38d75ca44
Linas Vepštas <linasvepstas@gmail.com>
2026-01-19 13:30:43 -0600

    Install scaffolding for TriggerLink

---
+++ opencog/atoms/parallel/TriggerLink.cc
@@ -0,0 +1,36 @@
+/*
+ * opencog/atoms/parallel/TriggerLink.cc
+ *
+ * Copyright (C) 2009, 2013-2015, 2020, 2024, 2025 Linas Vepstas
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atoms/parallel/TriggerLink.h>
+#include <opencog/atomspace/AtomSpace.h>
+
+using namespace opencog;
+
+/// TriggerLink
+
+TriggerLink::TriggerLink(const HandleSeq&& oset, Type t)
+    : PureExecLink(std::move(oset), t)
+{
+}
+
+DEFINE_LINK_FACTORY(TriggerLink, TRIGGER_LINK)
---
+++ opencog/atoms/parallel/TriggerLink.h
@@ -0,0 +1,53 @@
+/*
+ * opencog/atoms/parallel/TriggerLink.h
+ *
+ * Copyright (C) 2020, 2024 Linas Vepstas
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_TRIGGER_LINK_H
+#define _OPENCOG_TRIGGER_LINK_H
+
+#include <opencog/atoms/parallel/PureExecLink.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+class TriggerLink : public PureExecLink
+{
+protected:
+
+public:
+	TriggerLink(const HandleSeq&&, Type=TRIGGER_LINK);
+	TriggerLink(const TriggerLink&) = delete;
+	TriggerLink& operator=(const TriggerLink&) = delete;
+
+	static Handle factory(const Handle&);
+};
+
+LINK_PTR_DECL(TriggerLink)
+#define createTriggerLink CREATE_DECL(TriggerLink)
+
+/** @}*/
+}
+
+#endif // _OPENCOG_TRIGGER_LINK_H
